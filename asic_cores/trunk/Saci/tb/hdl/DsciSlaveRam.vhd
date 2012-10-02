40f6fd73d8f7835f9560fa8c2a784caaa5b0f5c0
bareese <bareese@slac.stanford.edu>
2012-10-02 22:43:03 +0000

    Renamed to be more appropriate.
    git-svn-id: file:///dev/shm/git-devel/svn-repo@2341 0848cbd7-7569-0410-ab05-8cc0a0c67708
    
    Former-commit-id: 98c74891552f9ee43a9c28a1c825967e4cea5d4d

---
+++ asic_cores/trunk/Saci/tb/hdl/DsciSlaveRam.vhd
@@ -0,0 +1,71 @@
+-------------------------------------------------------------------------------
+-- Title      : 
+-------------------------------------------------------------------------------
+-- File       : DsciSlaveRam.vhd
+-- Author     : Benjamin Reese  <bareese@slac.stanford.edu>
+-- Company    : SLAC National Accelerator Laboratory
+-- Created    : 2012-09-19
+-- Last update: 2012-09-19
+-- Platform   : 
+-- Standard   : VHDL'93/02
+-------------------------------------------------------------------------------
+-- Description:
+-- 
+-------------------------------------------------------------------------------
+-- Copyright (c) 2012 SLAC National Accelerator Laboratory
+-------------------------------------------------------------------------------
+
+library IEEE;
+use IEEE.std_logic_1164.all;
+use ieee.numeric_std.all;
+use work.StdRtlPkg.all;
+
+entity DsciSlaveRam is
+  
+  port (
+    dsciClkOut : in    sl;
+    exec       : in    sl;
+    ack        : out   sl;
+    readN      : in    sl;
+    cmd        : in    slv(6 downto 0);
+    addr       : in    slv(11 downto 0);
+    data       : inout slv(31 downto 0));
+
+end entity DsciSlaveRam;
+
+architecture rtl of DsciSlaveRam is
+
+  type RamType is array (0 to 2**19) of slv(31 downto 0);
+  signal ram : RamType;
+
+begin
+
+  p : process is
+    variable addrV : slv(18 downto 0);
+    variable indexV : integer;
+  begin
+    wait until dsciClkOut = '1';
+    ack <= '0';
+    data <= (others => 'Z');
+    -- Transaction rx'd
+    if (exec = '1') then
+      addrV := cmd & addr;
+      indexV := to_integer(unsigned(addrV));
+      if (readN = '0') then
+        data <= ram(indexV);
+        wait until dsciClkOut = '1';
+        ack  <= '1';
+        wait until exec = '0';
+        ack  <= '0';
+        data <= (others => 'Z');
+      else
+        ram(indexV) <= data;
+        wait until dsciClkOut = '1';
+          ack <= '1';
+        wait until exec = '0';
+        ack <= '0';
+      end if;
+    end if;
+  end process p;
+
+end architecture rtl;
