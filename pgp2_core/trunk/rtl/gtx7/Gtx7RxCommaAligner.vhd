27cedf3c99f1c323dc211dad6cdcb0ee4d3eb7c0
bareese <bareese@slac.stanford.edu>
2013-02-21 19:11:44 +0000

    Bugfixes + cleanup in Gtx7Core.vhd Gtx7FixedLatCore.vhd now uses Gtx7Core Cleanup in Gtx7RxCommaAligner.vhd
    git-svn-id: file:///dev/shm/git-devel/svn-repo@2528 0848cbd7-7569-0410-ab05-8cc0a0c67708
    
    Former-commit-id: 5ae2edd9316ac1d40606625a80791ef0280ad3f4

--- pgp2_core/trunk/rtl/gtx7/Gtx7RxCommaAligner.vhd
+++ pgp2_core/trunk/rtl/gtx7/Gtx7RxCommaAligner.vhd
@@ -1,15 +1,25 @@
 -------------------------------------------------------------------------------
 -- Title      : 
 -------------------------------------------------------------------------------
--- File       : Gtx7PgpWordAligner.vhd
+-- File       : Gtx7RxCommaAligner.vhd
 -- Author     : Benjamin Reese  <bareese@slac.stanford.edu>
 -- Company    : SLAC National Accelerator Laboratory
 -- Created    : 2012-11-06
--- Last update: 2012-12-14
--- Platform   : 
+-- Last update: 2012-12-19
+-- Platform   : Xilinx 7 Series
 -- Standard   : VHDL'93/02
 -------------------------------------------------------------------------------
--- Description: 
+-- Description:
+-- Used in conjunction for a Xilinx 7 Series GTX.
+-- Given raw 8b10b encoded data presented 2 bytes at a time (20 bits),
+-- attempts to align any observed comma to the lower byte.
+-- Assumes GTX comma align is enabled and in PMA mode.
+-- Comma is configurable through the COMMA_G generic.
+-- If an odd number of rxSlides is required for alignment, resets the GTX RX
+-- so that a new CDR lock can be obtained. The GTX in PMA Slide Mode shifts
+-- the phase of the output clock only every other slide. This module's
+-- purpose is to obtain an output clock that exactly matches the phase of the
+-- commas. 
 -------------------------------------------------------------------------------
 -- Copyright (c) 2012 SLAC National Accelerator Laboratory
 -------------------------------------------------------------------------------
@@ -18,24 +28,24 @@ library ieee;
 use ieee.std_logic_1164.all;
 use ieee.numeric_std.all;
 use work.Pgp2CoreTypesPkg.all;
+use work.StdRtlPkg.all;
 
 
 entity Gtx7RxCommaAligner is
   
   generic (
-    TPD_G : time := 1 ns;
-    COMMA_G : std_logic_vector(9 downto 0) := "0101111100";
-    SLIDE_WAIT_G : integer := 32);
+    TPD_G        : time            := 1 ns;
+    COMMA_G      : slv(9 downto 0) := "0101111100";
+    SLIDE_WAIT_G : integer         := 32);
 
   port (
-    gtRxUsrClk    : in  std_logic;
-    gtRxUsrClkRst : in  std_logic;
-    gtRxData      : in  std_logic_vector(19 downto 0);
-    codeErr        : in  std_logic_vector(1 downto 0);
-    dispErr        : in  std_logic_vector(1 downto 0);
-    gtRxSlide     : out std_logic;
-    gtRxReset  : out std_logic;
-    aligned        : out std_logic);
+    rxUsrClk    : in  sl;
+    rxUserRdy   : in  sl;               -- Used as active low reset
+    rxData      : in  slv(19 downto 0);  -- Encoded raw rx data
+    decodeErr   : in  sl;               -- From decoder. Used to determine when lock is lost
+    rxSlide     : out sl;               -- RXSLIDE input to GTX
+    rxUserReset : out sl;               -- Input to Rx reset state machine, resets the entire GTX Rx
+    aligned     : out sl);              -- Alignment has been achieved.
 
 end entity Gtx7RxCommaAligner;
 
@@ -45,48 +55,48 @@ architecture rtl of Gtx7RxCommaAligner is
 
   type RegType is record
     state       : StateType;
-    last        : std_logic_vector(9 downto 0);
+    last        : slv(9 downto 0);
     slideCount  : unsigned(4 downto 0);
     waitCounter : unsigned(4 downto 0);
 
     -- Outputs
-    gtRxSlide    : std_logic;
-    gtRxCdrReset : std_logic;
-    aligned       : std_logic;
+    rxSlide     : sl;
+    rxUserReset : sl;
+    aligned     : sl;
   end record RegType;
 
   signal r, rin : RegType;
 
 begin
 
-  seq : process (gtRxUsrClk, gtRxUsrClkRstN) is
+  seq : process (rxUsrClk, rxUserRdy) is
   begin
-    if (gtRxUsrClkRstN = '0') then
+    if (rxUserRdy = '0') then
       r.state       <= SEARCH_S        after TPD_G;
       r.last        <= (others => '0') after TPD_G;
       r.slideCount  <= (others => '0') after TPD_G;
       r.waitCounter <= (others => '0') after TPD_G;
 
-      r.gtRxSlide    <= '0' after TPD_G;
-      r.gtRxCdrReset <= '0' after TPD_G;
-      r.aligned       <= '0' after TPD_G;
-    elsif (rising_edge(gtRxUsrClk)) then
+      r.rxSlide     <= '0' after TPD_G;
+      r.rxUserReset <= '0' after TPD_G;
+      r.aligned     <= '0' after TPD_G;
+    elsif (rising_edge(rxUsrClk)) then
       r <= rin after TPD_G;
     end if;
   end process;
 
-  comb : process (r, gtRxData, codeErr, dispErr) is
+  comb : process (r, rxData, decodeErr) is
     variable v         : RegType;
-    variable searchVar : std_logic_vector(29 downto 0);
+    variable searchVar : slv(29 downto 0);
   begin
     v := r;
 
-    v.gtRxCdrReset := '0';
-    v.gtRxSlide    := '0';
-    v.aligned       := '0';
+    v.rxUserReset := '0';
+    v.rxSlide     := '0';
+    v.aligned     := '0';
 
-    v.last    := gtRxData(19 downto 10);  -- Save high byte
-    searchVar := gtRxData & r.last;
+    v.last    := rxData(19 downto 10);  -- Save high byte
+    searchVar := rxData & r.last;
 
     case r.state is
       when SEARCH_S =>
@@ -102,29 +112,29 @@ begin
               v.state      := SLIDE_S;
             else
               -- Reset the rx and hope for a new lock requiring an even number of slides
-              v.state         := RESET_S;
+              v.state := RESET_S;
             end if;
           end if;
         end loop;
 
       when RESET_S =>
         -- Async reset will eventually get everything back to SEARCH_S state
-        v.gtRxCdrReset := '1';
+        v.rxUserReset := '1';
 
 
       when SLIDE_S =>
-        v.gtRxSlide := '1';
-        v.state      := SLIDE_WAIT_S;
+        v.rxSlide := '1';
+        v.state   := SLIDE_WAIT_S;
 
       when SLIDE_WAIT_S =>
         -- Wait SLIDE_WAIT_G clocks between each slide
         v.waitCounter := r.waitCounter + 1;
         if (r.waitCounter = "11111") then
           if (r.slideCount = 0) then
-            v.state := SEARCH_S;          -- Double check that the slides worked
+            v.state := SEARCH_S;        -- Double check that the slides worked
           else
             v.slideCount := r.slideCount - 1;
-            v.state := SLIDE_S;
+            v.state      := SLIDE_S;
           end if;
         end if;
 
@@ -132,7 +142,7 @@ begin
         v.aligned := '1';
         -- Reuse wait counter to count 8b10b errors
         -- After several errors, reset
-        if (codeErr /= "00" or dispErr /= "00") then
+        if (decodeErr = '1') then
           v.waitCounter := r.waitCounter + 1;
         end if;
         if (r.waitCounter = "11111") then
@@ -143,9 +153,9 @@ begin
 
     rin <= v;
 
-    gtRxSlide    <= r.gtRxSlide;
-    gtRxCdrReset <= r.gtRxCdrReset;
-    aligned       <= r.aligned;
+    rxSlide     <= r.rxSlide;
+    rxUserReset <= r.rxUserReset;
+    aligned     <= r.aligned;
 
   end process comb;
 
