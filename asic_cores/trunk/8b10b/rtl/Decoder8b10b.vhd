80e986b8f47a6093c8ce8c35a21e571b72eff250
bareese <bareese@slac.stanford.edu>
2013-05-03 16:15:00 +0000

    Cleanup for directory structure. Original files downloaded from opencores now at orig/ Ported original files to VHDL. Code is in rtl/ Created a synthesis folder at syn_025um/ with scripts. Synthesized VHDL encoder and decoder.
    git-svn-id: file:///dev/shm/git-devel/svn-repo@2656 0848cbd7-7569-0410-ab05-8cc0a0c67708
    
    Former-commit-id: 79e9449a7abb252250465d6db479123228ef8d31

---
+++ asic_cores/trunk/8b10b/rtl/Decoder8b10b.vhd
@@ -0,0 +1,90 @@
+-------------------------------------------------------------------------------
+-- Title      : 
+-------------------------------------------------------------------------------
+-- File       : Decoder8b10b.vhd
+-- Author     : Benjamin Reese  <bareese@slac.stanford.edu>
+-- Company    : SLAC National Accelerator Laboratory
+-- Created    : 2012-11-15
+-- Last update: 2012-11-15
+-- Platform   : 
+-- Standard   : VHDL'93/02
+-------------------------------------------------------------------------------
+-- Description: 
+-------------------------------------------------------------------------------
+-- Copyright (c) 2012 SLAC National Accelerator Laboratory
+-------------------------------------------------------------------------------
+
+library ieee;
+use ieee.std_logic_1164.all;
+use work.StdRtlPkg.all;
+use work.Code8b10bPkg.all;
+
+entity Decoder8b10b is
+   
+   generic (
+      TPD_G       : time     := 1 ns;
+      NUM_BYTES_G : positive := 2);
+
+   port (
+      clk      : in  sl;
+      rstN     : in  sl;
+      dataIn   : in  slv(NUM_BYTES_G*10-1 downto 0);
+      dataOut  : out slv(NUM_BYTES_G*8-1 downto 0);
+      dataKOut : out slv(NUM_BYTES_G-1 downto 0);
+      codeErr  : out slv(NUM_BYTES_G-1 downto 0);
+      dispErr  : out slv(NUM_BYTES_G-1 downto 0));
+
+end entity Decoder8b10b;
+
+architecture rtl of Decoder8b10b is
+
+   type RegType is record
+      runDisp  : sl;
+      dataOut  : slv(NUM_BYTES_G*8-1 downto 0);
+      dataKOut : slv(NUM_BYTES_G-1 downto 0);
+      codeErr  : slv(NUM_BYTES_G-1 downto 0);
+      dispErr  : slv(NUM_BYTES_G-1 downto 0);
+   end record RegType;
+
+   signal r, rin : RegType;
+
+begin
+
+   comb : process (r, dataIn) is
+      variable v            : RegType;
+      variable dispChainVar : sl;
+   begin
+      v            := r;
+      dispChainVar := r.runDisp;
+      for i in 0 to NUM_BYTES_G-1 loop
+         decode8b10b(dataIn   => dataIn(i*10+9 downto i*10),
+                     dispIn   => dispChainVar,
+                     dataOut  => v.dataOut(i*8+7 downto i*8),
+                     dataKOut => v.dataKOut(i),
+                     dispOut  => dispChainVar,
+                     codeErr  => v.codeErr(i),
+                     dispErr  => v.dispErr(i));
+      end loop;
+      v.runDisp := dispChainVar;
+
+      rin      <= v;
+      dataOut  <= r.dataOut;
+      dataKOut <= r.dataKOut;
+      codeErr  <= r.codeErr;
+      dispErr  <= r.dispErr;
+   end process comb;
+
+   seq : process (clk, rstN) is
+   begin
+      if (rstN = '0') then
+         r.runDisp  <= '0';
+         r.dataOut  <= (others => '0');
+         r.dataKOut <= (others => '0');
+         r.codeErr  <= (others => '0');
+         r.dispErr  <= (others => '0');
+      elsif (rising_edge(clk)) then
+         r <= rin;
+      end if;
+   end process seq;
+
+end architecture rtl;
