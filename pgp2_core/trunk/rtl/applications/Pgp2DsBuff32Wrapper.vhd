a542941cb1b3d11406e988776017b106cd268e15
ruckman <ruckman@slac.stanford.edu>
2013-06-20 23:23:09 +0000

    fixed the formatting
    git-svn-id: file:///dev/shm/git-devel/svn-repo@2777 0848cbd7-7569-0410-ab05-8cc0a0c67708
    
    Former-commit-id: 1925cca620f75d91787a54db99637ce9376f3c44

--- pgp2_core/trunk/rtl/applications/Pgp2DsBuff32Wrapper.vhd
+++ pgp2_core/trunk/rtl/applications/Pgp2DsBuff32Wrapper.vhd
@@ -1,10 +1,10 @@
 -------------------------------------------------------------------------------
--- Title			  : Pretty Good Protocol, V2, Down Stream Buffer Block
--- Project		  : General Purpose Core
+-- Title         : Pretty Good Protocol, V2, Down Stream Buffer Block
+-- Project       : General Purpose Core
 -------------------------------------------------------------------------------
--- File			  : Pgp2DsBuff32Wrapper.vhd
--- Author		  : Larry Ruckman, ruckman@slac.stanford.edu
--- Created		  : 06/11/2013
+-- File          : Pgp2DsBuff32Wrapper.vhd
+-- Author        : Larry Ruckman, ruckman@slac.stanford.edu
+-- Created       : 06/11/2013
 -------------------------------------------------------------------------------
 -- Description:
 -- Wrapper for Pgp2DsBuff32
@@ -24,33 +24,33 @@ use ieee.std_logic_arith.all;
 use ieee.std_logic_unsigned.all;
 
 entity Pgp2DsBuff32Wrapper is
-	generic (
-		RxLane	: integer := 0;			 -- Receive Lanes Number (0 to 3) 
-		-- FifoType: (default = V5)
-		-- V4 = Virtex 4,	 V5 = Virtex 5, V6 = Virtex 6, V7 = Virtex 7, 
-		-- S6 = Spartan 6, A7 = Artix 7,	 K7 = kintex7
-		FifoType : string	 := "V5"
-		);
-	port (
+   generic (
+      RxLane   : integer := 0;          -- Receive Lanes Number (0 to 3) 
+      -- FifoType: (default = V5)
+      -- V4 = Virtex 4,  V5 = Virtex 5, V6 = Virtex 6, V7 = Virtex 7, 
+      -- S6 = Spartan 6, A7 = Artix 7,  K7 = kintex7
+      FifoType : string  := "V5"
+      );
+   port (
 
-		-- PGP Rx Clock And Reset
-		pgpRxClk	  : in sl;					 -- PGP Clock
-		pgpRxReset : in sl;					 -- Synchronous PGP Reset
+      -- PGP Rx Clock And Reset
+      pgpRxClk   : in sl;               -- PGP Clock
+      pgpRxReset : in sl;               -- Synchronous PGP Reset
 
-		-- Local clock and reset
-		locClk	: in sl;						 -- Local Clock
-		locReset : in sl;						 -- Synchronous Local Reset
+      -- Local clock and reset
+      locClk   : in sl;                 -- Local Clock
+      locReset : in sl;                 -- Synchronous Local Reset
 
-		-- PGP Signals, Virtual Channel Rx Only
-		PgpRxVcOut				  : in  PgpRxVcOutType;
-		PgpRxVcCommonOut		  : in  PgpRxVcCommonOutType;
-		pgpTxVcIn_locBuffAFull : out sl;
-		pgpTxVcIn_locBuffFull  : out sl;
+      -- PGP Signals, Virtual Channel Rx Only
+      PgpRxVcOut             : in  PgpRxVcOutType;
+      PgpRxVcCommonOut       : in  PgpRxVcCommonOutType;
+      pgpTxVcIn_locBuffAFull : out sl;
+      pgpTxVcIn_locBuffFull  : out sl;
 
-		-- Local data transfer signals
-		DsBuff32In	: in	DsBuff32InType;
-		DsBuff32Out : out DsBuff32OutType
-		);
+      -- Local data transfer signals
+      DsBuff32In  : in  DsBuff32InType;
+      DsBuff32Out : out DsBuff32OutType
+      );
 end Pgp2DsBuff32Wrapper;
 
 
@@ -58,27 +58,27 @@ end Pgp2DsBuff32Wrapper;
 architecture mapping of Pgp2DsBuff32Wrapper is
 
 begin
-	U_Pgp2DsBuff32 : entity work.Pgp2DsBuff32
-		generic map (
-			FifoType => FifoType
-			) port map ( 
-				pgpClk			=> pgpRxClk,
-				pgpReset			=> pgpRxReset,
-				locClk			=> locClk,
-				locReset			=> locReset,
-				vcFrameRxValid => pgpRxVcOut.frameRxValid,
-				vcFrameRxSOF	=> PgpRxVcCommonOut.frameRxSOF,
-				vcFrameRxEOF	=> PgpRxVcCommonOut.frameRxEOF,
-				vcFrameRxEOFE	=> PgpRxVcCommonOut.frameRxEOFE,
-				vcFrameRxData	=> PgpRxVcCommonOut.frameRxData(RxLane),
-				vcLocBuffAFull => pgpTxVcIn_locBuffAFull,
-				vcLocBuffFull	=> pgpTxVcIn_locBuffFull,
-				frameRxValid	=> DsBuff32Out.frameRxValid,
-				frameRxReady	=> DsBuff32In.frameRxReady,
-				frameRxSOF		=> DsBuff32Out.frameRxSOF,
-				frameRxEOF		=> DsBuff32Out.frameRxEOF,
-				frameRxEOFE		=> DsBuff32Out.frameRxEOFE,
-				frameRxData		=> DsBuff32Out.frameRxData
-				);
+   U_Pgp2DsBuff32 : entity work.Pgp2DsBuff32
+      generic map (
+         FifoType => FifoType
+         ) port map ( 
+            pgpClk         => pgpRxClk,
+            pgpReset       => pgpRxReset,
+            locClk         => locClk,
+            locReset       => locReset,
+            vcFrameRxValid => pgpRxVcOut.frameRxValid,
+            vcFrameRxSOF   => PgpRxVcCommonOut.frameRxSOF,
+            vcFrameRxEOF   => PgpRxVcCommonOut.frameRxEOF,
+            vcFrameRxEOFE  => PgpRxVcCommonOut.frameRxEOFE,
+            vcFrameRxData  => PgpRxVcCommonOut.frameRxData(RxLane),
+            vcLocBuffAFull => pgpTxVcIn_locBuffAFull,
+            vcLocBuffFull  => pgpTxVcIn_locBuffFull,
+            frameRxValid   => DsBuff32Out.frameRxValid,
+            frameRxReady   => DsBuff32In.frameRxReady,
+            frameRxSOF     => DsBuff32Out.frameRxSOF,
+            frameRxEOF     => DsBuff32Out.frameRxEOF,
+            frameRxEOFE    => DsBuff32Out.frameRxEOFE,
+            frameRxData    => DsBuff32Out.frameRxData
+            );
 end mapping;
 
--- pgp2_core/trunk/rtl/applications/Pgp2UsBuff128Wrapper.vhd
+++ pgp2_core/trunk/rtl/applications/Pgp2UsBuff128Wrapper.vhd
@@ -1,10 +1,10 @@
 -------------------------------------------------------------------------------
--- Title			  : Pretty Good Protocol, V2, Upstream Data Buffer
--- Project		  : General Purpose Core
+-- Title         : Pretty Good Protocol, V2, Upstream Data Buffer
+-- Project       : General Purpose Core
 -------------------------------------------------------------------------------
--- File			  : Pgp2UsBuff128Wrapper.vhd
--- Author		  : Larry Ruckman, ruckman@slac.stanford.edu
--- Created		  : 06/11/2013
+-- File          : Pgp2UsBuff128Wrapper.vhd
+-- Author        : Larry Ruckman, ruckman@slac.stanford.edu
+-- Created       : 06/11/2013
 -------------------------------------------------------------------------------
 -- Description:
 -- Wrapper for Pgp2DsBuff128
@@ -24,61 +24,61 @@ use ieee.std_logic_arith.all;
 use ieee.std_logic_unsigned.all;
 
 entity Pgp2UsBuff128Wrapper is
-	generic (
-		TxLane	: integer := 0;			 -- Transmit Lanes Number (0 to 3) 
-		-- FifoType: (default = V5)
-		-- V4 = Virtex 4,	 V5 = Virtex 5, V6 = Virtex 6, V7 = Virtex 7, 
-		-- S6 = Spartan 6, A7 = Artix 7,	 K7 = kintex7
-		FifoType : string	 := "V5"
-		);
-	port (
+   generic (
+      TxLane   : integer := 0;          -- Transmit Lanes Number (0 to 3) 
+      -- FifoType: (default = V5)
+      -- V4 = Virtex 4,  V5 = Virtex 5, V6 = Virtex 6, V7 = Virtex 7, 
+      -- S6 = Spartan 6, A7 = Artix 7,  K7 = kintex7
+      FifoType : string  := "V5"
+      );
+   port (
 
-		-- PGP Tx Clock And Reset
-		pgpTxClk	  : in sl;					 -- PGP Clock
-		pgpTxReset : in sl;					 -- Synchronous PGP Reset
+      -- PGP Tx Clock And Reset
+      pgpTxClk   : in sl;               -- PGP Clock
+      pgpTxReset : in sl;               -- Synchronous PGP Reset
 
-		-- Local clock and reset
-		locClk	: in sl;						 -- Local Clock
-		locReset : in sl;						 -- Synchronous Local Reset  
+      -- Local clock and reset
+      locClk   : in sl;                 -- Local Clock
+      locReset : in sl;                 -- Synchronous Local Reset  
 
-		-- PGP Transmit Signals
-		PgpTxVcIn					: out PgpTxVcInType;
-		PgpTxVcOut					: in	PgpTxVcOutType;
-		pgpRxVcOut_remBuffAFull : in	sl;
-		pgpRxVcOut_remBuffFull	: in	sl;
+      -- PGP Transmit Signals
+      PgpTxVcIn               : out PgpTxVcInType;
+      PgpTxVcOut              : in  PgpTxVcOutType;
+      pgpRxVcOut_remBuffAFull : in  sl;
+      pgpRxVcOut_remBuffFull  : in  sl;
 
-		-- Local data transfer signals
-		UsBuff128In	 : in	 UsBuff128InType;
-		UsBuff128Out : out UsBuff128OutType
-		);
+      -- Local data transfer signals
+      UsBuff128In  : in  UsBuff128InType;
+      UsBuff128Out : out UsBuff128OutType
+      );
 end Pgp2UsBuff128Wrapper;
 
 -- Define architecture
 architecture mapping of Pgp2UsBuff128Wrapper is
 
 begin
-	U_Pgp2UsBuff128 : entity work.Pgp2UsBuff128
-		generic map (
-			FifoType => FifoType
-			)
-		port map (
-			pgpClk			=> pgpTxClk,
-			pgpReset			=> pgpTxReset,
-			locClk			=> locClk,
-			locReset			=> locReset,
-			vcFrameTxValid => pgpTxVcIn.frameTxValid,
-			vcFrameTxReady => pgpTxVcOut.frameTxReady,
-			vcFrameTxSOF	=> pgpTxVcIn.frameTxSOF,
-			vcFrameTxEOF	=> pgpTxVcIn.frameTxEOF,
-			vcFrameTxEOFE	=> pgpTxVcIn.frameTxEOFE,
-			vcFrameTxData	=> pgpTxVcIn.FrameTxData(TxLane),
-			vcRemBuffAFull => pgpRxVcOut_remBuffAFull,
-			vcRemBuffFull	=> pgpRxVcOut_remBuffFull,
-			frameTxValid	=> UsBuff128In.frameTxEnable,
-			frameTxSOF		=> UsBuff128In.frameTxSOF,
-			frameTxEOF		=> UsBuff128In.frameTxEOF,
-			frameTxEOFE		=> UsBuff128In.frameTxEOFE,
-			frameTxData		=> UsBuff128In.frameTxData,
-			frameTxAFull	=> UsBuff128Out.frameTxAFull
-			);
+   U_Pgp2UsBuff128 : entity work.Pgp2UsBuff128
+      generic map (
+         FifoType => FifoType
+         )
+      port map (
+         pgpClk         => pgpTxClk,
+         pgpReset       => pgpTxReset,
+         locClk         => locClk,
+         locReset       => locReset,
+         vcFrameTxValid => pgpTxVcIn.frameTxValid,
+         vcFrameTxReady => pgpTxVcOut.frameTxReady,
+         vcFrameTxSOF   => pgpTxVcIn.frameTxSOF,
+         vcFrameTxEOF   => pgpTxVcIn.frameTxEOF,
+         vcFrameTxEOFE  => pgpTxVcIn.frameTxEOFE,
+         vcFrameTxData  => pgpTxVcIn.FrameTxData(TxLane),
+         vcRemBuffAFull => pgpRxVcOut_remBuffAFull,
+         vcRemBuffFull  => pgpRxVcOut_remBuffFull,
+         frameTxValid   => UsBuff128In.frameTxEnable,
+         frameTxSOF     => UsBuff128In.frameTxSOF,
+         frameTxEOF     => UsBuff128In.frameTxEOF,
+         frameTxEOFE    => UsBuff128In.frameTxEOFE,
+         frameTxData    => UsBuff128In.frameTxData,
+         frameTxAFull   => UsBuff128Out.frameTxAFull
+         );
 end mapping;
