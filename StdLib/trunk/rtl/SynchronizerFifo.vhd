ec7b507a39af07092165214fd5c3929973068b62
bareese <bareese@slac.stanford.edu>
2013-07-11 21:16:27 +0000

    Added SynchronizerFifo.
    git-svn-id: file:///dev/shm/git-devel/svn-repo@2924 0848cbd7-7569-0410-ab05-8cc0a0c67708

---
+++ StdLib/trunk/rtl/SynchronizerFifo.vhd
@@ -0,0 +1,70 @@
+-------------------------------------------------------------------------------
+-- Title      : 
+-------------------------------------------------------------------------------
+-- File       : SynchronizerFifo.vhd
+-- Author     : Ben Reese
+-- Company    : SLAC National Accelerator Laboratory
+-- Created    : 2013-07-10
+-- Last update: 2013-07-11
+-- Platform   : ISE 14.5
+-- Standard   : VHDL'93/02
+-------------------------------------------------------------------------------
+-- Description: 
+-------------------------------------------------------------------------------
+-- Copyright (c) 2013 SLAC National Accelerator Laboratory
+-------------------------------------------------------------------------------
+
+library ieee;
+use ieee.std_logic_1164.all;
+use ieee.numeric_std.all;
+use ieee.std_logic_unsigned.all;
+use ieee.std_logic_arith.all;
+
+use work.StdRtlPkg.all;
+
+entity SynchronizerFifo is
+   generic (
+      TPD_G         : time                       := 1 ns;
+      BRAM_EN_G     : boolean                    := false;
+      SYNC_STAGES_G : integer range 2 to (2**24) := 2;
+      DATA_WIDTH_G  : integer range 1 to (2**24) := 18);
+   port (
+      -- Asynchronous Reset
+      rst    : in  sl;
+      --Write Ports (wr_clk domain)
+      wr_clk : in  sl;
+      din    : in  slv(DATA_WIDTH_G-1 downto 0);
+      --Read Ports (rd_clk domain)
+      rd_clk : in  sl;
+      dout   : out slv(DATA_WIDTH_G-1 downto 0));
+end SynchronizerFifo;
+
+architecture rtl of SynchronizerFifo is
+begin
+   FifoAsync_1 : entity work.FifoAsync
+      generic map (
+         TPD_G        => TPD_G,
+         BRAM_EN_G    => BRAM_EN_G, 
+         DATA_WIDTH_G => DATA_WIDTH_G,
+         ADDR_WIDTH_G => 4)
+      port map (
+         rst           => rst,
+         wr_clk        => wr_clk,
+         wr_en         => '1',
+         din           => din,
+         wr_data_count => open,
+         wr_ack        => open,
+         overflow      => open,
+         prog_full     => open,
+         almost_full   => open,
+         full          => open,
+         rd_clk        => rd_clk,
+         rd_en         => '1',
+         dout          => dout,
+         rd_data_count => open,
+         valid         => open,
+         underflow     => open,
+         prog_empty    => open,
+         almost_empty  => open,
+         empty         => open);
+end architecture rtl;
