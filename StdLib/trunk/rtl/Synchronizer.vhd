f6a9a3a5c97eab20f1ce93046dc7ab605a2eab07
ruckman <ruckman@slac.stanford.edu>
2013-07-30 17:08:20 +0000

    added altera_attribute
    git-svn-id: file:///dev/shm/git-devel/svn-repo@3098 0848cbd7-7569-0410-ab05-8cc0a0c67708

--- StdLib/trunk/rtl/Synchronizer.vhd
+++ StdLib/trunk/rtl/Synchronizer.vhd
@@ -5,7 +5,7 @@
 -- Author     : Benjamin Reese  <bareese@slac.stanford.edu>
 -- Company    : SLAC National Accelerator Laboratory
 -- Created    : 2013-05-13
--- Last update: 2013-05-14
+-- Last update: 2013-07-30
 -- Platform   : 
 -- Standard   : VHDL'93/02
 -------------------------------------------------------------------------------
@@ -22,26 +22,29 @@ use work.StdRtlPkg.all;
 entity Synchronizer is
    generic (
       TPD_G          : time     := 1 ns;
-      RST_POLARITY_G : sl       := '1';        -- '1' for active high rst, '0' for active low
+      RST_POLARITY_G : sl       := '1';  -- '1' for active high rst, '0' for active low
       STAGES_G       : positive := 2;
       INIT_G         : slv      := "0"
       );
    port (
-      clk     : in  sl;                        -- clock to be sync'ed to
+      clk     : in  sl;                 -- clock to be sync'ed to
       aRst    : in  sl := not RST_POLARITY_G;  -- Optional async reset
       sRst    : in  sl := not RST_POLARITY_G;  -- Optional synchronous reset
-      dataIn  : in  sl;                        -- Data to be 'synced'
-      dataOut : out sl                         -- synced data
+      dataIn  : in  sl;                 -- Data to be 'synced'
+      dataOut : out sl                  -- synced data
       );
 begin
    assert (STAGES_G >= 2) report "STAGES_G must be >= 2" severity failure;
 end Synchronizer;
 
 architecture rtl of Synchronizer is
-   constant INIT_C : slv(STAGES_G-1 downto 0) := ite(INIT_G="0", slvZero(STAGES_G), INIT_G);
-   
+   constant INIT_C : slv(STAGES_G-1 downto 0) := ite(INIT_G = "0", slvZero(STAGES_G), INIT_G);
+
    signal r, rin : slv(STAGES_G-1 downto 0) := INIT_C;
 
+   -------------------------------
+   -- XST/Synplify Attributes
+   -------------------------------
    -- These attributes will stop Vivado translating the desired flip-flops into an
    -- SRL based shift register. (Breaks XST for some reason so keep commented for now).
    attribute ASYNC_REG      : string;
@@ -63,10 +66,24 @@ architecture rtl of Synchronizer is
    -- Don't let register balancing move logic between the register chain
    attribute register_balancing      : string;
    attribute register_balancing of r : signal is "no";
+
+   -------------------------------
+   -- Altera Attributes 
+   -- NOTE: These attributes have not been tested yet!!!
+   ------------------------------- 
+   attribute altera_attribute : string;
+
+   -- Disable Auto Shift Register Recognition logic option
+   -- Optimize for metastability
+   -- Don't let register balancing move logic between the register chain
+   attribute altera_attribute of r : signal is "-name AUTO_SHIFT_REGISTER_RECOGNITION OFF -name OPTIMIZE_FOR_METASTABILITY ON -name USE_LOGICLOCK_CONSTRAINTS_IN_BALANCING OFF";
+
+   -- This attribute will stop timing errors
+   attribute altera_attribute of rin : signal is "-name CUT ON -from dataIn -to rin";
    
 begin
 
-   comb : process (dataIn, sRst, r) is
+   comb : process (dataIn, r, sRst) is
    begin
       rin <= r(STAGES_G-2 downto 0) & dataIn;
 
@@ -78,7 +95,7 @@ begin
       dataOut <= r(STAGES_G-1);
    end process comb;
 
-   seq : process (clk, aRst) is
+   seq : process (aRst, clk) is
    begin
       if (rising_edge(clk)) then
          r <= rin after TPD_G;
--- StdLib/trunk/rtl/SynchronizerEdge.vhd
+++ StdLib/trunk/rtl/SynchronizerEdge.vhd
@@ -5,7 +5,7 @@
 -- Author     : Benjamin Reese  <bareese@slac.stanford.edu>
 -- Company    : SLAC National Accelerator Laboratory
 -- Created    : 2013-05-13
--- Last update: 2013-05-23
+-- Last update: 2013-07-30
 -- Platform   : 
 -- Standard   : VHDL'93/02
 -------------------------------------------------------------------------------
@@ -22,31 +22,34 @@ use work.StdRtlPkg.all;
 entity SynchronizerEdge is
    generic (
       TPD_G          : time     := 1 ns;
-      RST_POLARITY_G : sl       := '1';            -- '1' for active high rst, '0' for active low
+      RST_POLARITY_G : sl       := '1';  -- '1' for active high rst, '0' for active low
       STAGES_G       : positive := 3;
       INIT_G         : slv      := "0"
       );
    port (
-      clk         : in  sl;                        -- clock to be sync'ed to
+      clk         : in  sl;             -- clock to be sync'ed to
       aRst        : in  sl := not RST_POLARITY_G;  -- Optional async reset
       sRst        : in  sl := not RST_POLARITY_G;  -- Optional synchronous reset
-      dataIn      : in  sl;                        -- Data to be 'synced'
-      dataOut     : out sl;                        -- synced data
-      risingEdge  : out sl;                        -- Rising edge detected
-      fallingEdge : out sl                         -- Falling edge detected
+      dataIn      : in  sl;             -- Data to be 'synced'
+      dataOut     : out sl;             -- synced data
+      risingEdge  : out sl;             -- Rising edge detected
+      fallingEdge : out sl              -- Falling edge detected
       );
 begin
    assert (STAGES_G >= 3) report "STAGES_G must be >= 3" severity failure;
 end SynchronizerEdge;
 
 architecture rtl of SynchronizerEdge is
-   constant INIT_C : slv(STAGES_G-1 downto 0) := ite(INIT_G="0", slvZero(STAGES_G), INIT_G);
+   constant INIT_C : slv(STAGES_G-1 downto 0) := ite(INIT_G = "0", slvZero(STAGES_G), INIT_G);
 
    -- r(STAGES_G-1) used for edge detection.
    -- Optimized out if edge detection not used.
    signal r, rin : slv(STAGES_G-1 downto 0) := INIT_C;
 
 
+   -------------------------------
+   -- XST/Synplify Attributes
+   -------------------------------
    -- These attributes will stop Vivado translating the desired flip-flops into an
    -- SRL based shift register. (Breaks XST for some reason so keep commented for now).
    attribute ASYNC_REG      : string;
@@ -68,10 +71,24 @@ architecture rtl of SynchronizerEdge is
    -- Don't let register balancing move logic between the register chain
    attribute register_balancing      : string;
    attribute register_balancing of r : signal is "no";
+
+   -------------------------------
+   -- Altera Attributes 
+   -- NOTE: These attributes have not been tested yet!!!
+   ------------------------------- 
+   attribute altera_attribute : string;
+
+   -- Disable Auto Shift Register Recognition logic option
+   -- Optimize for metastability
+   -- Don't let register balancing move logic between the register chain
+   attribute altera_attribute of r : signal is "-name AUTO_SHIFT_REGISTER_RECOGNITION OFF -name OPTIMIZE_FOR_METASTABILITY ON -name USE_LOGICLOCK_CONSTRAINTS_IN_BALANCING OFF";
+
+   -- This attribute will stop timing errors
+   attribute altera_attribute of rin : signal is "-name CUT ON -from dataIn -to rin";
    
 begin
 
-   comb : process (dataIn, sRst, r) is
+   comb : process (dataIn, r, sRst) is
    begin
       rin <= r(STAGES_G-2 downto 0) & dataIn;
 
@@ -85,7 +102,7 @@ begin
       fallingEdge <= not r(STAGES_G-2) and r(STAGES_G-1);
    end process comb;
 
-   seq : process (clk, aRst) is
+   seq : process (aRst, clk) is
    begin
       if (rising_edge(clk)) then
          r <= rin after TPD_G;
