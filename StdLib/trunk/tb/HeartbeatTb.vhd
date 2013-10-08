a3cf9b526abaad9bfc8fdfe0ded35e952b167118
ruckman <ruckman@slac.stanford.edu>
2013-10-08 21:39:16 +0000

    backing up the testbed
    git-svn-id: file:///dev/shm/git-devel/svn-repo@3359 0848cbd7-7569-0410-ab05-8cc0a0c67708

--- StdLib/trunk/tb/HeartbeatTb.vhd
+++ StdLib/trunk/tb/HeartbeatTb.vhd
@@ -5,7 +5,7 @@
 -- Author     : Larry Ruckman  <ruckman@slac.stanford.edu>
 -- Company    : SLAC National Accelerator Laboratory
 -- Created    : 2013-09-26
--- Last update: 2013-09-26
+-- Last update: 2013-10-02
 -- Platform   : ISE 14.5
 -- Standard   : VHDL'93/02
 -------------------------------------------------------------------------------
@@ -25,29 +25,64 @@ use work.StdRtlPkg.all;
 entity HeartbeatTb is end HeartbeatTb;
 
 architecture testbed of HeartbeatTb is
-   constant PERIOD_IN_C  : time := 6.4 ns;
-   constant PERIOD_OUT_C : time := 1000 ms;
-
-   signal clk,
-      o : sl;
+   signal clkIn  : slv(2 downto 0);
+   signal clkOut : slv(2 downto 0);
 begin
-   CLK_Inst : entity work.ClkRst
+   CLK_0 : entity work.ClkRst
       generic map (
-         CLK_PERIOD_G      => PERIOD_IN_C,
+         CLK_PERIOD_G      => 4 ns,
          RST_START_DELAY_G => 1 ns,  -- Wait this long into simulation before asserting reset
          RST_HOLD_TIME_G   => 0.25 us)  -- Hold reset for this long)
       port map (
-         clkP => clk,
+         clkP => clkIn(0),
          clkN => open,
          rst  => open,
          rstL => open);
 
-   Heartbeat_Inst : entity work.Heartbeat
-      generic map(
-         PERIOD_IN_G  => PERIOD_IN_C,
-         PERIOD_OUT_G => PERIOD_OUT_C)
+   CLK_1 : entity work.ClkRst
+      generic map (
+         CLK_PERIOD_G      => 20 ns,
+         RST_START_DELAY_G => 1 ns,  -- Wait this long into simulation before asserting reset
+         RST_HOLD_TIME_G   => 0.25 us)  -- Hold reset for this long)
       port map (
-         clk => clk,
-         o   => o);      
+         clkP => clkIn(1),
+         clkN => open,
+         rst  => open,
+         rstL => open);
+
+   CLK_2 : entity work.ClkRst
+      generic map (
+         CLK_PERIOD_G      => 6.4 ns,
+         RST_START_DELAY_G => 1 ns,  -- Wait this long into simulation before asserting reset
+         RST_HOLD_TIME_G   => 0.25 us)  -- Hold reset for this long)
+      port map (
+         clkP => clkIn(2),
+         clkN => open,
+         rst  => open,
+         rstL => open);       
+
+   Heartbeat_0 : entity work.Heartbeat
+      generic map(
+         PERIOD_IN_G  => 4 ns,
+         PERIOD_OUT_G => 1000 ms)
+      port map (
+         clk => clkIn(0),
+         o   => clkOut(0));  
+
+   Heartbeat_1 : entity work.Heartbeat
+      generic map(
+         PERIOD_IN_G  => 20 ns,
+         PERIOD_OUT_G => 1000 ms)
+      port map (
+         clk => clkIn(1),
+         o   => clkOut(1));  
+
+   Heartbeat_2 : entity work.Heartbeat
+      generic map(
+         PERIOD_IN_G  => 6.4 ns,
+         PERIOD_OUT_G => 1000 ms)
+      port map (
+         clk => clkIn(2),
+         o   => clkOut(2));         
 
 end testbed;
