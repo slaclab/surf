db23e58c3ed8f55e258720a6cfb60f8f2c3b17b1
ruckman <ruckman@slac.stanford.edu>
2013-11-22 22:06:17 +0000

    adding comments to messaging modifications
    git-svn-id: file:///dev/shm/git-devel/svn-repo@3585 0848cbd7-7569-0410-ab05-8cc0a0c67708

--- StdLib/trunk/build/build_vivado_v1.tcl
+++ StdLib/trunk/build/build_vivado_v1.tcl
@@ -27,20 +27,22 @@ add_files -fileset constrs_1 -quiet ${XDC_FILES}
 # Set the top level
 set_property top ${PROJECT} [current_fileset]
 
-# Message Suppression: INFO: Synthesizing Module messages
-set_msg_config -suppress -id {Synth 8-256}
-set_msg_config -suppress -id {Synth 8-113}
-set_msg_config -suppress -id {Synth 8-226}
-set_msg_config -suppress -id {Synth 8-4472}
-
-# Message Suppression: WARNING: "ignoring unsynthesizable construct" due to assert error checking
-set_msg_config -suppress -id {Synth 8-312}
+# Messages Suppression: INFO
+set_msg_config -suppress -id {Synth 8-256};# SYNTH: done synthesizing module
+set_msg_config -suppress -id {Synth 8-113};# SYNTH: binding component instance 'RTL_Inst' to cell 'PRIMITIVE'
+set_msg_config -suppress -id {Synth 8-226};# SYNTH: default block is never used
+set_msg_config -suppress -id {Synth 8-312};# SYNTH: "ignoring unsynthesizable construct" due to assert error checking
+set_msg_config -suppress -id {Synth 8-4472};# SYNTH: Detected and applied attribute shreg_extract = no
 
 # Messages: Change from WARNING to ERROR
-set_msg_config -id {Vivado 12-508} -new_severity {ERROR}
+set_msg_config -id {Synth 8-3512} -new_severity {ERROR};# SYNTH: Assigned value in logic is out of range 
+set_msg_config -id {Synth 8-3919} -new_severity {ERROR};# SYNTH: Null Assignment in logic
+
+# Messages: Change from WARNING to CRITICAL_WARNING
+set_msg_config -id {Vivado 12-508} -new_severity {CRITICAL_WARNING};# XDC: No pins matched 
 
 # Messages: Change from CRITICAL_WARNING to ERROR
-set_msg_config -id {Vivado 12-1387} -new_severity {ERROR}
+set_msg_config -id {Vivado 12-1387} -new_severity {ERROR};# XDC: No valid object(s) found 
 
 # Enable implementation steps by default
 set_property STEPS.POWER_OPT_DESIGN.IS_ENABLED true [get_runs impl_1]
