0b8103985e42d07880cf3d506f18f04906694f32
ruckman <ruckman@slac.stanford.edu>
2013-12-11 19:09:44 +0000

    added getXorRand function
    git-svn-id: file:///dev/shm/git-devel/svn-repo@3662 0848cbd7-7569-0410-ab05-8cc0a0c67708
    
    Former-commit-id: 317b018f0c6cb22c03bcecf0252c9997f75dde2f

--- StdLib/trunk/rtl/PrbsPkg.vhd
+++ StdLib/trunk/rtl/PrbsPkg.vhd
@@ -5,7 +5,7 @@
 -- Author     : Larry Ruckman  <ruckman@slac.stanford.edu>
 -- Company    : SLAC National Accelerator Laboratory
 -- Created    : 2013-12-10
--- Last update: 2013-12-10
+-- Last update: 2013-12-11
 -- Platform   : ISE 14.7
 -- Standard   : VHDL'93/02
 -------------------------------------------------------------------------------
@@ -22,72 +22,100 @@ use work.StdRtlPkg.all;
 
 package PrbsPkg is
 
+   -- Randomizers based on the ITU-T O.150 Standard
    function getPrbs1xTap (input : slv; tap0 : natural) return slv;
    function getPrbs2xTap (input : slv; tap0, tap1 : natural) return slv;
    function getPrbs3xTap (input : slv; tap0, tap1, tap2 : natural) return slv;
 
+   -- Randomizer based LTC2270 IC
+   function getXorRand (input : slv; tap : natural) return slv;  -- NOTE: same function for encoding and decoding
+
 end PrbsPkg;
 
 package body PrbsPkg is
-
+-------------------------------------------------------------------------------
    function getPrbs1xTap (input : slv; tap0 : natural) return slv is
-      variable retVar : slv(input'LEFT downto 0) := (others => '0');
+      variable retVar : slv(input'left downto 0) := (others => '0');
    begin
+
       --check for a valid tap location
-      assert (tap0 <= input'LEFT) report "PrbsPkg: getPrbs's tap0 input is out of range" severity failure;
-      
+      assert (tap0 <= input'left) report "PrbsPkg: getPrbs1xTap's tap0 input is out of range" severity failure;
+
       -- shift register
-      for i in (input'LEFT - 1) downto 0 loop
+      for i in (input'left - 1) downto 0 loop
          retVar(i) := input(i+1);
       end loop;
-      
+
       -- calculate the "xor'd" feedback
-      retVar(input'LEFT) := input(0) xor input(tap0);
-      
+      retVar(input'left) := input(0) xor input(tap0);
+
       --return the result
       return retVar;
       
    end function;
-   
+-------------------------------------------------------------------------------   
    function getPrbs2xTap (input : slv; tap0, tap1 : natural) return slv is
-      variable retVar : slv(input'LEFT downto 0) := (others => '0');
+      variable retVar : slv(input'left downto 0) := (others => '0');
    begin
+
       --check for a valid tap location
-      assert (tap0 <= input'LEFT) report "PrbsPkg: getPrbs's tap0 input is out of range" severity failure;
-      assert (tap1 <= input'LEFT) report "PrbsPkg: getPrbs's tap1 input is out of range" severity failure;
-      
+      assert (tap0 <= input'left) report "PrbsPkg: getPrbs2xTap's tap0 input is out of range" severity failure;
+      assert (tap1 <= input'left) report "PrbsPkg: getPrbs2xTap's tap1 input is out of range" severity failure;
+
       -- shift register
-      for i in (input'LEFT - 1) downto 0 loop
+      for i in (input'left - 1) downto 0 loop
          retVar(i) := input(i+1);
       end loop;
-      
+
       -- calculate the "xor'd" feedback
-      retVar(input'LEFT) := input(0) xor input(tap0) xor input(tap1);
-      
+      retVar(input'left) := input(0) xor input(tap0) xor input(tap1);
+
       --return the result
       return retVar;
       
-   end function;   
-   
+   end function;
+-------------------------------------------------------------------------------   
    function getPrbs3xTap (input : slv; tap0, tap1, tap2 : natural) return slv is
-      variable retVar : slv(input'LEFT downto 0) := (others => '0');
+      variable retVar : slv(input'left downto 0) := (others => '0');
    begin
+
       --check for a valid tap location
-      assert (tap0 <= input'LEFT) report "PrbsPkg: getPrbs's tap0 input is out of range" severity failure;
-      assert (tap1 <= input'LEFT) report "PrbsPkg: getPrbs's tap1 input is out of range" severity failure;
-      assert (tap2 <= input'LEFT) report "PrbsPkg: getPrbs's tap2 input is out of range" severity failure;
-      
+      assert (tap0 <= input'left) report "PrbsPkg: getPrbs3xTap's tap0 input is out of range" severity failure;
+      assert (tap1 <= input'left) report "PrbsPkg: getPrbs3xTap's tap1 input is out of range" severity failure;
+      assert (tap2 <= input'left) report "PrbsPkg: getPrbs3xTap's tap2 input is out of range" severity failure;
+
       -- shift register
-      for i in (input'LEFT - 1) downto 0 loop
+      for i in (input'left - 1) downto 0 loop
          retVar(i) := input(i+1);
       end loop;
-      
+
       -- calculate the "xor'd" feedback
-      retVar(input'LEFT) := input(0) xor input(tap0) xor input(tap1) xor input(tap2);
-      
+      retVar(input'left) := input(0) xor input(tap0) xor input(tap1) xor input(tap2);
+
       --return the result
       return retVar;
       
-   end function;      
-   
+   end function;
+-------------------------------------------------------------------------------   
+   function getXorRand (input : slv; tap : natural) return slv is  -- NOTE: same function for encoding and decoding
+      variable retVar : slv(input'left downto 0) := (others => '0');
+   begin
+
+      --check for a valid tap location
+      assert (tap <= input'left) report "PrbsPkg: getXorRand's tap input is out of range" severity failure;
+
+      -- Encoder/Decoder
+      for i in input'left downto 0 loop
+         if i = tap then
+            retVar(i) := input(i);
+         else
+            retVar(i) := input(i) xor input(tap);
+         end if;
+      end loop;
+
+      --return the result
+      return retVar;
+      
+   end function;
+-------------------------------------------------------------------------------   
 end package body PrbsPkg;
