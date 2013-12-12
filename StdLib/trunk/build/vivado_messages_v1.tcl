fd0e9dbf22faf9ab57bdbe15645227bd9e65a710
ruckman <ruckman@slac.stanford.edu>
2013-12-12 22:12:08 +0000

    reverted the messaging back
    git-svn-id: file:///dev/shm/git-devel/svn-repo@3670 0848cbd7-7569-0410-ab05-8cc0a0c67708
    
    Former-commit-id: 5990f4b8ae33c0f978d9b176f28f438aeac23788

--- StdLib/trunk/build/vivado_messages_v1.tcl
+++ StdLib/trunk/build/vivado_messages_v1.tcl
@@ -24,7 +24,6 @@ set_msg_config -id {Vivado 12-508} -new_severity "CRITICAL WARNING";# XDC: No pi
 
 # Messages: Change from CRITICAL_WARNING to ERROR
 set_msg_config -id {Synth 8-3352} -new_severity ERROR;# SYNTH: multi-driven net
-set_msg_config -id {Route 35-39} -new_severity ERROR;# PAR: The design did not meet timing requirements. 
 set_msg_config -id {Vivado 12-1387} -new_severity ERROR;# XDC: No valid object(s) found 
 
 # Messages: Change from ERROR to WARNING
