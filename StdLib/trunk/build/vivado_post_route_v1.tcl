dae6a65b149fe5e5a807da15d5f25443ce135663
ruckman <ruckman@slac.stanford.edu>
2013-12-13 17:04:44 +0000

    removed some error messaging
    git-svn-id: file:///dev/shm/git-devel/svn-repo@3677 0848cbd7-7569-0410-ab05-8cc0a0c67708

--- StdLib/trunk/build/vivado_post_route_v1.tcl
+++ StdLib/trunk/build/vivado_post_route_v1.tcl
@@ -5,9 +5,6 @@
 set VIVADO_DIR     $::env(VIVADO_DIR)
 set VIVADO_BUILD_DIR $::env(VIVADO_BUILD_DIR)
 
-# Messages: Change from CRITICAL_WARNING to ERROR
-set_msg_config -id {Route 35-39} -new_severity ERROR;# PAR: The design did not meet timing requirements. 
-
 # Target post route script
 source ${VIVADO_DIR}/post_route.tcl
 
