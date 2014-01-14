d4e6a44bc2581c4b32753b73a094665cbe40688c
ruckman <ruckman@slac.stanford.edu>
2014-01-14 23:20:00 +0000

    fixed a bug in sourcing vivado_env_var_v1.tcl
    git-svn-id: file:///dev/shm/git-devel/svn-repo@3831 0848cbd7-7569-0410-ab05-8cc0a0c67708

--- StdLib/trunk/build/vivado_build_v1.tcl
+++ StdLib/trunk/build/vivado_build_v1.tcl
@@ -2,6 +2,7 @@
 # Project Batch-Mode Run Script
 
 # Get variables
+set VIVADO_BUILD_DIR $::env(VIVADO_BUILD_DIR)
 source -quiet ${VIVADO_BUILD_DIR}/vivado_env_var_v1.tcl
 
 # Load Custom Procedures
--- StdLib/trunk/build/vivado_post_synthesis_v1.tcl
+++ StdLib/trunk/build/vivado_post_synthesis_v1.tcl
@@ -2,6 +2,7 @@
 # Post-Synthesis Build Script
 
 # Get variables
+set VIVADO_BUILD_DIR $::env(VIVADO_BUILD_DIR)
 source -quiet ${VIVADO_BUILD_DIR}/vivado_env_var_v1.tcl
 
 # GUI Related:
