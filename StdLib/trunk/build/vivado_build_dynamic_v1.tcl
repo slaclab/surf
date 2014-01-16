8f40dadfbb3bfa681b7f3c1a0f2629145cb127bd
ruckman <ruckman@slac.stanford.edu>
2014-01-16 22:18:29 +0000

    added partial reconfiguration support
    git-svn-id: file:///dev/shm/git-devel/svn-repo@3841 0848cbd7-7569-0410-ab05-8cc0a0c67708
    
    Former-commit-id: 4f2e1f1134ce0b19598eda0556e741f52df03127

--- StdLib/trunk/build/vivado_build_dynamic_v1.tcl
+++ StdLib/trunk/build/vivado_build_dynamic_v1.tcl
@@ -1,5 +1,5 @@
 
-# Project Batch-Mode Run Script
+# Project Batch-Mode Run Script (Partial Reconfiguration: Dynamic)
 
 # Get variables
 set VIVADO_BUILD_DIR $::env(VIVADO_BUILD_DIR)
@@ -8,12 +8,22 @@ source -quiet ${VIVADO_BUILD_DIR}/vivado_env_var_v1.tcl
 # Load Custom Procedures
 source -quiet ${VIVADO_BUILD_DIR}/vivado_proc_v1.tcl
 
+# Check for a blank RECONFIG_NAME variable
+if { [CheckForReconfigCheckPoint] != true } {
+   exit -1
+}
+
 # Open the project
 open_project -quiet ${VIVADO_PROJECT}
 
 # Clean up runs
 reset_run synth_1
 reset_run impl_1
+
+# Prevents I/O insertion for synthesis and downstream tools
+# Note:  To synthesis in GUI (debuggin only, this property 
+#        should also be set in the project's vivado/project_setup.tcl file
+set_property -name {STEPS.SYNTH_DESIGN.ARGS.MORE OPTIONS} -value {-mode out_of_context} -objects [get_runs synth_1]
 
 # Synthesize
 launch_run  synth_1
@@ -28,6 +38,9 @@ if { [get_property PROGRESS [get_runs synth_1]]!="100\%" || \
    close_project
    exit -1
 }
+
+# Import static checkpoint
+ImportStaticReconfigDcp
 
 # Implement
 launch_run -to_step write_bitstream impl_1
@@ -49,7 +62,9 @@ if { [CheckTiming] != true } {
    exit -1
 }
 
+# Export partial configuration bit file
+ExportPartialReconfigBit
+
 # Close the project
 close_project
 exit 0
-
--- StdLib/trunk/build/vivado_proc_v1.tcl
+++ StdLib/trunk/build/vivado_proc_v1.tcl
@@ -1,6 +1,10 @@
 
 # Custom Procedure Script
 
+###############################################################
+#### General Functions ########################################
+###############################################################
+
 # Force "pwd" function to be "pwd -L" and not "pwd -P"
 proc pwd { } {
    return $::env(PWD)
@@ -38,6 +42,222 @@ proc CheckTiming { } {
       return true
    }
 }
+
+###############################################################
+#### Partial Reconfiguration Functions ########################
+###############################################################
+
+# Check if RECONFIG_NAME environmental variable
+proc CheckForReconfigName { } {
+   if { [info exists ::env(RECONFIG_NAME)] } {
+      return true
+   } else {
+      puts "\n\nNo RECONFIG_NAME environmental variable was found."
+      puts "Please check the project's Makefile\n\n"
+      return false   
+   }
+}
+
+# Check if RECONFIG_CHECKPOINT environmental variable exists
+proc CheckForReconfigCheckPoint { } {
+   if { [info exists ::env(RECONFIG_CHECKPOINT)] } {
+      return true
+   } else {
+      puts "\n\nNo RECONFIG_CHECKPOINT environmental variable was found."
+      puts "Please check the project's Makefile\n\n"
+      return false   
+   }
+}
+
+# Generate Partial Reconfiguration RTL Block function
+proc GenPartialReconfigDcp {rtlName} {
+
+   puts "\n\nGenerating ${rtlName} RTL ... \n\n"
+
+   # Get variables
+   set VIVADO_BUILD_DIR $::env(VIVADO_BUILD_DIR)
+   source -quiet ${VIVADO_BUILD_DIR}/vivado_env_var_v1.tcl
+   
+   # create a synthesis run
+   create_run -flow {Vivado Synthesis 2013} ${rtlName}_1
+   
+   # Get a list of all the constraint file sets
+   set CONSTRS_LIST [get_filesets constr*]
+   
+   # Search of the matching constraint
+   foreach constrPntr ${CONSTRS_LIST} {
+      set CONSTRS_NAME [get_files -of_objects ${constrPntr}]
+      if { [lsearch ${CONSTRS_NAME} *${rtlName}.xdc] == 0 } {
+         # Set the constraint file
+         set_property constrset ${constrPntr} [get_runs ${rtlName}_1]   
+      }
+   }    
+   
+   # Don't flatten the hierarchy
+   set_property STEPS.SYNTH_DESIGN.ARGS.FLATTEN_HIERARCHY none [get_runs ${rtlName}_1]
+   
+   # Prevents I/O insertion for synthesis and downstream tools
+   set_property -name {STEPS.SYNTH_DESIGN.ARGS.MORE OPTIONS} -value {-mode out_of_context} -objects [get_runs ${rtlName}_1]   
+   
+   # Message Filtering Script
+   source -quiet ${VIVADO_BUILD_DIR}/vivado_messages_v1.tcl   
+   
+   # Set the top level RTL
+   set_property top ${rtlName} [current_fileset]
+   
+   # Synthesize
+   launch_run  ${rtlName}_1
+   wait_on_run ${rtlName}_1   
+}
+
+# Insert the Partial Reconfiguration RTL Block(s) into top level checkpoint checkpoint
+proc InsertStaticReconfigDcp { } {
+
+   # Get variables
+   set VIVADO_BUILD_DIR $::env(VIVADO_BUILD_DIR)
+   set RECONFIG_NAME    $::env(RECONFIG_NAME)
+   source -quiet ${VIVADO_BUILD_DIR}/vivado_env_var_v1.tcl
+   
+   # Set common variables
+   set SYNTH_DIR ${OUT_DIR}/${PROJECT}_project.runs/synth_1
+   
+   # Enable the RTL Blocks(s) Logic
+   foreach rtlPntr ${RECONFIG_NAME} {
+      set_property is_enabled true [get_files *${rtlPntr}.vhd]
+   }      
+   
+   # Disable the top level HDL
+   set_property is_enabled false [get_files ${PROJECT}.vhd]
+   
+   # Generate Partial Reconfiguration RTL Block(s) checkpoints
+   foreach rtlPntr ${RECONFIG_NAME} {
+      GenPartialReconfigDcp ${rtlPntr}
+   }
+
+   # Reset the top level module
+   set_property is_enabled true [get_files ${PROJECT}.vhd]
+   set_property top ${PROJECT} [current_fileset]
+   
+   # Reset the "needs_refresh" flag because of top level assignment juggling
+   set_property needs_refresh false [get_runs synth_1]
+   foreach rtlPntr ${RECONFIG_NAME} {
+      set_property needs_refresh false [get_runs ${rtlPntr}_1]
+   }   
+   
+   # open the top level check point
+   open_checkpoint ${SYNTH_DIR}/${PROJECT}.dcp
+
+   # Backup the top level checkpoint and reports
+   write_checkpoint -force ${SYNTH_DIR}/${PROJECT}_backup.dcp
+   file rename -force ${SYNTH_DIR}/${PROJECT}_utilization_synth.rpt ${SYNTH_DIR}/${PROJECT}_utilization_synth_backup.rpt
+   file rename -force ${SYNTH_DIR}/${PROJECT}_utilization_synth.pb  ${SYNTH_DIR}/${PROJECT}_utilization_synth_backup.pb
+
+   # Load the top-level constraint file
+   read_xdc [lsearch -all -inline ${XDC_FILES} *${PROJECT}.xdc]
+
+   # Load the synthesized Partial Reconfiguration RTL Block's check points
+   foreach rtlPntr ${RECONFIG_NAME} {
+      read_checkpoint -cell ${rtlPntr}_Inst ${SYNTH_DIR}/../${rtlPntr}_1/${rtlPntr}.dcp
+   }
+
+   # Define each of these sub-modules as partially reconfigurable
+   foreach rtlPntr ${RECONFIG_NAME} {
+      set_property HD.RECONFIGURABLE 1 [get_cells ${rtlPntr}_Inst]
+   }
+
+   # Check for DRC
+   report_drc -file ${SYNTH_DIR}/${PROJECT}_reconfig_drc.txt
+
+   # Overwrite the existing synth_1 checkpoint, which is the 
+   # checkpoint that impl_1 will refer to
+   write_checkpoint -force ${SYNTH_DIR}/${PROJECT}.dcp   
+   
+   # Generate new top level reports to update GUI display
+   report_utilization -file ${SYNTH_DIR}/${PROJECT}_utilization_synth.rpt -pb ${SYNTH_DIR}/${PROJECT}_utilization_synth.pb
+   
+   # Close the opened design before launching the impl_1
+   close_design
+}
+
+# Export static checkpoint
+proc ExportStaticReconfigDcp { } {
+
+   # Get variables
+   set VIVADO_BUILD_DIR $::env(VIVADO_BUILD_DIR)
+   source -quiet ${VIVADO_BUILD_DIR}/vivado_env_var_v1.tcl
+   
+   # Set common variables
+   set IMPL_DIR ${OUT_DIR}/${PROJECT}_project.runs/impl_1
+   
+   # Make a copy of the .dcp file with a "_static" suffix for 
+   # the Makefile system to copy over
+   file copy -force ${IMPL_DIR}/${PROJECT}_routed.dcp ${IMPL_DIR}/${PROJECT}_static.dcp   
+   
+   # Make a copy of the .bit file with a "_static" suffix for 
+   # the Makefile system to copy over
+   file copy -force ${IMPL_DIR}/${PROJECT}.bit ${IMPL_DIR}/${PROJECT}_static.bit
+}
+
+# Import static checkpoint
+proc ImportStaticReconfigDcp { } {
+
+   # Get variables
+   set VIVADO_BUILD_DIR    $::env(VIVADO_BUILD_DIR)
+   set RECONFIG_CHECKPOINT $::env(RECONFIG_CHECKPOINT)
+   source -quiet ${VIVADO_BUILD_DIR}/vivado_env_var_v1.tcl
+   
+   # Set common variables
+   set SYNTH_DIR ${OUT_DIR}/${PROJECT}_project.runs/synth_1
+   
+   # Backup the Partial Reconfiguration RTL Block checkpoint and reports
+   file copy -force ${SYNTH_DIR}/${PROJECT}.dcp ${SYNTH_DIR}/${PROJECT}_backup.dcp
+   file rename -force ${SYNTH_DIR}/${PROJECT}_utilization_synth.rpt ${SYNTH_DIR}/${PROJECT}_utilization_synth_backup.rpt
+   file rename -force ${SYNTH_DIR}/${PROJECT}_utilization_synth.pb  ${SYNTH_DIR}/${PROJECT}_utilization_synth_backup.pb
+   
+   # Open the static design check point
+   open_checkpoint ${RECONFIG_CHECKPOINT}   
+   
+   # Clear out the targeted reconfigurable module logic
+   update_design -cell ${PROJECT}_Inst -black_box 
+   
+   # Lock down all placement and routing of the static design
+   lock_design -level routing     
+
+   # Read the targeted reconfiguration RTL block's checkpoint
+   read_checkpoint -cell ${PROJECT}_Inst ${SYNTH_DIR}/${PROJECT}.dcp   
+   
+   # Check for DRC
+   report_drc -file ${SYNTH_DIR}/${PROJECT}_reconfig_drc.txt   
+
+   # Overwrite the existing synth_1 checkpoint, which is the 
+   # checkpoint that impl_1 will refer to
+   write_checkpoint -force ${SYNTH_DIR}/${PROJECT}.dcp   
+   
+   # Generate new top level reports to update GUI display
+   report_utilization -file ${SYNTH_DIR}/${PROJECT}_utilization_synth.rpt -pb ${SYNTH_DIR}/${PROJECT}_utilization_synth.pb
+   
+   # Close the opened design before launching the impl_1
+   close_design
+}
+
+# Export partial configuration bit file
+proc ExportPartialReconfigBit { } {
+
+   # Get variables
+   set VIVADO_BUILD_DIR $::env(VIVADO_BUILD_DIR)
+   source -quiet ${VIVADO_BUILD_DIR}/vivado_env_var_v1.tcl
+   
+   # Set common variables
+   set IMPL_DIR ${OUT_DIR}/${PROJECT}_project.runs/impl_1
+ 
+   # Make a copy of the partial .bit file with a "_static" suffix for 
+   # the Makefile system to copy over
+   file copy -force ${IMPL_DIR}/${PROJECT}_pblock_${PROJECT}_partial.bit ${IMPL_DIR}/${PROJECT}_dynamic.bit
+}
+
+###############################################################
+#### Hardware Debugging Functions #############################
+###############################################################
 
 # Create a Debug Core Function
 proc CreateDebugCore {ilaName} {
