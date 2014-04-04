3c77408ebab188a1fe8363b3eaa2157e781e5a39
ruckman <ruckman@slac.stanford.edu>
2014-04-04 20:17:40 +0000

    minimized the latency of the SynchronizerOneShot.vhd module
    git-svn-id: file:///dev/shm/git-devel/svn-repo@4132 0848cbd7-7569-0410-ab05-8cc0a0c67708
    
    Former-commit-id: 791d074d01ad954cca1fe02a698924d9030b1f6d

--- StdLib/trunk/sync/rtl/SynchronizerOneShot.vhd
+++ StdLib/trunk/sync/rtl/SynchronizerOneShot.vhd
@@ -5,7 +5,7 @@
 -- Author     : Larry Ruckman  <ruckman@slac.stanford.edu>
 -- Company    : SLAC National Accelerator Laboratory
 -- Created    : 2014-02-06
--- Last update: 2014-02-06
+-- Last update: 2014-04-04
 -- Platform   : 
 -- Standard   : VHDL'93/02
 -------------------------------------------------------------------------------
@@ -21,39 +21,83 @@ use work.StdRtlPkg.all;
 
 entity SynchronizerOneShot is
    generic (
-      TPD_G          : time := 1 ns;    -- Simulation FF output delay
-      IN_POLARITY_G  : sl   := '1';     -- 0 for active LOW, 1 for active HIGH
-      OUT_POLARITY_G : sl   := '1');    -- 0 for active LOW, 1 for active HIGH
+      TPD_G           : time     := 1 ns;     -- Simulation FF output delay
+      RST_POLARITY_G  : sl       := '1';      -- '1' for active HIGH reset, '0' for active LOW reset
+      RST_ASYNC_G     : boolean  := false;    -- Reset is asynchronous
+      RELEASE_DELAY_G : positive := 3;        -- Delay between deassertion of async and sync resets
+      IN_POLARITY_G   : sl       := '1';      -- 0 for active LOW, 1 for active HIGH
+      OUT_POLARITY_G  : sl       := '1');     -- 0 for active LOW, 1 for active HIGH
    port (
-      clk     : in  sl;                 -- clock to be sync'ed to
-      dataIn  : in  sl;                 -- trigger to be sync'd
-      dataOut : out sl);                -- synced one-shot pulse
+      clk     : in  sl;                      -- clock to be SYNC'd to
+      rst     : in  sl := not RST_POLARITY_G;-- Optional reset
+      dataIn  : in  sl;                      -- trigger to be sync'd
+      dataOut : out sl);                     -- synced one-shot pulse
 end SynchronizerOneShot;
 
 architecture mapping of SynchronizerOneShot is
    
-   signal syncRst,
-      pulse : sl;
+   type RegType is record
+      syncRstDly : sl;
+      dataOut    : sl;
+   end record RegType;
+   constant REG_INIT_C : RegType := (
+      '1',
+      (not OUT_POLARITY_G));
+   signal r       : RegType := REG_INIT_C;
+   signal rin     : RegType;
+   signal syncRst : sl;
    
 begin
 
    RstSync_Inst : entity work.RstSync
       generic map (
-         TPD_G         => TPD_G,
-         IN_POLARITY_G => IN_POLARITY_G)   
+         TPD_G           => TPD_G,
+         RELEASE_DELAY_G => RELEASE_DELAY_G,
+         IN_POLARITY_G   => IN_POLARITY_G)   
       port map (
          clk      => clk,
          asyncRst => dataIn,
          syncRst  => syncRst); 
 
-   SynchronizerEdge_Inst : entity work.SynchronizerEdge
-      generic map (
-         TPD_G      => TPD_G)    
-      port map (
-         clk        => clk,
-         dataIn     => syncRst,
-         risingEdge => pulse);
+   comb : process (r, rst, syncRst) is
+      variable v : RegType;
+   begin
+      -- Latch the current value
+      v := r;
 
-   dataOut <= pulse when(OUT_POLARITY_G='1') else not(pulse);
+      -- Reset strobe signals
+      v.dataOut := not OUT_POLARITY_G;
+
+      -- Keep a record of the last syncRst
+      v.syncRstDly := syncRst;
+
+      -- Check for a rising edge of the syncRst
+      if (syncRst = '1') and (r.syncRstDly = '0') then
+         v.dataOut := OUT_POLARITY_G;
+      end if;
+
+      -- Sync Reset
+      if (RST_ASYNC_G = false and rst = RST_POLARITY_G) then
+         v := REG_INIT_C;
+      end if;
+
+      -- Register the variable for next clock cycle
+      rin <= v;
+
+      -- Outputs
+      dataOut <= r.dataOut;
+      
+   end process comb;
+
+   seq : process (clk, rst) is
+   begin
+      if (rising_edge(clk)) then
+         r <= rin after TPD_G;
+      end if;
+      -- Async Reset
+      if (RST_ASYNC_G and rst = RST_POLARITY_G) then
+         r <= REG_INIT_C after TPD_G;
+      end if;
+   end process seq;
 
 end architecture mapping;
