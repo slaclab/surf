d439ea7034a24fa4a2419fe6c3e52327ee2fa87b
ruckman <ruckman@slac.stanford.edu>
2014-04-21 18:43:04 +0000

    fixed typos
    git-svn-id: file:///dev/shm/git-devel/svn-repo@4271 0848cbd7-7569-0410-ab05-8cc0a0c67708
    
    Former-commit-id: 4a93a32a2ee0a1cf893ccf63daf5df4b7eac3de8

--- FirmwareDeviceLibrary/trunk/AxiSpfCore/rtl/AxiSpfPkg.vhd
+++ FirmwareDeviceLibrary/trunk/AxiSpfCore/rtl/AxiSpfPkg.vhd
@@ -3,56 +3,56 @@ use ieee.std_logic_1164.all;
 
 use work.StdRtlPkg.all;
 
-package AxiSpfPkg is
+package AxiSfpPkg is
    
-   type AxiSpfInType is record
-      spfRs      : slv(1 downto 0);
-      spfRxLoss  : sl;
-      spfAbs     : sl;
-      spfTxFault : sl;
+   type AxiSfpInType is record
+      sfpRs      : slv(1 downto 0);
+      sfpRxLoss  : sl;
+      sfpAbs     : sl;
+      sfpTxFault : sl;
    end record;
-   type AxiSpfInArray is array (natural range <>) of AxiSpfInType;
-   type AxiSpfInVectorArray is array (integer range<>, integer range<>)of AxiSpfInType;
-   constant AXI_SFP_IN_INIT_C : AxiSpfInType := (
+   type AxiSfpInArray is array (natural range <>) of AxiSfpInType;
+   type AxiSfpInVectorArray is array (integer range<>, integer range<>)of AxiSfpInType;
+   constant AXI_SFP_IN_INIT_C : AxiSfpInType := (
       (others => '0'),
       '0',
       '0',
       '0');   
 
-   type AxiSpfInOutType is record
-      spfScl : sl;
-      spfSda : sl;
+   type AxiSfpInOutType is record
+      sfpScl : sl;
+      sfpSda : sl;
    end record;
-   type AxiSpfInOutArray is array (natural range <>) of AxiSpfInOutType;
-   type AxiSpfInOutVectorArray is array (integer range<>, integer range<>)of AxiSpfInOutType;
-   constant AXI_SFP_IN_OUT_INIT_C : AxiSpfInOutType := (
+   type AxiSfpInOutArray is array (natural range <>) of AxiSfpInOutType;
+   type AxiSfpInOutVectorArray is array (integer range<>, integer range<>)of AxiSfpInOutType;
+   constant AXI_SFP_IN_OUT_INIT_C : AxiSfpInOutType := (
       'Z',
       'Z');       
 
-   type AxiSpfOutType is record
-      spfTxDisable : sl;
+   type AxiSfpOutType is record
+      sfpTxDisable : sl;
    end record;
-   type AxiSpfOutArray is array (natural range <>) of AxiSpfOutType;
-   type AxiSpfOutVectorArray is array (integer range<>, integer range<>)of AxiSpfOutType;
-   constant AXI_SFP_OUT_INIT_C : AxiSpfOutType := (
+   type AxiSfpOutArray is array (natural range <>) of AxiSfpOutType;
+   type AxiSfpOutVectorArray is array (integer range<>, integer range<>)of AxiSfpOutType;
+   constant AXI_SFP_OUT_INIT_C : AxiSfpOutType := (
       (others => '0'));  
 
-   type AxiSpfStatusType is record
-      spfRs      : slv(1 downto 0);
-      spfRxLoss  : sl;
-      spfAbs     : sl;
-      spfTxFault : sl;
+   type AxiSfpStatusType is record
+      sfpRs      : slv(1 downto 0);
+      sfpRxLoss  : sl;
+      sfpAbs     : sl;
+      sfpTxFault : sl;
    end record;
-   constant AXI_SFP_STATUS_INIT_C : AxiSpfStatusType := (
+   constant AXI_SFP_STATUS_INIT_C : AxiSfpStatusType := (
       (others => '0'),
       '0',
       '0',
       '0');
 
-   type AxiSpfConfigType is record
-      spfTxDisable : sl;
+   type AxiSfpConfigType is record
+      sfpTxDisable : sl;
    end record;
-   constant AXI_SFP_CONFIG_INIT_C : AxiSpfConfigType := (
+   constant AXI_SFP_CONFIG_INIT_C : AxiSfpConfigType := (
       (others => '0'));  
 
 end package;
