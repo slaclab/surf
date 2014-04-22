008f05dd5969e7ac1972f735901a552a98daa000
rherbst <rherbst@slac.stanford.edu>
2014-04-22 20:07:49 +0000

    git-svn-id: file:///dev/shm/git-devel/svn-repo@4304 0848cbd7-7569-0410-ab05-8cc0a0c67708

--- pgp2b_core/trunk/core/rtl/Pgp2bLane.vhd
+++ pgp2b_core/trunk/core/rtl/Pgp2bLane.vhd
@@ -30,42 +30,52 @@ entity Pgp2bLane is
       VC_INTERLEAVE_G   : integer              := 1; -- Interleave Frames
       EN_SHORT_CELLS_G  : integer              := 1; -- Enable short non-EOF cells
       PAYLOAD_CNT_TOP_G : integer              := 7; -- Top bit for payload counter
-      NUM_VC_EN_G       : integer range 1 to 4 := 4
+      NUM_VC_EN_G       : integer range 1 to 4 := 4;
+      TX_ENABLE_G       : boolean              := true; -- Enable TX direction
+      RX_ENABLE_G       : boolean              := true  -- Enable RX direction
    );
    port ( 
 
+      ---------------------------------
+      -- Transmitter Interface
+      ---------------------------------
+   
       -- System clock, reset & control
-      pgpTxClk          : in  sl;    -- Master clock
-      pgpTxClkRst        : in  sl;    -- Synchronous reset input
+      pgpTxClk          : in  sl := '0';
+      pgpTxClkRst       : in  sl := '0';
 
       -- Non-VC related IO
-      pgpTxIn           : in  PgpTxInType;
+      pgpTxIn           : in  PgpTxInType := PGP_TX_IN_INIT_C;
       pgpTxOut          : out PgpTxOutType;
 
       -- VC Interface
-      pgpTxVcData       : in  Vc64DataArray(3 downto 0);
+      pgpTxVcData       : in  Vc64DataArray(3 downto 0) := (others=>VC64_DATA_INIT_C);
       pgpTxVcCtrl       : out Vc64CtrlArray(3 downto 0);
 
       -- Phy interface
       phyTxLanesOut     : out PgpTxPhyLaneOutArray(0 to LANE_CNT_G-1);
-      phyTxReady        : in  sl;
+      phyTxReady        : in  sl := '0';
+
+      ---------------------------------
+      -- Receiver Interface
+      ---------------------------------
 
       -- System clock, reset & control
-      pgpRxClk          : in  sl;        -- Master clock
-      pgpRxClkRst       : in  sl;        -- Synchronous reset input
+      pgpRxClk          : in  sl := '0';
+      pgpRxClkRst       : in  sl := '0';
 
       -- Non-VC related IO
-      pgpRxIn           : in  PgpRxInType;
+      pgpRxIn           : in  PgpRxInType := PGP_RX_IN_INIT_C;
       pgpRxOut          : out PgpRxOutType;
 
       -- VC Outputs
       pgpRxVcData       : out Vc64DataType;
-      pgpRxVcCtrl       : in  Vc64CtrlArray(3 downto 0);
+      pgpRxVcCtrl       : in  Vc64CtrlArray(3 downto 0) := (others=>VC64_CTRL_FORCE_C);
 
       -- PHY interface
       phyRxLanesOut     : out PgpRxPhyLaneOutArray(0 to LANE_CNT_G-1);
-      phyRxLanesIn      : in  PgpRxPhyLaneInArray(0 to LANE_CNT_G-1);
-      phyRxReady        : in  sl;
+      phyRxLanesIn      : in  PgpRxPhyLaneInArray(0 to LANE_CNT_G-1) := (others=>PGP_RX_PHY_LANE_IN_INIT_C);
+      phyRxReady        : in  sl := '0';
       phyRxInit         : out sl
    );
 
@@ -83,45 +93,67 @@ architecture Pgp2bLane of Pgp2bLane is
 begin
 
 
-   -- Transmit
-   U_Pgp2bTx: entity work.Pgp2bTx 
-      generic map (
-         TPD_G              => TPD_G,
-         TX_LANE_CNT_G      => LANE_CNT_G,
-         VC_INTERLEAVE_G    => VC_INTERLEAVE_G,
-         PAYLOAD_CNT_TOP_G  => PAYLOAD_CNT_TOP_G,
-         NUM_VC_EN_G        => NUM_VC_EN_G
-      ) port map ( 
-         pgpTxClk           => pgpTxClk,
-         pgpTxClkRst        => pgpTxClkRst,
-         pgpTxIn            => pgpTxIn,
-         pgpTxOut           => pgpTxOut,
-         pgpTxVcData        => pgpTxVcData,
-         pgpTxVcCtrl        => intTxVcCtrl,
-         pgpTxLocVcCtrl     => intTxLocVcCtrl,
-         phyTxLanesOut      => phyTxLanesOut,
-         phyTxReady         => phyTxReady
-      );
+   U_TxEnGen: if TX_ENABLE_G generate
 
-   -- Receive
-   U_Pgp2bRx: entity work.Pgp2bRx 
-      generic map (
-         TPD_G              => TPD_G,
-         RX_LANE_CNT_G      => LANE_CNT_G,
-         EN_SHORT_CELLS_G   => EN_SHORT_CELLS_G,
-         PAYLOAD_CNT_TOP_G  => PAYLOAD_CNT_TOP_G
-      ) port map (
-         pgpRxClk          => pgpRxClk,
-         pgpRxClkRst       => pgpRxClkRst,
-         pgpRxIn           => pgpRxIn,
-         pgpRxOut          => pgpRxOut,
-         pgpRxVcData       => pgpRxVcData,
-         pgpRxRemVcCtrl    => intRxRemVcCtrl,
-         phyRxLanesOut     => phyRxLanesOut,
-         phyRxLanesIn      => phyRxLanesIn,
-         phyRxReady        => phyRxReady,
-         phyRxInit         => phyRxInit
-      );
+      -- Transmit
+      U_Pgp2bTx: entity work.Pgp2bTx 
+         generic map (
+            TPD_G              => TPD_G,
+            TX_LANE_CNT_G      => LANE_CNT_G,
+            VC_INTERLEAVE_G    => VC_INTERLEAVE_G,
+            PAYLOAD_CNT_TOP_G  => PAYLOAD_CNT_TOP_G,
+            NUM_VC_EN_G        => NUM_VC_EN_G
+         ) port map ( 
+            pgpTxClk           => pgpTxClk,
+            pgpTxClkRst        => pgpTxClkRst,
+            pgpTxIn            => pgpTxIn,
+            pgpTxOut           => pgpTxOut,
+            pgpTxVcData        => pgpTxVcData,
+            pgpTxVcCtrl        => intTxVcCtrl,
+            pgpTxLocVcCtrl     => intTxLocVcCtrl,
+            phyTxLanesOut      => phyTxLanesOut,
+            phyTxReady         => phyTxReady
+         );
+   end generate;
+
+   U_TxDisGen: if TX_ENABLE_G = false generate
+      pgpTxOut      <= PGP_TX_OUT_INIT_C;
+      intTxVcCtrl   <= (others=>VC64_CTRL_FORCE_C);
+      phyTxLanesOut <= (others=>PGP_TX_PHY_LANE_OUT_INIT_C);
+   end generate;
+
+
+   U_RxEnGen: if RX_ENABLE_G generate
+
+      -- Receive
+      U_Pgp2bRx: entity work.Pgp2bRx 
+         generic map (
+            TPD_G              => TPD_G,
+            RX_LANE_CNT_G      => LANE_CNT_G,
+            EN_SHORT_CELLS_G   => EN_SHORT_CELLS_G,
+            PAYLOAD_CNT_TOP_G  => PAYLOAD_CNT_TOP_G
+         ) port map (
+            pgpRxClk          => pgpRxClk,
+            pgpRxClkRst       => pgpRxClkRst,
+            pgpRxIn           => pgpRxIn,
+            pgpRxOut          => pgpRxOut,
+            pgpRxVcData       => pgpRxVcData,
+            pgpRxRemVcCtrl    => intRxRemVcCtrl,
+            phyRxLanesOut     => phyRxLanesOut,
+            phyRxLanesIn      => phyRxLanesIn,
+            phyRxReady        => phyRxReady,
+            phyRxInit         => phyRxInit
+         );
+   end generate;
+
+   U_RxDisGen: if RX_ENABLE_G = false generate
+      pgpRxOut       <= PGP_RX_OUT_INIT_C;
+      pgpRxVcData    <= VC64_DATA_INIT_C;
+      intRxRemVcCtrl <= (others=>VC64_CTRL_FORCE_C);
+      phyRxLanesOut  <= (others=>PGP_RX_PHY_LANE_OUT_INIT_C);
+      phyRxInit      <= '0';
+   end generate;
+
 
    U_VcCtrlGen: for i in 0 to 3 generate
 
