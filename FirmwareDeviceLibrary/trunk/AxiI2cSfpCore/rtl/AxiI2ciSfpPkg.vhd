8cbde920037027a1c36f19384811950c3b0961af
ruckman <ruckman@slac.stanford.edu>
2014-04-23 16:09:56 +0000

    renaming
    git-svn-id: file:///dev/shm/git-devel/svn-repo@4307 0848cbd7-7569-0410-ab05-8cc0a0c67708

--- FirmwareDeviceLibrary/trunk/AxiI2cSfpCore/rtl/AxiI2ciSfpPkg.vhd
+++ FirmwareDeviceLibrary/trunk/AxiI2cSfpCore/rtl/AxiI2ciSfpPkg.vhd
@@ -3,55 +3,55 @@ use ieee.std_logic_1164.all;
 
 use work.StdRtlPkg.all;
 
-package AxiSfpPkg is
+package AxiI2cSfpPkg is
    
-   type AxiSfpInType is record
+   type AxiI2cSfpInType is record
       rxLoss     : sl;  -- Loss of Signal – High indicates loss of received optical signal 
       moduleDetL : sl;  -- Module Definition 0 (MOD-DEF0) – Grounded in module (module present indicator) 
       txFault    : sl;  -- Transmitter Fault Indication – High indicates a fault condition 
    end record;
-   type AxiSfpInArray is array (natural range <>) of AxiSfpInType;
-   type AxiSfpInVectorArray is array (integer range<>, integer range<>)of AxiSfpInType;
-   constant AXI_SFP_IN_INIT_C : AxiSfpInType := (
+   type AxiI2cSfpInArray is array (natural range <>) of AxiI2cSfpInType;
+   type AxiI2cSfpInVectorArray is array (integer range<>, integer range<>)of AxiI2cSfpInType;
+   constant AXI_SFP_IN_INIT_C : AxiI2cSfpInType := (
       '0',
       '0',
       '0');   
 
-   type AxiSfpInOutType is record
+   type AxiI2cSfpInOutType is record
       rateSel : slv(1 downto 0);        -- Bit Rate Parametric Optimization 
       scl     : sl;  -- Module Definition 1 (MOD-DEF1) – Two wire serial ID interface clock line (SCL) 
       sda     : sl;  -- Module Definition 2 (MOD-DEF2) – Two wire serial ID interface data line (SDA) 
    end record;
-   type AxiSfpInOutArray is array (natural range <>) of AxiSfpInOutType;
-   type AxiSfpInOutVectorArray is array (integer range<>, integer range<>)of AxiSfpInOutType;
-   constant AXI_SFP_IN_OUT_INIT_C : AxiSfpInOutType := (
+   type AxiI2cSfpInOutArray is array (natural range <>) of AxiI2cSfpInOutType;
+   type AxiI2cSfpInOutVectorArray is array (integer range<>, integer range<>)of AxiI2cSfpInOutType;
+   constant AXI_SFP_IN_OUT_INIT_C : AxiI2cSfpInOutType := (
       (others => 'Z'),
       'Z',
       'Z');       
 
-   type AxiSfpOutType is record
+   type AxiI2cSfpOutType is record
       txDisable : sl;  -- Transmitter Disable – Module electrical input disables on high or open 
    end record;
-   type AxiSfpOutArray is array (natural range <>) of AxiSfpOutType;
-   type AxiSfpOutVectorArray is array (integer range<>, integer range<>)of AxiSfpOutType;
-   constant AXI_SFP_OUT_INIT_C : AxiSfpOutType := (
+   type AxiI2cSfpOutArray is array (natural range <>) of AxiI2cSfpOutType;
+   type AxiI2cSfpOutVectorArray is array (integer range<>, integer range<>)of AxiI2cSfpOutType;
+   constant AXI_SFP_OUT_INIT_C : AxiI2cSfpOutType := (
       (others => '0'));  
 
-   type AxiSfpStatusType is record
+   type AxiI2cSfpStatusType is record
       rxLoss     : sl;
       moduleDetL : sl;
       txFault    : sl;
    end record;
-   constant AXI_SFP_STATUS_INIT_C : AxiSfpStatusType := (
+   constant AXI_SFP_STATUS_INIT_C : AxiI2cSfpStatusType := (
       '0',
       '0',
       '0');
 
-   type AxiSfpConfigType is record
+   type AxiI2cSfpConfigType is record
       rateSel   : slv(1 downto 0);
       txDisable : sl;
    end record;
-   constant AXI_SFP_CONFIG_INIT_C : AxiSfpConfigType := (
+   constant AXI_SFP_CONFIG_INIT_C : AxiI2cSfpConfigType := (
       (others => '1'),
       '0');  
 
