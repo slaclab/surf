309365e4bd265e97581f35228caed70983c76bbf
ruckman <ruckman@slac.stanford.edu>
2014-04-26 00:01:40 +0000

    fixed the NEEDS_REFRESH warning that gets generated due to the pre-synthesis run script (updating version.vhd) in the synth run when only using the GUI design flow
    git-svn-id: file:///dev/shm/git-devel/svn-repo@4354 0848cbd7-7569-0410-ab05-8cc0a0c67708

--- StdLib/trunk/build/vivado_post_synthesis_v1.tcl
+++ StdLib/trunk/build/vivado_post_synthesis_v1.tcl
@@ -8,10 +8,5 @@ source -quiet ${VIVADO_BUILD_DIR}/vivado_env_var_v1.tcl
 # Load Custom Procedures
 source -quiet ${VIVADO_BUILD_DIR}/vivado_proc_v1.tcl
 
-# GUI Related:
-# Disable a refresh due to the changes 
-# in the Version.vhd file during synthesis 
-set_property NEEDS_REFRESH false [get_runs synth_1]
-
 # Target post synthesis script
 source ${VIVADO_DIR}/post_synthesis.tcl
---
+++ StdLib/trunk/build/vivado_pre_synth_run_v1.tcl
@@ -0,0 +1,24 @@
+
+# Pre-Synthesis Run Script
+
+# Get variables
+set VIVADO_BUILD_DIR $::env(VIVADO_BUILD_DIR)
+source -quiet ${VIVADO_BUILD_DIR}/vivado_env_var_v1.tcl
+
+# Load Custom Procedures
+source -quiet ${VIVADO_BUILD_DIR}/vivado_proc_v1.tcl
+
+# Setup build string
+set DATE [exec date]
+set USER [exec whoami]
+set BSTR "Built $DATE by $USER"
+set SEDS "s|\\(constant BUILD_STAMP_C : string := \\).*|\\1\"${BSTR}\";|"
+
+# Update the timestamp in Version.vhd
+exec sed ${SEDS} ${PROJ_DIR}/Version.vhd > ${PROJ_DIR}/Version.new
+
+# Move the file
+exec mv ${PROJ_DIR}/Version.new ${PROJ_DIR}/Version.vhd
+
+# Message Filtering Script
+source -quiet ${VIVADO_BUILD_DIR}/vivado_messages_v1.tcl
