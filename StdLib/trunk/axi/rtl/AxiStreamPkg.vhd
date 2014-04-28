92587c7d08723617a7c4897d4338381a01995847
bareese <bareese@slac.stanford.edu>
2014-04-28 17:42:52 +0000

    Added AxiStreamFifoStatusType.
    git-svn-id: file:///dev/shm/git-devel/svn-repo@4362 0848cbd7-7569-0410-ab05-8cc0a0c67708

--- StdLib/trunk/axi/rtl/AxiStreamPkg.vhd
+++ StdLib/trunk/axi/rtl/AxiStreamPkg.vhd
@@ -5,7 +5,7 @@
 -- Author     : Benjamin Reese  <bareese@slac.stanford.edu>
 -- Company    : SLAC National Accelerator Laboratory
 -- Created    : 2014-04-24
--- Last update: 2014-04-25
+-- Last update: 2014-04-28
 -- Platform   : 
 -- Standard   : VHDL'93/02
 -------------------------------------------------------------------------------
@@ -25,25 +25,23 @@ package AxiStreamPkg is
    type AxiStreamMasterType is record
       tValid : sl;
       tData  : slv(127 downto 0);
-      tStrb  : slv(15  downto 0);
-      tKeep  : slv(15  downto 0);
+      tStrb  : slv(15 downto 0);
+      tKeep  : slv(15 downto 0);
       tLast  : sl;
-      tDest  : slv(127 downto 0);
-      tId    : slv(127 downto 0);
-      tUser  : slv(127 downto 0);
+      tDest  : slv(7 downto 0);
+      tId    : slv(7 downto 0);
+      tUser  : slv(15 downto 0);
    end record AxiStreamMasterType;
 
    constant AXI_STREAM_MASTER_INIT_C : AxiStreamMasterType := (
       tValid => '0',
-      tData  => (others=>'0'),
-      tStrb  => (others=>'0'),
-      tKeep  => (others=>'0'),
+      tData  => (others => '0'),
+      tStrb  => (others => '1'),
+      tKeep  => (others => '1'),
       tLast  => '0',
-      tDest  => (others=>'0'),
-      tId    => (others=>'0'),
-      tUser  => (others=>'0')
-   );
-
+      tDest  => (others => '0'),
+      tId    => (others => '0'),
+      tUser  => (others => '0'));
    type AxiStreamMasterArray is array (natural range<>) of AxiStreamMasterType;
 
    type AxiStreamSlaveType is record
@@ -51,30 +49,38 @@ package AxiStreamPkg is
    end record AxiStreamSlaveType;
 
    constant AXI_STREAM_SLAVE_INIT_C : AxiStreamSlaveType := (
-      tReady => '0'
-   );
+      tReady => '0');
 
-   type AxiStreamSlaveArray is array (natural range<>) of AxiStreamSlaveType;
+   constant AXI_STREAM_SLAVE_FORCE_C : AxiStreamSlaveType := (
+      tReady => '1');
+
+   type TUserModeType is (TUSER_NORMAL_C, TUSER_LAST_C);
+
 
    type AxiStreamConfigType is record
-      TKEEP_EN_C            : boolean;
-      TSTRB_EN_C            : boolean;
-      TDATA_BYTES_C         : natural range 1 to 16;
-      TDEST_BITS_C          : natural range 0 to 128;
-      TID_BITS_C            : natural range 0 to 128;
-      TUSER_BITS_PER_BYTE_C : natural range 0 to 8;
+      TKEEP_EN_C    : boolean;
+      TSTRB_EN_C    : boolean;
+      TDATA_BYTES_C : natural;
+      TDEST_BITS_C  : natural;
+      TID_BITS_C    : natural;
+      TUSER_BITS_C  : natural;
+      TUSER_MODE_C  : TUserModeType
    end record AxiStreamConfigType;
 
-   type AxiStreamConfigArray is array (natural range<>) of AxiStreamConfigType;
-
    constant AXI_STREAM_CONFIG_INIT_C : AxiStreamConfigType := (
-      TKEEP_EN_C            => false,
-      TSTRB_EN_C            => false,
-      TDATA_BYTES_C         => 16,
-      TDEST_BITS_C          => 4,
-      TID_BITS_C            => 0,
-      TUSER_BITS_PER_BYTE_C => 4
-   );
+      TKEEP_EN_C    => false,
+      TSTRB_EN_C    => false,
+      TDATA_BYTES_C => 16,
+      TDEST_BITS_C  => 4,
+      TID_BITS_C    => 0,
+      TUSER_BITS_C  => 4,
+      TUSER_MODE_C  => TUSER_NORMAL_C);
 
+   type AxiStreamFifoStatusType is record
+      almostFull : sl;
+      overflow   : sl;
+   end record AxiStreamFifoStatusType;
+
+   
 end package AxiStreamPkg;
 
