410a1d9ce0a064ceeca0d3d309276bfea795aa23
ruckman <ruckman@slac.stanford.edu>
2014-04-29 21:07:08 +0000

    moved the source vivado_proc_v1.tcl call into the vivado_env_var_v1.tcl
    git-svn-id: file:///dev/shm/git-devel/svn-repo@4410 0848cbd7-7569-0410-ab05-8cc0a0c67708

--- StdLib/trunk/build/vivado_gui_v1.tcl
+++ StdLib/trunk/build/vivado_gui_v1.tcl
@@ -5,9 +5,6 @@
 set VIVADO_BUILD_DIR $::env(VIVADO_BUILD_DIR)
 source -quiet ${VIVADO_BUILD_DIR}/vivado_env_var_v1.tcl
 
-# Load Custom Procedures
-source -quiet ${VIVADO_BUILD_DIR}/vivado_proc_v1.tcl
-
 # Open the project
 open_project -quiet ${VIVADO_PROJECT}
 #update_compile_order -fileset sources_1
--- StdLib/trunk/build/vivado_pre_synthesis_v1.tcl
+++ StdLib/trunk/build/vivado_pre_synthesis_v1.tcl
@@ -5,8 +5,5 @@
 set VIVADO_BUILD_DIR $::env(VIVADO_BUILD_DIR)
 source -quiet ${VIVADO_BUILD_DIR}/vivado_env_var_v1.tcl
 
-# Load Custom Procedures
-source -quiet ${VIVADO_BUILD_DIR}/vivado_proc_v1.tcl
-
 # Target pre synthesis script
 #source ${VIVADO_DIR}/pre_synthesis.tcl
