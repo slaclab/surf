9869463895e9ca5dd917e4c87c1fa3630c23b3f6
rherbst <rherbst@slac.stanford.edu>
2014-04-29 11:38:32 +0000

    git-svn-id: file:///dev/shm/git-devel/svn-repo@4386 0848cbd7-7569-0410-ab05-8cc0a0c67708

--- StdLib/trunk/axi/simlink/AxiSharedMem.h
+++ StdLib/trunk/axi/simlink/AxiSharedMem.h
@@ -2,6 +2,13 @@
 #define __AXI_SHARED_MEM_H__
 
 #include <sys/types.h>
+#include <sys/types.h>
+#include <string.h>
+#include <stdio.h>
+#include <sys/mman.h>
+#include <sys/stat.h>
+#include <fcntl.h>   
+#include <unistd.h>
 
 // Write address record
 typedef struct {
@@ -80,64 +87,186 @@ typedef struct {
    uint         _readDataAck;
 } AxiSharedMem;
 
+// Init variables
+static inline void init(AxiSharedMem *ptr) {
+   ptr->_clkCnt       = 0;
+   ptr->_writeAddrReq = 0;
+   ptr->_writeAddrAck = 0;
+   ptr->_writeDataReq = 0;
+   ptr->_writeDataAck = 0;
+   ptr->_writeCompReq = 0;
+   ptr->_writeCompAck = 0;
+   ptr->_readAddrReq  = 0;
+   ptr->_readAddrAck  = 0;
+   ptr->_readDataReq  = 0;
+   ptr->_readDataAck  = 0;
+}
+
 // Map and create shared memory object
-AxiSharedMem * sim_open ( char *system, uint id, int uid );
+static inline AxiSharedMem * sim_open ( char *system, uint id, int uid ) {
+   AxiSharedMem * ptr;
+   int            smemFd;
+   char           shmName[200];
+   int            lid;
+
+   // ID to use?
+   if ( uid == -1 ) lid = getuid();
+   else lid = uid;
+
+   // Generate shared memory
+   sprintf(shmName,"axi_shared.%i.%s.%i",lid,system,id);
+
+   // Attempt to open existing shared memory
+   if ( (smemFd = shm_open(shmName, O_RDWR, (S_IRUSR | S_IWUSR | S_IRGRP | S_IWGRP | S_IROTH | S_IWOTH)) ) < 0 ) {
+
+      // Otherwise open and create shared memory
+      if ( (smemFd = shm_open(shmName, (O_CREAT | O_RDWR), (S_IRUSR | S_IWUSR | S_IRGRP | S_IWGRP | S_IROTH | S_IWOTH)) ) < 0 ) return(NULL);
+
+      // Force permissions regardless of umask
+      fchmod(smemFd, (S_IRUSR | S_IWUSR | S_IRGRP | S_IWGRP | S_IROTH | S_IWOTH));
+    
+      // Set the size of the shared memory segment
+      ftruncate(smemFd, sizeof(AxiSharedMem));
+   }
+
+   // Map the shared memory
+   if((ptr = (AxiSharedMem *)mmap(0, sizeof(AxiSharedMem),
+             (PROT_READ | PROT_WRITE), MAP_SHARED, smemFd, 0)) == MAP_FAILED) return(NULL);
+
+   // Store variables
+   strcpy(ptr->_smemPath,shmName);
+   ptr->_smemId = smemFd;
+
+   init(ptr);
+
+   return(ptr);
+}
 
 // close shared memory object
-void sim_close ( AxiSharedMem *smem );
+static inline void sim_close ( AxiSharedMem *smem ) {
+   char shmName[200];
 
-// Init variables
-void init(AxiSharedMem *ptr);
+   // Get shared name
+   strcpy(shmName,smem->_smemPath);
+
+   // Unlink
+   shm_unlink(shmName);
+}
 
 // Increment clock count
-void incrClkCnt(AxiSharedMem *ptr);
+static inline void incrClkCnt(AxiSharedMem *ptr) {
+   ptr->_clkCnt++;
+}
 
 // Read clock count
-uint getClkCnt(AxiSharedMem *ptr);
+static inline uint getClkCnt(AxiSharedMem *ptr) {
+   return(ptr->_clkCnt);
+}
 
 // Set write addr
-void setWriteAddr (AxiSharedMem *ptr, AxiWriteAddr *writeAddr );
+static inline void setWriteAddr ( AxiSharedMem *ptr, AxiWriteAddr *writeAddr ) {
+   memcpy(&(ptr->_writeAddr),writeAddr,sizeof(AxiWriteAddr));
+   ptr->_writeAddrReq++;
+}
 
 // Get write addr
-uint getWriteAddr (AxiSharedMem *ptr, AxiWriteAddr *writeAddr );
+static inline uint getWriteAddr ( AxiSharedMem *ptr, AxiWriteAddr *writeAddr ) {
+   if ( ptr->_writeAddrReq == ptr->_writeAddrAck ) return(0);
+   
+   memcpy(writeAddr,&(ptr->_writeAddr),sizeof(AxiWriteAddr));
+   ptr->_writeAddrAck = ptr->_writeAddrReq;
 
-// Get write addr Ready
-uint readyWriteAddr (AxiSharedMem *ptr);
+   return(1);
+}
+
+// Get write addr Busy
+static inline uint readyWriteAddr (AxiSharedMem *ptr) {
+   return ( ptr->_writeAddrReq == ptr->_writeAddrAck );
+}
 
 // Set write data
-void setWriteData (AxiSharedMem *ptr, AxiWriteData *writeData );
+static inline void setWriteData (AxiSharedMem *ptr,  AxiWriteData *writeData ) {
+   memcpy(&(ptr->_writeData),writeData,sizeof(AxiWriteData));
+   ptr->_writeDataReq++;
+}
 
 // Get write data
-uint getWriteData (AxiSharedMem *ptr, AxiWriteData *writeData );
+static inline uint getWriteData (AxiSharedMem *ptr,  AxiWriteData *writeData ) {
+   if ( ptr->_writeDataReq == ptr->_writeDataAck ) return(0);
+   
+   memcpy(writeData,&(ptr->_writeData),sizeof(AxiWriteData));
+   ptr->_writeDataAck = ptr->_writeDataReq;
 
-// Get write data Ready
-uint readyWriteData (AxiSharedMem *ptr);
+   return(1);
+}
+
+// Get write data Busy
+static inline uint readyWriteData (AxiSharedMem *ptr) {
+   return ( ptr->_writeDataReq == ptr->_writeDataAck );
+}
 
 // Set write comp
-void setWriteComp (AxiSharedMem *ptr, AxiWriteComp *writeComp );
+static inline void setWriteComp (AxiSharedMem *ptr,  AxiWriteComp *writeComp ) {
+   memcpy(&(ptr->_writeComp),writeComp,sizeof(AxiWriteComp));
+   ptr->_writeCompReq++;
+}
 
 // Get write comp
-uint getWriteComp (AxiSharedMem *ptr, AxiWriteComp *writeComp );
+static inline uint getWriteComp (AxiSharedMem *ptr,  AxiWriteComp *writeComp ) {
+   if ( ptr->_writeCompReq == ptr->_writeCompAck ) return(0);
+   
+   memcpy(writeComp,&(ptr->_writeComp),sizeof(AxiWriteComp));
+   ptr->_writeCompAck = ptr->_writeCompReq;
 
-// Get write comp Ready
-uint readyWriteComp (AxiSharedMem *ptr);
+   return(1);
+}
+
+// Get write comp Busy
+static inline uint readyWriteComp (AxiSharedMem *ptr) {
+   return ( ptr->_writeCompReq == ptr->_writeCompAck );
+}
 
 // Set read addr
-void setReadAddr (AxiSharedMem *ptr, AxiReadAddr *readAddr );
+static inline void setReadAddr (AxiSharedMem *ptr,  AxiReadAddr *readAddr ) {
+   memcpy(&(ptr->_readAddr),readAddr,sizeof(AxiReadAddr));
+   ptr->_readAddrReq++;
+}
 
 // Get read addr
-uint getReadAddr (AxiSharedMem *ptr, AxiReadAddr *readAddr );
+static inline uint getReadAddr (AxiSharedMem *ptr,  AxiReadAddr *readAddr ) {
+   if ( ptr->_readAddrReq == ptr->_readAddrAck ) return(0);
+   
+   memcpy(readAddr,&(ptr->_readAddr),sizeof(AxiReadAddr));
+   ptr->_readAddrAck = ptr->_readAddrReq;
 
-// Get read addr Ready
-uint readyReadAddr (AxiSharedMem *ptr);
+   return(1);
+}
+
+// Get read addr Busy
+static inline uint readyReadAddr (AxiSharedMem *ptr) {
+   return ( ptr->_readAddrReq == ptr->_readAddrAck );
+}
 
 // Set read data
-void setReadData (AxiSharedMem *ptr, AxiReadData *readData );
+static inline void setReadData (AxiSharedMem *ptr,  AxiReadData *readData ) {
+   memcpy(&(ptr->_readData),readData,sizeof(AxiReadData));
+   ptr->_readDataReq++;
+}
 
 // Get read data
-uint getReadData (AxiSharedMem *ptr, AxiReadData *readData );
+static inline uint getReadData (AxiSharedMem *ptr,  AxiReadData *readData ) {
+   if ( ptr->_readDataReq == ptr->_readDataAck ) return(0);
+   
+   memcpy(readData,&(ptr->_readData),sizeof(AxiReadData));
+   ptr->_readDataAck = ptr->_readDataReq;
 
-// Get read data Ready
-uint readyReadData (AxiSharedMem *ptr);
+   return(1);
+}
+
+// Get read data Busy
+static inline uint readyReadData (AxiSharedMem *ptr) {
+   return ( ptr->_readDataReq == ptr->_readDataAck );
+}
 
 #endif
+
