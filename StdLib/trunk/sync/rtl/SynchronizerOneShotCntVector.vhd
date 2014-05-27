10b049bb53a27c7527109620aff519c5ff087ccc
bareese <bareese@slac.stanford.edu>
2014-05-27 19:12:22 +0000

    Minor cleanup.
    git-svn-id: file:///dev/shm/git-devel/svn-repo@4765 0848cbd7-7569-0410-ab05-8cc0a0c67708

--- StdLib/trunk/sync/rtl/SynchronizerOneShotCntVector.vhd
+++ StdLib/trunk/sync/rtl/SynchronizerOneShotCntVector.vhd
@@ -21,44 +21,38 @@ use work.StdRtlPkg.all;
 
 entity SynchronizerOneShotCntVector is
    generic (
-      TPD_G             : time     := 1 ns; -- Simulation FF output delay
-      RST_POLARITY_G    : sl       := '1';  -- '1' for active HIGH reset, '0' for active LOW reset
-      RST_ASYNC_G       : boolean  := false;-- true if reset is asynchronous, false if reset is synchronous
-      COMMON_CLK_G      : boolean  := false;-- True if wrClk and rdClk are the same clock
-      RELEASE_DELAY_G   : positive := 3;    -- Delay between deassertion of async and sync resets
-      IN_POLARITY_G     : slv      := "1";  -- 0 for active LOW, 1 for active HIGH (dataIn port)
-      OUT_POLARITY_G    : slv      := "1";  -- 0 for active LOW, 1 for active HIGH (dataOut port)
-      USE_DSP48_G       : string   := "no"; -- "no" for no DSP48 implementation, "yes" to use DSP48 slices
-      SYNTH_CNT_G       : slv      := "1";  -- Set to 1 for synthesising counter RTL, '0' to not synthesis the counter
-      CNT_RST_EDGE_G    : boolean  := true; -- true if counter reset should be edge detected, else level detected
-      CNT_WIDTH_G       : positive := 16;   
-      WIDTH_G           : positive := 16);
+      TPD_G           : time     := 1 ns;  -- Simulation FF output delay
+      RST_POLARITY_G  : sl       := '1';  -- '1' for active HIGH reset, '0' for active LOW reset
+      RST_ASYNC_G     : boolean  := false;  -- true if reset is asynchronous, false if reset is synchronous
+      COMMON_CLK_G    : boolean  := false;  -- True if wrClk and rdClk are the same clock
+      RELEASE_DELAY_G : positive := 3;  -- Delay between deassertion of async and sync resets
+      IN_POLARITY_G   : slv      := "1";  -- 0 for active LOW, 1 for active HIGH (dataIn port)
+      OUT_POLARITY_G  : slv      := "1";  -- 0 for active LOW, 1 for active HIGH (dataOut port)
+      USE_DSP48_G     : string   := "no";  -- "no" for no DSP48 implementation, "yes" to use DSP48 slices
+      SYNTH_CNT_G     : slv      := "1";  -- Set to 1 for synthesising counter RTL, '0' to not synthesis the counter
+      CNT_RST_EDGE_G  : boolean  := true;  -- true if counter reset should be edge detected, else level detected
+      CNT_WIDTH_G     : positive := 16;
+      WIDTH_G         : positive := 16);
    port (
       -- Write Ports (wrClk domain)    
-      dataIn     : in  slv(WIDTH_G-1 downto 0); -- Data to be 'synced'
+      dataIn     : in  slv(WIDTH_G-1 downto 0);   -- Data to be 'synced'
       -- Read Ports (rdClk domain)    
-      rollOverEn : in  slv(WIDTH_G-1 downto 0); -- '1' allows roll over of the counter
-      cntRst     : in  sl := not RST_POLARITY_G;-- Optional counter reset
-      dataOut    : out slv(WIDTH_G-1 downto 0); -- Synced data
+      rollOverEn : in  slv(WIDTH_G-1 downto 0);   -- '1' allows roll over of the counter
+      cntRst     : in  sl := not RST_POLARITY_G;  -- Optional counter reset
+      dataOut    : out slv(WIDTH_G-1 downto 0);   -- Synced data
       cntOut     : out SlVectorArray(WIDTH_G-1 downto 0, CNT_WIDTH_G-1 downto 0);  -- Synced counter
       -- Clocks and Reset Ports
       wrClk      : in  sl;
       wrRst      : in  sl := not RST_POLARITY_G;
-      rdClk      : in  sl;-- clock to be SYNC'd to
+      rdClk      : in  sl;              -- clock to be SYNC'd to
       rdRst      : in  sl := not RST_POLARITY_G);      
 end SynchronizerOneShotCntVector;
 
 architecture mapping of SynchronizerOneShotCntVector is
 
---   type MyVectorArray is array (WIDTH_G-1 downto 0) of sl;
-
-   function fillVectorArray (INPUT : slv)
-      return slv is
-      variable retVar : slv(WIDTH_G-1 downto 0);
+   function fillVectorArray (INPUT : slv) return slv is
    begin
-      return ite(INPUT = "1", slvOne(WIDTH_G),
-             ite(INPUT = "0", slvZero(WIDTH_G),
-                              INPUT));
+      return ite(INPUT'size = 1, slvAll(WIDTH_G, INPUT(0)), INPUT);
    end function fillVectorArray;
 
    constant IN_POLARITY_C  : slv(WIDTH_G-1 downto 0) := fillVectorArray(IN_POLARITY_G);
@@ -75,17 +69,17 @@ begin
       
       SyncOneShotCnt_Inst : entity work.SynchronizerOneShotCnt
          generic map (
-            TPD_G             => TPD_G,
-            RST_POLARITY_G    => RST_POLARITY_G,
-            RST_ASYNC_G       => RST_ASYNC_G,
-            COMMON_CLK_G      => COMMON_CLK_G,
-            RELEASE_DELAY_G   => RELEASE_DELAY_G,
-            IN_POLARITY_G     => IN_POLARITY_C(i),
-            OUT_POLARITY_G    => OUT_POLARITY_C(i),
-            USE_DSP48_G       => USE_DSP48_G,
-            SYNTH_CNT_G       => SYNTH_CNT_C(i),
-            CNT_RST_EDGE_G    => CNT_RST_EDGE_G,
-            CNT_WIDTH_G       => CNT_WIDTH_G)           
+            TPD_G           => TPD_G,
+            RST_POLARITY_G  => RST_POLARITY_G,
+            RST_ASYNC_G     => RST_ASYNC_G,
+            COMMON_CLK_G    => COMMON_CLK_G,
+            RELEASE_DELAY_G => RELEASE_DELAY_G,
+            IN_POLARITY_G   => IN_POLARITY_C(i),
+            OUT_POLARITY_G  => OUT_POLARITY_C(i),
+            USE_DSP48_G     => USE_DSP48_G,
+            SYNTH_CNT_G     => SYNTH_CNT_C(i),
+            CNT_RST_EDGE_G  => CNT_RST_EDGE_G,
+            CNT_WIDTH_G     => CNT_WIDTH_G)           
          port map (
             -- Write Ports (wrClk domain)    
             dataIn     => dataIn(i),
