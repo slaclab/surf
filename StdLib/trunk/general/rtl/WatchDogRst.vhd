07b61cab285ee16b6c964fbfe42318a6de208319
ruckman <ruckman@slac.stanford.edu>
2014-06-17 22:24:12 +0000

    adding WatchDogRst.vhd module
    git-svn-id: file:///dev/shm/git-devel/svn-repo@4954 0848cbd7-7569-0410-ab05-8cc0a0c67708

---
+++ StdLib/trunk/general/rtl/WatchDogRst.vhd
@@ -0,0 +1,87 @@
+-------------------------------------------------------------------------------
+-- Title      : 
+-------------------------------------------------------------------------------
+-- File       : WatchDogRst.vhd
+-- Author     : Larry Ruckman  <ruckman@slac.stanford.edu>
+-- Company    : SLAC National Accelerator Laboratory
+-- Created    : 2014-06-17
+-- Last update: 2014-06-17
+-- Platform   : 
+-- Standard   : VHDL'93/02
+-------------------------------------------------------------------------------
+-- Description: 
+-------------------------------------------------------------------------------
+-- Copyright (c) 2014 SLAC National Accelerator Laboratory
+-------------------------------------------------------------------------------
+
+library ieee;
+use ieee.std_logic_1164.all;
+use ieee.std_logic_unsigned.all;
+use ieee.std_logic_arith.all;
+
+use work.StdRtlPkg.all;
+
+entity WatchDogRst is
+   generic (
+      TPD_G          : time                           := 1 ns;
+      IN_POLARITY_G  : sl                             := '1';
+      OUT_POLARITY_G : sl                             := '1';
+      USE_DSP48_G    : string                         := "no";
+      DURATION_G     : natural range 0 to ((2**30)-1) := 156250000);
+   port (
+      clk    : in  sl;
+      monIn  : in  sl;
+      rstOut : out sl);
+end WatchDogRst;
+
+architecture rtl of WatchDogRst is
+
+   signal rst      : sl := not(OUT_POLARITY_G);
+   signal monInput : sl;
+
+   signal cnt : natural range 0 to DURATION_G := 0;
+
+   attribute use_dsp48        : string;
+   attribute use_dsp48 of cnt : signal is USE_DSP48_G;
+   
+begin
+
+   -- USE_DSP48_G check
+   assert ((USE_DSP48_G = "yes") or (USE_DSP48_G = "no") or (USE_DSP48_G = "auto") or (USE_DSP48_G = "automax"))
+      report "USE_DSP48_G must be either yes, no, auto, or automax"
+      severity failure;
+
+   Synchronizer_Inst : entity work.Synchronizer
+      generic map (
+         TPD_G => TPD_G)      
+      port map (
+         clk     => clk,
+         dataIn  => monIn,
+         dataOut => monInput);          
+
+   process (clk)
+   begin
+      if rising_edge(clk) then
+         -- Reset the flag
+         rst <= not(OUT_POLARITY_G) after TPD_G;
+         -- Check the monitoring input
+         if monInput = IN_POLARITY_G then
+            -- Reset the counter
+            cnt <= 0 after TPD_G;
+         else
+            -- Increment the counter
+            cnt <= cnt + 1 after TPD_G;
+            -- Check the counter value
+            if cnt = DURATION_G then
+               -- Reset the counter
+               cnt <= 0              after TPD_G;
+               -- Set the flag
+               rst <= OUT_POLARITY_G after TPD_G;
+            end if;
+         end if;
+      end if;
+   end process;
+
+   rstOut <= rst;
+   
+end rtl;
