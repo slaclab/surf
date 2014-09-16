0e048b78a79eaf6b75c7aaa2453420b4409a07d4
bareese <bareese@slac.stanford.edu>
2014-09-16 17:10:28 +0000

    Created new submodule for SPI bus code.
    git-svn-id: file:///dev/shm/git-devel/svn-repo@5608 0848cbd7-7569-0410-ab05-8cc0a0c67708

---
+++ StdLib/trunk/spi/rtl/SpiMaster.vhd
@@ -0,0 +1,199 @@
+-------------------------------------------------------------------------------
+-- Title      : 
+-------------------------------------------------------------------------------
+-- File       : FlashMain.vhd
+-- Author     : Larry Ruckman  <ruckman@slac.stanford.edu>
+-- Company    : SLAC National Accelerator Laboratory
+-- Created    : 2013-05-24
+-- Last update: 2014-03-18
+-- Platform   : ISE 14.6
+-- Standard   : VHDL'93/02
+-------------------------------------------------------------------------------
+-- Description: 
+-------------------------------------------------------------------------------
+-- Copyright (c) 2013 SLAC National Accelerator Laboratory
+-------------------------------------------------------------------------------
+
+library ieee;
+use ieee.std_logic_1164.all;
+use ieee.std_logic_unsigned.all;
+use ieee.std_logic_arith.all;
+use ieee.math_real.all;
+use work.StdRtlPkg.all;
+
+entity SpiMaster is
+   generic (
+      TPD_G             : time                  := 1 ns;
+      NUM_CHIPS_G       : positive range 1 to 8 := 4;
+      DATA_SIZE_G       : natural               := 16;
+      CPHA_G            : sl                    := '0';
+      CPOL_G            : sl                    := '0';
+      CLK_PERIOD_G      : real                  := 8.0E-9;
+      SPI_SCLK_PERIOD_G : real                  := 1.0E-6);  -- 1 MHz
+   port (
+      --Global Signals
+      clk     : in  sl;
+      sRst    : in  sl;
+      -- Parallel interface
+      chipSel : in  slv(bitSize(NUM_CHIPS_G)-1 downto 0);
+      wrEn    : in  sl;
+      wrData  : in  slv(DATA_SIZE_G-1 downto 0);
+      rdEn    : out sl;
+      rdData  : out slv(DATA_SIZE_G-1 downto 0);
+      --SPI interface
+      spiCsL  : out slv(NUM_CHIPS_G-1 downto 0);
+      spiSclk : out sl;
+      spiSdi  : out sl;
+      spiSdo  : in  sl);                
+end SpiMaster;
+
+architecture rtl of SpiMaster is
+
+   constant SPI_CLK_PERIOD_DIV2_CYCLES_C : integer := 2 * integer(SPI_SCLK_PERIOD_G / CLK_PERIOD_G);
+   constant SCLK_COUNTER_SIZE_C          : integer := bitSize(SPI_CLK_PERIOD_DIV2_CYCLES_C);
+
+
+   -- Types
+   type StateType is (
+      IDLE_S,
+      SHIFT_S,
+      SAMPLE_S,
+      DONE_S);
+
+   type RegType is record
+      state       : StateType;
+      rdEn        : sl;
+      rdData      : slv(DATA_SIZE_G-1 downto 0);
+      wrData      : slv(DATA_SIZE_G-1 downto 0);
+      dataCounter : slv(bitSize(DATA_SIZE_G)-1 downto 0);
+      sclkCounter : slv(SCLK_COUNTER_SIZE_C-1 downto 0);
+
+      spiCsL  : slv(NUM_CHIPS_G-1 downto 0);
+      spiSclk : sl;
+      spiSdi  : sl;
+   end record RegType;
+
+   constant REG_INIT_C : RegType := (
+      state       => IDLE_S,
+      rdEn        => '0',
+      rdData      => (others => '0'),
+      wrData      => (others => '0'),
+      dataCounter => (others => '0'),
+      sclkCounter => (others => '0'),
+      spiCsL      => (others => '1'),
+      spiSclk     => '0',
+      spiSdi      => '0');
+
+   signal r   : RegType := REG_INIT_C;
+   signal rin : RegType;
+
+   signal spiSdoRes : sl;
+
+begin
+
+   spiSdoRes <= to_x01z(spiSdo);
+
+   comb : process (chipSel, r, sRst, spiSdoRes, wrData, wrEn) is
+      variable v : RegType;
+   begin
+      v := r;
+
+      case (r.state) is
+         when IDLE_S =>
+
+            v.spiSclk     := CPOL_G;
+            v.spiSdi      := '0';
+            v.dataCounter := (others => '0');
+            v.sclkCounter := (others => '0');
+            v.rdEn   := '1';         -- rdEn always valid between txns, indicates ready for next txn
+   
+            if (wrEn = '1') then
+               v.rdEn        := '0';
+               v.wrData      := wrData;
+               v.rdData      := (others => '0');
+               v.spiCsL      := not (decode(chipSel)(NUM_CHIPS_G-1 downto 0));
+
+               if (CPHA_G = '0') then
+                  -- Sample on first sclk edge so shift here before that happens
+                  v.spiSdi := wrData(DATA_SIZE_G-1);
+                  v.wrData := wrData(DATA_SIZE_G-2 downto 0) & '0';
+                  v.state  := SAMPLE_S;
+               else
+                  v.state := SHIFT_S;
+               end if;
+            end if;
+
+         when SHIFT_S =>
+            -- Wait half a clock period then shift out the next data bit
+            v.sclkCounter := r.sclkCounter + 1;
+            if (r.sclkCounter = SPI_CLK_PERIOD_DIV2_CYCLES_C) then
+               v.sclkCounter := (others => '0');
+               v.spiSclk     := not r.spiSclk;
+               v.spiSdi      := r.wrData(DATA_SIZE_G-1);
+               v.wrData      := r.wrData(DATA_SIZE_G-2 downto 0) & '0';
+               v.state       := SAMPLE_S;
+
+               if (CPHA_G = '0') then
+                  v.dataCounter := r.dataCounter + 1;
+                  if (r.dataCounter = DATA_SIZE_G-1) then
+                     v.state := DONE_S;
+                  end if;
+               end if;
+            end if;
+
+         when SAMPLE_S =>
+            -- Wait half a clock period then sample the next data bit
+            v.sclkCounter := r.sclkCounter + 1;
+            if (r.sclkCounter = SPI_CLK_PERIOD_DIV2_CYCLES_C) then
+               v.sclkCounter := (others => '0');
+               v.spiSclk     := not r.spiSclk;
+               v.rdData      := r.rdData(DATA_SIZE_G-2 downto 0) & spiSdoRes;
+               v.state       := SHIFT_S;
+
+               if (CPHA_G = '1') then
+                  v.dataCounter := r.dataCounter + 1;
+                  if (r.dataCounter = DATA_SIZE_G-1) then
+                     v.state := DONE_S;
+                  end if;
+               end if;
+            end if;
+            
+         when DONE_S =>
+            -- Assert rdEn after half a SPI clk period
+            -- Go back to idle after one SPI clk period
+            -- Otherwise back to back operations happen too fast.
+            v.sclkCounter := r.sclkCounter + 1;
+            if (r.sclkCounter = SPI_CLK_PERIOD_DIV2_CYCLES_C) then
+               v.sclkCounter := (others => '0');
+               v.spiCsL := (others => '1');
+
+               if (r.spiCsL = slvOne(NUM_CHIPS_G)) then
+                  v.state := IDLE_S;
+               end if;
+            end if;
+         when others => null;
+      end case;
+
+      if (sRst = '1') then
+         v := REG_INIT_C;
+      end if;
+
+      rin <= v;
+
+      spiSclk <= r.spiSclk;
+      spiSdi  <= r.spiSdi;
+      spiCsL  <= r.spiCsL;
+
+      rdEn   <= r.rdEn;
+      rdData <= r.rdData;
+      
+   end process comb;
+
+   seq : process (clk) is
+   begin
+      if (rising_edge(clk)) then
+         r <= rin after TPD_G;
+      end if;
+   end process seq;
+
+end rtl;
---
+++ StdLib/trunk/spi/rtl/SpiSlave.vhd
@@ -0,0 +1,212 @@
+-------------------------------------------------------------------------------
+-- Title      : SPI Slave
+-------------------------------------------------------------------------------
+-- File       : SpiSlave.vhd
+-- Author     : Benjamin Reese  <bareese@slac.stanford.edu>
+-- Company    : SLAC National Accelerator Laboratory
+-- Created    : 2014-01-27
+-- Last update: 2014-03-14
+-- Platform   : 
+-- Standard   : VHDL'93
+-------------------------------------------------------------------------------
+-- Description: 
+-------------------------------------------------------------------------------
+-- Copyright (c) 2014 SLAC National Accelerator Laboratory
+-------------------------------------------------------------------------------
+
+library ieee;
+use ieee.std_logic_1164.all;
+use ieee.std_logic_arith.all;
+use ieee.std_logic_unsigned.all;
+use work.StdRtlPkg.all;
+
+entity SpiSlave is
+   generic (
+      TPD_G       : time     := 1 ns;
+      CPOL_G      : sl       := '0';
+      CPHA_G      : sl       := '1';
+      WORD_SIZE_G : positive := 16);
+   port (
+      clk : in sl;
+      rst : in sl;
+
+      sclk : in  sl;
+      mosi : in  sl;
+      miso : out sl;
+      selL : in  sl;
+
+      rdData : in slv(WORD_SIZE_G-1 downto 0);
+      rdStb  : in sl;
+
+      wrData : out slv(WORD_SIZE_G-1 downto 0);
+      wrStb  : out sl);
+
+end entity SpiSlave;
+
+architecture rtl of SpiSlave is
+
+   constant MAX_COUNT_C : integer := WORD_SIZE_G-1;
+
+   type RegType is
+   record
+      -- Input sync and edge detection regs
+      mosiLast : sl;
+      sclkLast : sl;
+      selLLast : sl;
+
+      --Internal Shift regs and counters
+      shiftReg : slv(WORD_SIZE_G downto 0);
+      shiftCnt : slv(log2(WORD_SIZE_G)-1 downto 0);
+      gotSclk  : sl;
+
+      -- Outputs
+      wrStb : sl;
+   end record;
+
+   constant REG_INIT_C : RegType := (
+      mosiLast => '0',
+      sclkLast => '0',
+      selLLast => '1',
+      shiftReg => (others => '0'),
+      shiftCnt => toSlv(MAX_COUNT_C, log2(WORD_SIZE_G)),
+      gotSclk  => '0',
+      wrStb    => '0');
+
+   signal r   : RegType := REG_INIT_C;
+   signal rin : RegType;
+
+   signal mosiSync : sl;
+   signal sclkSync : sl;
+   signal selLSync : sl;
+
+begin
+
+   SEL_SYNCHRONIZER : entity work.Synchronizer
+      generic map (
+         TPD_G  => TPD_G,
+         STAGES_G => 3,
+         INIT_G => "111")
+      port map (
+         clk     => clk,
+         rst     => rst,
+         dataIn  => selL,
+         dataOut => selLSync);
+
+   SCLK_SYNCHRONIZER : entity work.Synchronizer
+      generic map (
+         TPD_G => TPD_G)
+      port map (
+         clk     => clk,
+         rst     => rst,
+         dataIn  => sclk,
+         dataOut => sclkSync);
+
+   MOSI_SYNCHRONIZER : entity work.Synchronizer
+      generic map (
+         TPD_G => TPD_G)
+      port map (
+         clk     => clk,
+         rst     => rst,
+         dataIn  => mosi,
+         dataOut => mosiSync);
+
+   seq : process (clk) is
+   begin
+      if (rising_edge(clk)) then
+         r <= rin after TPD_G;
+      end if;
+      
+   end process seq;
+
+
+   comb : process (mosiSync, r, rdData, rdStb, rst, sclkSync, selLSync) is
+      variable v : RegType;
+
+      impure function isLeadingEdge
+         return boolean is
+      begin
+         return selLSync = '0' and r.sclkLast = CPOL_G and sclkSync = not CPOL_G;
+      end function;
+
+      impure function isTrailingEdge
+         return boolean is
+      begin
+         return selLSync = '0' and r.sclkLast = not CPOL_G and sclkSync = CPOL_G;
+      end function;
+
+      -- Shift a new bit out
+      procedure shift is
+      begin
+         v.shiftReg := r.shiftReg(WORD_SIZE_G-1 downto 0) & '0';
+      end procedure;
+
+      -- Clock in the current mosi bit and increment counter
+      procedure sample is
+      begin
+         v.shiftReg(0) := mosiSync;
+
+         v.shiftCnt := r.shiftCnt + 1;
+         if (r.shiftCnt = MAX_COUNT_C) then
+            v.shiftCnt := (others => '0');
+         end if;
+      end procedure;
+      
+   begin
+      v := r;
+
+      v.sclkLast := sclkSync;
+      v.mosiLast := mosiSync;
+      v.selLLast := selLSync;
+
+      -- CPHA_G = 0 is a special case
+      -- Do a shift on falling edge of selL
+      if (CPHA_G = '0' and r.selLLast = '1' and selLSync = '0') then
+         shift;
+      end if;
+
+      if (isLeadingEdge) then
+         v.gotSclk := '1';
+         if (CPHA_G = '1') then
+            shift;
+         elsif (CPHA_G = '0') then
+            sample;
+         end if;
+      end if;
+
+      if (isTrailingEdge) then
+         if (CPHA_G = '1') then
+            sample;
+         elsif (CPHA_G = '0' and r.shiftCnt /= MAX_COUNT_C) then
+            shift;
+         end if;
+      end if;
+
+      -- Assert wrStb when max count reached
+      if (r.shiftCnt = MAX_COUNT_C and isTrailingEdge and r.gotSclk = '1') then
+         v.wrStb := '1';
+      end if;
+
+      -- Read strobe only allowed when Write strobe is high
+      if (r.wrStb = '1' and (rdStb = '1' or isLeadingEdge)) then
+         v.wrStb                            := '0';
+         v.shiftReg(WORD_SIZE_G-1 downto 0) := rdData;
+      end if;
+
+      if (rst = '1') then
+         v := REG_INIT_C;
+      end if;
+
+      rin <= v;
+
+      -- Outputs
+      miso <= 'Z';
+      if (selLSync = '0') then
+         miso <= r.shiftReg(WORD_SIZE_G);
+      end if;
+
+      wrData <= r.shiftReg(WORD_SIZE_G-1 downto 0);
+      wrStb  <= r.wrStb;
+
+   end process;
+
+end architecture rtl;
