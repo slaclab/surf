d9172ecdcdab5fee87d56b30108bc11041058279
bareese <bareese@slac.stanford.edu>
2014-09-23 17:57:39 +0000

    Promgen would not always output the file correctly to AFS space. This fixes it.
    git-svn-id: file:///dev/shm/git-devel/svn-repo@5668 0848cbd7-7569-0410-ab05-8cc0a0c67708

--- StdLib/trunk/build/vivado_promgen_v1.tcl
+++ StdLib/trunk/build/vivado_promgen_v1.tcl
@@ -6,7 +6,18 @@ source -quiet ${VIVADO_BUILD_DIR}/vivado_env_var_v1.tcl
 source -quiet ${VIVADO_BUILD_DIR}/vivado_proc_v1.tcl
 
 # Target PROMGEN script
+set inputFile "$::env(IMPL_DIR)/$::env(PROJECT).bit"
+set outputFile "$::env(IMPL_DIR)/$::env(PROJECT).mcs"
+set imagesFile "$::env(IMAGES_DIR)/$::env(PROJECT)_$::env(PRJ_VERSION).mcs"
+set loadbit   "up 0x0 ${inputFile}"
+
 source ${VIVADO_DIR}/promgen.tcl
+
+puts ${inputFile}
+puts ${outputFile}
+puts ${loadbit}
+
+#puts write_cfgmem -force -format ${format} -interface ${inteface} -size ${size} -loadbit ${loadbit} -file ${outputFile}
 
 write_cfgmem -force \
    -format ${format} \
@@ -14,3 +25,5 @@ write_cfgmem -force \
    -size ${size} \
    -loadbit ${loadbit} \
    -file ${outputFile}
+
+exec cp ${outputFile} ${imagesFile}
