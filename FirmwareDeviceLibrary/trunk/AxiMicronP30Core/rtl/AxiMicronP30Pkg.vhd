73eea13644425ebde95302dc33996734f00826de
ruckman <ruckman@slac.stanford.edu>
2015-01-13 18:07:32 +0000

    emacs VHDL clean up
    git-svn-id: file:///dev/shm/git-devel/svn-repo@6341 0848cbd7-7569-0410-ab05-8cc0a0c67708

--- FirmwareDeviceLibrary/trunk/AxiMicronP30Core/rtl/AxiMicronP30Pkg.vhd
+++ FirmwareDeviceLibrary/trunk/AxiMicronP30Core/rtl/AxiMicronP30Pkg.vhd
@@ -6,25 +6,25 @@ use work.StdRtlPkg.all;
 package AxiMicronP30Pkg is
 
    type AxiMicronP30InType is record
-      flashWait : sl; 
+      flashWait : sl;
    end record;
    type AxiMicronP30InArray is array (natural range <>) of AxiMicronP30InType;
    type AxiMicronP30InVectorArray is array (integer range<>, integer range<>)of AxiMicronP30InType;
-   
+
    type AxiMicronP30InOutType is record
-      dq : slv(15 downto 0); 
+      dq : slv(15 downto 0);
    end record;
    type AxiMicronP30InOutArray is array (natural range <>) of AxiMicronP30InOutType;
    type AxiMicronP30InOutVectorArray is array (integer range<>, integer range<>)of AxiMicronP30InOutType;
-   
+
    type AxiMicronP30OutType is record
-      ceL    : sl;
-      oeL    : sl;
-      weL    : sl;      
-      addr  : slv(30 downto 0);
-      adv   : sl;
-      clk   : sl;
-      rstL   : sl;
+      ceL  : sl;
+      oeL  : sl;
+      weL  : sl;
+      addr : slv(30 downto 0);
+      adv  : sl;
+      clk  : sl;
+      rstL : sl;
    end record;
    type AxiMicronP30OutArray is array (natural range <>) of AxiMicronP30OutType;
    type AxiMicronP30OutVectorArray is array (integer range<>, integer range<>)of AxiMicronP30OutType;
