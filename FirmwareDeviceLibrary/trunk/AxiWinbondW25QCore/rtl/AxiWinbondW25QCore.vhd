34b86c61bad04702cb53c2cc9d2a607fc4fdf33f
ruckman <ruckman@slac.stanford.edu>
2015-03-04 23:41:51 +0000

    Updating sources
    git-svn-id: file:///dev/shm/git-devel/svn-repo@6717 0848cbd7-7569-0410-ab05-8cc0a0c67708

--- FirmwareDeviceLibrary/trunk/AxiWinbondW25QCore/rtl/AxiWinbondW25QCore.vhd
+++ FirmwareDeviceLibrary/trunk/AxiWinbondW25QCore/rtl/AxiWinbondW25QCore.vhd
@@ -1,54 +1,71 @@
 -------------------------------------------------------------------------------
 -- Title      : 
 -------------------------------------------------------------------------------
--- File       : AxiW25Q128Core.vhd
+-- File       : AxiWinbondW25QCore.vhd
 -- Author     : Larry Ruckman  <ruckman@slac.stanford.edu>
 -- Company    : SLAC National Accelerator Laboratory
 -- Created    : 2014-04-25
--- Last update: 2015-01-13
+-- Last update: 2015-03-03
 -- Platform   : 
 -- Standard   : VHDL'93/02
 -------------------------------------------------------------------------------
--- Description: AXI-Lite interface to W25Q128 FLASH Memory IC
+-- Description: AXI-Lite interface to W25Q FLASH Memory IC
 --
 --    Note: This module doesn't support DSPI or QSPI interface yet.
 --
 --    Note: Set the addrBits on the crossbar for this module to 10 bits wide
 -------------------------------------------------------------------------------
--- Copyright (c) 2014 SLAC National Accelerator Laboratory
+-- Copyright (c) 2015 SLAC National Accelerator Laboratory
 -------------------------------------------------------------------------------
 
 library ieee;
 use ieee.std_logic_1164.all;
-use ieee.numeric_std.all;
 
 use work.StdRtlPkg.all;
 use work.AxiLitePkg.all;
-use work.AxiW25Q128Pkg.all;
+use work.AxiStreamPkg.all;
+use work.SsiPkg.all;
+use work.AxiWinbondW25QPkg.all;
 
 library unisim;
 use unisim.vcomponents.all;
 
-entity AxiW25Q128Core is
+entity AxiWinbondW25QCore is
    generic (
-      TPD_G                 : time            := 1 ns;
-      FORCE_ADDR_MSB_HIGH_G : boolean         := false;  -- Set true to prevent any operation in the lower half of the address space
-      AXI_CLK_FREQ_G        : real            := 200.0E+6;  -- units of Hz
-      SPI_CLK_FREQ_G        : real            := 50.0E+6;   -- units of Hz
-      AXI_ERROR_RESP_G      : slv(1 downto 0) := AXI_RESP_SLVERR_C);     
+      TPD_G            : time                := 1 ns;
+      MEM_ADDR_MASK_G  : slv(23 downto 0)    := x"000000";
+      AXI_CLK_FREQ_G   : real                := 200.0E+6;  -- units of Hz
+      SPI_CLK_FREQ_G   : real                := 50.0E+6;   -- units of Hz
+      AXI_CONFIG_G     : AxiStreamConfigType := ssiAxiStreamConfig(4);
+      AXI_ERROR_RESP_G : slv(1 downto 0)     := AXI_RESP_SLVERR_C);     
    port (
       -- FLASH Memory Ports
-      spiOut         : out   AxiW25Q128OutType;
-      spiInOut       : inout AxiW25Q128InOutType;
+      spiOut         : out   AxiWinbondW25QOutType;
+      spiInOut       : inout AxiWinbondW25QInOutType;
       spiSck         : out   sl;  -- Copy of serial clock for use with STARTUPE2 when interfacing to FPGA's CCLK
       -- AXI-Lite Register Interface
       axiReadMaster  : in    AxiLiteReadMasterType;
       axiReadSlave   : out   AxiLiteReadSlaveType;
       axiWriteMaster : in    AxiLiteWriteMasterType;
       axiWriteSlave  : out   AxiLiteWriteSlaveType;
+      -- AXI Streaming Interface (Optional)
+      mAxisMaster    : out   AxiStreamMasterType;
+      mAxisSlave     : in    AxiStreamSlaveType  := AXI_STREAM_SLAVE_FORCE_C;
+      sAxisMaster    : in    AxiStreamMasterType := AXI_STREAM_MASTER_INIT_C;
+      sAxisSlave     : out   AxiStreamSlaveType;
       -- Clocks and Resets
       axiClk         : in    sl;
       axiRst         : in    sl);
+end AxiWinbondW25QCore;
+
+architecture mapping of AxiWinbondW25QCore is
+   
+   signal sck  : sl;
+   signal csL  : sl;
+   signal dout : slv(3 downto 0);
+   signal din  : slv(3 downto 0);
+   signal oeL  : slv(3 downto 0);
+   
 begin
    -- Check SPI_CLK_FREQ_G
    -- Note: Max. read frequency is 50 MHz
@@ -56,20 +73,9 @@ begin
       report "SPI_CLK_FREQ_G must be <= 50.0E+6"
       severity failure;
    -- Check AXI_CLK_FREQ_G >= 2*SPI_CLK_FREQ_G
-   assert (AXI_CLK_FREQ_G >= getRealMult(2, SPI_CLK_FREQ_G))
+   assert (AXI_CLK_FREQ_G >= getRealMult(2.0, SPI_CLK_FREQ_G))
       report "AXI_CLK_FREQ_G must be >= 2*SPI_CLK_FREQ_G"
       severity failure;
-end AxiW25Q128Core;
-
-architecture mapping of AxiW25Q128Core is
-   
-   signal sck,
-      csL : sl;
-   signal dout,
-      din,
-      oeL : slv(3 downto 0);
-   
-begin
 
    spiOut.csL <= csL;
    spiOut.sck <= sck;
@@ -85,13 +91,14 @@ begin
             T  => oeL(i));              -- 3-state enable input, high=input, low=output        
    end generate GEN_SDIO;
 
-   AxiW25Q128Reg_Inst : entity work.AxiW25Q128Reg
+   AxiWinbondW25QReg_Inst : entity work.AxiWinbondW25QReg
       generic map(
-         TPD_G                 => TPD_G,
-         FORCE_ADDR_MSB_HIGH_G => FORCE_ADDR_MSB_HIGH_G,
-         AXI_CLK_FREQ_G        => AXI_CLK_FREQ_G,
-         SPI_CLK_FREQ_G        => SPI_CLK_FREQ_G,
-         AXI_ERROR_RESP_G      => AXI_ERROR_RESP_G)
+         TPD_G            => TPD_G,
+         MEM_ADDR_MASK_G  => MEM_ADDR_MASK_G,
+         AXI_CLK_FREQ_G   => AXI_CLK_FREQ_G,
+         SPI_CLK_FREQ_G   => SPI_CLK_FREQ_G,
+         AXI_CONFIG_G     => AXI_CONFIG_G,
+         AXI_ERROR_RESP_G => AXI_ERROR_RESP_G)
       port map(
          -- FLASH Memory Ports
          csL            => csL,
@@ -104,6 +111,11 @@ begin
          axiReadSlave   => axiReadSlave,
          axiWriteMaster => axiWriteMaster,
          axiWriteSlave  => axiWriteSlave,
+         -- AXI Streaming Interface (Optional)
+         mAxisMaster    => mAxisMaster,
+         mAxisSlave     => mAxisSlave,
+         sAxisMaster    => sAxisMaster,
+         sAxisSlave     => sAxisSlave,
          -- Clocks and Resets
          axiClk         => axiClk,
          axiRst         => axiRst);   
