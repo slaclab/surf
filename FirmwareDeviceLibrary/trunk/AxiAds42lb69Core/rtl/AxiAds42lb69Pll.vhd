cb217081e149a05f7b58451436196f4183131c0c
ruckman <ruckman@slac.stanford.edu>
2015-03-23 17:28:53 +0000

    adding sources
    git-svn-id: file:///dev/shm/git-devel/svn-repo@6853 0848cbd7-7569-0410-ab05-8cc0a0c67708
    
    Former-commit-id: 855a51cbabed86bf23bdd9425e8060da9f5e2029
    Former-commit-id: a9f5c36bd96a3371fc0432aa27b1ee8fe5fb6957

---
+++ FirmwareDeviceLibrary/trunk/AxiAds42lb69Core/rtl/AxiAds42lb69Pll.vhd
@@ -0,0 +1,217 @@
+-------------------------------------------------------------------------------
+-- Title      : 
+-------------------------------------------------------------------------------
+-- File       : AxiAds42lb69Pll.vhd
+-- Author     : Larry Ruckman  <ruckman@slac.stanford.edu>
+-- Company    : SLAC National Accelerator Laboratory
+-- Created    : 2015-03-20
+-- Last update: 2015-03-20
+-- Platform   : 
+-- Standard   : VHDL'93/02
+-------------------------------------------------------------------------------
+-- Description: 
+-------------------------------------------------------------------------------
+-- Copyright (c) 2015 SLAC National Accelerator Laboratory
+-------------------------------------------------------------------------------
+
+library ieee;
+use ieee.std_logic_1164.all;
+
+use work.StdRtlPkg.all;
+
+library unisim;
+use unisim.vcomponents.all;
+
+entity AxiAds42lb69Pll is
+   generic (
+      TPD_G          : time    := 1 ns;
+      USE_PLL_G      : boolean := false;
+      ADC_CLK_FREQ_G : real    := 250.0E+6);
+   port (
+      -- ADC Clocking ports
+      adcClkP   : out sl;
+      adcClkN   : out sl;
+      adcSyncP  : out sl;
+      adcSyncN  : out sl;
+      adcClkFbP : in  sl;
+      adcClkFbN : in  sl;
+      -- ADC Reference Signals
+      adcSync   : in  sl;
+      adcClk    : in  sl;
+      adcRst    : in  sl;              
+      adcClock  : out  sl);                
+end AxiAds42lb69Pll;
+
+architecture mapping of AxiAds42lb69Pll is
+
+   constant ADC_CLK_PERIOD_C    : real := getRealDiv(1, ADC_CLK_FREQ_G);
+   constant ADC_CLK_PERIOD_NS_C : real := getRealMult(1.0E+9, ADC_CLK_PERIOD_C);
+   constant CLKFBOUT_MULT_F_C   : real := getRealDiv(1.0E+9, ADC_CLK_FREQ_G);
+
+   signal clkFeedBack    : sl;
+   signal clkFeedBackIn  : sl;
+   signal clkFeedBackOut : sl;
+
+   signal sync     : sl;
+   signal syncOut  : sl;
+   signal adcInClk : sl;
+
+begin
+
+   GEN_PLL : if (USE_PLL_G = true) generate
+
+      IBUFGDS_0 : IBUFGDS
+         port map (
+            I  => adcClkFbP,
+            IB => adcClkFbN,
+            O  => clkFeedBackIn);
+
+      MMCME2_ADV_0 : MMCME2_ADV
+         generic map(
+            BANDWIDTH            => "LOW",
+            CLKOUT4_CASCADE      => false,
+            COMPENSATION         => "ZHOLD",
+            STARTUP_WAIT         => false,
+            DIVCLK_DIVIDE        => 1,
+            CLKFBOUT_MULT_F      => CLKFBOUT_MULT_F_C,
+            CLKFBOUT_PHASE       => 0.000,
+            CLKFBOUT_USE_FINE_PS => false,
+            CLKIN1_PERIOD        => ADC_CLK_PERIOD_NS_C,
+            REF_JITTER1          => 0.100)
+         port map (
+            -- Output clocks
+            CLKFBOUT     => clkFeedBack,
+            CLKFBOUTB    => open,
+            CLKOUT0      => open,
+            CLKOUT0B     => open,
+            CLKOUT1      => open,
+            CLKOUT1B     => open,
+            CLKOUT2      => open,
+            CLKOUT2B     => open,
+            CLKOUT3      => open,
+            CLKOUT3B     => open,
+            CLKOUT4      => open,
+            CLKOUT5      => open,
+            CLKOUT6      => open,
+            -- Input clock control
+            CLKFBIN      => clkFeedBackIn,
+            CLKIN1       => adcClk,
+            CLKIN2       => '0',
+            -- Tied to always select the primary input clock
+            CLKINSEL     => '1',
+            -- Ports for dynamic reconfiguration
+            DADDR        => (others => '0'),
+            DCLK         => '0',
+            DEN          => '0',
+            DI           => (others => '0'),
+            DO           => open,
+            DRDY         => open,
+            DWE          => '0',
+            -- Ports for dynamic phase shift
+            PSCLK        => '0',
+            PSEN         => '0',
+            PSINCDEC     => '0',
+            PSDONE       => open,
+            -- Other control and status signals
+            LOCKED       => open,
+            CLKINSTOPPED => open,
+            CLKFBSTOPPED => open,
+            PWRDWN       => '0',
+            RST          => adcRst);
+
+      BUFH_0 : BUFH
+         port map (
+            I => clkFeedBack,
+            O => clkFeedBackOut);     
+
+      ClkOutBufDiff_0 : entity work.ClkOutBufDiff
+         port map (
+            clkIn   => clkFeedBackOut,
+            clkOutP => adcClkP,
+            clkOutN => adcClkN);
+
+      SynchronizerOneShot_0 : entity work.SynchronizerOneShot
+         generic map (
+            TPD_G         => TPD_G,
+            BYPASS_SYNC_G => false)
+         port map (
+            clk     => clkFeedBackOut,
+            dataIn  => adcSync,
+            dataOut => sync);
+
+      ODDR_0 : ODDR
+         generic map(
+            DDR_CLK_EDGE => "OPPOSITE_EDGE",  -- "OPPOSITE_EDGE" or "SAME_EDGE" 
+            INIT         => '0',              -- Initial value for Q port ('1' or '0')
+            SRTYPE       => "SYNC")           -- Reset Type ("ASYNC" or "SYNC")
+         port map (
+            D1 => sync,                       -- 1-bit data input (positive edge)
+            D2 => sync,                       -- 1-bit data input (negative edge)
+            Q  => syncOut,                    -- 1-bit DDR output
+            C  => clkFeedBackOut,             -- 1-bit clock input
+            CE => '1',                        -- 1-bit clock enable input
+            R  => '0',                        -- 1-bit reset
+            S  => '0');                       -- 1-bit set
+
+      OBUFDS_0 : OBUFDS
+         port map(
+            I  => syncOut,
+            O  => adcSyncP,
+            OB => adcSyncN);  
+
+      adcClock <= adcClk;
+
+   end generate;
+
+   GEN_NO_PLL : if (USE_PLL_G = false) generate
+      
+      ClkOutBufDiff_1 : entity work.ClkOutBufDiff
+         port map (
+            clkIn   => adcClk,
+            rstIn   => adcRst,
+            clkOutP => adcClkP,
+            clkOutN => adcClkN);   
+
+      SynchronizerOneShot_1 : entity work.SynchronizerOneShot
+         generic map (
+            TPD_G         => TPD_G,
+            BYPASS_SYNC_G => true)
+         port map (
+            clk     => adcClk,
+            dataIn  => adcSync,
+            dataOut => sync);
+
+      ODDR_1 : ODDR
+         generic map(
+            DDR_CLK_EDGE => "OPPOSITE_EDGE",  -- "OPPOSITE_EDGE" or "SAME_EDGE" 
+            INIT         => '0',              -- Initial value for Q port ('1' or '0')
+            SRTYPE       => "SYNC")           -- Reset Type ("ASYNC" or "SYNC")
+         port map (
+            D1 => sync,                       -- 1-bit data input (positive edge)
+            D2 => sync,                       -- 1-bit data input (negative edge)
+            Q  => syncOut,                    -- 1-bit DDR output
+            C  => adcClk,                     -- 1-bit clock input
+            CE => '1',                        -- 1-bit clock enable input
+            R  => '0',                        -- 1-bit reset
+            S  => '0');                       -- 1-bit set
+
+      OBUFDS_1 : OBUFDS
+         port map(
+            I  => syncOut,
+            O  => adcSyncP,
+            OB => adcSyncN);         
+
+      IBUFGDS_1 : IBUFGDS
+         port map (
+            I  => adcClkFbP,
+            IB => adcClkFbN,
+            O  => adcInClk);
+
+      BUFG_1 : BUFG
+         port map (
+            I => adcInClk,
+            O => adcClock); 
+
+   end generate;
+   
+end mapping;
