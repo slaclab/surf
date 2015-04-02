adeac0bb52b23acfd537dde0a2639cf02977c217
ruckman <ruckman@slac.stanford.edu>
2015-04-02 23:52:29 +0000

    Updated the example wrapper w.r.t. testing with the NetFPGA-SUME development board
    git-svn-id: file:///dev/shm/git-devel/svn-repo@6955 0848cbd7-7569-0410-ab05-8cc0a0c67708
    
    Former-commit-id: cff2e2400937a59ce838ef544799f7350c189159
    Former-commit-id: 6cd3c7e9e9d6cb5a72804e8dafda81bdaaa1ae9c

--- pgp2b_core/trunk/gth7/rtl/Pgp2bGth7VarLatWrapper.vhd
+++ pgp2b_core/trunk/gth7/rtl/Pgp2bGth7VarLatWrapper.vhd
@@ -5,13 +5,13 @@
 -- Author     : Larry Ruckman  <ruckman@slac.stanford.edu>
 -- Company    : SLAC National Accelerator Laboratory
 -- Created    : 2015-04-01
--- Last update: 2015-04-01
+-- Last update: 2015-04-02
 -- Platform   : 
 -- Standard   : VHDL'93/02
 -------------------------------------------------------------------------------
 -- Description: Example PGP 3.125 Gbps front end wrapper
 -- Note: Default generic configurations are for the Digilent NetFPGA-SUME development board
--- Note: Default uses SFP_CLK = 312.5 MHz reference clock
+-- Note: Default uses FPGA fabric clock = 156.25 MHz reference clock
 -------------------------------------------------------------------------------
 -- Copyright (c) 2015 SLAC National Accelerator Laboratory
 -------------------------------------------------------------------------------
@@ -29,16 +29,15 @@ use unisim.vcomponents.all;
 entity Pgp2bGth7VarLatWrapper is
    generic (
       TPD_G             : time                 := 1 ns;
-      -- CPLL Configurations (Defaults: gtClkP = 312.5 MHz Configuration)
-      CPLL_REFCLK_SEL_G : bit_vector           := "001";
-      CPLL_FBDIV_G      : natural              := 2;
+      -- CPLL Configurations (Defaults: pgpClk = 156.25 MHz Configuration)
+      CPLL_FBDIV_G      : natural              := 4;
       CPLL_FBDIV_45_G   : natural              := 5;
       CPLL_REFCLK_DIV_G : natural              := 1;
-      -- MGT Configurations (Defaults: gtClkP = 312.5 MHz Configuration)
+      -- MGT Configurations (Defaults: pgpClk = 156.25 MHz Configuration)
       RXOUT_DIV_G       : natural              := 2;
       TXOUT_DIV_G       : natural              := 2;
-      RX_CLK25_DIV_G    : natural              := 13;
-      TX_CLK25_DIV_G    : natural              := 13;
+      RX_CLK25_DIV_G    : natural              := 7;
+      TX_CLK25_DIV_G    : natural              := 7;
       RX_OS_CFG_G       : bit_vector           := "0000010000000";           -- Set by wizard
       RXCDR_CFG_G       : bit_vector           := x"0002007FE1000C2200018";  -- Set by wizard
       RXDFEXYDEN_G      : sl                   := '1';                       -- Set by wizard 
@@ -46,8 +45,8 @@ entity Pgp2bGth7VarLatWrapper is
       NUM_VC_EN_G       : natural range 1 to 4 := 4);
    port (
       -- Clocks and Reset
-      pgpClk       : out sl;
-      pgpRst       : out sl;
+      pgpClk       : in  sl;
+      pgpRst       : in  sl;
       -- Non VC TX Signals
       pgpTxIn      : in  Pgp2bTxInType;
       pgpTxOut     : out Pgp2bTxOutType;
@@ -61,8 +60,6 @@ entity Pgp2bGth7VarLatWrapper is
       pgpRxMasters : out AxiStreamMasterArray(3 downto 0);
       pgpRxCtrl    : in  AxiStreamCtrlArray(3 downto 0);
       -- GT Pins
-      gtClkP       : in  sl;
-      gtClkN       : in  sl;
       gtTxP        : out sl;
       gtTxN        : out sl;
       gtRxP        : in  sl;
@@ -71,44 +68,15 @@ end Pgp2bGth7VarLatWrapper;
 
 architecture mapping of Pgp2bGth7VarLatWrapper is
 
-   signal refClk     : sl;
-   signal refClkDiv2 : sl;
-
-   signal pgpClock : sl;
-   signal pgpReset : sl;
-
 begin
-
-   pgpClk <= pgpClock;
-   pgpRst <= pgpReset;
-
-   IBUFDS_GTE2_Inst : IBUFDS_GTE2
-      port map (
-         I     => gtClkP,
-         IB    => gtClkN,
-         CEB   => '0',
-         ODIV2 => refClkDiv2,
-         O     => refClk);    
-
-   BUFG_Inst : BUFG
-      port map (
-         I => refClkDiv2,
-         O => pgpClock);           
-
-   PwrUpRst_Inst : entity work.PwrUpRst
-      generic map(
-         TPD_G => TPD_G)   
-      port map (
-         clk    => pgpClock,
-         rstOut => pgpReset);          
-
+   
    Pgp2bGth7VarLat_Inst : entity work.Pgp2bGth7VarLat
       generic map (
          TPD_G             => TPD_G,
          -- CPLL Configurations
          TX_PLL_G          => "CPLL",
          RX_PLL_G          => "CPLL",
-         CPLL_REFCLK_SEL_G => CPLL_REFCLK_SEL_G,
+         CPLL_REFCLK_SEL_G => "111",
          CPLL_FBDIV_G      => CPLL_FBDIV_G,
          CPLL_FBDIV_45_G   => CPLL_FBDIV_45_G,
          CPLL_REFCLK_DIV_G => CPLL_REFCLK_DIV_G,
@@ -124,8 +92,8 @@ begin
          NUM_VC_EN_G       => NUM_VC_EN_G)          
       port map (
          -- GT Clocking
-         stableClk        => pgpClock,
-         gtCPllRefClk     => refClk,
+         stableClk        => pgpClk,
+         gtCPllRefClk     => pgpClk,
          gtCPllLock       => open,
          gtQPllRefClk     => '0',
          gtQPllClk        => '0',
@@ -138,15 +106,15 @@ begin
          gtRxP            => gtRxP,
          gtRxN            => gtRxN,
          -- Tx Clocking
-         pgpTxReset       => pgpReset,
+         pgpTxReset       => pgpRst,
          pgpTxRecClk      => open,
-         pgpTxClk         => pgpClock,
+         pgpTxClk         => pgpClk,
          pgpTxMmcmReset   => open,
          pgpTxMmcmLocked  => '1',
          -- Rx clocking
-         pgpRxReset       => pgpReset,
+         pgpRxReset       => pgpRst,
          pgpRxRecClk      => open,
-         pgpRxClk         => pgpClock,
+         pgpRxClk         => pgpClk,
          pgpRxMmcmReset   => open,
          pgpRxMmcmLocked  => '1',
          -- Non VC TX Signals
