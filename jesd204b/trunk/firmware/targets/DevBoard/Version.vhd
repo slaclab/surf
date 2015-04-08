aa934ac215003fdfe8dcaa2629743791206f32b4
bareese <bareese@slac.stanford.edu>
2015-04-08 21:03:30 +0000

    Added project files.
    git-svn-id: file:///dev/shm/git-devel/svn-repo@7017 0848cbd7-7569-0410-ab05-8cc0a0c67708
    
    Former-commit-id: 20219309837e370b5b1090411e800b619ac1ba73
    Former-commit-id: efa5c89d0b4fa20394c3da4ff884f6ab741d19a3

--- jesd204b/trunk/firmware/targets/DevBoard/Version.vhd
+++ jesd204b/trunk/firmware/targets/DevBoard/Version.vhd
@@ -1,29 +1,28 @@
 -------------------------------------------------------------------------------
--- Title      : 
+-- Title         : Version Constant File
+-- Project       : COB Zynq DTM
 -------------------------------------------------------------------------------
--- File       : Version.vhd
--- Author     : Benjamin Reese  <bareese@slac.stanford.edu>
--- Company    : SLAC National Accelerator Laboratory
--- Created    : 2013-09-29
--- Last update: 2015-04-02
--- Platform   : 
--- Standard   : VHDL'93/02
+-- File          : Version.vhd
+-- Author        : Ryan Herbst, rherbst@slac.stanford.edu
+-- Created       : 05/18/2014
 -------------------------------------------------------------------------------
--- Description: 
+-- Description:
+-- Version Constant Module
 -------------------------------------------------------------------------------
--- Copyright (c) 2013 SLAC National Accelerator Laboratory
+-- Copyright (c) 2012 by SLAC. All rights reserved.
 -------------------------------------------------------------------------------
-
-
-library ieee;
-use ieee.std_logic_1164.all;
+LIBRARY ieee;
+USE ieee.std_logic_1164.ALL;
 
 package Version is
 
-  constant FPGA_VERSION_C : std_logic_vector(31 downto 0) := X"00000000";  -- MAKE_VERSION
-  constant BUILD_STAMP_C : string := "Feb: Vivado v2014.4 (x86_64) Built Tue Mar 24 15:44:36 PDT 2015 by bareese";
+constant FPGA_VERSION_C : std_logic_vector(31 downto 0) := x"00000000"; -- MAKE_VERSION
+
+constant BUILD_STAMP_C : string := "DataDpm: Vivado v2014.4 (x86_64) Built Wed Apr  8 11:23:07 PDT 2015 by bareese";
 
 end Version;
-
+ 
 -------------------------------------------------------------------------------
 -- Revision History:
+-------------------------------------------------------------------------------
+
