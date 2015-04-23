09e5c3bc6e2ae6c5c504b1da7543ae1420a11208
bareese <bareese@slac.stanford.edu>
2015-04-23 18:38:35 +0000

    Commit of what I have, though it is still incomplete.
    git-svn-id: file:///dev/shm/git-devel/svn-repo@7142 0848cbd7-7569-0410-ab05-8cc0a0c67708

--- jesd204b/trunk/firmware/targets/DevBoard/rtl/DevBoard.vhd
+++ jesd204b/trunk/firmware/targets/DevBoard/rtl/DevBoard.vhd
@@ -5,7 +5,7 @@
 -- Author     : Benjamin Reese  <bareese@slac.stanford.edu>
 -- Company    : SLAC National Accelerator Laboratory
 -- Created    : 2013-08-22
--- Last update: 2015-04-02
+-- Last update: 2015-04-22
 -- Platform   : 
 -- Standard   : VHDL'93/02
 -------------------------------------------------------------------------------
@@ -24,110 +24,118 @@ use UNISIM.VCOMPONENTS.all;
 use work.StdRtlPkg.all;
 use work.AxiLitePkg.all;
 use work.AxiStreamPkg.all;
-use work.SsiPkg.all;
-use work.ClockManager7Pkg.all;
-use work.AxiMicronP30Pkg.all;
 
 entity DevBoard is
    
    generic (
-      TPD_G          : time           := 1 ns);
+      TPD_G                  : time    := 1 ns;
+      -- PGP Config
+      PGP_REFCLK_FREQ_G      : real    := 125.0E6;
+      PGP_LINE_RATE_G        : real    := 3.125E9;
+      -- AXIL Config
+      AXIL_CLK_FREQ_C        : real    := 125.0E6;
+      -- AXIS Config
+      AXIS_CLK_FREQ_G        : real : 160.0E6;
+      AXIS_FIFO_ADDR_WIDTH_G : integer := 9);
    port (
-      -- GTP Reference Clocks
-      gtRefClk125P : in sl;
-      gtRefClk125N : in sl;
-      gtRefClk250P : in sl;
-      gtRefClk250N : in sl;
+      pgpRefClkP : in sl;
+      pgpRefClkN : in sl;
 
-      -- Fixed Latency GTP link
-      sysGtTxP : out sl;
-      sysGtTxN : out sl;
-      sysGtRxP : in  sl;
-      sysGtRxN : in  sl;
+      -- PGP MGT signals
+      pgpGtRxN : in  sl;                -- SFP+ 
+      pgpGtRxP : in  sl;
+      pgpGtTxN : out sl;
+      pgpGtTxP : out sl;
 
-      dataGtTxP : out slv(3 downto 0);
-      dataGtTxN : out slv(3 downto 0);
-      dataGtRxP : in  slv(3 downto 0);
-      dataGtRxN : in  slv(3 downto 0);
+      -- FMC Signals -- 
+      -- Signals from clock manager
+      fpgaDevClkaP : in sl;             -- GBT_CLK_0_P - FMC D3
+      fpgaDevClkaN : in sl;             -- GBT_CLK_0_N - FMC D4
+      fpgaDevClkbP : in sl;             -- LA00_P_CC - FMC G6
+      fpgaDevClkbN : in sl;             -- LA00_N_CC - FMC G7
+      fpgaSysRefP  : in sl;             -- LA03_P - FMC G9
+      fpgaSysRefN  : in sl;             -- LA04_N - FMC G10
 
-      -- XADC Interface
-      vAuxP : in slv(15 downto 0);
-      vAuxN : in slv(15 downto 0);
-      vPIn  : in sl;
-      vNIn  : in sl;
+      -- Signals to ADC (if clock manager not used)
+      adcDevClkP : out sl;              -- LA01_P_CC - FMC D7
+      adcDevClkN : out sl;              -- LA01_N_CC - FMC D8
+      adcSysRefP : out sl;              -- LA05_P_CC - FMC D11
+      adcSysRefN : out sl;              -- LA05_N_CC - FMC D12
 
-      leds : out slv(7 downto 0);       -- Test outputs
+      -- JESD MGT signals
+      adcGtTxP : out slv(3 downto 0);   -- FMC HPC DP[3:0]
+      adcGtTxN : out slv(3 downto 0);
+      adcGtRxP : in  slv(3 downto 0);
+      adcGtRxN : in  slv(3 downto 0);
 
-      -- Flash PROM Interface
-      flashDq   : inout slv(15 downto 0);
-      flashAddr : out   slv(25 downto 0);
-      flashCeL  : out   sl;
-      flashOeL  : out   sl;
-      flashWeL  : out   sl;
-      flashAdv  : out   sl;
-      flashWait : in    sl
-      );
+      -- Synchronization JESD signals (Used in subclass 0 and subclass 2 modes)
+      syncbP : out sl;                  -- LA08_P - FMC G12
+      syncbN : out sl;                  -- LA08_N - FMC G13
+
+      -- Adc OVR/trigger signals
+      ovraTrigRdy : in sl;              -- LA25_P - FMC G27
+      ovrbTrigger : in sl;              -- LA26_P - FMC D26
+
+      -- ADC SPI config interface
+      spiSclk : out sl;                 -- FMC H37
+      spiSdi  : out sl;                 -- FMC G36
+      spiSdo  : in  sl;                 -- FMC G37
+      spiCsL  : out sl;                 -- FMC H38
+
+      -- Onboard LEDs
+      leds : out slv(3 downto 0));
+
 
 end entity DevBoard;
 
 architecture rtl of DevBoard is
 
+   constant PGP_REFCLK_PERIOD_C : real := 1.0 / PGP_REFCLK_FREQ_C;
+   constant AXI_CLK_FREQ_C      : real := PGP_LINE_RATE_C / 20;
 
    -------------------------------------------------------------------------------------------------
    -- Clock Signals
    -------------------------------------------------------------------------------------------------
    signal gtRefClk125  : sl;
    signal gtRefClk125G : sl;
-   signal gtRefClk250  : sl;
 
-   signal axiClk        : sl;
-   signal axiRst        : sl;
-   signal axiClkMmcmRst : sl;
-
-   signal clk250MmcmRst : sl;
-   signal clk200        : sl;
-   signal clk200Rst     : sl;
-   signal clk250        : sl;
-   signal clk250Rst     : sl;
+   signal axilClk    : sl;
+   signal axilClkRst : sl;
+   signal pgpClk     : sl;
+   signal pgpClkRst  : sl;
+   signal jesdClk    : sl;
+   signal jesdClkRst : sl;
+   signal mmcmRst    : sl;
 
    signal powerOnReset : sl;
    signal masterReset  : sl;
    signal fpgaReload   : sl;
 
    -------------------------------------------------------------------------------------------------
-   -- AXI Signals
+   -- PGP MGT PLL Config
    -------------------------------------------------------------------------------------------------
+   constant PGP_GTX_CPLL_CONFIG_G : Gtx7CfgType := getGtx7CPllCfg(PGP_REFCLK_FREQ_C, PGP_LINE_RATE_C);
 
-   constant NUM_AXI_MASTERS_C : natural := 4;
+   -------------------------------------------------------------------------------------------------
+   -- AXI Lite Config and Signals
+   -------------------------------------------------------------------------------------------------
+   constant NUM_AXI_MASTERS_C : natural := 2;
 
-   constant XADC_AXI_INDEX_C    : natural := 0;
-   constant VERSION_AXI_INDEX_C : natural := 1;
-   constant PGP_AXI_INDEX_C     : natural := 2;
-   constant PROM_AXI_INDEX_C    : natural := 3;
+   constant VERSION_AXI_INDEX_C : natural := 0;
+   constant ADC_SPI_AXI_INDEX_C : natural := 1;
 
-   constant XADC_AXI_BASE_ADDR_C    : slv(31 downto 0) := X"00000000";
-   constant VERSION_AXI_BASE_ADDR_C : slv(31 downto 0) := X"00200000";
-   constant PGP_AXI_BASE_ADDR_C     : slv(31 downto 0) := X"00210000";
-   constant PROM_AXI_BASE_ADDR_C    : slv(31 downto 0) := X"00800000";
+   constant VERSION_AXI_BASE_ADDR_C : slv(31 downto 0) := X"00000000";
+   constant ADC_SPI_AXI_BASE_ADDR_C : slv(31 downto 0) := X"00010000";
 
    constant AXI_CROSSBAR_MASTERS_CONFIG_C : AxiLiteCrossbarMasterConfigArray(NUM_AXI_MASTERS_C-1 downto 0) := (
-      XADC_AXI_INDEX_C    => (
-         baseAddr         => XADC_AXI_BASE_ADDR_C,
-         addrBits         => 12,
-         connectivity     => X"0001"),
       VERSION_AXI_INDEX_C => (
          baseAddr         => VERSION_AXI_BASE_ADDR_C,
          addrBits         => 12,
          connectivity     => X"0001"),
-      PGP_AXI_INDEX_C     => (
-         baseAddr         => PGP_AXI_BASE_ADDR_C,
+      ADC_SPI_AXI_INDEX_C => (
+         baseAddr         => ADC_SPI_AXI_BASE_ADDR_C,
          addrBits         => 14,
-         connectivity     => X"0001"),
-      PROM_AXI_INDEX_C    => (
-         baseAddr         => PROM_AXI_BASE_ADDR_C,
-         addrBits         => 8,
          connectivity     => X"0001"));
-
 
    signal extAxilWriteMaster : AxiLiteWriteMasterType;
    signal extAxilWriteSlave  : AxiLiteWriteSlaveType;
@@ -140,20 +148,13 @@ architecture rtl of DevBoard is
    signal locAxilReadSlaves   : AxiLiteReadSlaveArray(NUM_AXI_MASTERS_C-1 downto 0);
 
    -------------------------------------------------------------------------------------------------
-   -- PROM Streaming interface
+   -- PGP Signals and Virtual Channels
    -------------------------------------------------------------------------------------------------
-   signal promRxAxisMaster : AxiStreamMasterType;
-   signal promRxAxisSlave  : AxiStreamSlaveType;
-   signal promTxAxisMaster : AxiStreamMasterType;
-   signal promTxAxisSlave  : AxiStreamSlaveType;
+   signal axisTxMasters : AxiStreamMasterArray(1 downto 0);
+   signal axisTxSlaves  : AxiStreamSlaveArray(1 downto 0);
 
-   -------------------------------------------------------------------------------------------------
-   -- Flash prom local io records
-   -------------------------------------------------------------------------------------------------
-   signal flashIn    : AxiMicronP30InType;
-   signal flashOut   : AxiMicronP30OutType;
-   signal flashInOut : AxiMicronP30InOutType;
-
+   signal axisFifoPauseThreshold : slv(AXIS_FIFO_ADDR_WIDTH_G-1 downto 0);
+   
 begin
 
    -------------------------------------------------------------------------------------------------
@@ -161,34 +162,15 @@ begin
    -------------------------------------------------------------------------------------------------
    IBUFDS_GTE2_GTREFCLK125 : IBUFDS_GTE2
       port map (
-         I   => gtRefClk125P,
-         IB  => gtRefClk125N,
+         I   => pgpRefClkP,
+         IB  => pgpRefClkN,
          CEB => '0',
-         O   => gtRefClk125);
-
-   IBUFDS_GTE2_GTREFCLK250 : IBUFDS_GTE2
-      port map (
-         I   => gtRefClk250P,
-         IB  => gtRefClk250N,
-         CEB => '0',
-         O   => gtRefClk250);
+         O   => pgpRefClk);
 
    GTREFCLK125_BUFG : BUFG
       port map (
-         I => gtRefClk125,
-         O => gtRefClk125G);
-
-   IBUFDS_GTE2_DAQREFCLK : IBUFDS_GTE2
-      port map (
-         I   => daqRefClkP,
-         IB  => daqRefClkN,
-         CEB => '0',
-         O   => daqRefClk);
-
-   DAQREFCLK_BUFG : BUFG
-      port map (
-         I => daqRefClk,
-         O => daqRefClkG);
+         I => pgpRefClk,
+         O => pgpRefClkG);
 
    PwrUpRst_1 : entity work.PwrUpRst
       generic map (
@@ -197,13 +179,13 @@ begin
          IN_POLARITY_G  => '1',
          OUT_POLARITY_G => '1')
       port map (
-         clk    => gtRefClk125G,
+         clk    => pgpRefClkG,
          rstOut => powerOnReset);
 
    -------------------------------------------------------------------------------------------------
    -- Create global clocks from gt ref clocks
    -------------------------------------------------------------------------------------------------
-   axiClkMmcmRst <= masterReset or powerOnReset;
+   mmcmRst <= masterReset or powerOnReset;
 
    U_CtrlClockManager7 : entity work.ClockManager7
       generic map (
@@ -213,80 +195,71 @@ begin
          FB_BUFG_G          => true,
          NUM_CLOCKS_G       => 1,
          BANDWIDTH_G        => "OPTIMIZED",
-         CLKIN_PERIOD_G     => 8.0,
+         CLKIN_PERIOD_G     => PGP_REFCLK_PERIOD_C*1.0E9,
          DIVCLK_DIVIDE_G    => 1,
-         CLKFBOUT_MULT_F_G  => 8.0,
-         CLKOUT0_DIVIDE_F_G => 8.0,
-         CLKOUT0_RST_HOLD_G => 100)
+         CLKFBOUT_MULT_F_G  => 7.5,
+         CLKOUT0_DIVIDE_F_G => 7.5,
+         CLKOUT0_RST_HOLD_G => 16,
+         CLKOUT1_DIVIDE_G => 6,
+         CLKOUT1_RST_HOLD_G => 16)
       port map (
-         clkIn     => gtRefClk125G,
-         rstIn     => axiClkMmcmRst,
-         clkOut(0) => axiClk,
-         rstOut(0) => axiRst);
-
-
-   clk250MmcmRst <= masterReset or powerOnReset;
-
-   U_DataClockManager7 : entity work.ClockManager7
-      generic map (
-         TPD_G              => TPD_G,
-         TYPE_G             => "MMCM",
-         INPUT_BUFG_G       => true,
-         FB_BUFG_G          => true,
-         NUM_CLOCKS_G       => 2,
-         BANDWIDTH_G        => "OPTIMIZED",
-         CLKIN_PERIOD_G     => 4.0,
-         DIVCLK_DIVIDE_G    => 1,
-         CLKFBOUT_MULT_F_G  => DATA_MMCM_CFG_C.CLKFBOUT_MULT_F_G,
-         CLKOUT0_DIVIDE_F_G => DATA_MMCM_CFG_C.CLKOUT0_DIVIDE_F_G,
-         CLKOUT0_RST_HOLD_G => DATA_MMCM_CFG_C.CLKOUT0_RST_HOLD_G,
-         CLKOUT1_DIVIDE_G   => DATA_MMCM_CFG_C.CLKOUT1_DIVIDE_G,
-         CLKOUT1_RST_HOLD_G => DATA_MMCM_CFG_C.CLKOUT1_RST_HOLD_G)
-      port map (
-         clkIn     => gtRefClk250,
-         rstIn     => clk250MmcmRst,
-         clkOut(0) => clk250,
-         clkOut(1) => pgpDataClk,
-         rstOut(0) => clk250Rst,
-         rstOut(1) => pgpDataClkRst);
-
+         clkIn     => ctrlRefClkG,
+         rstIn     => axilClkMmcmRst,
+         clkOut(0) => axilClk,
+         clkOut(1) => pgpClk,
+         rstOut(0) => axilClkRst,
+         rstOut(1) => pgpClkRst);
 
    -------------------------------------------------------------------------------------------------
    -- LED Test Outputs
    -------------------------------------------------------------------------------------------------
-   Heartbeat_1 : entity work.Heartbeat
+   Heartbeat_axilClk : entity work.Heartbeat
       generic map (
          TPD_G        => TPD_G,
          PERIOD_IN_G  => 8.0E-9,
          PERIOD_OUT_G => 0.8)
       port map (
-         clk => axiClk,
+         clk => axilClk,
          o   => leds(0));
-
-   Heartbeat_2 : entity work.Heartbeat
-      generic map (
-         TPD_G        => TPD_G,
-         PERIOD_IN_G  => 5.0E-9,
-         PERIOD_OUT_G => 0.5)
-      port map (
-         clk => pgpDataClk,
-         o   => leds(1));
-
    
-   Heartbeat_3 : entity work.Heartbeat
+   Heartbeat_pgpClk : entity work.Heartbeat
       generic map (
          TPD_G        => TPD_G,
-         PERIOD_IN_G  => 8.0E-9,
-         PERIOD_OUT_G => 0.8)
+         PERIOD_IN_G  => 6.4E-9,
+         PERIOD_OUT_G => 0.64)
       port map (
-         clk => daqClk125,
-         rst => daqClk125Rst,
-         o   => leds(2));
-
-
+         clk => pgpClk,
+         o   => leds(1));
    -------------------------------------------------------------------------------------------------
    -- PGP Interface 
    -------------------------------------------------------------------------------------------------
+   PgpFrontEnd_1 : entity work.PgpFrontEnd
+      generic map (
+         TPD_G                 => TPD_G,
+         PGP_REFCLK_FREQ_G     => PGP_REFCLK_FREQ_G,
+         PGP_LINE_RATE_G       => PGP_LINE_RATE_G,
+         AXI_LITE_CLK_FREQ_G   => AXI_LITE_CLK_FREQ_G,
+         AXI_STREAM_CLK_FREQ_G => AXI_STREAM_CLK_FREQ_G,
+         AXI_STREAM_CONFIG_C   => AXI_STREAM_CONFIG_C)
+      port map (
+         pgpRefClk       => pgpRefClk,
+         pgpClk          => pgpClk,
+         pgpGtRxN        => pgpGtRxN,
+         pgpGtRxP        => pgpGtRxP,
+         pgpGtTxN        => pgpGtTxN,
+         pgpGtTxP        => pgpGtTxP,
+         axilClk         => axilClk,
+         axilClkRst      => axilClkRst,
+         axilWriteMaster => extAxilWriteMaster,
+         axilWriteSlave  => extAxilWriteSlave,
+         axilReadMaster  => extAxilReadMaster,
+         axilReadSlave   => extAxilReadSlave,
+         axisClk         => axisClk,
+         axisClkRst      => axisClkRst,
+         axisTxMasters   => axisTxMasters,
+         axisTxSlaves    => axisTxSlaves,
+         axisTxCtrl      => axisTxCtrl,
+         leds            => leds(3 downto 2));
 
    -------------------------------------------------------------------------------------------------
    -- Top Axi Crossbar
@@ -298,8 +271,8 @@ begin
          NUM_MASTER_SLOTS_G => NUM_AXI_MASTERS_C,
          MASTERS_CONFIG_G   => AXI_CROSSBAR_MASTERS_CONFIG_C)
       port map (
-         axiClk              => axiClk,
-         axiClkRst           => axiRst,
+         axiClk              => axilClk,
+         axiClkRst           => axilClkRst,
          sAxiWriteMasters(0) => extAxilWriteMaster,
          sAxiWriteSlaves(0)  => extAxilWriteSlave,
          sAxiReadMasters(0)  => extAxilReadMaster,
@@ -309,73 +282,26 @@ begin
          mAxiReadMasters     => locAxilReadMasters,
          mAxiReadSlaves      => locAxilReadSlaves);
 
-   AxiXadcWrapper_1 : entity work.AxiXadcWrapper
-      generic map (
-         TPD_G => TPD_G)
-      port map (
-         axiClk         => axiClk,
-         axiRst         => axiRst,
-         axiReadMaster  => locAxilReadMasters(XADC_AXI_INDEX_C),
-         axiReadSlave   => locAxilReadSlaves(XADC_AXI_INDEX_C),
-         axiWriteMaster => locAxilWriteMasters(XADC_AXI_INDEX_C),
-         axiWriteSlave  => locAxilWriteSlaves(XADC_AXI_INDEX_C),
-         vPIn           => vPIn,
-         vNIn           => vNIn,
-         vAuxP          => vAuxP,
-         vAuxN          => vAuxN);
-
-
    -------------------------------------------------------------------------------------------------
    -- Put version info on AXI Bus
    -------------------------------------------------------------------------------------------------
    AxiVersion_1 : entity work.AxiVersion
       generic map (
-         TPD_G              => TPD_G,
-         EN_DEVICE_DNA_G    => true,
-         EN_DS2411_G        => true,
-         EN_ICAP_G          => true,
-         AUTO_RELOAD_EN_G   => true,
-         AUTO_RELOAD_TIME_G => 10.0,
-         AUTO_RELOAD_ADDR_G => X"04000000")
-      port map (
-         axiClk         => axiClk,
-         axiRst         => axiRst,
-         axiReadMaster  => locAxilReadMasters(VERSION_AXI_INDEX_C),
-         axiReadSlave   => locAxilReadSlaves(VERSION_AXI_INDEX_C),
-         axiWriteMaster => locAxilWriteMasters(VERSION_AXI_INDEX_C),
-         axiWriteSlave  => locAxilWriteSlaves(VERSION_AXI_INDEX_C),
-         masterReset    => masterReset,
-         fdSerSdio      => fdSerSdio);
-
-   -------------------------------------------------------------------------------------------------
-   -- FLASH Interface
-   -------------------------------------------------------------------------------------------------
-
-   AxiMicronP30Core_1 : entity work.AxiMicronP30Core
-      generic map (
          TPD_G            => TPD_G,
-         AXI_CLK_FREQ_G   => 125.0E6,
-         AXI_ERROR_RESP_G => AXI_RESP_SLVERR_C)
+         EN_DEVICE_DNA_G  => true,
+         EN_DS2411_G      => true,
+         EN_ICAP_G        => true,
+         AUTO_RELOAD_EN_G => false)
       port map (
-         flashIn        => flashIn,
-         flashInOut.dq  => flashDq,
-         flashOut       => flashOut,
-         axiReadMaster  => locAxilReadMasters(PROM_AXI_INDEX_C),
-         axiReadSlave   => locAxilReadSlaves(PROM_AXI_INDEX_C),
-         axiWriteMaster => locAxilWriteMasters(PROM_AXI_INDEX_C),
-         axiWriteSlave  => locAxilWriteSlaves(PROM_AXI_INDEX_C),
-         mAxisMaster    => promTxAxisMaster,
-         mAxisSlave     => promTxAxisSlave,
-         sAxisMaster    => promRxAxisMaster,
-         sAxisSlave     => promRxAxisSlave,
-         axiClk         => axiClk,
-         axiRst         => axiRst);
+         axiClk                                           => axilClk,
+         axiRst                                           => axilClkRst,
+         axiReadMaster                                    => locAxilReadMasters(VERSION_AXI_INDEX_C),
+         axiReadSlave                                     => locAxilReadSlaves(VERSION_AXI_INDEX_C),
+         axiWriteMaster                                   => locAxilWriteMasters(VERSION_AXI_INDEX_C),
+         axiWriteSlave                                    => locAxilWriteSlaves(VERSION_AXI_INDEX_C),
+         masterReset                                      => masterReset,
+         userValues(0)(AXIS_FIFO_ADDR_WIDTH_G-1 downto 0) => axisFifoPauseThreshold);
 
-   flashAddr         <= flashOut.addr(25 downto 0);
-   flashCel          <= flashOut.ceL;
-   flashOel          <= flashOut.oeL;
-   flashWeL          <= flashOut.weL;
-   flashAdv          <= flashOut.adv;
-   flashIn.flashWait <= flashWait;
+
 
 end architecture rtl;
