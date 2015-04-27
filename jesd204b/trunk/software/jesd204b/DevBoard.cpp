ba851427b306a706c9df744c1fa15af86d52bb56
bareese <bareese@slac.stanford.edu>
2015-04-27 16:20:13 +0000

    Added DevBoard System and Device classes.
    git-svn-id: file:///dev/shm/git-devel/svn-repo@7163 0848cbd7-7569-0410-ab05-8cc0a0c67708

---
+++ jesd204b/trunk/software/jesd204b/DevBoard.cpp
@@ -0,0 +1,75 @@
+//-----------------------------------------------------------------------------
+// File          : DevBoard.cpp
+// Author        : Ben Reese <bareese@slac.stanford.edu>
+// Created       : 11/14/2013
+// Project       : HPS SVT
+//-----------------------------------------------------------------------------
+// Description :
+// Device container for AxiVersion.vhd
+//-----------------------------------------------------------------------------
+// Copyright (c) 2013 by SLAC. All rights reserved.
+// Proprietary and confidential to SLAC.
+//-----------------------------------------------------------------------------
+// Modification history :
+// 11/14/2013: created
+//-----------------------------------------------------------------------------
+#include <DevBoard.h>
+#include <AxiVersion.h>
+#include <Pgp2bAxi.h>
+#include <AxiMicronP30.h>
+#include <PrbsRx.h>
+#include <PrbsTx.h>
+#include <Register.h>
+#include <Variable.h>
+#include <sstream>
+#include <iostream>
+#include <string>
+#include <iomanip>
+using namespace std;
+
+// Constructor
+DevBoard::DevBoard ( uint linkConfig, uint baseAddress, uint index, Device *parent, uint addrSize) : 
+                        Device(linkConfig,baseAddress,"DevBoard",index,parent) {
+
+   // Description
+   desc_ = "JESD Dev Board";
+
+   //addDevice(new FebCore    ( linkConfig, baseAddress | (0x00000000 >> 2)*addrSize, 0, this, addrSize));
+   addDevice(new Pgp2bAxi   ( linkConfig, baseAddress | (0x00F00000 * (addrSize/4)), 0, this, addrSize));   
+   addDevice(new AxiVersion ( linkConfig, baseAddress | (0x00000000 * (addrSize/4)), 0, this, addrSize));
+
+//    addDevice(new PrbsTx   ( linkConfig, baseAddress | (0x00212000 >> 2)*addrSize, 1, this, addrSize));
+//    addDevice(new PrbsTx   ( linkConfig, baseAddress | (0x00212100 >> 2)*addrSize, 2, this, addrSize));
+
+   //addDevice(new AxiMicronP30(    linkConfig, baseAddress | (0x00800000 >> 2)*addrSize, 0, this, addrSize));
+   
+}
+
+// Deconstructor
+DevBoard::~DevBoard ( ) { }
+
+// Method to process a command
+// void DevBoard::command ( string name, string arg) {
+//    Device::command(name, arg);
+// }
+   
+// // Method to read status registers and update variables
+// void DevBoard::readStatus ( ) {
+//      // Sub devices
+//    Device::readStatus();
+// }
+
+// void DevBoard::readConfig ( ) {
+//    // Sub devices
+//    Device::readConfig();
+// }
+
+// Method to write configuration registers
+void DevBoard::writeConfig ( bool force ) {
+   // Sub devices
+   Device::writeConfig(force);
+}
+
+// void DevBoard::verifyConfig() {
+//    Device::verifyConfig();
+// }
