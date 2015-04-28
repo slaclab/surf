077edc7fc3addcda3257eed49fc452e794ac5be0
ruckman <ruckman@slac.stanford.edu>
2015-04-28 19:57:22 +0000

    Added Ultra Scale support
    git-svn-id: file:///dev/shm/git-devel/svn-repo@7175 0848cbd7-7569-0410-ab05-8cc0a0c67708

--- StdLib/trunk/general/rtl/ClkOutBufSingle.vhd
+++ StdLib/trunk/general/rtl/ClkOutBufSingle.vhd
@@ -5,13 +5,13 @@
 -- Author     : Benjamin Reese  <bareese@slac.stanford.edu>
 -- Company    : SLAC National Accelerator Laboratory
 -- Created    : 2012-12-07
--- Last update: 2013-06-24
+-- Last update: 2015-04-28
 -- Platform   : 
 -- Standard   : VHDL'93/02
 -------------------------------------------------------------------------------
 -- Description: Special buffer for outputting a clock on Xilinx FPGA pins.
 -------------------------------------------------------------------------------
--- Copyright (c) 2012 SLAC National Accelerator Laboratory
+-- Copyright (c) 2015 SLAC National Accelerator Laboratory
 -------------------------------------------------------------------------------
 
 library IEEE;
@@ -23,12 +23,13 @@ use UNISIM.VCOMPONENTS.all;
 
 entity ClkOutBufSingle is
    generic (
+      XIL_DEVICE_G   : string  := "7SERIES";
       RST_POLARITY_G : sl      := '1';
       INVERT_G       : boolean := false);
    port (
-      rstIn  : in  sl := not RST_POLARITY_G;-- Optional reset
+      rstIn  : in  sl := not RST_POLARITY_G;  -- Optional reset
       clkIn  : in  sl;
-      clkOut : out sl);                 -- Single ended output buffer
+      clkOut : out sl);                       -- Single ended output buffer
 end ClkOutBufSingle;
 
 architecture rtl of ClkOutBufSingle is
@@ -38,17 +39,29 @@ architecture rtl of ClkOutBufSingle is
 
 begin
 
-   rst <= rstIn when(RST_POLARITY_G='1') else not(rstIn);
+   rst <= rstIn when(RST_POLARITY_G = '1') else not(rstIn);
 
-   ODDR_I : ODDR
-      port map (
-         C  => clkIn,
-         Q  => clkDdr,
-         CE => '1',
-         D1 => toSl(not INVERT_G),
-         D2 => toSl(INVERT_G),
-         R  => rst,
-         S  => '0');
+   GEN_7SERIES : if (XIL_DEVICE_G = "7SERIES") generate
+      ODDR_I : ODDR
+         port map (
+            C  => clkIn,
+            Q  => clkDdr,
+            CE => '1',
+            D1 => toSl(not INVERT_G),
+            D2 => toSl(INVERT_G),
+            R  => rst,
+            S  => '0');
+   end generate;
+
+   GEN_ULTRA_SCALE : if (XIL_DEVICE_G = "ULTRASCALE") generate
+      ODDR_I : ODDRE1
+         port map (
+            C  => clkIn,
+            Q  => clkDdr,
+            D1 => toSl(not INVERT_G),
+            D2 => toSl(INVERT_G),
+            SR => rst);
+   end generate;
 
    -- Single ended output buffer
    OBUF_I : OBUF
