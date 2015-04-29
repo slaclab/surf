33d2f653360c175f528963236a505ce74c4cd9a7
ulegat <ulegat@slac.stanford.edu>
2015-04-29 17:00:01 +0000

    Removing GT core when in test
    git-svn-id: file:///dev/shm/git-devel/svn-repo@7184 0848cbd7-7569-0410-ab05-8cc0a0c67708
    
    Former-commit-id: f263a5ed0790b5a47282dc3df3752ee760b0945b
    Former-commit-id: 2cbd48298a333b8ab31eeec20bb198b91a26b28e

--- jesd204b/trunk/firmware/modules/Jesd204bCore/rtl/Jesd204bGtx7.vhd
+++ jesd204b/trunk/firmware/modules/Jesd204bCore/rtl/Jesd204bGtx7.vhd
@@ -32,6 +32,9 @@ entity Jesd204bGtx7 is
    generic (
       TPD_G             : time                        := 1 ns;
       
+   -- Test tx module instead of GTX
+      TEST_G            : boolean                     := true;
+      
    -- GT Settings
    ----------------------------------------------------------------------------------------------     
    -- Sim Generics
@@ -158,6 +161,7 @@ begin
    Jesd204b_INST: entity work.Jesd204b
    generic map (
       TPD_G             => TPD_G,
+      TEST_G            => TEST_G,
       AXI_ERROR_RESP_G  => AXI_ERROR_RESP_G,
       AXI_PACKET_SIZE_G => AXI_PACKET_SIZE_G,
       F_G               => F_G,
@@ -185,172 +189,173 @@ begin
    --------------------------------------------------------------------------------------------------
    -- Generate the GTX channels
    --------------------------------------------------------------------------------------------------
-    
-   
-   GTX7_CORE_GEN : for I in (L_G-1) downto 0  generate
-      -- Channel Bonding
-      Bond_Master : if (I = 0) generate
-         rxChBondIn(I) <= "00000";
-      end generate Bond_Master;
-      Bond_Slaves : if (I /= 0) generate
-         rxChBondIn(I) <= rxChBondOut(I-1);
-      end generate Bond_Slaves;
-      
-      -- Generate GT reset from user reset and global reset
-      -- devRst_i - is holding the module in reset for one minute after power-up
-      -- User holds the core in reset when the JESD lane is disabled
-      s_gtReset(I) <= s_gtUserReset(I) or devRst_i;
-      
-      Gtx7Core_Inst : entity work.Gtx7Core
-         generic map (
-            TPD_G                    => TPD_G,
-            SIM_GTRESET_SPEEDUP_G    => SIM_GTRESET_SPEEDUP_G,
-            SIM_VERSION_G            => SIM_VERSION_G,
-            STABLE_CLOCK_PERIOD_G    => STABLE_CLOCK_PERIOD_G,
-            CPLL_REFCLK_SEL_G        => CPLL_REFCLK_SEL_G,
-            CPLL_FBDIV_G             => CPLL_FBDIV_G,
-            CPLL_FBDIV_45_G          => CPLL_FBDIV_45_G,
-            CPLL_REFCLK_DIV_G        => CPLL_REFCLK_DIV_G,
-            RXOUT_DIV_G              => RXOUT_DIV_G,
-            TXOUT_DIV_G              => TXOUT_DIV_G,
-            RX_CLK25_DIV_G           => RX_CLK25_DIV_G,
-            TX_CLK25_DIV_G           => TX_CLK25_DIV_G,
-            PMA_RSV_G                => PMA_RSV_G,
-            TX_PLL_G                 => TX_PLL_G,
-            RX_PLL_G                 => RX_PLL_G,
-            
-            -- Data width
-            TX_EXT_DATA_WIDTH_G      => GT_WORD_SIZE_G*8,
-            TX_INT_DATA_WIDTH_G      => GT_WORD_SIZE_G*8+GT_WORD_SIZE_G*2,
-            TX_8B10B_EN_G            => true,
-            
-            -- Data width
-            RX_EXT_DATA_WIDTH_G      => GT_WORD_SIZE_G*8,
-            RX_INT_DATA_WIDTH_G      => GT_WORD_SIZE_G*8+GT_WORD_SIZE_G*2,
-            RX_8B10B_EN_G            => true,
-            
-       
-            TX_BUF_EN_G              => TX_BUF_EN_G,
-            TX_OUTCLK_SRC_G          => TX_OUTCLK_SRC_G,
-            TX_DLY_BYPASS_G          => TX_DLY_BYPASS_G,
-            TX_PHASE_ALIGN_G         => TX_PHASE_ALIGN_G,
-            TX_BUF_ADDR_MODE_G       => TX_BUF_ADDR_MODE_G,
-            RX_BUF_EN_G              => true,
-            RX_OUTCLK_SRC_G          => "OUTCLKPMA",
-            RX_USRCLK_SRC_G          => "RXOUTCLK",    -- Not 100% sure, doesn't really matter
-            RX_DLY_BYPASS_G          => '1',
-            RX_DDIEN_G               => '0',
-            RX_BUF_ADDR_MODE_G       => "FULL",
-            RX_ALIGN_MODE_G          => "GT",          -- Default
-            ALIGN_COMMA_DOUBLE_G     => "FALSE",       -- Default
-            ALIGN_COMMA_ENABLE_G     => "1111111111",  -- Default
-            ALIGN_COMMA_WORD_G       => 2,             -- Default
-            ALIGN_MCOMMA_DET_G       => "TRUE",
-            ALIGN_MCOMMA_VALUE_G     => "1010000011",  -- Default
-            ALIGN_MCOMMA_EN_G        => '1',
-            ALIGN_PCOMMA_DET_G       => "TRUE",
-            ALIGN_PCOMMA_VALUE_G     => "0101111100",  -- Default
-            ALIGN_PCOMMA_EN_G        => '1',
-            SHOW_REALIGN_COMMA_G     => "FALSE",
-            RXSLIDE_MODE_G           => "AUTO",
-            RX_DISPERR_SEQ_MATCH_G   => "TRUE",        -- Default
-            DEC_MCOMMA_DETECT_G      => "TRUE",        -- Default
-            DEC_PCOMMA_DETECT_G      => "TRUE",        -- Default
-            DEC_VALID_COMMA_ONLY_G   => "FALSE",       -- Default
-            CBCC_DATA_SOURCE_SEL_G   => "DECODED",     -- Default
-            CLK_COR_SEQ_2_USE_G      => "FALSE",       -- Default
-            CLK_COR_KEEP_IDLE_G      => "FALSE",       -- Default
-            CLK_COR_MAX_LAT_G        => 21,
-            CLK_COR_MIN_LAT_G        => 18,
-            CLK_COR_PRECEDENCE_G     => "TRUE",        -- Default
-            CLK_COR_REPEAT_WAIT_G    => 0,             -- Default
-            CLK_COR_SEQ_LEN_G        => 4,
-            CLK_COR_SEQ_1_ENABLE_G   => "1111",        -- Default
-            CLK_COR_SEQ_1_1_G        => "0110111100",
-            CLK_COR_SEQ_1_2_G        => "0100011100",
-            CLK_COR_SEQ_1_3_G        => "0100011100",
-            CLK_COR_SEQ_1_4_G        => "0100011100",
-            CLK_CORRECT_USE_G        => "TRUE",
-            CLK_COR_SEQ_2_ENABLE_G   => "0000",        -- Default
-            CLK_COR_SEQ_2_1_G        => "0000000000",  -- Default
-            CLK_COR_SEQ_2_2_G        => "0000000000",  -- Default
-            CLK_COR_SEQ_2_3_G        => "0000000000",  -- Default
-            CLK_COR_SEQ_2_4_G        => "0000000000",  -- Default
-            RX_CHAN_BOND_EN_G        => true,
-            RX_CHAN_BOND_MASTER_G    => (i = 0),
-            CHAN_BOND_KEEP_ALIGN_G   => "FALSE",       -- Default
-            CHAN_BOND_MAX_SKEW_G     => 10,
-            CHAN_BOND_SEQ_LEN_G      => 1,             -- Default
-            CHAN_BOND_SEQ_1_1_G      => "0110111100",
-            CHAN_BOND_SEQ_1_2_G      => "0111011100",
-            CHAN_BOND_SEQ_1_3_G      => "0111011100",
-            CHAN_BOND_SEQ_1_4_G      => "0111011100",
-            CHAN_BOND_SEQ_1_ENABLE_G => "1111",        -- Default
-            CHAN_BOND_SEQ_2_1_G      => "0000000000",  -- Default
-            CHAN_BOND_SEQ_2_2_G      => "0000000000",  -- Default
-            CHAN_BOND_SEQ_2_3_G      => "0000000000",  -- Default
-            CHAN_BOND_SEQ_2_4_G      => "0000000000",  -- Default
-            CHAN_BOND_SEQ_2_ENABLE_G => "0000",        -- Default
-            CHAN_BOND_SEQ_2_USE_G    => "FALSE",       -- Default
-            FTS_DESKEW_SEQ_ENABLE_G  => "1111",        -- Default
-            FTS_LANE_DESKEW_CFG_G    => "1111",        -- Default
-            FTS_LANE_DESKEW_EN_G     => "FALSE",       -- Default
-            RX_OS_CFG_G              => RX_OS_CFG_G,
-            RXCDR_CFG_G              => RXCDR_CFG_G,
-            RX_EQUALIZER_G           => "DFE",         -- Xilinx recommends this for 8b10b
-            RXDFEXYDEN_G             => RXDFEXYDEN_G,
-            RX_DFE_KL_CFG2_G         => RX_DFE_KL_CFG2_G)
-         port map (
-            stableClkIn      => stableClk,
-            cPllRefClkIn     => '0',
-            cPllLockOut      => open,
-            
-            qPllRefClkIn     => qPllRefClkIn,
-            qPllClkIn        => qPllClkIn,
-            qPllLockIn       => qPllLockIn,
-            qPllRefClkLostIn => qPllRefClkLostIn,
-            qPllResetOut     => qPllResetOut(I),
-             
-            gtRxRefClkBufg   => stableClk,  -- TODO check        
-            
-            
-            gtTxP            => gtTxP(I),
-            gtTxN            => gtTxN(I),
-            gtRxP            => gtRxP(I),
-            gtRxN            => gtRxN(I),
-            
-            rxOutClkOut      => open,
-            rxUsrClkIn       => devClk_i,
-            rxUsrClk2In      => devClk2_i,
-            rxUserRdyOut     => open,
-            rxMmcmResetOut   => open,
-            rxMmcmLockedIn   => '1',
-            rxUserResetIn    => s_gtReset(I),
-            rxResetDoneOut   => r_jesdGtRxArr(I).rstDone,
-            rxDataValidIn    => '1',
-            rxSlideIn        => '0',
-            rxDataOut        => r_jesdGtRxArr(I).data,
-            rxCharIsKOut     => r_jesdGtRxArr(I).dataK,
-            rxDecErrOut      => r_jesdGtRxArr(I).decErr,
-            rxDispErrOut     => r_jesdGtRxArr(I).dispErr,
-            rxPolarityIn     => '0',
-            rxBufStatusOut   => open,
-            rxChBondLevelIn  => slv(to_unsigned((L_G-1-I), 3)),
-            rxChBondIn       => rxChBondIn(I),
-            rxChBondOut      => rxChBondOut(I),
-            txOutClkOut      => open,
-            txUsrClkIn       => '0',
-            txUsrClk2In      => '0',
-            txUserRdyOut     => open,
-            txMmcmResetOut   => open,
-            txMmcmLockedIn   => '1',
-            txUserResetIn    => '1',
-            txResetDoneOut   => open,
-            txDataIn         => (r_jesdGtRxArr(I).data'range  => '0'),
-            txCharIsKIn      => (r_jesdGtRxArr(I).dataK'range => '0'),
-            txBufStatusOut   => open,
-            loopbackIn       => "000"
-        );
-    end generate GTX7_CORE_GEN;
+   GT_OPER_GEN: if TEST_G = false generate
+      GTX7_CORE_GEN : for I in (L_G-1) downto 0  generate
+         -- Channel Bonding
+         Bond_Master : if (I = 0) generate
+            rxChBondIn(I) <= "00000";
+         end generate Bond_Master;
+         Bond_Slaves : if (I /= 0) generate
+            rxChBondIn(I) <= rxChBondOut(I-1);
+         end generate Bond_Slaves;
+         
+         -- Generate GT reset from user reset and global reset
+         -- devRst_i - is holding the module in reset for one minute after power-up
+         -- User holds the core in reset when the JESD lane is disabled
+         s_gtReset(I) <= s_gtUserReset(I) or devRst_i;
+         
+         Gtx7Core_Inst : entity work.Gtx7Core
+            generic map (
+               TPD_G                    => TPD_G,
+               SIM_GTRESET_SPEEDUP_G    => SIM_GTRESET_SPEEDUP_G,
+               SIM_VERSION_G            => SIM_VERSION_G,
+               STABLE_CLOCK_PERIOD_G    => STABLE_CLOCK_PERIOD_G,
+               CPLL_REFCLK_SEL_G        => CPLL_REFCLK_SEL_G,
+               CPLL_FBDIV_G             => CPLL_FBDIV_G,
+               CPLL_FBDIV_45_G          => CPLL_FBDIV_45_G,
+               CPLL_REFCLK_DIV_G        => CPLL_REFCLK_DIV_G,
+               RXOUT_DIV_G              => RXOUT_DIV_G,
+               TXOUT_DIV_G              => TXOUT_DIV_G,
+               RX_CLK25_DIV_G           => RX_CLK25_DIV_G,
+               TX_CLK25_DIV_G           => TX_CLK25_DIV_G,
+               PMA_RSV_G                => PMA_RSV_G,
+               TX_PLL_G                 => TX_PLL_G,
+               RX_PLL_G                 => RX_PLL_G,
+               
+               -- Data width
+               TX_EXT_DATA_WIDTH_G      => GT_WORD_SIZE_G*8,
+               TX_INT_DATA_WIDTH_G      => GT_WORD_SIZE_G*8+GT_WORD_SIZE_G*2,
+               TX_8B10B_EN_G            => true,
+               
+               -- Data width
+               RX_EXT_DATA_WIDTH_G      => GT_WORD_SIZE_G*8,
+               RX_INT_DATA_WIDTH_G      => GT_WORD_SIZE_G*8+GT_WORD_SIZE_G*2,
+               RX_8B10B_EN_G            => true,
+               
+          
+               TX_BUF_EN_G              => TX_BUF_EN_G,
+               TX_OUTCLK_SRC_G          => TX_OUTCLK_SRC_G,
+               TX_DLY_BYPASS_G          => TX_DLY_BYPASS_G,
+               TX_PHASE_ALIGN_G         => TX_PHASE_ALIGN_G,
+               TX_BUF_ADDR_MODE_G       => TX_BUF_ADDR_MODE_G,
+               RX_BUF_EN_G              => true,
+               RX_OUTCLK_SRC_G          => "OUTCLKPMA",
+               RX_USRCLK_SRC_G          => "RXOUTCLK",    -- Not 100% sure, doesn't really matter
+               RX_DLY_BYPASS_G          => '1',
+               RX_DDIEN_G               => '0',
+               RX_BUF_ADDR_MODE_G       => "FULL",
+               RX_ALIGN_MODE_G          => "GT",          -- Default
+               ALIGN_COMMA_DOUBLE_G     => "FALSE",       -- Default
+               ALIGN_COMMA_ENABLE_G     => "1111111111",  -- Default
+               ALIGN_COMMA_WORD_G       => 2,             -- Default
+               ALIGN_MCOMMA_DET_G       => "TRUE",
+               ALIGN_MCOMMA_VALUE_G     => "1010000011",  -- Default
+               ALIGN_MCOMMA_EN_G        => '1',
+               ALIGN_PCOMMA_DET_G       => "TRUE",
+               ALIGN_PCOMMA_VALUE_G     => "0101111100",  -- Default
+               ALIGN_PCOMMA_EN_G        => '1',
+               SHOW_REALIGN_COMMA_G     => "FALSE",
+               RXSLIDE_MODE_G           => "AUTO",
+               RX_DISPERR_SEQ_MATCH_G   => "TRUE",        -- Default
+               DEC_MCOMMA_DETECT_G      => "TRUE",        -- Default
+               DEC_PCOMMA_DETECT_G      => "TRUE",        -- Default
+               DEC_VALID_COMMA_ONLY_G   => "FALSE",       -- Default
+               CBCC_DATA_SOURCE_SEL_G   => "DECODED",     -- Default
+               CLK_COR_SEQ_2_USE_G      => "FALSE",       -- Default
+               CLK_COR_KEEP_IDLE_G      => "FALSE",       -- Default
+               CLK_COR_MAX_LAT_G        => 21,
+               CLK_COR_MIN_LAT_G        => 18,
+               CLK_COR_PRECEDENCE_G     => "TRUE",        -- Default
+               CLK_COR_REPEAT_WAIT_G    => 0,             -- Default
+               CLK_COR_SEQ_LEN_G        => 4,
+               CLK_COR_SEQ_1_ENABLE_G   => "1111",        -- Default
+               CLK_COR_SEQ_1_1_G        => "0110111100",
+               CLK_COR_SEQ_1_2_G        => "0100011100",
+               CLK_COR_SEQ_1_3_G        => "0100011100",
+               CLK_COR_SEQ_1_4_G        => "0100011100",
+               CLK_CORRECT_USE_G        => "TRUE",
+               CLK_COR_SEQ_2_ENABLE_G   => "0000",        -- Default
+               CLK_COR_SEQ_2_1_G        => "0000000000",  -- Default
+               CLK_COR_SEQ_2_2_G        => "0000000000",  -- Default
+               CLK_COR_SEQ_2_3_G        => "0000000000",  -- Default
+               CLK_COR_SEQ_2_4_G        => "0000000000",  -- Default
+               RX_CHAN_BOND_EN_G        => true,
+               RX_CHAN_BOND_MASTER_G    => (i = 0),
+               CHAN_BOND_KEEP_ALIGN_G   => "FALSE",       -- Default
+               CHAN_BOND_MAX_SKEW_G     => 10,
+               CHAN_BOND_SEQ_LEN_G      => 1,             -- Default
+               CHAN_BOND_SEQ_1_1_G      => "0110111100",
+               CHAN_BOND_SEQ_1_2_G      => "0111011100",
+               CHAN_BOND_SEQ_1_3_G      => "0111011100",
+               CHAN_BOND_SEQ_1_4_G      => "0111011100",
+               CHAN_BOND_SEQ_1_ENABLE_G => "1111",        -- Default
+               CHAN_BOND_SEQ_2_1_G      => "0000000000",  -- Default
+               CHAN_BOND_SEQ_2_2_G      => "0000000000",  -- Default
+               CHAN_BOND_SEQ_2_3_G      => "0000000000",  -- Default
+               CHAN_BOND_SEQ_2_4_G      => "0000000000",  -- Default
+               CHAN_BOND_SEQ_2_ENABLE_G => "0000",        -- Default
+               CHAN_BOND_SEQ_2_USE_G    => "FALSE",       -- Default
+               FTS_DESKEW_SEQ_ENABLE_G  => "1111",        -- Default
+               FTS_LANE_DESKEW_CFG_G    => "1111",        -- Default
+               FTS_LANE_DESKEW_EN_G     => "FALSE",       -- Default
+               RX_OS_CFG_G              => RX_OS_CFG_G,
+               RXCDR_CFG_G              => RXCDR_CFG_G,
+               RX_EQUALIZER_G           => "DFE",         -- Xilinx recommends this for 8b10b
+               RXDFEXYDEN_G             => RXDFEXYDEN_G,
+               RX_DFE_KL_CFG2_G         => RX_DFE_KL_CFG2_G)
+            port map (
+               stableClkIn      => stableClk,
+               cPllRefClkIn     => '0',
+               cPllLockOut      => open,
+               
+               qPllRefClkIn     => qPllRefClkIn,
+               qPllClkIn        => qPllClkIn,
+               qPllLockIn       => qPllLockIn,
+               qPllRefClkLostIn => qPllRefClkLostIn,
+               qPllResetOut     => qPllResetOut(I),
+                
+               gtRxRefClkBufg   => stableClk,  -- TODO check        
+               
+               
+               gtTxP            => gtTxP(I),
+               gtTxN            => gtTxN(I),
+               gtRxP            => gtRxP(I),
+               gtRxN            => gtRxN(I),
+               
+               rxOutClkOut      => open,
+               rxUsrClkIn       => devClk_i,
+               rxUsrClk2In      => devClk2_i,
+               rxUserRdyOut     => open,
+               rxMmcmResetOut   => open,
+               rxMmcmLockedIn   => '1',
+               rxUserResetIn    => s_gtReset(I),
+               rxResetDoneOut   => r_jesdGtRxArr(I).rstDone,
+               rxDataValidIn    => '1',
+               rxSlideIn        => '0',
+               rxDataOut        => r_jesdGtRxArr(I).data,
+               rxCharIsKOut     => r_jesdGtRxArr(I).dataK,
+               rxDecErrOut      => r_jesdGtRxArr(I).decErr,
+               rxDispErrOut     => r_jesdGtRxArr(I).dispErr,
+               rxPolarityIn     => '0',
+               rxBufStatusOut   => open,
+               rxChBondLevelIn  => slv(to_unsigned((L_G-1-I), 3)),
+               rxChBondIn       => rxChBondIn(I),
+               rxChBondOut      => rxChBondOut(I),
+               txOutClkOut      => open,
+               txUsrClkIn       => '0',
+               txUsrClk2In      => '0',
+               txUserRdyOut     => open,
+               txMmcmResetOut   => open,
+               txMmcmLockedIn   => '1',
+               txUserResetIn    => '1',
+               txResetDoneOut   => open,
+               txDataIn         => (r_jesdGtRxArr(I).data'range  => '0'),
+               txCharIsKIn      => (r_jesdGtRxArr(I).dataK'range => '0'),
+               txBufStatusOut   => open,
+               loopbackIn       => "000"
+           );
+       end generate GTX7_CORE_GEN;
+   -----------------------------------------
+   end generate GT_OPER_GEN;    
    -----------------------------------------------------
 end rtl;
