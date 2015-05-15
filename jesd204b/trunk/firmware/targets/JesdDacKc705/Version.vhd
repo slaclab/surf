f8d245c40eff8dbaa6268b72712434414821e5b8
ulegat <ulegat@slac.stanford.edu>
2015-05-15 00:44:32 +0000

    Adding DAC target
    git-svn-id: file:///dev/shm/git-devel/svn-repo@7267 0848cbd7-7569-0410-ab05-8cc0a0c67708

--- jesd204b/trunk/firmware/targets/JesdDacKc705/Version.vhd
+++ jesd204b/trunk/firmware/targets/JesdDacKc705/Version.vhd
@@ -18,7 +18,7 @@ package Version is
 
 constant FPGA_VERSION_C : std_logic_vector(31 downto 0) := x"00000000"; -- MAKE_VERSION
 
-constant BUILD_STAMP_C : string := "JesdAdcKc705: Vivado v2014.4 (x86_64) Built Thu May 14 15:38:31 PDT 2015 by ulegat";
+constant BUILD_STAMP_C : string := "JesdDacKc705: Vivado v2014.4 (x86_64) Built Thu May 14 17:00:44 PDT 2015 by ulegat";
 
 end Version;
  
