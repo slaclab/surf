7a01b35f2bef5522d596f37a45a3f11f7585af59
bareese <bareese@slac.stanford.edu>
2015-05-18 23:42:37 +0000

    Added debug lines for chipscope and rebuilt image.
    git-svn-id: file:///dev/shm/git-devel/svn-repo@7278 0848cbd7-7569-0410-ab05-8cc0a0c67708
    
    Former-commit-id: 4c533699e2a7f5a3954a6c47b4782d2bfd5f4ace
    Former-commit-id: ead1436b5024979dc290c7a17affa8f3241873b9

--- jesd204b/trunk/firmware/targets/JesdAdcKc705/Version.vhd
+++ jesd204b/trunk/firmware/targets/JesdAdcKc705/Version.vhd
@@ -16,9 +16,9 @@ USE ieee.std_logic_1164.ALL;
 
 package Version is
 
-constant FPGA_VERSION_C : std_logic_vector(31 downto 0) := x"00000004"; -- MAKE_VERSION
+constant FPGA_VERSION_C : std_logic_vector(31 downto 0) := x"00000005"; -- MAKE_VERSION
 
-constant BUILD_STAMP_C : string := "JesdAdcKc705: Vivado v2014.4 (x86_64) Built Mon May 18 14:16:33 PDT 2015 by ulegat";
+constant BUILD_STAMP_C : string := "JesdAdcKc705: Vivado v2014.4 (x86_64) Built Mon May 18 15:46:12 PDT 2015 by bareese";
 
 end Version;
  
@@ -29,4 +29,5 @@ end Version;
 -- 05/15/2015 - 00000001      - ADC F22 61.44MHz out. 184.32MHz ref 7.3728GHz
 -- 05/18/2015 - 00000002      - ADC F22 61.44MHz out. 184.32MHz ref 7.3728GHz. Pol = '1'
 -- 05/18/2015 - 00000003      - ADC F22 61.44MHz out. 184.32MHz ref 7.3728GHz. Pol = '1', byte swapped
--- 05/18/2015 - 00000003      - ADC F22 61.44MHz out. 184.32MHz ref 7.3728GHz. Pol = '0', byte swapped
+-- 05/18/2015 - 00000004      - ADC F22 61.44MHz out. 184.32MHz ref 7.3728GHz. Pol = '0', byte swapped
+-- 05/18/2015 - 00000005      - Chipscope test
