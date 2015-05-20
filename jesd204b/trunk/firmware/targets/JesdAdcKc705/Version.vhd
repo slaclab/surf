1d6df3f0b36415557cba1cc67a2e6ef2fbce4310
ulegat <ulegat@slac.stanford.edu>
2015-05-20 20:30:24 +0000

    dual lane 2-byte CPLL FPGA Clock reference out - jesdClk to ADC
    git-svn-id: file:///dev/shm/git-devel/svn-repo@7296 0848cbd7-7569-0410-ab05-8cc0a0c67708
    
    Former-commit-id: 90e15953583f7f7b77518de407d981de7f33c744
    Former-commit-id: 19ea6548334b637bb47388982932944bbd037cdb

--- jesd204b/trunk/firmware/targets/JesdAdcKc705/Version.vhd
+++ jesd204b/trunk/firmware/targets/JesdAdcKc705/Version.vhd
@@ -16,9 +16,9 @@ USE ieee.std_logic_1164.ALL;
 
 package Version is
 
-constant FPGA_VERSION_C : std_logic_vector(31 downto 0) := x"0000000D"; -- MAKE_VERSION
+constant FPGA_VERSION_C : std_logic_vector(31 downto 0) := x"0000000F; -- MAKE_VERSION
 
-constant BUILD_STAMP_C : string := "JesdAdcKc705: Vivado v2014.4 (x86_64) Built Wed May 20 12:44:31 PDT 2015 by ulegat";
+constant BUILD_STAMP_C : string := "JesdAdcKc705: Vivado v2014.4 (x86_64) Built Wed May 20 13:18:16 PDT 2015 by ulegat";
 
 end Version;
  
@@ -38,4 +38,5 @@ end Version;
 -- 05/19/2015 - 0000000A      - dual lane - out clk reset tied to '0'
 -- 05/19/2015 - 0000000B      - CPLL 2-byte
 -- 05/19/2015 - 0000000C      - CPLL 2-byte - GT parameters as in PGP
--- 05/19/2015 - 0000000D      - CPLL 2-byte - FPGA refclk to ADC
+-- 05/20/2015 - 0000000D      - CPLL 2-byte - FPGA refclk to ADC
+-- 05/20/2015 - 0000000E      - JESD out
