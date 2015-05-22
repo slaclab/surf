d8ce73c741f26450b04bc7e7201cd5a463ba5080
ulegat <ulegat@slac.stanford.edu>
2015-05-22 18:49:55 +0000

    Clk from FPGA to ADC Through LMK
    git-svn-id: file:///dev/shm/git-devel/svn-repo@7305 0848cbd7-7569-0410-ab05-8cc0a0c67708
    
    Former-commit-id: 18fcfd5833f3f8974552644cfaa713b96a228fb0
    Former-commit-id: 0ed1a9c107ed0ccc3170eb8a663496ccaef94945

--- jesd204b/trunk/firmware/targets/JesdAdcKc705/Version.vhd
+++ jesd204b/trunk/firmware/targets/JesdAdcKc705/Version.vhd
@@ -16,9 +16,9 @@ USE ieee.std_logic_1164.ALL;
 
 package Version is
 
-constant FPGA_VERSION_C : std_logic_vector(31 downto 0) := x"00000010"; -- MAKE_VERSION
+constant FPGA_VERSION_C : std_logic_vector(31 downto 0) := x"00000012"; -- MAKE_VERSION
 
-constant BUILD_STAMP_C : string := "JesdAdcKc705: Vivado v2014.4 (x86_64) Built Thu May 21 14:01:19 PDT 2015 by ulegat";
+constant BUILD_STAMP_C : string := "JesdAdcKc705: Vivado v2014.4 (x86_64) Built Fri May 22 11:20:17 PDT 2015 by ulegat";
 
 end Version;
  
@@ -42,4 +42,6 @@ end Version;
 -- 05/20/2015 - 0000000E      - JESD out /2
 -- 05/20/2015 - 0000000F      - JESD out /2
 -- 05/20/2015 - 00000010      - Recovered clock
+-- 05/20/2015 - 00000011      - 2-byte - LMK reference
+-- 05/20/2015 - 00000012      - 2-byte - F2 - Dual lane
 
