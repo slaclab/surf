7f03a8c410d06eab2e4624929251b824e006b09c
ulegat <ulegat@slac.stanford.edu>
2015-06-10 18:36:50 +0000

    adding the Rx ultra+cleanup
    git-svn-id: file:///dev/shm/git-devel/svn-repo@7426 0848cbd7-7569-0410-ab05-8cc0a0c67708
    
    Former-commit-id: 5850583e51c63045f901336e48a859d291d9b1c3
    Former-commit-id: 051be26fecdf3b020788465e3a3f956dce747601

--- jesd204b/trunk/firmware/modules/Jesd204bCore/rtl/Jesd204bRxGthUltra.vhd
+++ jesd204b/trunk/firmware/modules/Jesd204bCore/rtl/Jesd204bRxGthUltra.vhd
@@ -1,7 +1,7 @@
 -------------------------------------------------------------------------------
--- Title      : JESD204b module containing the gth ultrascale MGT transmitter modules
+-- Title      : JESD204b receiver module containing the GTH Ultrascale MGT
 -------------------------------------------------------------------------------
--- File       : Jesd204bTxGthUltra.vhd
+-- File       : Jesd204bRxGthUltra.vhd
 -- Author     : Uros Legat  <ulegat@slac.stanford.edu>
 -- Company    : SLAC National Accelerator Laboratory (Cosylab)
 -- Created    : 2015-04-14
@@ -9,12 +9,12 @@
 -- Platform   : 
 -- Standard   : VHDL'93/02
 -------------------------------------------------------------------------------
--- Description: Wrapper module for JESD.
+-- Description: Wrapper module for JESD receiver.
 --              GTH coregen generated core 2 GTH modules
 --              Note: Intended only for two serial lanes L_G=2.
 --                    7.4 GHz lane rate and 370MHz reference, Freerunning clk 185 MHz
 --                    If different amount of lanes or freq is required the Core has to be regenerated 
---                    by Xilinx Coregen. 
+--                    by Xilinx Coregen.               
 -------------------------------------------------------------------------------
 -- Copyright (c) 2015 SLAC National Accelerator Laboratory
 -------------------------------------------------------------------------------
@@ -33,21 +33,18 @@ use work.AxiStreamPkg.all;
 use work.SsiPkg.all;
 use work.Jesd204bPkg.all;
 
-entity Jesd204bTxGthUltra is
+entity Jesd204bRxGthUltra is
    generic (
       TPD_G             : time                        := 1 ns;
-           
+      
+   -- Test tx module instead of GTX
+      TEST_G            : boolean                     := false;
+      
       -- Internal SYSREF SYSREF_GEN_G= TRUE else 
       -- External SYSREF
-      SYSREF_GEN_G       : boolean                    := false; 
+      SYSREF_GEN_G        : boolean                    := false; 
       
-      -- Simulation disconnect the GTX
-      SIM_G              : boolean                    := false; 
-      
-      
-   -- GT Settings
-   ----------------------------------------------------------------------------------------------     
-    -- AXI Lite and AXI stream generics
+   -- AXI Lite and AXI stream generics
    ----------------------------------------------------------------------------------------------
       AXI_ERROR_RESP_G  : slv(1 downto 0)             := AXI_RESP_SLVERR_C;
 
@@ -60,11 +57,10 @@ entity Jesd204bTxGthUltra is
 
    port (
    -- GT Interface
-   ----------------------------------------------------------------------------------------------
+   ----------------------------------------------------------------------------------------------     
       -- GT Clocking
-      stableClk        : in  sl;                      -- GT needs a stable clock to "boot up"(buffered refClkDiv2)
+      stableClk        : in  sl;                      -- GT needs a stable clock to "boot up"(buffered refClkDiv2) 
       refClk           : in  sl;                      -- GT Reference clock directly from GT GTH diff. input buffer   
-      
       -- Gt Serial IO
       gtTxP            : out slv(L_G-1 downto 0);         -- GT Serial Transmit Positive
       gtTxN            : out slv(L_G-1 downto 0);         -- GT Serial Transmit Negative
@@ -81,42 +77,38 @@ entity Jesd204bTxGthUltra is
    ------------------------------------------------------------------------------------------------   
       axiClk         : in    sl;
       axiRst         : in    sl;  
-           
-      -- AXI-Lite RX Register Interface
-      axilReadMasterTx  : in    AxiLiteReadMasterType;
-      axilReadSlaveTx   : out   AxiLiteReadSlaveType;
-      axilWriteMasterTx : in    AxiLiteWriteMasterType;
-      axilWriteSlaveTx  : out   AxiLiteWriteSlaveType;
+      
+      -- AXI-Lite Register Interface
+      axilReadMaster  : in    AxiLiteReadMasterType;
+      axilReadSlave   : out   AxiLiteReadSlaveType;
+      axilWriteMaster : in    AxiLiteWriteMasterType;
+      axilWriteSlave  : out   AxiLiteWriteSlaveType;
       
       -- AXI Streaming Interface
-      rxAxisMasterArr_i : in  AxiStreamMasterArray(L_G-1 downto 0);
-      rxAxisSlaveArr_o  : out AxiStreamSlaveArray(L_G-1 downto 0); 
-
-      -- External sample data input
-      extSampleDataArray_i : in sampleDataArray(L_G-1 downto 0);      
+      txAxisMasterArr : out   AxiStreamMasterArray(L_G-1 downto 0);
+      txCtrlArr       : in    AxiStreamCtrlArray(L_G-1 downto 0);   
       
    -- JESD
    ------------------------------------------------------------------------------------------------   
 
       -- SYSREF for subcalss 1 fixed latency
-      sysRef_i       : in  sl;
+      sysRef_i       : in    sl;
       
       -- SYSREF out when it is generated internally SYSREF_GEN_G=True     
       sysRef_o       : out    sl;
-      
-      -- Synchronisation output combined from all receivers 
-      nSync_i        : in  sl;
 
+      -- Synchronisation output combined from all receivers 
+      nSync_o        : out   sl;
+      
       -- Out to led
       leds_o    : out   slv(1 downto 0);
       
       -- Out to led     
-      qPllLock_o : out sl
+      qPllLock_o : out sl      
    );
-end Jesd204bTxGthUltra;
+end Jesd204bRxGthUltra;
 
-architecture rtl of Jesd204bTxGthUltra is
-
+architecture rtl of Jesd204bRxGthUltra is
 ---------------------------------------   
    component gthultrascalejesdcoregen
       port (
@@ -170,69 +162,64 @@ architecture rtl of Jesd204bTxGthUltra is
    end component;
 -------------------------------------
 
--- Internal signals   
-   signal r_jesdGtTxArr : jesdGtTxLaneTypeArray(L_G-1 downto 0);  
-   
+-- Internal signals
+   signal r_jesdGtRxArr : jesdGtRxLaneTypeArray(L_G-1 downto 0);       
+
    -- Rx Channel Bonding
    -- signal rxChBondLevel : slv(2 downto 0);
    signal rxChBondIn    : Slv5Array(L_G-1 downto 0);
    signal rxChBondOut   : Slv5Array(L_G-1 downto 0);
 
    -- GT reset
-   signal s_gtRxUserReset   : slv(L_G-1 downto 0);
-   signal s_gtxRst          : sl;
-
-   signal s_gtTxUserReset   : slv(L_G-1 downto 0);
-   signal s_gtTxReset       : slv(L_G-1 downto 0); 
-   -- 
-   signal s_gtTxReady       : slv(L_G-1 downto 0);
+   signal s_gtUserReset   : slv(L_G-1 downto 0);
+   signal s_gtReset       : sl;
    
    -- Generated or external
    signal s_sysRef      : sl;
-   
+
    -- GT signals
-   signal s_data  : slv(63 downto 0);
-   signal s_dataK : slv(15 downto 0);
+   signal s_rxctrl0 : slv(31 downto 0);
+   signal s_rxctrl1 : slv(31 downto 0);
+   signal s_rxctrl2 : slv(15 downto 0);
+   signal s_rxctrl3 : slv(15 downto 0);
+
+   signal s_data  : slv(63 downto 0);   
+
    signal s_devClkVec : slv(1 downto 0);
    signal s_devClk2Vec : slv(1 downto 0);
    
-   signal s_txDone : sl;
+   signal s_rxDone : sl;
    
-
 begin
-   -- Check generics TODO add others
-   assert (1 <= L_G and L_G <= 8)                      report "L_G must be between 1 and 8"   severity failure;
-
+ 
    --------------------------------------------------------------------------------------------------
-   -- JESD transmitter core
+   -- JESD receiver core
    --------------------------------------------------------------------------------------------------  
-   Jesd204bTx_INST: entity work.Jesd204bTx
+   Jesd204b_INST: entity work.Jesd204bRx
    generic map (
-      TPD_G            => TPD_G,
-      AXI_ERROR_RESP_G => AXI_ERROR_RESP_G,
-      F_G              => F_G,
-      K_G              => K_G,
-      L_G              => L_G)
+      TPD_G             => TPD_G,
+      TEST_G            => TEST_G,
+      AXI_ERROR_RESP_G  => AXI_ERROR_RESP_G,
+      F_G               => F_G,
+      K_G               => K_G,
+      L_G               => L_G)
    port map (
       axiClk            => axiClk,
       axiRst            => axiRst,
-      axilReadMaster    => axilReadMasterTx,
-      axilReadSlave     => axilReadSlaveTx,
-      axilWriteMaster   => axilWriteMasterTx,
-      axilWriteSlave    => axilWriteSlaveTx,
-      rxAxisMasterArr_i => rxAxisMasterArr_i,
-      rxAxisSlaveArr_o  => rxAxisSlaveArr_o,
-      extSampleDataArray_i => extSampleDataArray_i,
+      axilReadMaster    => axilReadMaster,
+      axilReadSlave     => axilReadSlave,
+      axilWriteMaster   => axilWriteMaster,
+      axilWriteSlave    => axilWriteSlave,
+      txAxisMasterArr_o => txAxisMasterArr,
+      txCtrlArr_i       => txCtrlArr,
       devClk_i          => devClk_i,
       devRst_i          => devRst_i,
       sysRef_i          => s_sysRef,
-      nSync_i           => nSync_i,
-      gtTxReady_i       => s_gtTxReady,
-      gtTxReset_o       => s_gtTxUserReset,
-      r_jesdGtTxArr     => r_jesdGtTxArr,
+      r_jesdGtRxArr     => r_jesdGtRxArr,
+      gt_reset_o        => s_gtUserReset,
+      nSync_o           => nSync_o,
       leds_o            => leds_o
    );
-
    --------------------------------------------------------------------------------------------------
    -- Generate the internal or external SYSREF depending on SYSREF_GEN_G
    --------------------------------------------------------------------------------------------------
@@ -261,21 +248,36 @@ begin
    end generate OPER_GEN;
    
    --------------------------------------------------------------------------------------------------
-   -- GTH signals. Only for L_G = 2
+   -- GTH signals assignments. Only for L_G = 2
    --------------------------------------------------------------------------------------------------
-   s_gtxRst <= devRst_i or uOr(s_gtTxUserReset);
-   s_data  <= r_jesdGtTxArr(1).data & r_jesdGtTxArr(0).data;
-   s_dataK <= x"0" & r_jesdGtTxArr(1).dataK & X"0" & r_jesdGtTxArr(0).dataK;
+   s_gtReset <= devRst_i or uOr(s_gtUserReset);
+
+   r_jesdGtRxArr(0).data      <= s_data(31 downto 0);
+   r_jesdGtRxArr(1).data      <= s_data(63 downto 32);
+   
+   r_jesdGtRxArr(0).dataK     <= s_rxctrl0(3  downto  0);
+   r_jesdGtRxArr(1).dataK     <= s_rxctrl0(19 downto 16);
+   
+   r_jesdGtRxArr(0).dispErr   <= s_rxctrl1(3  downto  0);   
+   r_jesdGtRxArr(1).dispErr   <= s_rxctrl1(19 downto 16);   
+   
+   r_jesdGtRxArr(0).decErr   <= s_rxctrl3(3  downto  0);   
+   r_jesdGtRxArr(1).decErr   <= s_rxctrl3(12 downto  9);
+
+   r_jesdGtRxArr(0).rstDone  <= s_rxDone;  
+   r_jesdGtRxArr(1).rstDone  <= s_rxDone;
+   
    s_devClkVec   <= devClk_i & devClk_i;
    s_devClk2Vec  <= devClk2_i & devClk2_i;
    s_gtTxReady   <= s_txDone & s_txDone;
+   
    -- debug
-   qPllLock_o <= s_txDone;
-
+   --qPllLock_o <= s_rxDone;
+      
    --------------------------------------------------------------------------------------------------
    -- Include Core from Coregen Vivado 15.1 
    --------------------------------------------------------------------------------------------------
-   --GT_OPER_GEN: if SIM_G = false generate
+   GT_OPER_GEN: if TEST_G = false generate
       GthUltrascaleJesdCoregen_INST: GthUltrascaleJesdCoregen
       port map (
          -- Clocks
@@ -283,33 +285,27 @@ begin
          gtwiz_userclk_rx_active_in(0)        => '1',
          gtwiz_reset_clk_freerun_in(0)        => stableClk,
          
-         gtwiz_reset_all_in(0)                   => s_gtxRst,
-         gtwiz_reset_tx_pll_and_datapath_in(0)   => s_gtxRst,
-         gtwiz_reset_tx_datapath_in(0)           => s_gtxRst,
-         gtwiz_reset_rx_pll_and_datapath_in(0)   => s_gtxRst,
-         gtwiz_reset_rx_datapath_in(0)           => s_gtxRst,
+         gtwiz_reset_all_in(0)                   => s_gtReset,
+         gtwiz_reset_tx_pll_and_datapath_in(0)   => s_gtReset,
+         gtwiz_reset_tx_datapath_in(0)           => s_gtReset,
+         gtwiz_reset_rx_pll_and_datapath_in(0)   => s_gtReset,
+         gtwiz_reset_rx_datapath_in(0)           => s_gtReset,
          gtwiz_reset_rx_cdr_stable_out        => open,
-         gtwiz_reset_tx_done_out(0)           => s_txDone,
-         gtwiz_reset_rx_done_out              => open,
-         gtwiz_userdata_tx_in                 => s_data,
-         gtwiz_userdata_rx_out                => open,
+         gtwiz_reset_tx_done_out              => open,
+         gtwiz_reset_rx_done_out(0)           => s_rxDone,
+         gtwiz_userdata_tx_in                 => open,
+         gtwiz_userdata_rx_out                => s_data,
          gtrefclk00_in(0)                     => refClk,
          qpll0outclk_out                      => open,
          qpll0outrefclk_out                   => open,
          gthrxn_in                            => gtRxN,
          gthrxp_in                            => gtRxP,
-         qpll0lock_out                        => open,--qPllLock_o,
-         rx8b10ben_in                         => "11",
-         rxcommadeten_in                      => "11",
-         rxmcommaalignen_in                   => "11",
-         rxpcommaalignen_in                   => "11",
-         rxpolarity_in                        => "11",  -- Changed to '1' after receiving weird data (sometimes ok sometimes wrong)
-         rxusrclk_in                          => s_devClkVec,
-         rxusrclk2_in                         => s_devClk2Vec,
+         qpll0lock_out                        => qPllLock_o,
+
          tx8b10ben_in                         => "11",
          txctrl0_in                           => X"0000_0000",
          txctrl1_in                           => X"0000_0000",
-         txctrl2_in                           => s_dataK,
+         txctrl2_in                           => X"0000",
          txpolarity_in                        => "00",
          txusrclk_in                          => s_devClkVec,
          txusrclk2_in                         => s_devClk2Vec,
@@ -318,18 +314,26 @@ begin
          txoutclk_out                         => open,
          txpmaresetdone_out                   => open,
          
-         -- RX settings 
+         -- RX settings
+         rx8b10ben_in                         => "11",
+         rxcommadeten_in                      => "11",
+         rxmcommaalignen_in                   => "11",
+         rxpcommaalignen_in                   => "11",
+         rxpolarity_in                        => "11",  -- TODO Check Changed to '1' after receiving weird data (sometimes ok sometimes wrong)
+         rxusrclk_in                          => s_devClkVec,
+         rxusrclk2_in                         => s_devClk2Vec,
+
          rxbyteisaligned_out                  => open,
          rxbyterealign_out                    => open,
          rxcommadet_out                       => open,
-         rxctrl0_out                          => open, -- x"000" & r_jesdGtRxArr(1).dataK & X"000" & r_jesdGtRxArr(0).dataK,
-         rxctrl1_out                          => open, -- x"000" & r_jesdGtRxArr(1).dispErr & X"000" & r_jesdGtRxArr(0).dispErr,
-         rxctrl2_out                          => open, -- open -- comma detected on corresponding byte
-         rxctrl3_out                          => open, -- x"0" & r_jesdGtRxArr(1).decErr & X"0" & r_jesdGtRxArr(0).decErr,
+         rxctrl0_out                          => s_rxctrl0, -- x"000" & r_jesdGtRxArr(1).dataK & X"000" & r_jesdGtRxArr(0).dataK,
+         rxctrl1_out                          => s_rxctrl1, -- x"000" & r_jesdGtRxArr(1).dispErr & X"000" & r_jesdGtRxArr(0).dispErr,
+         rxctrl2_out                          => s_rxctrl2, -- open -- comma detected on corresponding byte
+         rxctrl3_out                          => s_rxctrl3, -- x"0" & r_jesdGtRxArr(1).decErr & X"0" & r_jesdGtRxArr(0).decErr,
          rxoutclk_out                         => open,
          rxpmaresetdone_out                   => open
-     );
+     );   
    -----------------------------------------
-   -- end generate GT_OPER_GEN;    
+   end generate GT_OPER_GEN;    
    -----------------------------------------------------
 end rtl;
