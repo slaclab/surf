90873ca001935d338e5a4ebb33df92332704fb71
ulegat <ulegat@slac.stanford.edu>
2015-06-12 17:28:50 +0000

    ADC GT ok, sync problem
    git-svn-id: file:///dev/shm/git-devel/svn-repo@7438 0848cbd7-7569-0410-ab05-8cc0a0c67708
    
    Former-commit-id: a2b83f597481a17415aadbffc3a05c9e62580d62
    Former-commit-id: 2f3c73f0f730b3113f8ab1167dbbb37bc699b528

--- jesd204b/trunk/firmware/targets/JesdAdcKc705/Version.vhd
+++ jesd204b/trunk/firmware/targets/JesdAdcKc705/Version.vhd
@@ -1,50 +1,28 @@
 -------------------------------------------------------------------------------
--- Title         : Version Constant File
--- Project       : COB Zynq DTM
+-- Title      : Version Constant File
 -------------------------------------------------------------------------------
--- File          : Version.vhd
--- Author        : Ryan Herbst, rherbst@slac.stanford.edu
--- Created       : 05/18/2014
+-- File       : Version.vhd
+-- Author     : Uros Legat  <ulegat@slac.stanford.edu>
+-- Company    : SLAC National Accelerator Laboratory (Cosylab)
+-- Created    : 2015-06-04
+-- Last update: 2015-06-04
+-- Platform   : 
+-- Standard   : VHDL'93/02
 -------------------------------------------------------------------------------
--- Description:
--- Version Constant Module
--------------------------------------------------------------------------------
--- Copyright (c) 2012 by SLAC. All rights reserved.
+-- Copyright (c) 2013 SLAC National Accelerator Laboratory
 -------------------------------------------------------------------------------
 LIBRARY ieee;
 USE ieee.std_logic_1164.ALL;
 
 package Version is
 
-constant FPGA_VERSION_C : std_logic_vector(31 downto 0) := x"00000016"; -- MAKE_VERSION
+constant FPGA_VERSION_C : std_logic_vector(31 downto 0) := x"00000000"; -- MAKE_VERSION
 
-constant BUILD_STAMP_C : string := "JesdAdcKc705: Vivado v2014.4 (x86_64) Built Mon Jun  1 12:29:41 PDT 2015 by ulegat";
+constant BUILD_STAMP_C : string := "JesdAdcKcu105: Vivado v2015.1 (x86_64) Built Wed Jun 10 16:33:12 PDT 2015 by ulegat";
 
 end Version;
  
 -------------------------------------------------------------------------------
 -- Revision History:
 -------------------------------------------------------------------------------
--- 05/15/2015 - 00000000      - ADC F22 61.44MHz out. 370MHz ref
--- 05/15/2015 - 00000001      - ADC F22 61.44MHz out. 184.32MHz ref 7.3728GHz
--- 05/18/2015 - 00000002      - ADC F22 61.44MHz out. 184.32MHz ref 7.3728GHz. Pol = '1'
--- 05/18/2015 - 00000003      - ADC F22 61.44MHz out. 184.32MHz ref 7.3728GHz. Pol = '1', byte swapped
--- 05/18/2015 - 00000004      - ADC F22 61.44MHz out. 184.32MHz ref 7.3728GHz. Pol = '0', byte swapped
--- 05/18/2015 - 00000005      - Chipscope test
--- 05/19/2015 - 00000006      - ADC F22 368.64MHz out. 184.32MHz ref 7.3728GHz(CS).
--- 05/19/2015 - 00000007      - ADC F22 156.25MHz out. 78.125MHz ref 3.125GHz(CS).
--- 05/19/2015 - 00000008      - (two byte word) ADC F22 156.25MHz out. 156.25MHz ref 3.125GHz(CS).
--- 05/19/2015 - 00000009      - one lane (two byte word) ADC F22 156.25MHz out. 156.25MHz ref 3.125GHz(CS).
--- 05/19/2015 - 0000000A      - dual lane - out clk reset tied to '0'
--- 05/19/2015 - 0000000B      - CPLL 2-byte
--- 05/19/2015 - 0000000C      - CPLL 2-byte - GT parameters as in PGP
--- 05/20/2015 - 0000000D      - CPLL 2-byte - FPGA refclk to ADC
--- 05/20/2015 - 0000000E      - JESD out /2
--- 05/20/2015 - 0000000F      - JESD out /2
--- 05/20/2015 - 00000010      - Recovered clock
--- 05/20/2015 - 00000011      - 2-byte - LMK reference
--- 05/20/2015 - 00000012      - 2-byte - F2 - Dual lane
--- 05/26/2015 - 00000013      - 2-byte - LMK reference - comma alignment parameters fixed
--- 05/26/2015 - 00000014      - 4-byte - LMK reference - AXIS starts on enable and trigger
--- 05/28/2015 - 00000015      - 4-byte - 7.4GHz
--- 05/29/2015 - 00000016      - 4-byte - 7.4GHz - Synced AXIS - Worked over night, Subclass added to registers
+-- 06/10/2015 - 00000000      - First complete version
