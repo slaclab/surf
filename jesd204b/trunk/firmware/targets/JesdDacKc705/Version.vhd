8380f039a068d8960942f09c3e1b99fc4937fbc0
ulegat <ulegat@slac.stanford.edu>
2015-06-23 23:53:32 +0000

    Adding images
    git-svn-id: file:///dev/shm/git-devel/svn-repo@7510 0848cbd7-7569-0410-ab05-8cc0a0c67708
    
    Former-commit-id: 23f002535c1736bc53b3bbd6b6b43771ff34be57
    Former-commit-id: bca8d0c010cbbe0011b32f355958bcb2465f0b2b

--- jesd204b/trunk/firmware/targets/JesdDacKc705/Version.vhd
+++ jesd204b/trunk/firmware/targets/JesdDacKc705/Version.vhd
@@ -18,7 +18,7 @@ package Version is
 
 constant FPGA_VERSION_C : std_logic_vector(31 downto 0) := x"00000001"; -- MAKE_VERSION
 
-constant BUILD_STAMP_C : string := "JesdDacKc705: Vivado v2015.1 (x86_64) Built Tue Jun 23 11:00:11 PDT 2015 by ulegat";
+constant BUILD_STAMP_C : string := "JesdDacKc705: Vivado v2015.1 (x86_64) Built Tue Jun 23 16:15:39 PDT 2015 by ulegat";
 
 end Version;
  
