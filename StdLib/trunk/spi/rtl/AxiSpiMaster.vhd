2dcd9e5027647e66aad946d327e9a995894f0d59
ulegat <ulegat@slac.stanford.edu>
2015-07-09 19:56:26 +0000

    Generic AXI Spi master
    git-svn-id: file:///dev/shm/git-devel/svn-repo@7532 0848cbd7-7569-0410-ab05-8cc0a0c67708
    
    Former-commit-id: d289a4bce711590a729979282a3b4eb0ff510357
    Former-commit-id: 7ce0f2eda50afa5b06ac8470059f8116c32b7e0f

---
+++ StdLib/trunk/spi/rtl/AxiSpiMaster.vhd
@@ -0,0 +1,186 @@
+-------------------------------------------------------------------------------
+-- Title      : Axi lite interface for a single chip SPI master
+-------------------------------------------------------------------------------
+-- File       : AxiSpiMaster.vhd
+-- Author     : Benjamin Reese  <bareese@slac.stanford.edu>
+--            : Uros Legat Modified <ulegat@slac.stanford.edu>
+-- Company    : SLAC National Accelerator Laboratory
+-- Created    : 2015-01-12
+-- Last update: 2015-07-09
+-- Platform   : 
+-- Standard   : VHDL'93/02
+-------------------------------------------------------------------------------
+-- Description:   For multiple chips on single bus connect multiple cores
+--                to multiple AXI crossbar slaves and use Chip select outputs
+--                (coreCsb) to multiplex select the addressed outputs (coreSDout and
+--                coreSclk).
+--                The coreCsb is active low. And active only if the corresponding 
+--                Axi Crossbar Slave is addressed.
+--                DATA_SIZE_G - Corresponds to total read or write command size (not just data size).
+--                              Example: DATA_SIZE_G = 24
+--                                       1-bit command, 15-bit address word and 8-bit data
+-------------------------------------------------------------------------------
+-- Copyright (c) 2013 SLAC National Accelerator Laboratory
+-------------------------------------------------------------------------------
+library ieee;
+use ieee.std_logic_1164.all;
+use ieee.std_logic_arith.all;
+use ieee.std_logic_unsigned.all;
+
+library unisim;
+use unisim.vcomponents.all;
+
+use work.StdRtlPkg.all;
+use work.AxiLitePkg.all;
+use work.I2cPkg.all;
+
+entity AxiSpiMaster is
+   
+   generic (
+      TPD_G        : time    := 1 ns;
+      DATA_SIZE_G  : natural := 24;
+      CLK_PERIOD_G      : real := 6.4E-9;
+      SPI_SCLK_PERIOD_G : real := 100.0E-6
+   );
+   port (
+      axiClk : in sl;
+      axiRst : in sl;
+
+      axiReadMaster  : in  AxiLiteReadMasterType;
+      axiReadSlave   : out AxiLiteReadSlaveType;
+      axiWriteMaster : in  AxiLiteWriteMasterType;
+      axiWriteSlave  : out AxiLiteWriteSlaveType;
+
+      coreSclk : out sl;
+      coreSDin : in  sl;
+      coreSDout: out sl;
+      coreCsb  : out sl
+   );
+
+end entity AxiSpiMaster;
+
+architecture rtl of AxiSpiMaster is
+
+   -- AdcCore Outputs
+   signal rdData : slv(23 downto 0);
+   signal rdEn   : sl;
+
+   type StateType is (WAIT_AXI_TXN_S, WAIT_CYCLE_S, WAIT_SPI_TXN_DONE_S);
+
+   -- Registers
+   type RegType is record
+      state         : StateType;
+      axiReadSlave  : AxiLiteReadSlaveType;
+      axiWriteSlave : AxiLiteWriteSlaveType;
+      -- Adc Core Inputs
+      wrData        : slv(23 downto 0);
+      wrEn          : sl;
+   end record RegType;
+
+   constant REG_INIT_C : RegType := (
+      state         => WAIT_AXI_TXN_S,
+      axiReadSlave  => AXI_LITE_READ_SLAVE_INIT_C,
+      axiWriteSlave => AXI_LITE_WRITE_SLAVE_INIT_C,
+      wrData        => (others => '0'),
+      wrEn          => '0');
+
+   signal r   : RegType := REG_INIT_C;
+   signal rin : RegType;
+
+begin
+
+   comb : process (axiRst, axiReadMaster, axiWriteMaster, r, rdData, rdEn) is
+      variable v         : RegType;
+      variable axiStatus : AxiLiteStatusType;
+   begin
+      v := r;
+
+      axiSlaveWaitTxn(axiWriteMaster, axiReadMaster, v.axiWriteSlave, v.axiReadSlave, axiStatus);
+
+      case (r.state) is
+         when WAIT_AXI_TXN_S =>
+
+            if (axiStatus.writeEnable = '1') then
+               v.wrData(23)           := '0';                                -- Write bit
+               v.wrData(22 downto 21) := "00";                               -- Number of bytes (1)
+               v.wrData(20 downto 16) := "00000";                            -- Unused address bits
+               v.wrData(15 downto 8)  := axiWriteMaster.awaddr(9 downto 2);  -- Address
+               v.wrData(7 downto 0)   := axiWriteMaster.wdata(7 downto 0);   -- Data
+               v.wrEn                 := '1';
+               v.state                := WAIT_CYCLE_S;
+            end if;
+
+            if (axiStatus.readEnable = '1') then
+               v.wrData(23)           := '1';              -- read bit
+               v.wrData(22 downto 21) := "00";             -- Number of bytes (1)
+               v.wrData(20 downto 16) := "00000";          -- Unused address bits
+               v.wrData(15 downto 8)  := axiReadMaster.araddr(9 downto 2);  -- Address
+               v.wrData(7 downto 0)   := (others => '1');  -- Make bus float to Z so slave can
+                                                           -- drive during data segment
+               v.wrEn                 := '1';
+               v.state                := WAIT_CYCLE_S;
+            end if;
+
+         when WAIT_CYCLE_S =>
+            -- Wait 1 cycle for rdEn to drop
+            v.wrEn  := '0';
+            v.state := WAIT_SPI_TXN_DONE_S;
+
+         when WAIT_SPI_TXN_DONE_S =>
+
+            if (rdEn = '1') then
+               v.state := WAIT_AXI_TXN_S;
+               if (r.wrData(23) = '0') then
+                  -- Finish write
+                  axiSlaveWriteResponse(v.axiWriteSlave);
+               else
+                  -- Finish read
+                  v.axiReadSlave.rdata             := (others => '0');
+                  v.axiReadSlave.rdata(7 downto 0) := rdData(7 downto 0);
+                  axiSlaveReadResponse(v.axiReadSlave);
+               end if;
+            end if;
+
+         when others => null;
+      end case;
+
+      if (axiRst = '1') then
+         v := REG_INIT_C;
+      end if;
+
+      rin <= v;
+
+      axiWriteSlave <= r.axiWriteSlave;
+      axiReadSlave  <= r.axiReadSlave;
+      
+   end process comb;
+
+   seq : process (axiClk) is
+   begin
+      if (rising_edge(axiClk)) then
+         r <= rin after TPD_G;
+      end if;
+   end process seq;
+
+   SpiMaster_1 : entity work.SpiMaster
+      generic map (
+         TPD_G             => TPD_G,
+         NUM_CHIPS_G       => 1,
+         DATA_SIZE_G       => DATA_SIZE_G,
+         CPHA_G            => '0',      -- Sample on leading edge
+         CPOL_G            => '0',      -- Sample on rising edge
+         CLK_PERIOD_G      => CLK_PERIOD_G, -- 8.0E-9,
+         SPI_SCLK_PERIOD_G => SPI_SCLK_PERIOD_G) --ite(SIMULATION_G, 100.0E-9, 100.0E-6))
+      port map (
+         clk       => axiClk,
+         sRst      => axiRst,
+         chipSel   => "0",
+         wrEn      => r.wrEn,
+         wrData    => r.wrData,
+         rdEn      => rdEn,
+         rdData    => rdData,
+         spiCsL(0) => coreCsb,
+         spiSclk   => coreSclk,
+         spiSdi    => coreSDout,
+         spiSdo    => coreSDin);
+end architecture rtl;
