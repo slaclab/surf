46e604809a6a6741fb19ee5b882a3f45ac46b83e
ruckman <ruckman@slac.stanford.edu>
2015-07-10 18:09:54 +0000

    default the extRst to '0'
    git-svn-id: file:///dev/shm/git-devel/svn-repo@7536 0848cbd7-7569-0410-ab05-8cc0a0c67708
    
    Former-commit-id: 9d85017924698b983c1f11df8bb4dffdb71fbf7a
    Former-commit-id: 05888d1bfb722240451dba09032cbb021a6aa0a6

--- FirmwareCoreLibrary/trunk/XauiCore/gthUltraScale/rtl/XauiGthUltraScaleWrapper.vhd
+++ FirmwareCoreLibrary/trunk/XauiCore/gthUltraScale/rtl/XauiGthUltraScaleWrapper.vhd
@@ -5,7 +5,7 @@
 -- Author     : Larry Ruckman <ruckman@slac.stanford.edu>
 -- Company    : SLAC National Accelerator Laboratory
 -- Created    : 2015-04-08
--- Last update: 2015-05-01
+-- Last update: 2015-07-10
 -- Platform   : 
 -- Standard   : VHDL'93/02
 -------------------------------------------------------------------------------
@@ -66,7 +66,7 @@ entity XauiGthUltraScaleWrapper is
       axiLiteWriteMaster : in  AxiLiteWriteMasterType := AXI_LITE_WRITE_MASTER_INIT_C;
       axiLiteWriteSlave  : out AxiLiteWriteSlaveType;
       -- Misc. Signals
-      extRst             : in  sl;
+      extRst             : in  sl                     := '0';
       phyClk             : out sl;
       phyRst             : out sl;
       phyReady           : out sl;
