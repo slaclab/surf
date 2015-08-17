8920fff509746247b79063c7d6d9a72ce68652f8
ruckman <ruckman@slac.stanford.edu>
2015-08-17 15:41:53 +0000

    Removed the IpMacTable.vhd module
    git-svn-id: file:///dev/shm/git-devel/svn-repo@7667 0848cbd7-7569-0410-ab05-8cc0a0c67708

--- FirmwareCoreLibrary/trunk/IpEngine/rtl/IpV4EnginePkg.vhd
+++ FirmwareCoreLibrary/trunk/IpEngine/rtl/IpV4EnginePkg.vhd
@@ -5,7 +5,7 @@
 -- Author     : Larry Ruckman  <ruckman@slac.stanford.edu>
 -- Company    : SLAC National Accelerator Laboratory
 -- Created    : 2015-08-11
--- Last update: 2015-08-11
+-- Last update: 2015-08-17
 -- Platform   : 
 -- Standard   : VHDL'93/02
 -------------------------------------------------------------------------------
@@ -16,6 +16,8 @@
 
 library ieee;
 use ieee.std_logic_1164.all;
+use ieee.std_logic_unsigned.all;
+use ieee.std_logic_arith.all;
 
 use work.StdRtlPkg.all;
 use work.AxiStreamPkg.all;
@@ -23,36 +25,87 @@ use work.SsiPkg.all;
 
 package IpV4EnginePkg is
 
-   -- Used for both Ethernet frames and IP/MAC AXIS buses
    constant IP_ENGINE_CONFIG_C : AxiStreamConfigType := ssiAxiStreamConfig(16);
-   
-   -- Only used for IP/MAC AXIS buses
-   constant IP_MAC_MASTER_INIT_C : AxiStreamMasterType := (
-      tValid => '0',
-      tData  => (others => '0'),
-      tStrb  => (others => '1'),
-      tKeep  => (others => '1'),
-      tLast  => '1',-- single word transfers
-      tDest  => (others => '0'),
-      tId    => (others => '0'),
-      tUser  => (others => '0'));    
 
-   constant BROADCAST_MAC_C   : slv(47 downto 0) := (others=>'1');
+   -- EtherTypes
+   constant ARP_TYPE_C  : slv(15 downto 0) := x"0608";  -- EtherType = ARP = 0x0806
+   constant IPV4_TYPE_C : slv(15 downto 0) := x"0008";  -- EtherType = IPV4 = 0x0800
+   constant VLAN_TYPE_C : slv(15 downto 0) := x"0081";  -- EtherType = VLAN = 0x8100
 
-   constant ARP_TYPE_C        : slv(15 downto 0) := x"0608";-- EtherType = ARP = 0x0806
-   constant IPV4_TYPE_C       : slv(15 downto 0) := x"0008";-- EtherType = IPV4 = 0x0800
-   constant VLAN_TYPE_C       : slv(15 downto 0) := x"0081";-- EtherType = VLAN = 0x8100
-   
-   constant HARDWWARE_TYPE_C : slv(15 downto 0) := x"0100";-- HardwareType = ETH = 0x0001
-   constant PROTOCOL_TYPE_C  : slv(15 downto 0) := x"0008";-- ProtocolType = IP  = 0x0800
+   -- IPV4 Protocol Constants
+   constant UDP_C : slv(7 downto 0) := x"11";  -- Protocol = UDP = 0x11
+   constant TCP_C : slv(7 downto 0) := x"06";  -- Protocol = TCP = 0x06
 
-   constant HARDWWARE_LEN_C  : slv(7 downto 0)  := x"06";  -- HardwareLength = 6 (6 Bytes/MAC)
-   constant PROTOCOL_LEN_C   : slv(7 downto 0)  := x"04";  -- ProtocolLength = 4 (6 Bytes/IP)
-   
-   constant ARP_REQ_C       : slv(15 downto 0) := x"0100"; -- OpCode = ARP Request  = 0x0001
-   constant ARP_REPLY_C     : slv(15 downto 0) := x"0200"; -- OpCode = ARP Reply    = 0x0002
-   
-   constant UDP_C : slv(7 downto 0) := x"11";
-   constant TCP_C : slv(7 downto 0) := x"06";
+   procedure GetIpV4Checksum (
+      hdr      : in    Slv8Array(19 downto 0);
+      sum0     : inout Slv32Array(3 downto 0);
+      sum1     : inout Slv32Array(1 downto 0);
+      sum2     : inout Slv32Array(1 downto 0);
+      sum3     : inout slv(31 downto 0);
+      ibValid  : inout sl;
+      checksum : inout slv(15 downto 0));  
 
-end package;
+end package IpV4EnginePkg;
+
+package body IpV4EnginePkg is
+
+   procedure GetIpV4Checksum (
+      hdr      : in    Slv8Array(19 downto 0);
+      sum0     : inout Slv32Array(3 downto 0);
+      sum1     : inout Slv32Array(1 downto 0);
+      sum2     : inout Slv32Array(1 downto 0);
+      sum3     : inout slv(31 downto 0);
+      ibValid  : inout sl;
+      checksum : inout slv(15 downto 0)) is
+      variable i          : natural;
+      variable ibChecksum : slv(15 downto 0);
+      variable header     : Slv32Array(9 downto 0);
+      variable sum4       : slv(15 downto 0);
+   begin
+      -- Convert to 32-bit (little endian) words
+      for i in 9 downto 0 loop
+         header(i)(31 downto 16) := x"0000";
+         -- Check for inbound checksum
+         if i = 5 then
+            header(i)(15 downto 0)  := x"0000";  -- Mask off the inbound checksum
+            ibChecksum(15 downto 8) := hdr(2*i+0);
+            ibChecksum(7 downto 0)  := hdr(2*i+1);
+         else
+            header(i)(15 downto 8) := hdr(2*i+0);
+            header(i)(7 downto 0)  := hdr(2*i+1);
+         end if;
+      end loop;
+
+      -- Summation: Level0
+      for i in 3 downto 0 loop
+         sum0(i) := header(2*i+0) + header(2*i+1);
+      end loop;
+
+      -- Summation: Level1
+      for i in 1 downto 0 loop
+         sum1(i) := sum0(2*i+0) + sum0(2*i+1);
+      end loop;
+
+      -- Summation: Level2
+      sum2(0) := sum1(0) + sum1(1);
+      sum2(1) := header(8) + header(9);
+
+      -- Summation: Level3
+      sum3 := sum2(0) + sum2(1);
+
+      -- Summation: Level4
+      sum4 := sum3(31 downto 16) + sum3(15 downto 0);
+
+      -- Perform 1's complement
+      checksum := not(sum4);
+
+      -- Check for valid inbound checksum
+      if checksum = ibChecksum then
+         ibValid := '1';
+      else
+         ibValid := '0';
+      end if;
+      
+   end procedure;
+
+end package body IpV4EnginePkg;
