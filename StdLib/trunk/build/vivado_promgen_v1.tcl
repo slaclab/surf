2bf075a9c211d6b705dddf233e3eb0654311eca3
ruckman <ruckman@slac.stanford.edu>
2015-08-20 19:50:21 +0000

    Added non-configuration data file support to PROM generation
    git-svn-id: file:///dev/shm/git-devel/svn-repo@7698 0848cbd7-7569-0410-ab05-8cc0a0c67708
    
    Former-commit-id: 0deb1d46d84d748b6beebc9e75ff84bafdf6c7b7
    Former-commit-id: 4e98a1113407d1ba695e464a276fd68a3e9aab32

--- StdLib/trunk/build/vivado_promgen_v1.tcl
+++ StdLib/trunk/build/vivado_promgen_v1.tcl
@@ -9,21 +9,33 @@ source -quiet ${VIVADO_BUILD_DIR}/vivado_proc_v1.tcl
 set inputFile "$::env(IMPL_DIR)/$::env(PROJECT).bit"
 set outputFile "$::env(IMPL_DIR)/$::env(PROJECT).mcs"
 set imagesFile "$::env(IMAGES_DIR)/$::env(PROJECT)_$::env(PRJ_VERSION).mcs"
-set loadbit   "up 0x0 ${inputFile}"
+set loadbit    "up 0x0 ${inputFile}"
+set loaddata   ""
 
 source ${VIVADO_DIR}/promgen.tcl
 
-puts ${inputFile}
-puts ${outputFile}
-puts ${loadbit}
-
-#puts write_cfgmem -force -format ${format} -interface ${inteface} -size ${size} -loadbit ${loadbit} -file ${outputFile}
-
-write_cfgmem -force \
-   -format ${format} \
-   -interface ${inteface} \
-   -size ${size} \
-   -loadbit ${loadbit} \
-   -file ${outputFile}
+if { ${loaddata} != "" } {
+   puts ${inputFile}
+   puts ${outputFile}
+   puts ${loadbit}   
+   puts ${loaddata}   
+   write_cfgmem -force \
+      -format ${format} \
+      -interface ${inteface} \
+      -size ${size} \
+      -loadbit ${loadbit} \
+      -loaddata ${loaddata} \
+      -file ${outputFile}
+} else {   
+   puts ${inputFile}
+   puts ${outputFile}
+   puts ${loadbit}   
+   write_cfgmem -force \
+      -format ${format} \
+      -interface ${inteface} \
+      -size ${size} \
+      -loadbit ${loadbit} \
+      -file ${outputFile}   
+}
 
 exec cp ${outputFile} ${imagesFile}
