3df5d7d31611bbf03ca64452e82c94709b5180d2
ruckman <ruckman@slac.stanford.edu>
2015-08-26 21:09:08 +0000

    adding bash script
    git-svn-id: file:///dev/shm/git-devel/svn-repo@7742 0848cbd7-7569-0410-ab05-8cc0a0c67708
    
    Former-commit-id: 7c357687e50dc50d7847419c4a27d7a4a254da9b
    Former-commit-id: 79be9de56fc02497e9608b3aef3f0acd2c9515f6

---
+++ StdLib/trunk/build/svn_create_new_project.sh
@@ -0,0 +1,73 @@
+#!/bin/sh
+
+if [ $# -ne 1 ]
+then
+   echo ""
+   echo "Usage: svn_create_new_project.sh NewProjectName"
+   echo ""
+   exit;
+fi
+
+echo ""
+echo "---------------------------------------"
+echo "Creating new project: $1"
+echo "---------------------------------------"
+echo ""
+
+## SVN Path Variables
+BASE="file:///afs/slac/g/reseng/svn/repos/"
+PROJ="$BASE/$1"
+TRUNK="$PROJ/trunk"
+TAGS="$PROJ/tags"
+BRANCHES="$PROJ/branches"
+SOFTWARE="$TRUNK/software"
+FIRMWARE="$TRUNK/firmware"
+MODULES="$FIRMWARE/modules"
+TARGETS="$FIRMWARE/targets"
+
+svn mkdir -m "Creating new project: $1" $PROJ $TRUNK $TAGS $BRANCHES $SOFTWARE $FIRMWARE $MODULES $TARGETS
+echo "---------------------------------------"
+echo "Added the following the SVN repository:"
+echo "   $PROJ"
+echo "   $TRUNK"
+echo "   $TAGS"
+echo "   $BRANCHES"
+echo "   $SOFTWARE"
+echo "   $FIRMWARE"
+echo "   $MODULES"
+echo "   $TARGETS"
+echo "---------------------------------------"
+
+echo "---------------------------------------"
+echo "Checking out the new SVN project:"
+svn checkout $TRUNK $1
+echo "---------------------------------------"
+
+## Local Variables
+PROJ="$PWD/$1"
+FIRMWARE="$PROJ/firmware"
+MODULES="$FIRMWARE/modules"
+TARGETS="$FIRMWARE/targets"
+
+echo "---------------------------------------"
+echo "Adding common firmware externals to project directory:"
+STDLIB="^/StdLib/trunk StdLib"
+I2CLIB="^/i2c_core/trunk i2c_core"
+MGTLIB="^/MgtLib/trunk MgtLib"
+PGP2BLIB="^/pgp2b_core/trunk pgp2b"
+IPV4LIB="^/FirmwareCoreLibrary/trunk/IpV4Engine IpV4Engine"
+UDPLIB="^/FirmwareCoreLibrary/trunk/UdpEngine UdpEngine"
+
+echo $STDLIB   >  temp.txt
+echo $I2CLIB   >> temp.txt
+echo $MGTLIB   >> temp.txt
+echo $PGP2BLIB >> temp.txt
+echo $IPV4LIB  >> temp.txt
+echo $UDPLIB   >> temp.txt
+
+svn propset svn:externals -F temp.txt $MODULES 
+svn propset svn:externals "^/ExampleProject/trunk/firmware/targets/EmptyTarget EmptyTarget" $TARGETS 
+svn propset svn:externals "^/ExampleProject/trunk/firmware/setup_env.csh setup_env.csh"     $FIRMWARE 
+svn update $PROJ
+svn commit $PROJ -m "Adding common external to new project: $1"
+echo "---------------------------------------"
