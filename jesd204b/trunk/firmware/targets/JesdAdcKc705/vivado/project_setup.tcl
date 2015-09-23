8fef38372289a28b8a3927d7df0a695459681fd6
ulegat <ulegat@slac.stanford.edu>
2015-09-23 18:28:53 +0000

    git-svn-id: file:///dev/shm/git-devel/svn-repo@7950 0848cbd7-7569-0410-ab05-8cc0a0c67708
    Former-commit-id: e75843fd499ff07e0e62a5d3ae07f8ab74e89cc9
    Former-commit-id: 7d1257b65b6fa92c90a9f026abcbf465e78081a5

--- jesd204b/trunk/firmware/targets/JesdAdcKc705/vivado/project_setup.tcl
+++
@@ -1,4 +0,0 @@
-
-set_property STEPS.SYNTH_DESIGN.ARGS.FLATTEN_HIERARCHY none [get_runs synth_1]
-set_property STEPS.SYNTH_DESIGN.ARGS.BUFG 32 [get_runs synth_1]
-set_property top {DevBoardTb} [get_filesets sim_1]
