fe28e49291dd9bc17ffebfa44d743c7179a81d66
ruckman <ruckman@slac.stanford.edu>
2015-10-14 17:55:02 +0000

    Backing up progress on getting the base firmware to place and route in Vivado 2015.3
    git-svn-id: file:///dev/shm/git-devel/svn-repo@8135 0848cbd7-7569-0410-ab05-8cc0a0c67708
    
    Former-commit-id: e94320854fe4ce27d71d63580f682cacfc25463a
    Former-commit-id: 8973278d4745099a0aa03676ef9896db83578fe2

--- FirmwareCoreLibrary/trunk/SaltCore/core/rtl/SaltDelayCtrl.vhd
+++ FirmwareCoreLibrary/trunk/SaltCore/core/rtl/SaltDelayCtrl.vhd
@@ -5,7 +5,7 @@
 -- Author     : Larry Ruckman  <ruckman@slac.stanford.edu>
 -- Company    : SLAC National Accelerator Laboratory
 -- Created    : 2015-06-16
--- Last update: 2015-09-04
+-- Last update: 2015-10-14
 -- Platform   : 
 -- Standard   : VHDL'93/02
 -------------------------------------------------------------------------------
@@ -25,6 +25,7 @@ use UNISIM.vcomponents.all;
 entity SaltDelayCtrl is
    generic (
       TPD_G           : time   := 1 ns;
+      SIM_DEVICE_G    : string := "7SERIES";  -- Either "7SERIES" or "ULTRASCALE"
       IODELAY_GROUP_G : string := "SALT_IODELAY_GRP");   
    port (
       iDelayCtrlRdy : out sl;
@@ -33,16 +34,35 @@ entity SaltDelayCtrl is
 end SaltDelayCtrl;
 
 architecture mapping of SaltDelayCtrl is
-   
+
+   signal syncRst : sl;
+
+   attribute dont_touch            : string;
+   attribute dont_touch of syncRst : signal is "TRUE";
+
    attribute IODELAY_GROUP                          : string;
    attribute IODELAY_GROUP of SALT_IDELAY_CTRL_Inst : label is IODELAY_GROUP_G;
+
+   attribute KEEP_HIERARCHY                          : string;
+   attribute KEEP_HIERARCHY of SALT_IDELAY_CTRL_Inst : label is "TRUE";
+   attribute KEEP_HIERARCHY of RstSync_Inst          : label is "TRUE";
    
 begin
-   
+
+   RstSync_Inst : entity work.RstSync
+      generic map (
+         TPD_G => TPD_G)
+      port map (
+         clk      => refClk,
+         asyncRst => refRst,
+         syncRst  => syncRst);
+
    SALT_IDELAY_CTRL_Inst : IDELAYCTRL
+      generic map (
+         SIM_DEVICE => SIM_DEVICE_G) 
       port map (
          RDY    => iDelayCtrlRdy,       -- 1-bit output: Ready output
          REFCLK => refClk,              -- 1-bit input: Reference clock input
-         RST    => refRst);             -- 1-bit input: Active high reset input
+         RST    => syncRst);            -- 1-bit input: Active high reset input
 
 end mapping;
