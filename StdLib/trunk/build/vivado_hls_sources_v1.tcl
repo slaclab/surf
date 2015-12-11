a66c484c3bbdb49af9a505b2e91ac3b01780f0f5
ruckman <ruckman@slac.stanford.edu>
2015-12-11 01:03:56 +0000

    1) Added directives.tcl support 2) Default the csim_design and cosim_design to have the optimization (-O) enabled
    git-svn-id: file:///dev/shm/git-devel/svn-repo@8574 0848cbd7-7569-0410-ab05-8cc0a0c67708
    
    Former-commit-id: fd6e4e2c1fe02bdd4d85be82c371b377ed52d167
    Former-commit-id: e68d7ed40b8268448f94cd02cc0c4daf6257853c

--- StdLib/trunk/build/vivado_hls_sources_v1.tcl
+++ StdLib/trunk/build/vivado_hls_sources_v1.tcl
@@ -17,7 +17,7 @@ source ${PROJ_DIR}/sources.tcl
 open_solution "solution1"
 
 ## Setup the csim ldflags
-csim_design -setup -ldflags ${LDFLAGS} -argv ${ARGV}
+csim_design -O -setup -ldflags ${LDFLAGS} -argv ${ARGV}
 
 ## Target specific solution setup script
 source ${PROJ_DIR}/solution.tcl
@@ -27,4 +27,25 @@ close_solution
 
 ## Close the project
 close_project
+
+## Check if directives.tcl exists in the source tree
+if { [file exists  ${PROJ_DIR}/directives.tcl] == 0 } {
+   exec echo ' ' > ${PROJ_DIR}/directives.tcl
+}
+
+## Check if solution1.directive exists in the source tree
+if { [file exists  ${PROJ_DIR}/solution1.directive] == 0 } {
+   exec echo ' ' > ${PROJ_DIR}/solution1.directive
+}
+
+## Make symbolic links for the directives.tcl file
+if { [file exists  ${OUT_DIR}/${PROJECT}_project/solution1/directives.tcl] == 0 } {
+   exec ln -s  ${PROJ_DIR}/directives.tcl ${OUT_DIR}/${PROJECT}_project/solution1/directives.tcl
+}
+
+## Make symbolic links for the solution1.directive file
+if { [file exists  ${OUT_DIR}/${PROJECT}_project/solution1/solution1.directive] == 0 } {
+   exec ln -s  ${PROJ_DIR}/solution1.directive ${OUT_DIR}/${PROJECT}_project/solution1/solution1.directive
+}
+
 exit 0
