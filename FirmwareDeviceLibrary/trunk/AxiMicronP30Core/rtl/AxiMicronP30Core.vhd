b3e8c876ca58a37e1d026e0ccb2d45e775f35076
rherbst <rherbst@slac.stanford.edu>
2016-01-08 23:15:18 +0000

    Added SLAC licensing
    git-svn-id: file:///dev/shm/git-devel/svn-repo@8699 0848cbd7-7569-0410-ab05-8cc0a0c67708

--- FirmwareDeviceLibrary/trunk/AxiMicronP30Core/rtl/AxiMicronP30Core.vhd
+++ FirmwareDeviceLibrary/trunk/AxiMicronP30Core/rtl/AxiMicronP30Core.vhd
@@ -13,7 +13,13 @@
 --
 --    Note: Set the addrBits on the crossbar for this module to 12 bits wide
 -------------------------------------------------------------------------------
--- Copyright (c) 2014 SLAC National Accelerator Laboratory
+-- This file is part of 'SLAC Micron P30 Support Core'.
+-- It is subject to the license terms in the LICENSE.txt file found in the 
+-- top-level directory of this distribution and at: 
+--    https://confluence.slac.stanford.edu/display/ppareg/LICENSE.html. 
+-- No part of 'SLAC Micron P30 Support Core', including this file, 
+-- may be copied, modified, propagated, or distributed except according to 
+-- the terms contained in the LICENSE.txt file.
 -------------------------------------------------------------------------------
 
 library ieee;
