93e4fbb37739be949abb144d92a9601f0883dc70
rherbst <rherbst@slac.stanford.edu>
2016-01-08 22:26:36 +0000

    Added SLAC licensing.
    git-svn-id: file:///dev/shm/git-devel/svn-repo@8688 0848cbd7-7569-0410-ab05-8cc0a0c67708

--- FirmwareCoreLibrary/trunk/EthernetLib/EthMacCore/rtl/EthMacPauseRx.vhd
+++ FirmwareCoreLibrary/trunk/EthernetLib/EthMacCore/rtl/EthMacPauseRx.vhd
@@ -10,7 +10,13 @@
 -- Generic pause frame receiver for Ethernet MACs. Pause frames are dropped
 -- from the incoming data stream.
 -------------------------------------------------------------------------------
--- Copyright (c) 2008 by Ryan Herbst. All rights reserved.
+-- This file is part of 'SLAC Ethernet Library'.
+-- It is subject to the license terms in the LICENSE.txt file found in the 
+-- top-level directory of this distribution and at: 
+--    https://confluence.slac.stanford.edu/display/ppareg/LICENSE.html. 
+-- No part of 'SLAC Ethernet Library', including this file, 
+-- may be copied, modified, propagated, or distributed except according to 
+-- the terms contained in the LICENSE.txt file.
 -------------------------------------------------------------------------------
 -- Modification history:
 -- 09/21/2015: created.
