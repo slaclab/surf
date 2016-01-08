5092a1afa82780db0b3937617edf1b3d3c568dde
rherbst <rherbst@slac.stanford.edu>
2016-01-08 22:30:18 +0000

    Added SLAC licensing.
    git-svn-id: file:///dev/shm/git-devel/svn-repo@8690 0848cbd7-7569-0410-ab05-8cc0a0c67708
    
    Former-commit-id: cfaff28a92febb454c35a552ecab8daaef3d5f59
    Former-commit-id: 406b1273ae777903d06ca532be30b77ac7e17835

--- MgtLib/trunk/gth7/rtl/Gth7QuadPll.vhd
+++ MgtLib/trunk/gth7/rtl/Gth7QuadPll.vhd
@@ -11,7 +11,13 @@
 -------------------------------------------------------------------------------
 -- Description: 
 -------------------------------------------------------------------------------
--- Copyright (c) 2015 SLAC National Accelerator Laboratory
+-- This file is part of 'SLAC MGT Library'.
+-- It is subject to the license terms in the LICENSE.txt file found in the 
+-- top-level directory of this distribution and at: 
+--    https://confluence.slac.stanford.edu/display/ppareg/LICENSE.html. 
+-- No part of 'SLAC MGT Library', including this file, 
+-- may be copied, modified, propagated, or distributed except according to 
+-- the terms contained in the LICENSE.txt file.
 -------------------------------------------------------------------------------
 library ieee;
 use ieee.std_logic_1164.all;
