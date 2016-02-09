db258360dd6b145807b9aa99b283e146a9c2baaf
Larry Ruckman <ruckman@slac.stanford.edu>
2016-02-09 23:07:56 +0000

    Added DCP targets
    git-svn-id: file:///afs/slac/g/reseng/svn/repos/ExampleProject/trunk@9183 0848cbd7-7569-0410-ab05-8cc0a0c67708

---
+++ Version.vhd
@@ -0,0 +1,18 @@
+LIBRARY ieee;
+USE ieee.std_logic_1164.ALL;
+
+package Version is
+
+constant FPGA_VERSION_C : std_logic_vector(31 downto 0) := x"00000001"; -- MAKE_VERSION
+
+constant BUILD_STAMP_C : string := "GigEthGtx7Dcp: Vivado v2015.3 (x86_64) Built Tue Feb  9 14:29:33 PST 2016 by ruckman";
+
+end Version;
+
+-------------------------------------------------------------------------------
+-- Revision History:
+--
+-- 12/05/2014 (0x00000001): Initial Build
+--
+-------------------------------------------------------------------------------
+
