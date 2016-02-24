5e58934e1e5975bb263c4d98da808dfdf4a3b6e2
ruckman <ruckman@slac.stanford.edu>
2016-02-24 23:57:51 +0000

    Adding Vivado 2015.4 support
    git-svn-id: file:///dev/shm/git-devel/svn-repo@9429 0848cbd7-7569-0410-ab05-8cc0a0c67708
    
    Former-commit-id: 39f74ed6128d7083d62f1c1a324b2c81236a72f9
    Former-commit-id: 63f941f39e09324e24d00d92c26a1f745704cd96
    Former-commit-id: 70cb9bfb101b619bcd67655f688c1df805a089a4

--- StdLib/trunk/build/vivado_messages_v1.tcl
+++ StdLib/trunk/build/vivado_messages_v1.tcl
@@ -53,10 +53,13 @@ set_msg_config -suppress -id {Pwropt 34-142};# Post-Place Power Opt: power_opt d
 
 # Messages: Change from WARNING to ERROR
 set_msg_config -id {Synth 8-3512} -new_severity ERROR;# SYNTH: Assigned value in logic is out of range 
-
 set_msg_config -id {Synth 8-153}  -new_severity ERROR;# SYNTH: Case statement has an input that will never be executed
-set_msg_config -id {Synth 8-63}   -new_severity ERROR;# SYNTH: RTL assertion
 set_msg_config -id {VRFC 10-664}  -new_severity ERROR;# SIM:   expression has XXX elements ; expected XXX
+
+## Check for version 2015.3 (or older)
+if { ${VersionNumber} <= 2015.3 } {
+   set_msg_config -id {Synth 8-63}   -new_severity ERROR;# SYNTH: RTL assertion
+}
 
 # Messages: Change from WARNING to CRITICAL_WARNING
 set_msg_config -id {Vivado 12-508} -new_severity "CRITICAL WARNING";# XDC: No pins matched 
