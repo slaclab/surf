04601ae2526543a1171ab7b219bc8ae61a42b93e
ruckman <ruckman@slac.stanford.edu>
2016-03-17 17:22:58 +0000

    Major Update: Fixed the bug where the build wouldn't stop if build breaks
    git-svn-id: file:///dev/shm/git-devel/svn-repo@9758 0848cbd7-7569-0410-ab05-8cc0a0c67708
    
    Former-commit-id: 1db595a9b380469918fec77e53e166919ea3cfac
    Former-commit-id: af91aac6918cee1ef22fc9c82a3eaec70602cdcd
    Former-commit-id: 061c59e59611207f4ba663be2054369159c031e9

--- StdLib/trunk/build/system_build_all_v1.mk
+++ StdLib/trunk/build/system_build_all_v1.mk
@@ -1,10 +1,24 @@
+
+export BUILD_SCRIPT_DIR  = $(dir $(BUILD_SCRIPT))
+export BUILD_SCRIPT_NAME = $(notdir $(BUILD_SCRIPT))
+
+export SETUP_DIR  = $(dir $(SETUP_ENV))
+export SETUP_NAME = $(notdir $(SETUP_ENV))
+
+.PHONY: all build clean
+
 # Default
 all: build
 
 # Check variables
 test:
-	@echo PARALLEL_BUILD: $(PARALLEL_BUILD)
-	@echo SETUP_ENV: $(SETUP_ENV)
+	@echo PARALLEL_BUILD:    $(PARALLEL_BUILD)
+	@echo BUILD_SCRIPT:      $(BUILD_SCRIPT)
+	@echo BUILD_SCRIPT_DIR:  $(BUILD_SCRIPT_DIR)
+	@echo BUILD_SCRIPT_NAME: $(BUILD_SCRIPT_NAME)
+	@echo SETUP_ENV:         $(SETUP_ENV)
+	@echo SETUP_DIR:         $(SETUP_DIR)
+	@echo SETUP_NAME:        $(SETUP_NAME)
 	@echo TARGET_DIRS:
 	@echo -e "$(foreach ARG,$(TARGET_DIRS),\t$(ARG)\n)"
 
@@ -14,18 +28,6 @@ clean:
       cd $$i; make clean; \
    done
 
-# Build all firmware
-# Note: Builds happen in clusters of up to $(PARALLEL_BUILD) 
-# builds at a time to prevent over subscribing the server
+# Build targets
 build:
-	@n=0 ; \
-   for i in $(TARGET_DIRS); do \
-      let "n+=1";\
-      if test $$n != $(PARALLEL_BUILD); then \
-         (konsole --workdir $$i --new-tab --noclose -e tcsh -c 'source $(SETUP_ENV); make'); \
-      fi; \
-      if test $$n = $(PARALLEL_BUILD); then \
-         cd $$i; tcsh -c 'source $(SETUP_ENV); make'; \
-         let "n=0";\
-      fi; \
-   done
+	@tclsh $(BUILD_SCRIPT_DIR)/system_build_all_v1.tcl
