9810ea08462b498be503f1cf4d4028ae7a978b86
ruckman <ruckman@slac.stanford.edu>
2016-04-06 01:06:02 +0000

    Adding vivado_yaml_v1.tcl
    git-svn-id: file:///dev/shm/git-devel/svn-repo@9939 0848cbd7-7569-0410-ab05-8cc0a0c67708
    
    Former-commit-id: 9137232360c723dc4524b169f04e4592ad9e6c5a
    Former-commit-id: 82737ab554f4f47dbb37f5ee445738bb55ccbfd8
    Former-commit-id: bd99d7cfd9b4af1733ff49c8bc4b0597b164d2ab

---
+++ StdLib/trunk/build/vivado_yaml_v1.tcl
@@ -0,0 +1,40 @@
+##############################################################################
+## This file is part of 'SLAC Firmware Standard Library'.
+## It is subject to the license terms in the LICENSE.txt file found in the 
+## top-level directory of this distribution and at: 
+##    https://confluence.slac.stanford.edu/display/ppareg/LICENSE.html. 
+## No part of 'SLAC Firmware Standard Library', including this file, 
+## may be copied, modified, propagated, or distributed except according to 
+## the terms contained in the LICENSE.txt file.
+##############################################################################
+
+########################################################
+## Get variables and Custom Procedures
+########################################################
+set VIVADO_BUILD_DIR $::env(VIVADO_BUILD_DIR)
+source -quiet ${VIVADO_BUILD_DIR}/vivado_env_var_v1.tcl
+source -quiet ${VIVADO_BUILD_DIR}/vivado_proc_v1.tcl
+
+# Common Variable
+set ProjYamlDir "${OUT_DIR}/${PROJECT}_project.yaml"
+
+# Check if the directory exists
+if [file exists ${ProjYamlDir}] {
+   exec rm -rf ${ProjYamlDir}/*
+} else {
+   exec mkdir ${ProjYamlDir}
+}
+
+# Loop through the module directories
+foreach dir $::env(MODULE_DIRS) {
+   set ModYamlDir "${dir}/yaml"
+   # Check for a yaml directory
+   if [file exists ${ModYamlDir}] {
+      foreach filePntr [glob -dir ${ModYamlDir} *] {
+          exec cp -f ${filePntr} ${ProjYamlDir}/.
+      }   
+   }
+}
+
+# Compress the project's yaml directory to the target's image directory
+exec tar -zcvf  ${IMAGES_DIR}/${PROJECT}_${PRJ_VERSION}.tar.gz -C ${OUT_DIR} ${PROJECT}_project.yaml
