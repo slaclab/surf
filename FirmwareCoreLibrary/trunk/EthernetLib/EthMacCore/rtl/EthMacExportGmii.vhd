940e5af70e260b186ed9f5f61f76c636f12c221c
jjo <jjo@slac.stanford.edu>
2016-05-04 23:00:14 +0000

    progress
    git-svn-id: file:///dev/shm/git-devel/svn-repo@10296 0848cbd7-7569-0410-ab05-8cc0a0c67708
    
    Former-commit-id: d8b430b5fd6d8f939f45eb2600d6d280e3d41a78
    Former-commit-id: 07401bfb53d8cedab73302d8c17f3a3a1b58447d
    Former-commit-id: 87e17849c20eaf3a3df6a88620a90145a2ba26e4

--- FirmwareCoreLibrary/trunk/EthernetLib/EthMacCore/rtl/EthMacExportGmii.vhd
+++ FirmwareCoreLibrary/trunk/EthernetLib/EthMacCore/rtl/EthMacExportGmii.vhd
@@ -1,3 +1,15 @@
+-----------------------------------------------------------------
+--                                                             --
+-----------------------------------------------------------------
+--
+--	EthMacExportGmii.vhd - 
+--
+--	Copyright(c) SLAC National Accelerator Laboratory 2000
+--
+--	Author: Jeff Olsen
+--	Created on: 4/15/2016 3:02:34 PM
+--	Last change: JO 5/2/2016 2:38:24 PM
+--
 -------------------------------------------------------------------------------
 -- Title         : 1G MAC / Export Interface
 -- Project       : RCE 1G-bit MAC
@@ -45,29 +57,54 @@ entity EthMacExportGmii is
       -- AXIS Interface   
       macObMaster    : in  AxiStreamMasterType;
       macObSlave     : out AxiStreamSlaveType;
+
       -- PHY Interface
       gmiiTxEn       : out sl;
       gmiiTxEr       : out sl;
       gmiiTxd        : out slv(7 downto 0);
       phyReady       : in  sl;
+
       -- Configuration
       interFrameGap  : in  slv(3 downto 0);
       macAddress     : in  slv(47 downto 0);
+
       -- Status
       txCountEn      : out sl;
       txUnderRun     : out sl;
-      txLinkNotReady : out sl);
+      txLinkNotReady : out sl
+		);
 end EthMacExportGmii;
 
 architecture rtl of EthMacExportGmii is
+
+
+	type StateType is
+	(
+		Idle_s,
+		TxPreamble_s,
+		TxData_s,
+		Wait_s,
+		Pad_s,
+		TxCrc_s,
+		TxCrcA_s,
+		TxCrcB_s,
+		TxCrcC_s,
+		TxCrcD_s,
+		Dump_s
+	);
 
    type RegType is record
       gmiiTxEn       : sl;
       gmiiTxEr       : sl;
       gmiiTxd        : slv(7 downto 0);
-      txCountEn      : sl;
+      txCount      	: slv(7 downto 0);
+		txData_d			: slv(7 downto 0);
+      txCountEn		: sl;
       txUnderRun     : sl;
       txLinkNotReady : sl;
+		crcReset			: sl;
+		crcDataValid	: sl;
+		ExportState		: StateType;
       macSlave       : AxiStreamSlaveType;
    end record;
 
@@ -75,9 +112,14 @@ architecture rtl of EthMacExportGmii is
       gmiiTxEn       => '0',
       gmiiTxEr       => '0',
       gmiiTxd        => (others => '0'),
-      txCountEn      => '0',
+      txCount	      => (others => '0'),
+		txData_d			=> (others => '0'),
+      txCountEn		=> '0',
       txUnderRun     => '0',
       txLinkNotReady => '0',
+		crcDataValid	=> '0',
+		crcReset			=> '0',
+		ExportState		=> Idle_s,
       macSlave       => AXI_STREAM_SLAVE_INIT_C);
 
    signal r   : RegType := REG_INIT_C;
@@ -85,9 +127,11 @@ architecture rtl of EthMacExportGmii is
 
    signal macMaster : AxiStreamMasterType;
    signal macSlave  : AxiStreamSlaveType;
+	signal crcOut : slv(31 downto 0);
 
    -- attribute dont_touch               : string;
    -- attribute dont_touch of r          : signal is "TRUE";   
+
 
 begin
 
@@ -119,7 +163,7 @@ begin
          mAxisMaster => macMaster,                      -- 8-bit AXI stream interface 
          mAxisSlave  => macSlave);  
 
-   comb : process (ethClkRst, r) is
+   comb : process (ethClkRst, r, macMaster) is
       variable v : RegType;
    begin
       -- Latch the current value
@@ -128,13 +172,114 @@ begin
       -- Reset the flags
       v.macSlave := AXI_STREAM_SLAVE_INIT_C;
 
-      ----------------------------------
-      ----------------------------------
-      ----------------------------------
-      --- Place holder for code here ---
-      ----------------------------------
-      ----------------------------------
-      ----------------------------------
+		Case r.ExportState is
+		When Idle_s =>
+			v.CrcDataValid			:= '0';
+			v.crcReset 				:= '1';
+			v.TxCount 				:= x"00";
+			v.txData_d 				:= x"77";
+			v.gmiiTxd				:= x"77";
+      	v.gmiiTxEn           := '0';
+			v.gmiiTxEr       		:= '0';
+			v.macSlave.tReady 	:= '0';
+			-- Wait for start flag
+         if ((macMaster.tValid = '1') and (ethClkRst = '0')) then
+			-- Phy is ready
+				if phyReady = '1' then
+					v.ExportState 			:=	TxPreamble_s;
+			-- Phy is not ready dump data
+				else
+					v.ExportState 			:= Dump_s;
+					v.txLinkNotReady 		:= '1';
+				end if;
+			else
+					v.ExportState 			:= Idle_s;
+			end if;
+
+		When TxPreamble_s =>
+      	v.gmiiTxEn       		:= '1';
+			if (r.TxCount = x"07") then
+				v.CrcReset			:= '0';
+				v.txData_d			:= x"D5";
+				v.gmiiTxd			:= r.txData_d;
+				v.TxCount 			:= x"00";
+				v.MacSlave.tReady   := '0';
+				v.CrcDataValid		:= '1';
+				v.ExportState 		:= TxData_s;
+			else
+				v.TxCount			:= r.TxCount +1;
+				v.txData_d			:= x"55";
+				v.gmiiTxd			:= r.txData_d;
+				v.ExportState 		:= TxPreamble_s;
+			end if;
+
+		When TxData_s =>
+
+			if (r.TxCount < x"3C") then                  -- Minimum frame of 64 includes 4byte FCS
+				v.TxCount			:= r.TxCount + 1;
+			end if;
+			if ((macMaster.tValid = '1') and (macMaster.tlast = '0')) then
+				v.CrcDataValid		:= '1';
+				v.txData_d			:= macMaster.tdata(7 downto 0);
+				v.gmiiTxd			:= r.txData_d;
+			   v.MacSlave.tReady    := '1';
+				v.ExportState		:= TxData_s;
+			elsif ((macMaster.tValid = '1') and (macMaster.tlast = '1')) then
+			    v.MacSlave.tReady    := '1';
+				v.CrcDataValid		:= '0';
+				v.txData_d			:= macMaster.tdata(7 downto 0);
+				v.gmiiTxd			:= r.txData_d;
+				if (r.TxCount = x"3B") then
+					v.ExportState 	:= TxCrc_s;
+				else
+					v.ExportState 	:= Pad_s;
+				end if;
+			else
+			    v.MacSlave.tReady    := '0';
+			    v.CrcDataValid		:= '0';
+				v.gmiiTxEr 			:= '1';
+				v.ExportState 		:= Dump_s;
+			end if;
+
+		when Pad_s =>
+			if (r.TxCount < x"3C") then
+				v.TxCount 		:= v.TxCount + 1;
+				v.ExportState 	:= Pad_s;
+			else
+				v.ExportState 	:= TxCrc_s;
+			end if;
+			
+-- Ugly but it works
+		When TxCrc_s =>
+				v.gmiiTxd			:= r.txData_d;
+				v.ExportState 	:= TxCrcA_s;
+
+		When TxCrcA_s =>
+				v.gmiitxd		:= crcOut(7 downto 0);
+				v.ExportState 	:= TxCrcB_s;
+		When TxCrcB_s =>
+            v.gmiitxd      := crcOut(15 downto 8);
+				v.ExportState 	:= TxCrcC_s;
+		When TxCrcC_s =>
+            v.gmiitxd      := crcOut(23 downto 16);
+				v.ExportState 	:= TxCrcD_s;
+		When TxCrcD_s =>
+            v.gmiitxd      := crcOut(31 downto 24);
+				v.ExportState 	:= Idle_s;
+				
+		When Dump_s =>
+			if ((macMaster.tValid = '1') and (macMaster.tlast = '1')) then
+				v.macSlave.tReady 	:= '0';
+				v.ExportState			:= Idle_s;
+			else
+				v.macSlave.tReady 	:= '1';
+				v.ExportState			:= Dump_s;
+			end if;
+
+		When Others =>
+			v.ExportState 		:= Idle_s;
+			
+		end case;
 
       -- Reset
       if (ethClkRst = '1') then
@@ -155,11 +300,27 @@ begin
       
    end process comb;
 
+	
    seq : process (ethClk) is
    begin
       if rising_edge(ethClk) then
          r <= rin after TPD_G;
       end if;
    end process seq;
+
+-- CRC
+U_Crc32 : entity work.Crc32Parallel
+generic map (
+	BYTE_WIDTH_G => 1
+)
+port map (
+	crcOut		  => crcOut,
+	crcClk		  => ethClk,
+	crcDataValid  => r.CrcDataValid,
+	crcDataWidth  => "000",
+	crcIn			  => macMaster.tdata(7 downto 0),
+	crcReset		  => r.crcReset
+); 
+
 
 end rtl;
--- FirmwareCoreLibrary/trunk/EthernetLib/EthMacCore/rtl/EthMacImportGmii.vhd
+++ FirmwareCoreLibrary/trunk/EthernetLib/EthMacCore/rtl/EthMacImportGmii.vhd
@@ -1,3 +1,15 @@
+-----------------------------------------------------------------
+--                                                             --
+-----------------------------------------------------------------
+--
+--	EthMacImportGmii.vhd - 
+--
+--	Copyright(c) SLAC National Accelerator Laboratory 2000
+--
+--	Author: Jeff Olsen
+--	Created on: 2/23/2016 9:04:26 AM
+--	Last change: JO 5/2/2016 11:39:55 AM
+--
 -------------------------------------------------------------------------------
 -- Title         : 1G MAC / Import Interface
 -- Project       : RCE 1G-bit MAC
@@ -5,7 +17,7 @@
 -- File       : EthMacImportGmii.vhd
 -- Author     : Jeff Olsen  <jjo@slac.stanford.edu>
 -- Company    : SLAC National Accelerator Laboratory
--- Created    : 2015-02-04
+-- Created    : 2016-02-04
 -- Last update: 2016-02-24
 -- Platform   : 
 -- Standard   : VHDL'93/02
@@ -56,27 +68,68 @@ end EthMacImportGmii;
 
 architecture rtl of EthMacImportGmii is
 
+Constant SFD_C 		: slv(7 downto 0) := x"D5";
+
+	type StateType is
+	(
+		waitSFD_s,
+		waitData_s,
+		getData_s,
+		delA_s,
+		delB_s,
+		Crc_s,
+		Term_s
+	);
+
+
+signal crcOut : slv(31 downto 0);
+signal crcIn    : slv(31 downto 0);
+
    type RegType is record
-      rxCountEn  : sl;
-      rxCrcError : sl;
+      rxCountEn   	: sl;
+      rxCrcError  	: sl;
+		crcValid			: sl;
+		crcReset			: sl;
+		delRxDv			: sl;
+		delRxDvSr		: slv(7 downto 0);
+		writeFF			: sl;
+		crcGood			: sl;
+		crcDataValid	: sl;
+		state				: StateType;
+		macData			: slv(63 downto 0);
       macMaster  : AxiStreamMasterType;
    end record;
 
    constant REG_INIT_C : RegType := (
-      rxCountEn  => '0',
-      rxCrcError => '0',
-      macMaster  => AXI_STREAM_MASTER_INIT_C);
+      rxCountEn   	=> '0',
+      rxCrcError  	=> '0',
+		crcValid			=> '0',
+		crcReset			=> '0',
+		delRxDv			=> '0',
+		delRxDvSr		=> (Others => '0'),
+		writeFF			=> '0',
+		crcGood			=> '0',
+		crcDataValid	=> '0',
+		state				=> waitSFD_s,
+		macData			=> (others => '0'),
+      macMaster  		=> AXI_STREAM_MASTER_INIT_C
+      );
+
 
    signal r   : RegType := REG_INIT_C;
    signal rin : RegType;
 
+   signal FFData : slv(7 downto 0);
    signal macMaster : AxiStreamMasterType;
    signal macSlave  : AxiStreamSlaveType;
+
 
    -- attribute dont_touch               : string;
    -- attribute dont_touch of r          : signal is "TRUE";   
 
 begin
+
+
 
    TX_DATA_MUX : entity work.AxiStreamFifo
       generic map (
@@ -92,7 +145,7 @@ begin
          CASCADE_SIZE_G      => 1,
          FIFO_ADDR_WIDTH_G   => 4,
          -- AXI Stream Port Configurations
-         SLAVE_AXI_CONFIG_G  => ssiAxiStreamConfig(1),  -- 8-bit AXI stream interface  
+         SLAVE_AXI_CONFIG_G  => ssiAxiStreamConfig(1),  --  8-bit AXI stream interface  
          MASTER_AXI_CONFIG_G => ssiAxiStreamConfig(8))  -- 64-bit AXI stream interface          
       port map (
          -- Slave Port
@@ -106,7 +159,8 @@ begin
          mAxisMaster => macIbMaster,                    -- 64-bit AXI stream interface
          mAxisSlave  => AXI_STREAM_SLAVE_FORCE_C);  
 
-   comb : process (ethClkRst, macSlave, r) is
+
+   comb : process (ethClkRst, macSlave, r, gmiiRxDv, gmiiRxEr, gmiiRxd, phyReady, FFData, crcOut ) is
       variable v : RegType;
    begin
       -- Latch the current value
@@ -117,13 +171,91 @@ begin
          v.macMaster := AXI_STREAM_MASTER_INIT_C;
       end if;
 
-      ----------------------------------
-      ----------------------------------
-      ----------------------------------
-      --- Place holder for code here ---
-      ----------------------------------
-      ----------------------------------
-      ----------------------------------
+--		if r.intFirstLine = '1' then
+--			axiStreamSetUserBit(EMAC_AXIS_CONFIG_C, v.macMaster, EMAC_SOF_BIT_C, '1', 0);
+--		end if;
+
+		if r.MacMaster.tlast = '1' then
+--			axiStreamSetUserBit(EMAC_AXIS_CONFIG_C, v.macMaster, EMAC_EOFE_BIT_C,  not(r.crcGood));
+			axiStreamSetUserBit(EMAC_AXIS_CONFIG_C, v.macMaster, EMAC_EOFE_BIT_C,  '1');
+		end if;
+
+	
+		v.rxCountEn  := r.MacMaster.tlast and r.crcGood;
+
+-- Delay data to avoid sending the crc
+		v.macData(63 downto 0) := r.macData(55 downto 0) & gmiiRxd;
+
+--
+
+		v.delRxDvSr := r.delRxDvSr(6 downto 0) & r.delRxDv ;
+
+		v.crcReset            := r.delRxDvSr(2) or ethClkRst or (not phyReady);
+		
+		case r.state is
+			when waitSFD_s =>
+				v.macMaster.tlast 	:= '0';
+--				v.intFirstLine	:= '0';
+				v.macMaster.tvalid	:= '0';
+				v.crcDataValid			:= '0';
+				if ((gmiiRxd = SFD_C) and (gmiiRxDv = '1') and (gmiiRxEr = '0') and (phyReady = '1')) then
+					v.delRxDv			:= '1';
+					v.state	:= waitData_s;
+				else
+					v.delRxDv			:= '0';
+					v.state := waitSFD_s;
+				end if;
+				
+			when waitData_s =>
+				v.delRxDv			:= '0';
+				if (r.delRxDvSr(3) = '1') then
+					v.state := getData_s;
+				else
+					v.state := waitData_s;
+				end if;
+
+			when getData_s =>
+				if ((gmiiRxEr = '1') or (phyReady = '0')) then  -- Error
+					v.macMaster.tvalid 	:= '1';
+					v.macMaster.tlast		:= '1';
+					v.crcDataValid			:= '0';
+					v.state 					:= Term_s;
+				else
+					v.macMaster.tdata(7 downto 0)		:= FFData;	 
+					if (gmiiRxDv = '0') then
+						v.macMaster.tvalid	:= '1';				-- Transmit 64bit Data
+						v.macMaster.tlast		:= '1';
+						v.crcDataValid			:= '1';
+						v.state 				:= delA_s;
+					else
+						v.macMaster.tvalid	:= '1';
+						v.crcDataValid			:= '1';
+					end if;
+				end if;
+
+            when delA_s =>
+  				v.crcDataValid			:= '0';
+ 				v.state 				:= delB_s;               
+
+            when delB_s =>
+
+ 				v.state 				:= crc_s;               
+
+			when crc_s =>
+			 if (crcIn /= crcOut) then
+			     rxCrcError <= '1';
+			 end if;
+				v.macMaster.tvalid 	:= '0';
+				v.macMaster.tlast  	:= '0';
+				v.crcDataValid			:= '0';		
+				v.state					:= term_s;
+						
+			when term_s =>
+				v.macMaster.tvalid 	:= '0';
+				v.macMaster.tlast  	:= '0';
+				v.crcDataValid			:= '0';		
+				v.state					:= waitSFD_s;
+		end case;
 
       -- Reset
       if (ethClkRst = '1') then
@@ -146,5 +278,27 @@ begin
          r <= rin after TPD_G;
       end if;
    end process seq;
+
+-- CRC Input
+    crcIn(31 downto 24) <= r.macData(31 downto 24);
+    crcIn(23 downto 16) <= r.macData(39 downto 32);
+    crcIn(15 downto  8) <= r.macData(47 downto 40);
+    crcIn(7  downto  0) <= r.macData(55 downto 48);
+
+		FFData									<= r.macData(47 downto 40);	
+-- CRC
+U_Crc32 : entity work.Crc32Parallel
+generic map (
+	BYTE_WIDTH_G => 1
+)
+port map (
+	crcOut		  => crcOut,
+	crcClk		  => ethClk,
+	crcDataValid  => r.crcDataValid,
+	crcDataWidth  => "000",
+	crcIn			  => FFData,
+	crcReset		  => r.crcReset
+); 
+
 
 end rtl;
