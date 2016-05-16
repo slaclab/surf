08c5e765e14033f730ee46271bcd2476ff5abc5f
ruckman <ruckman@slac.stanford.edu>
2016-05-16 22:42:02 +0000

    A) Added synthesis error checking in the post synthesis script B) Added more multi-driven net checking/support
    git-svn-id: file:///dev/shm/git-devel/svn-repo@10588 0848cbd7-7569-0410-ab05-8cc0a0c67708
    
    Former-commit-id: dbad2975c4c752bc6c57c4714205d78ad3ceff07
    Former-commit-id: 3940fb8b3176249308be001a8bc7a5e21ae828bf
    Former-commit-id: 91a6d745370694534a7a22049e2bb100e55cbf4b

--- StdLib/trunk/build/vivado_post_synthesis_v1.tcl
+++ StdLib/trunk/build/vivado_post_synthesis_v1.tcl
@@ -17,6 +17,31 @@ set VIVADO_BUILD_DIR $::env(VIVADO_BUILD_DIR)
 source -quiet ${VIVADO_BUILD_DIR}/vivado_env_var_v1.tcl
 source -quiet ${VIVADO_BUILD_DIR}/vivado_proc_v1.tcl
 
+# Get the number of errors and multi-driven nets during synthesis
+open_run synth_1
+set NumErr [llength [lsearch -all -regexp [split [read [open ${OUT_DIR}/${VIVADO_PROJECT}.runs/synth_1/runme.log]]] "^ERROR:"]]
+set MDRV [report_drc -checks {MDRV-1}]
+close_design
+
+# Check for errors during synthesis
+if { ${NumErr} != 0 } {
+   puts "\n\n\nErrors detected during synthesis!!!"
+   puts "\tOpen the GUI to review the error messages\n\n\n" 
+   close_project
+   exit 0   
+}
+
+# Check if Multi-Driven Nets are not allowed
+set AllowMultiDriven [expr {[info exists ::env(ALLOW_MULTI_DRIVEN)] && [string is true -strict $::env(ALLOW_MULTI_DRIVEN)]}]  
+if { ${AllowMultiDriven} != 1 } {
+   # Check if any multi-driven nets during synthesis
+   if { ${MDRV} != 0 } {
+      puts "\n\n\nMulti-driven nets detected during synthesis!!!n\n\n"    
+      close_project
+      exit 0   
+   }
+}
+
 # GUI Related:
 # Disable a refresh due to the changes 
 # in the Version.vhd file during synthesis 
