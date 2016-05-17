14adce9610d7911107d56799963de7fca292cacd
ruckman <ruckman@slac.stanford.edu>
2016-05-17 18:28:49 +0000

    Adding post route directory
    git-svn-id: file:///dev/shm/git-devel/svn-repo@10594 0848cbd7-7569-0410-ab05-8cc0a0c67708

---
+++ FirmwareCoreLibrary/trunk/MicroblazeBasicCore/vivado/post_route.tcl
@@ -0,0 +1,10 @@
+# Post-Route Build Script
+
+########################################################
+## Get variables and Custom Procedures
+########################################################
+set VIVADO_BUILD_DIR $::env(VIVADO_BUILD_DIR)
+source -quiet ${VIVADO_BUILD_DIR}/vivado_env_var_v1.tcl
+source -quiet ${VIVADO_BUILD_DIR}/vivado_proc_v1.tcl
+
+
