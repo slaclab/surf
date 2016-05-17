476a59194d47270267ec740dfc05584d75b106e8
ruckman <ruckman@slac.stanford.edu>
2016-05-17 22:17:56 +0000

    Adding code
    git-svn-id: file:///dev/shm/git-devel/svn-repo@10598 0848cbd7-7569-0410-ab05-8cc0a0c67708

--- FirmwareCoreLibrary/trunk/MicroblazeBasicCore/vivado/post_route.tcl
+++ FirmwareCoreLibrary/trunk/MicroblazeBasicCore/vivado/post_route.tcl
@@ -7,4 +7,8 @@ set VIVADO_BUILD_DIR $::env(VIVADO_BUILD_DIR)
 source -quiet ${VIVADO_BUILD_DIR}/vivado_env_var_v1.tcl
 source -quiet ${VIVADO_BUILD_DIR}/vivado_proc_v1.tcl
 
+## Make the SDK directory
+file mkdir ${OUT_DIR}/${VIVADO_PROJECT}.sdk
 
+## Export the hardware to the image directory
+file copy -force ${OUT_DIR}/${VIVADO_PROJECT}.runs/impl_1/${PROJECT}.sysdef ${IMAGES_DIR}/${PROJECT}_${PRJ_VERSION}.hdf
