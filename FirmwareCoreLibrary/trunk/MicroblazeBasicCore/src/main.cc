27b2efb461fdc8f79e87f2adfe6377ac147e6445
ruckman <ruckman@slac.stanford.edu>
2016-05-19 05:07:35 +0000

    backing up progress
    git-svn-id: file:///dev/shm/git-devel/svn-repo@10621 0848cbd7-7569-0410-ab05-8cc0a0c67708

--- FirmwareCoreLibrary/trunk/MicroblazeBasicCore/src/main.cc
+++ FirmwareCoreLibrary/trunk/MicroblazeBasicCore/src/main.cc
@@ -1,14 +1,30 @@
-
 #include <stdio.h>
-#include "xparameters.h"
-#include "xil_cache.h"
-int main() 
-{
-   // HERE!!!!
+#include <string.h>
 
-   Xil_ICacheEnable();
-   Xil_DCacheEnable();
-   Xil_DCacheDisable();
-   Xil_ICacheDisable();
+#include "xil_types.h"
+#include "xil_io.h"
+
+#define BUS_OFFSET  0x80000000
+#define AXI_VERSION (BUS_OFFSET+0x00000000)
+#define AXI_MEMORY  (BUS_OFFSET+0x00020000)
+
+int main() {
+   
+   u32 i;
+   u32 value;
+   char myString[64] = "hello world";
+
+   // Copy the "hello world" string to memory via memcpy()
+   u32 *memAddr = (u32 *)(BUS_OFFSET+AXI_MEMORY);
+   memcpy(memAddr,(u32 *)myString,sizeof(myString));
+   
+   // Read/write via xil_io.h's functions
+   for(i=0;i<64;i++){
+      // Get the build string from the AXI Version module
+      value = Xil_In32(AXI_MEMORY + 0x800 + (i*4));
+      // Echo the build string into the generic AXI-Lite memory
+      Xil_Out32( (AXI_MEMORY + 0x800 + (i*4)), value);
+   }
+   
    return 0;
 }
