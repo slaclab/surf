400e2210fae98a86e8b2ca98d81dc02a530a49b9
ruckman <ruckman@slac.stanford.edu>
2016-05-20 18:13:07 +0000

    Cleaned up the build system messaging TCL script
    git-svn-id: file:///dev/shm/git-devel/svn-repo@10650 0848cbd7-7569-0410-ab05-8cc0a0c67708
    
    Former-commit-id: 2a9e82a4e42b73448b7105531da59b91e3fd831c
    Former-commit-id: 3bbc0410075846f7a4087d9e63b4cd3efe9988a9
    Former-commit-id: 76bffb3dd27da9fc00b88f55088dd237b3954f28

--- StdLib/trunk/build/vivado_messages_v1.tcl
+++ StdLib/trunk/build/vivado_messages_v1.tcl
@@ -18,6 +18,10 @@ source -quiet ${VIVADO_BUILD_DIR}/vivado_proc_v1.tcl
 set AllowMultiDriven [expr {[info exists ::env(ALLOW_MULTI_DRIVEN)] && [string is true -strict $::env(ALLOW_MULTI_DRIVEN)]}]  
 set VersionNumber [version -short]
 
+########################################################
+## Message Suppression
+########################################################
+
 # Messages Suppression: INFO
 set_msg_config -suppress -id {Synth 8-256}; # SYNTH: done synthesizing module
 set_msg_config -suppress -id {Synth 8-113}; # SYNTH: binding component instance 'RTL_Inst' to cell 'PRIMITIVE'
@@ -45,17 +49,20 @@ set_msg_config -suppress -id {Designutils 20-1318};# DESIGN_UTILS: Multiple VHDL
 set_msg_config -suppress -id {Common 17-301};# DESIGN_INIT: Failed to get a license: Internal_bitstream
 set_msg_config -suppress -id {Pwropt 34-142};# Post-Place Power Opt: power_opt design has already been performed within this design hierarchy. Skipping
 set_msg_config -suppress -id {Common 17-1361};# The existing rule will be replaced.
+set_msg_config -suppress -id {Vivado 12-4430};# Overriding default DRC messaging
+set_msg_config -suppress -id {Vivado 12-1790};# IP core licensing warning
 
-# Messages Suppression: CRITICAL_WARNING
-# TBD Place holder
+########################################################
+## Modifying WARNING messaging
+########################################################
 
-# Messages Suppression: ERROR
-# TBD Place holder
+# Messages: Change from WARNING to INFO
+set_msg_config -id {Timing 38-3} -new_severity INFO; #User defined clocks are common and should be info, not warning.
 
 # Messages: Change from WARNING to ERROR
 set_msg_config -id {Synth 8-3512} -new_severity ERROR;# SYNTH: Assigned value in logic is out of range 
-# set_msg_config -id {Synth 8-3848} -new_severity ERROR;# SYNTH: Input doesn't does not have driver
 set_msg_config -id {VRFC 10-664}  -new_severity ERROR;# SIM:   expression has XXX elements ; expected XXX
+# set_msg_config -id {Synth 8-3848} -new_severity ERROR;# SYNTH: Input doesn't does not have driver
 
 ## Check for version 2015.3 (or older)
 if { ${VersionNumber} <= 2015.3 } {
@@ -68,11 +75,17 @@ set_msg_config -id {Synth 8-3330}  -new_severity "CRITICAL WARNING";# SYNTH: an 
 set_msg_config -id {Synth 8-3919}  -new_severity "CRITICAL WARNING";# SYNTH: Null Assignment in logic
 set_msg_config -id {Synth 8-153}  -new_severity "CRITICAL WARNING";# SYNTH: Case statement has an input that will never be executed
 
+########################################################
+## Modifying CRITICAL_WARNING messaging
+########################################################
+
 # Messages: Change from CRITICAL_WARNING to WARNING
 set_msg_config -id {Vivado 12-4430} -new_severity {Warning};# Modifying [get_drc_checks REQP-52]
 set_msg_config -id {Vivado 12-1387} -new_severity {Warning};# No valid object(s) found for set_false_path constraint
+set_msg_config -id {BD 41-968}      -new_severity {Warning};# No associated to any clock port on a block design bus
 
-# TBD Place holder
+# DRC: Change from CRITICAL_WARNING to WARNING
+set_property SEVERITY {Warning} [get_drc_checks NSTD-1];  # DRC: I/O standard (IOSTANDARD) value 'DEFAULT', instead of a user assigned specific value
 
 # Messages: Change from CRITICAL_WARNING to ERROR
 set_msg_config -id {Vivado 12-1411} -new_severity ERROR;# SYNTH: Cannot set LOC property of differential pair ports
@@ -80,36 +93,9 @@ set_msg_config -id {HDL 9-806}      -new_severity ERROR;# SYNTH: Syntax error ne
 set_msg_config -id {Opt 31-80}      -new_severity ERROR;# IMPL: Multi-driver net found in the design
 set_msg_config -id {Route 35-14}    -new_severity ERROR;# IMPL: Multi-driver net found in the design
 
-# Check if Multi-Driven Nets are allowed
-if { ${AllowMultiDriven} == 1 } {
-    set_msg_config -id {Synth 8-3352} -new_severity INFO;# SYNTH: multi-driven net
-    set_msg_config -id {MDRV-1}       -new_severity INFO;# DRC: multi-driven net	
-} else {
-    set_msg_config -id {Synth 8-3352} -new_severity ERROR;	
-    set_msg_config -id {MDRV-1}       -new_severity ERROR;	
-}
-
-set_msg_config -id {Timing 38-3} -new_severity INFO; #User defined clocks are common and should be info, not warning.
-
-##set_msg_config -id {Route 35-39}    -new_severity ERROR;# IMPL: The design did not meet timing requirements. 
-## NOTE: we don't change this message to ERROR severity because we want to impl_1 to finish 
-## and print CheckTiming procedure's statement. 
-##
-## Here's an example of this terminal print statement:
-##
-## *******************************************************
-## ********************************************************
-## ********************************************************
-## The design did not meet timing or unable to route:
-##         Setup: Worst Negative Slack (WNS): -5.121338 ns
-##         Setup: Total Negative Slack (TNS): -12800.591797 ns
-##         Hold: Worst Hold Slack (WHS): 0.045176 ns
-##         Hold: Total Hold Slack (THS): 0.000000 ns
-##         Pulse Width: Total Pulse Width Negative Slack (TPWS): 0.000000 ns
-##         Routing: Number of Failed Nets: 0
-## ********************************************************
-## ********************************************************
-## ********************************************************
+########################################################
+## Modifying ERROR messaging
+########################################################
 
 # DRC: Change from ERROR to WARNING
 set_property SEVERITY {Warning} [get_drc_checks {REQP-52}]; # DRC: using the GTGREFCLK port on a MGT  (GTP7 & GTX7)
@@ -120,11 +106,18 @@ set_property SEVERITY {Warning} [get_drc_checks {REQP-49}]; # DRC: using the GTG
 set_property SEVERITY {Warning} [get_drc_checks {REQP-1753}]; # DRC: using the GTGREFCLK port on CPLL (GTH7)
 set_property SEVERITY {Warning} [get_drc_checks {UCIO-1}];  # DRC: using the XADC's VP/VN ports
 
-# DRC: Change from CRITICAL_WARNING to WARNING
-set_property SEVERITY {Warning} [get_drc_checks NSTD-1];  # DRC: I/O standard (IOSTANDARD) value 'DEFAULT', instead of a user assigned specific value
+########################################################
+# Check if Multi-Driven Nets are allowed
+########################################################
+if { ${AllowMultiDriven} == 1 } {
+    set_msg_config -id {Synth 8-3352} -new_severity INFO;# SYNTH: multi-driven net
+    set_msg_config -id {MDRV-1}       -new_severity INFO;# DRC: multi-driven net	
+} else {
+    set_msg_config -id {Synth 8-3352} -new_severity ERROR;	
+    set_msg_config -id {MDRV-1}       -new_severity ERROR;	
+}
 
-# Messages: Change from ERROR to CRITICAL_WARNING
-# TBD Place holder
-
+########################################################
 # Target specific messages script
+########################################################
 SourceTclFile ${VIVADO_DIR}/messages.tcl
