303f164a2f4a9b993baf7241fa757fd560836629
ruckman <ruckman@slac.stanford.edu>
2016-07-05 21:22:23 +0000

    Using the global VIVADO_VERSION env variable (instead of locally generating it)
    git-svn-id: file:///dev/shm/git-devel/svn-repo@11236 0848cbd7-7569-0410-ab05-8cc0a0c67708
    
    Former-commit-id: a366834ffc878eefc21b667d7f30c7841300e5e6
    Former-commit-id: 2000aeeabf19890297015d771673b36cd80c705c
    Former-commit-id: 267df30605181347b8da0a502af34a1597a947ea

--- StdLib/trunk/build/vivado_pre_synth_run_v1.tcl
+++ StdLib/trunk/build/vivado_pre_synth_run_v1.tcl
@@ -20,9 +20,8 @@ source -quiet ${VIVADO_BUILD_DIR}/vivado_proc_v1.tcl
 # Setup build string
 set DATE [exec date]
 set USER [exec whoami]
-set VERSION [version -short]
 set MACHINE [exec uname -m]
-set BSTR "${PROJECT}: Vivado v${VERSION} (${MACHINE}) Built ${DATE} by ${USER}"
+set BSTR "${PROJECT}: Vivado v${VIVADO_VERSION} (${MACHINE}) Built ${DATE} by ${USER}"
 set SEDS "s|\\(constant BUILD_STAMP_C : string := \\).*|\\1\"${BSTR}\";|"
 
 # Update the timestamp in Version.vhd
