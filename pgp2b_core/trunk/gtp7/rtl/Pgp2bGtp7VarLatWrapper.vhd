07630148d3a9bd039556b296b61947400dca0a49
ruckman <ruckman@slac.stanford.edu>
2016-07-05 20:54:15 +0000

    Fixed broken build
    git-svn-id: file:///dev/shm/git-devel/svn-repo@11234 0848cbd7-7569-0410-ab05-8cc0a0c67708
    
    Former-commit-id: 3fdcb91cc96c1927aaf611fb41af93bb7e52077f
    Former-commit-id: 63158d55538fcb111d752fa4d402c36ec8d3272f
    Former-commit-id: 51d2776dadc62df3fea93bf35a71e09b534d0c94

--- pgp2b_core/trunk/gtp7/rtl/Pgp2bGtp7VarLatWrapper.vhd
+++ pgp2b_core/trunk/gtp7/rtl/Pgp2bGtp7VarLatWrapper.vhd
@@ -55,8 +55,8 @@ entity Pgp2bGtp7VarLatWrapper is
       RXLPM_INCM_CFG_G     : bit                     := '0';
       RXLPM_IPCM_CFG_G     : bit                     := '1';
       -- Configure PGP
-      PGP_RX_ENABLE_G      : boolean                 := true;
-      PGP_TX_ENABLE_G      : boolean                 := true;
+      RX_ENABLE_G          : boolean                 := true;
+      TX_ENABLE_G          : boolean                 := true;
       PAYLOAD_CNT_TOP_G    : integer                 := 7;     -- Top bit for payload counter
       VC_INTERLEAVE_G      : integer                 := 1;     -- Interleave Frames
       NUM_VC_EN_G          : integer range 1 to 4    := 4);
@@ -200,8 +200,8 @@ begin
          TX_PLL_G          => "PLL0",
          RX_PLL_G          => "PLL1",
          -- Configure PGP
-         PGP_RX_ENABLE_G   => PGP_RX_ENABLE_G,
-         PGP_TX_ENABLE_G   => PGP_TX_ENABLE_G,
+         RX_ENABLE_G       => RX_ENABLE_G,
+         TX_ENABLE_G       => TX_ENABLE_G,
          PAYLOAD_CNT_TOP_G => PAYLOAD_CNT_TOP_G,
          VC_INTERLEAVE_G   => VC_INTERLEAVE_G,
          NUM_VC_EN_G       => NUM_VC_EN_G)
