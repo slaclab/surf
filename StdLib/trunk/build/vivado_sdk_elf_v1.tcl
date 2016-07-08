9fb74b5dcf0cd217e4d821cc1c8367de7cecfc12
ruckman <ruckman@slac.stanford.edu>
2016-07-08 18:59:45 +0000

    Added external SDK common library support
    git-svn-id: file:///dev/shm/git-devel/svn-repo@11287 0848cbd7-7569-0410-ab05-8cc0a0c67708
    
    Former-commit-id: 3852a740b137b5cb03621319136f9c16052bd9e6
    Former-commit-id: 94edb928ae4dee6717cc7bf5983e012d9352548d
    Former-commit-id: 06d54b0712dcaa1f89f71acd04d9a928d561c419

--- StdLib/trunk/build/vivado_sdk_elf_v1.tcl
+++ StdLib/trunk/build/vivado_sdk_elf_v1.tcl
@@ -16,13 +16,13 @@
 set VIVADO_BUILD_DIR $::env(VIVADO_BUILD_DIR)
 source ${VIVADO_BUILD_DIR}/vivado_env_var_v1.tcl
 
-# Check the Vivado version
+# Check the Vivado version (Refer to AR#66629)
 if { ${VIVADO_VERSION} < 2016.1 } {
-   # Generate .ELF for Vivado 2015.4 (or earlier)
+   # Generate .ELF for Vivado 2015.4 (or earlier) ....  Refer to AR#66629
    sdk set_workspace ${SDK_PRJ}
    sdk build_project  -type all
 } else {
-   # Generate .ELF for Vivado 2016.1 (or later)
+   # Generate .ELF for Vivado 2016.1 (or later) ....  Refer to AR#66629
    sdk setws ${SDK_PRJ}
    sdk projects -build  -type all
 }   
