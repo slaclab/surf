d733736613f5744454ce0996c45ab1a848c744f2
ruckman <ruckman@slac.stanford.edu>
2016-09-02 23:45:49 +0000

    Added SLAC licensing.
    git-svn-id: file:///afs/slac/g/reseng/svn/repos@12223 0848cbd7-7569-0410-ab05-8cc0a0c67708
    
    Former-commit-id: 5489114dbd036c6814a08e72c30dfc79ceaf6264
    Former-commit-id: d688c86e9af1e59ee3d11c4f9380ca07d41e8f0a
    Former-commit-id: 9ce0c9ca2c9f8d6954e4182fc6e1bb3ed02ed1cb

--- FirmwareDeviceLibrary/trunk/AxiAd9249Core/rtl/Ad9249Pkg.vhd
+++ FirmwareDeviceLibrary/trunk/AxiAd9249Core/rtl/Ad9249Pkg.vhd
@@ -11,13 +11,13 @@
 -------------------------------------------------------------------------------
 -- Description: 
 -------------------------------------------------------------------------------
--- This file is part of <PROJECT_NAME>. It is subject to
--- the license terms in the LICENSE.txt file found in the top-level directory
--- of this distribution and at:
---    https://confluence.slac.stanford.edu/display/ppareg/LICENSE.html.
--- No part of <PROJECT_NAME>, including this file, may be
--- copied, modified, propagated, or distributed except according to the terms
--- contained in the LICENSE.txt file.
+-- This file is part of 'AXI-Lite AD9249 Core Firmware'.
+-- It is subject to the license terms in the LICENSE.txt file found in the 
+-- top-level directory of this distribution and at: 
+--    https://confluence.slac.stanford.edu/display/ppareg/LICENSE.html. 
+-- No part of 'AXI-Lite AD9249 Core Firmware', including this file, 
+-- may be copied, modified, propagated, or distributed except according to 
+-- the terms contained in the LICENSE.txt file.
 -------------------------------------------------------------------------------
 
 
