2deb919054d329f55ec7f966922f14baccee7801
ruckman <ruckman@slac.stanford.edu>
2016-09-06 16:17:41 +0000

    Added SLAC licensing.
    git-svn-id: file:///afs/slac/g/reseng/svn/repos@12249 0848cbd7-7569-0410-ab05-8cc0a0c67708

--- FirmwareDeviceLibrary/trunk/AxiAd9467Core/rtl/AxiAd9467Reg.vhd
+++ FirmwareDeviceLibrary/trunk/AxiAd9467Core/rtl/AxiAd9467Reg.vhd
@@ -11,7 +11,13 @@
 -------------------------------------------------------------------------------
 -- Description: 
 -------------------------------------------------------------------------------
--- Copyright (c) 2014 SLAC National Accelerator Laboratory
+-- This file is part of 'AXI-Lite AD9467 Core'.
+-- It is subject to the license terms in the LICENSE.txt file found in the 
+-- top-level directory of this distribution and at: 
+--    https://confluence.slac.stanford.edu/display/ppareg/LICENSE.html. 
+-- No part of 'AXI-Lite AD9467 Core', including this file, 
+-- may be copied, modified, propagated, or distributed except according to 
+-- the terms contained in the LICENSE.txt file.
 -------------------------------------------------------------------------------
 
 library ieee;
