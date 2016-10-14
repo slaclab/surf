2a0676f956b78f9c15e43bc97c0c31ca1d91ede3
ruckman <ruckman@slac.stanford.edu>
2016-10-14 19:26:45 +0000

    bring the RX FIFO configurations to the top level because they can vary between application to application
    git-svn-id: file:///afs/slac/g/reseng/svn/repos@12758 0848cbd7-7569-0410-ab05-8cc0a0c67708

--- FirmwareCoreLibrary/trunk/EthernetLib/EthMacCore/rtl/EthMacTop.vhd
+++ FirmwareCoreLibrary/trunk/EthernetLib/EthMacCore/rtl/EthMacTop.vhd
@@ -5,7 +5,7 @@
 -- Author     : Ryan Herbst <rherbst@slac.stanford.edu>
 -- Company    : SLAC National Accelerator Laboratory
 -- Created    : 2015-09-22
--- Last update: 2016-10-10
+-- Last update: 2016-10-13
 -- Platform   : 
 -- Standard   : VHDL'93/02
 -------------------------------------------------------------------------------
@@ -32,26 +32,33 @@ use work.EthMacPkg.all;
 entity EthMacTop is
    generic (
       -- Simulation Generics
-      TPD_G             : time                     := 1 ns;
+      TPD_G               : time                     := 1 ns;
       -- MAC Configurations
-      PAUSE_EN_G        : boolean                  := true;
-      PAUSE_512BITS_G   : positive range 1 to 1024 := 8;
-      PHY_TYPE_G        : string                   := "XGMII";  -- "GMII", "XGMII", or "XLGMII"
-      DROP_ERR_PKT_G    : boolean                  := true;
-      JUMBO_G           : boolean                  := true;
+      PAUSE_EN_G          : boolean                  := true;
+      PAUSE_512BITS_G     : positive range 1 to 1024 := 8;
+      PHY_TYPE_G          : string                   := "XGMII";  -- "GMII", "XGMII", or "XLGMII"
+      DROP_ERR_PKT_G      : boolean                  := true;
+      JUMBO_G             : boolean                  := true;
+      -- RX FIFO Configurations
+      INT_PIPE_STAGES_G   : natural                  := 1;
+      PIPE_STAGES_G       : natural                  := 1;
+      FIFO_ADDR_WIDTH_G   : positive                 := 10;
+      CASCADE_SIZE_G      : positive                 := 2;
+      FIFO_PAUSE_THRESH_G : positive                 := 1000;
+      CASCADE_PAUSE_SEL_G : natural                  := 0;
       -- Non-VLAN Configurations
-      FILT_EN_G         : boolean                  := false;
-      PRIM_COMMON_CLK_G : boolean                  := false;
-      PRIM_CONFIG_G     : AxiStreamConfigType      := EMAC_AXIS_CONFIG_C;
-      BYP_EN_G          : boolean                  := false;
-      BYP_ETH_TYPE_G    : slv(15 downto 0)         := x"0000";
-      BYP_COMMON_CLK_G  : boolean                  := false;
-      BYP_CONFIG_G      : AxiStreamConfigType      := EMAC_AXIS_CONFIG_C;
+      FILT_EN_G           : boolean                  := false;
+      PRIM_COMMON_CLK_G   : boolean                  := false;
+      PRIM_CONFIG_G       : AxiStreamConfigType      := EMAC_AXIS_CONFIG_C;
+      BYP_EN_G            : boolean                  := false;
+      BYP_ETH_TYPE_G      : slv(15 downto 0)         := x"0000";
+      BYP_COMMON_CLK_G    : boolean                  := false;
+      BYP_CONFIG_G        : AxiStreamConfigType      := EMAC_AXIS_CONFIG_C;
       -- VLAN Configurations
-      VLAN_EN_G         : boolean                  := false;
-      VLAN_CNT_G        : positive range 1 to 8    := 1;
-      VLAN_COMMON_CLK_G : boolean                  := false;
-      VLAN_CONFIG_G     : AxiStreamConfigType      := EMAC_AXIS_CONFIG_C);      
+      VLAN_EN_G           : boolean                  := false;
+      VLAN_CNT_G          : positive range 1 to 8    := 1;
+      VLAN_COMMON_CLK_G   : boolean                  := false;
+      VLAN_CONFIG_G       : AxiStreamConfigType      := EMAC_AXIS_CONFIG_C);      
    port (
       -- Core Clock and Reset
       ethClk           : in  sl;
@@ -303,18 +310,23 @@ begin
    ----------         
    U_RxFifo : entity work.EthMacRxFifo
       generic map (
-         TPD_G             => TPD_G,
-         JUMBO_G           => JUMBO_G,
-         DROP_ERR_PKT_G    => DROP_ERR_PKT_G,
-         PRIM_COMMON_CLK_G => PRIM_COMMON_CLK_G,
-         PRIM_CONFIG_G     => PRIM_CONFIG_G,
-         BYP_EN_G          => BYP_EN_G,
-         BYP_COMMON_CLK_G  => BYP_COMMON_CLK_G,
-         BYP_CONFIG_G      => BYP_CONFIG_G,
-         VLAN_EN_G         => VLAN_EN_G,
-         VLAN_CNT_G        => VLAN_CNT_G,
-         VLAN_COMMON_CLK_G => VLAN_COMMON_CLK_G,
-         VLAN_CONFIG_G     => VLAN_CONFIG_G)
+         TPD_G               => TPD_G,
+         DROP_ERR_PKT_G      => DROP_ERR_PKT_G,
+         INT_PIPE_STAGES_G   => INT_PIPE_STAGES_G,
+         PIPE_STAGES_G       => PIPE_STAGES_G,
+         FIFO_ADDR_WIDTH_G   => FIFO_ADDR_WIDTH_G,
+         CASCADE_SIZE_G      => CASCADE_SIZE_G,
+         FIFO_PAUSE_THRESH_G => FIFO_PAUSE_THRESH_G,
+         CASCADE_PAUSE_SEL_G => CASCADE_PAUSE_SEL_G,
+         PRIM_COMMON_CLK_G   => PRIM_COMMON_CLK_G,
+         PRIM_CONFIG_G       => PRIM_CONFIG_G,
+         BYP_EN_G            => BYP_EN_G,
+         BYP_COMMON_CLK_G    => BYP_COMMON_CLK_G,
+         BYP_CONFIG_G        => BYP_CONFIG_G,
+         VLAN_EN_G           => VLAN_EN_G,
+         VLAN_CNT_G          => VLAN_CNT_G,
+         VLAN_COMMON_CLK_G   => VLAN_COMMON_CLK_G,
+         VLAN_CONFIG_G       => VLAN_CONFIG_G)
       port map (
          -- Slave Clock and Reset
          sClk         => ethClk,
