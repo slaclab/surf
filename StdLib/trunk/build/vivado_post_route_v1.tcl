759541857b1d85cb784cb4eaeee0f090a6d98e04
ruckman <ruckman@slac.stanford.edu>
2016-10-26 17:10:13 +0000

    Adding support for Vivado 2016.3 Partial Reconfiguration support
    git-svn-id: file:///afs/slac/g/reseng/svn/repos@12909 0848cbd7-7569-0410-ab05-8cc0a0c67708
    
    Former-commit-id: d83b3219dffb6d88185ea1f6da7f3fd363864a73
    Former-commit-id: aed5b2977eaddc9ff624876ec294381ba1c70a05
    Former-commit-id: b4686abb605b452c56b2ebc8b637ede64f4f7188

--- StdLib/trunk/build/vivado_post_route_v1.tcl
+++ StdLib/trunk/build/vivado_post_route_v1.tcl
@@ -30,12 +30,6 @@ if { [CheckTiming false] == true } {
    if { ${VIVADO_VERSION} >= 2015.3 } {
       exec cp -f ${IMPL_DIR}/${topLevel}_routed.dcp ${OUT_DIR}/IncrementalBuild.dcp
    }
-   
-   ########################################################
-   ## Make a copy of the .bit file and .bit.gz
-   ######################################################## 
-   exec cp -f ${IMPL_DIR}/${topLevel}.bit ${IMAGES_DIR}/${PROJECT}_${PRJ_VERSION}.bit
-   exec gzip -c -f -9 ${IMPL_DIR}/${topLevel}.bit > ${IMAGES_DIR}/${PROJECT}_${PRJ_VERSION}.bit.gz
 
    #########################################################
    ## Check if need to include YAML files with the .BIT file
