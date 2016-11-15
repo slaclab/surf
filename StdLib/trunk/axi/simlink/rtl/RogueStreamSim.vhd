0902b311a92052f2eb97db55fc08a79a2105ad98
rherbst <rherbst@slac.stanford.edu>
2016-11-15 04:57:13 +0000

    Added rogue simulation link.
    git-svn-id: file:///afs/slac/g/reseng/svn/repos@13119 0848cbd7-7569-0410-ab05-8cc0a0c67708

---
+++ StdLib/trunk/axi/simlink/rtl/RogueStreamSim.vhd
@@ -0,0 +1,48 @@
+------------------------------------------------------------------------------
+-- This file is part of 'SLAC Firmware Standard Library'.
+-- It is subject to the license terms in the LICENSE.txt file found in the 
+-- top-level directory of this distribution and at: 
+--    https://confluence.slac.stanford.edu/display/ppareg/LICENSE.html. 
+-- No part of 'SLAC Firmware Standard Library', including this file, 
+-- may be copied, modified, propagated, or distributed except according to 
+-- the terms contained in the LICENSE.txt file.
+------------------------------------------------------------------------------
+
+LIBRARY ieee;
+USE work.ALL;
+use ieee.std_logic_1164.all;
+use ieee.std_logic_arith.all;
+use ieee.std_logic_unsigned.all;
+
+entity RogueStreamSim is port (
+      clock        : in    std_logic;
+      reset        : in    std_logic;
+      dest         : in    std_logic_vector(7  downto 0);
+
+      obValid      : out   std_logic;
+      obReady      : in    std_logic;
+      obDataLow    : out   std_logic_vector(31 downto 0);
+      obDataHigh   : out   std_logic_vector(31 downto 0);
+      obUserLow    : out   std_logic_vector(31 downto 0);
+      obUserHigh   : out   std_logic_vector(31 downto 0);
+      obKeep       : out   std_logic_vector(7  downto 0);
+      obLast       : out   std_logic;
+
+      ibValid      : in    std_logic;
+      ibReady      : out   std_logic;
+      ibDataLow    : in    std_logic_vector(31 downto 0);
+      ibDataHigh   : in    std_logic_vector(31 downto 0);
+      ibUserLow    : in    std_logic_vector(31 downto 0);
+      ibUserHigh   : in    std_logic_vector(31 downto 0);
+      ibKeep       : in    std_logic_vector(7  downto 0);
+      ibLast       : in    std_logic
+   );
+end RogueStreamSim;
+
+-- Define architecture
+architecture RogueStreamSim of RogueStreamSim is
+   Attribute FOREIGN of RogueStreamSim: architecture is 
+      "vhpi:AxiSim:VhpiGenericElab:RogueStreamSimInit:RogueStreamSim";
+begin
+end RogueStreamSim;
+
