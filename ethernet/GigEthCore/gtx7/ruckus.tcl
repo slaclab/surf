0ef2bbf3cbe873d63bfda5f9e80820e2317c453c
Larry Ruckman <ruckman@slac.stanford.edu>
2016-11-15 11:47:53 -0800

    1) Adding missing ruckus.tcl 2) fixed a path bug in gtx7/ruckus.tcl
    Former-commit-id: 5627ffb53cc96ba0fa8fe3d43c3f3f6c0e705474
    Former-commit-id: 0e128d97aa6c817eec93f8b88d7537c56e880b20
    Former-commit-id: f1c683fe67af6f6cc6bc688fcaf331aca76916ea

--- ethernet/GigEthCore/gtx7/ruckus.tcl
+++ ethernet/GigEthCore/gtx7/ruckus.tcl
@@ -3,4 +3,4 @@ source -quiet $::env(RUCKUS_DIR)/vivado_proc.tcl
 
 # Load Source Code
 loadSource -dir  "$::DIR_PATH/rtl"
-loadSource -path "$::DIR_PATH/coregen/GigEthGtx7Core.dcp"
+loadSource -path "$::DIR_PATH/dcp/images/GigEthGtx7Core_00000001.dcp"
