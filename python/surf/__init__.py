2aed8f6a562500f00b60a20057db8e70b34be5fe
ruck314 <ruckman@slac.stanford.edu>
2017-01-18 10:12:46 -0800

    Added Ad5780 and Ltc2270 device classes
    Former-commit-id: c6121b50a24df3a4244ca7a8b8db28183ebadbd6
    Former-commit-id: 510c85eec9c5990f1fe8cd272a1d5f554cdc18fa
    Former-commit-id: 5e349ddb7ffd226143820c8c3f3c974352081aa8

--- python/surf/__init__.py
+++ python/surf/__init__.py
@@ -6,4 +6,6 @@ from surf._xadc import *
 from surf._rssi import *
 from surf._udpclient import *
 from surf._udpserver import *
+from surf._ad5780 import *
+from surf._ltc2270 import *
 #from _proms import *
