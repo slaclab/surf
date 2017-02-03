584aa996353e5f64b548032f3d090b929d88ca3e
ruck314 <ruckman@slac.stanford.edu>
2017-02-03 09:39:57 -0800

    bug fix for SALT xilinxUltraScale
    Former-commit-id: f4030c5b39271f3a3227436469a2e00d82627add
    Former-commit-id: b8d43802ac19bb5829b9732b0732b3827d0e3168
    Former-commit-id: 86a407237e5dcda260ad048521ea640c87b92502

--- protocols/salt/xilinxUltraScale/ruckus.tcl
+++ protocols/salt/xilinxUltraScale/ruckus.tcl
@@ -5,7 +5,7 @@ source -quiet $::env(RUCKUS_DIR)/vivado_proc.tcl
 loadSource -dir  "$::DIR_PATH/rtl"
 # loadIpCore -path "$::DIR_PATH/coregen/SaltUltraScaleCore.xci"
 loadSource -path "$::DIR_PATH/coregen/SaltUltraScaleCore.dcp"
-loadSource -path "$::DIR_PATH/coregen/SaltUltraScaleRxOnly.dcp"
+loadSource -path "$::DIR_PATH/dcp/images/SaltUltraScaleRxOnly_00000001.dcp"
 
 # Load Simulation
 loadSource -sim_only -dir "$::DIR_PATH/tb/"
