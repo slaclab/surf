fa7041f07abe28ca38b91ea85f7c00d5095b3e73
Larry Ruckman <ruckman@slac.stanford.edu>
2017-02-08 16:49:16 -0800

    adding 7 series SEM module.  I still need to add the SSI wrapper from Ben HeavyPhoton source code

---
+++ xilinx/7Series/sem/rtl/SemPkg.vhd
@@ -0,0 +1,57 @@
+-------------------------------------------------------------------------------
+-- File       : SemPkg.vhd
+-- Company    : SLAC National Accelerator Laboratory
+-- Created    : 2017-02-08
+-- Last update: 2017-02-08
+-------------------------------------------------------------------------------
+-- Description: 
+-------------------------------------------------------------------------------
+-- This file is part of 'BCS LCLS1 ACM'.
+-- It is subject to the license terms in the LICENSE.txt file found in the 
+-- top-level directory of this distribution and at: 
+--    https://confluence.slac.stanford.edu/display/ppareg/LICENSE.html. 
+-- No part of 'BCS LCLS1 ACM', including this file, 
+-- may be copied, modified, propagated, or distributed except according to 
+-- the terms contained in the LICENSE.txt file.
+-------------------------------------------------------------------------------
+
+library ieee;
+use ieee.std_logic_1164.all;
+
+use work.StdRtlPkg.all;
+
+package SemPkg is
+
+   type SemObType is record
+      heartbeat      : sl;
+      initialization : sl;
+      observation    : sl;
+      correction     : sl;
+      classification : sl;
+      injection      : sl;
+      essential      : sl;
+      uncorrectable  : sl;
+      txData         : slv(7 downto 0);
+      txWrite        : sl;
+      rxRead         : sl;
+      iprogIcapReq   : sl;
+   end record;
+
+   type SemIbType is record
+      injectStrobe   : sl;
+      injectAddress  : slv(39 downto 0);
+      txFull         : sl;
+      rxData         : slv(7 downto 0);
+      rxEmpty        : sl;
+      iprogIcapGrant : sl;
+   end record;
+
+   constant SEM_IB_INIT_C : SemIbType := (
+      injectStrobe   => '0',
+      injectAddress  => (others => '0'),
+      txFull         => '1',            -- Init with backpreassure
+      rxData         => (others => '0'),
+      rxEmpty        => '0',            -- Init with backpreassure
+      iprogIcapGrant => '0');  -- '0' = SEM access ICAP, '1' = IPROG access ICAP
+
+end package;
---
+++ xilinx/7Series/sem/rtl/UartSem.vhd
@@ -0,0 +1,110 @@
+-------------------------------------------------------------------------------
+-- File       : UartSem.vhd
+-- Company    : SLAC National Accelerator Laboratory
+-- Created    : 2017-02-08
+-- Last update: 2017-02-08
+-- Platform   : 
+-- Standard   : VHDL'93/02
+-------------------------------------------------------------------------------
+-- Description: 
+-------------------------------------------------------------------------------
+-- This file is part of 'SLAC Firmware Standard Library'.
+-- It is subject to the license terms in the LICENSE.txt file found in the 
+-- top-level directory of this distribution and at: 
+--    https://confluence.slac.stanford.edu/display/ppareg/LICENSE.html. 
+-- No part of 'SLAC Firmware Standard Library', including this file, 
+-- may be copied, modified, propagated, or distributed except according to 
+-- the terms contained in the LICENSE.txt file.
+-------------------------------------------------------------------------------
+
+library ieee;
+use ieee.std_logic_1164.all;
+use ieee.std_logic_unsigned.all;
+use ieee.std_logic_arith.all;
+
+use work.StdRtlPkg.all;
+use work.SemPkg.all;
+
+entity UartSem is
+   generic (
+      TPD_G             : time     := 1 ns;
+      CLK_FREQ_G        : real     := 100.0E+6;
+      BAUD_RATE_G       : positive := 115200;
+      FIFO_BRAM_EN_G    : boolean  := false;
+      FIFO_ADDR_WIDTH_G : positive := 5);
+   port (
+      -- Clock and Reset
+      semClk         : in  sl;
+      semRst         : in  sl;
+      -- IPROG Interface
+      fpgaReload     : in  sl               := '0';
+      fpgaReloadAddr : in  slv(31 downto 0) := (others => '0');
+      -- UART Serial Interface
+      uartTx         : out sl;
+      uartRx         : in  sl);
+end entity UartSem;
+
+architecture mapping of UartSem is
+
+   signal wrData  : sl;
+   signal wrValid : sl;
+   signal wrFull  : sl;
+   signal wrReady : sl;
+
+   signal rdData  : sl;
+   signal rdValid : sl;
+   signal rdEmpty : sl;
+   signal rdReady : sl;
+
+   signal semIb : SemIbType := SEM_IB_INIT_C;
+   signal semOb : SemObType;
+
+begin
+
+   ------------------------------   
+   --  Soft Error Mitigation Core
+   ------------------------------   
+   U_Sem : entity work.SemWrapper
+      generic map (
+         TPD_G => TPD_G)
+      port map (
+         -- Clock and Reset
+         semClk => clk,
+         semRst => rst,
+         -- SEM Interface
+         semIb  => semIb,
+         semOb  => semOb);
+
+   --------------
+   -- Flowcontrol
+   --------------
+   semIb.txFull  <= not(wrReady);
+   semIb.rxEmpty <= not(rdValid);
+
+   --------------------
+   --  UART Serdes Core
+   --------------------
+   U_Uart : entity work.UartWrapper
+      generic map (
+         TPD_G             => TPD_G,
+         CLK_FREQ_G        => CLK_FREQ_G,
+         BAUD_RATE_G       => BAUD_RATE_G,
+         FIFO_BRAM_EN_G    => FIFO_BRAM_EN_G,
+         FIFO_ADDR_WIDTH_G => FIFO_ADDR_WIDTH_G)
+      port map (
+         -- Clock and Reset
+         clk     => clk,
+         rst     => rst,
+         -- Write Interface
+         wrData  => semOb.txData,
+         wrValid => semOb.txWrite,
+         wrReady => wrReady,
+         -- Read Interface
+         rdData  => semIb.rxData,
+         rdValid => rdValid,
+         rdReady => semOb.rxRead,
+         -- UART Serial Interface
+         tx      => tx,
+         rx      => rx);
+
+end mapping;
