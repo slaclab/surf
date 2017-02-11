37706ee5339a0ee23564e443b52774238db832fb
Larry Ruckman <ruckman@slac.stanford.edu>
2017-02-10 19:46:38 -0800

    timing constraint fix
    Former-commit-id: 7b185f8f30cd91f82551d0c6ff7f239c0174f892
    Former-commit-id: 97bc3a3ddea0d3955a7e8349dd98262797c359d4
    Former-commit-id: 0d158accc339ebe7a06b4404941d16c6ca0b0fdd

--- protocols/salt/xilinxUltraScale/txonly/Version.vhd
+++ protocols/salt/xilinxUltraScale/txonly/Version.vhd
@@ -5,7 +5,7 @@ package Version is
 
 constant FPGA_VERSION_C : std_logic_vector(31 downto 0) := x"00000001"; -- MAKE_VERSION
 
-constant BUILD_STAMP_C : string := "SaltUltraScaleTxOnly: Vivado v2015.3 (x86_64) Built Fri Feb 10 17:06:05 PST 2017 by ruckman";
+constant BUILD_STAMP_C : string := "SaltUltraScaleTxOnly: Vivado v2015.3 (x86_64) Built Fri Feb 10 18:02:49 PST 2017 by ruckman";
 
 end Version;
 
