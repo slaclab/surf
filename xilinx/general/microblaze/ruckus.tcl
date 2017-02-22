bec05bd5e52cc74f857aacc15370c0ec978d6280
Larry Ruckman <ruckman@slac.stanford.edu>
2017-02-22 12:39:38 -0800

    adding more Vivado 2016.4 support
    Former-commit-id: fc5211dade46a4367d991ec535d17205843da39a
    Former-commit-id: 7712173c1154f1d07feed66f5bddd1ac5c9d7bff
    Former-commit-id: aed2009dedb5c190fdc2a3139a2a12605fed4747

--- xilinx/general/microblaze/ruckus.tcl
+++ xilinx/general/microblaze/ruckus.tcl
@@ -6,6 +6,8 @@ loadSource -path "$::DIR_PATH/bd/MicroblazeBasicCoreWrapper.vhd"
 
 if { $::env(VIVADO_VERSION) <= 2016.2 } {
    loadBlockDesign -path "$::DIR_PATH/bd/2016.2/MicroblazeBasicCore.bd"
+} elseif { $::env(VIVADO_VERSION) == 2016.3 } {
+   puts "\n\nError: $::DIR_PATH/bd/MicroblazeBasicCore doesn't support Vivado 2016.2\n\n"
 } else {
-   loadBlockDesign -path "$::DIR_PATH/bd/2016.3/MicroblazeBasicCore.bd"
+   loadBlockDesign -path "$::DIR_PATH/bd/2016.4/MicroblazeBasicCore.bd"
 }
