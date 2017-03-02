cbec4894db255dbee07a40e1db819ec4b58e001b
Benjamin Reese <bareese@slac.stanford.edu>
2017-03-02 12:30:11 -0800

    Remove SEM XCI from includes list. It is rarely used. If a target needs it it should manually declare it in it's own ruckus.tcl

--- xilinx/7Series/sem/ruckus.tcl
+++ xilinx/7Series/sem/ruckus.tcl
@@ -3,5 +3,9 @@ source -quiet $::env(RUCKUS_DIR)/vivado_proc.tcl
 
 # Load Source Code
 loadSource -dir  "$::DIR_PATH/rtl/"
-loadIpCore -path "$::DIR_PATH/coregen/SemCore.xci"
+
+# SEM Core XCI is not included by default since very few targets will actually need it.
+# Targets that do need it should specify it in their top level ruckus.tcl
+
+# loadIpCore -path "$::DIR_PATH/coregen/SemCore.xci"
 # loadSource -path "$::DIR_PATH/coregen/SemMon.dcp"
