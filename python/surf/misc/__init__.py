902bceb7ebd417f413d295cbabcfac9cefcdd0d2
Jesus Vasquez <jvasquez@slac.stanford.edu>
2017-05-15 11:41:46 -0700

    Merged duplicated generic memory class

--- python/surf/misc/__init__.py
+++ python/surf/misc/__init__.py
@@ -8,5 +8,4 @@
 ## may be copied, modified, propagated, or distributed except according to 
 ## the terms contained in the LICENSE.txt file.
 ##############################################################################
-from surf.misc._genericmemory import *
 from surf.misc._GenericMemory import *
