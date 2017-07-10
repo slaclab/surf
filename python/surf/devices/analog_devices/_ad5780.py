5eb982750a3184d45b492f77c75ddc3a1a00d640
Benjamin Reese <bareese@slac.stanford.edu>
2017-07-10 11:19:25 -0700

    Remove deprecated pyrogue calls. Update to rogue v2.

--- python/surf/devices/analog_devices/_ad5780.py
+++ python/surf/devices/analog_devices/_ad5780.py
@@ -25,46 +25,104 @@ class Ad5780(pr.Device):
         super(self.__class__, self).__init__(name, "Ad5780 Module",
                                              memBase, offset, hidden)
                                              
-        self.add(pr.Variable(name='dacRefreshRate',
-                description='DAC Rate (in units of Hz)',
-                offset=0x040, bitSize=32, bitOffset=0, base='uint', mode='RO'))
+        self.add(pr.RemoteVariable(
+            name='dacRefreshRate',
+            description='DAC Rate (in units of Hz)',
+            offset=0x040,
+            bitSize=32,
+            bitOffset=0,
+            base=pr.UInt,
+            disp={:d},
+            mode='RO'))
 
-        self.add(pr.Variable(name='dacData',
-                description='DAC Data',
-                offset=0x0C0, bitSize=18, bitOffset=0, base='hex', mode='RO')) 
+        self.add(pr.RemoteVariable(
+            name='dacData',
+            description='DAC Data',
+            offset=0x0C0,
+            bitSize=18,
+            bitOffset=0,
+            base=pr.UInt,
+            mode='RO')) 
 
-        self.add(pr.Variable(name='debugMux',
-                description='debugMux',
-                offset=0x200, bitSize=1, bitOffset=0, base='bool', mode='RW'))                 
-
-        self.add(pr.Variable(name='debugData',
-                description='debugData',
-                offset=0x240, bitSize=18, bitOffset=0, base='hex', mode='RW'))                 
+        self.add(pr.RemoteVariable(
+            name='debugMux',
+            description='debugMux',
+            offset=0x200,
+            bitSize=1,
+            bitOffset=0,
+            base=pr.Bool,
+            mode='RW'))
+        
+        self.add(pr.RemoteVariable(
+            name='debugData',
+            description='debugData',
+            offset=0x240,
+            bitSize=18,
+            bitOffset=0,
+            base=pr.UInt,
+            mode='RW'))                 
                 
-        self.add(pr.Variable(name='sdoDisable',
-                description='sdoDisable',
-                offset=0x280, bitSize=1, bitOffset=0, base='bool', mode='RW')) 
+        self.add(pr.RemoteVariable(
+            name='sdoDisable',
+            description='sdoDisable',
+            offset=0x280,
+            bitSize=1,
+            bitOffset=0,
+            base=pr.Bool,
+            mode='RW')) 
                 
-        self.add(pr.Variable(name='binaryOffset',
-                description='binaryOffset',
-                offset=0x284, bitSize=1, bitOffset=0, base='bool', mode='RW'))  
+        self.add(pr.RemoteVariable(
+            name='binaryOffset',
+            description='binaryOffset',
+            offset=0x284,
+            bitSize=1,
+            bitOffset=0,
+            base=pr.Bool,
+            mode='RW'))  
                 
-        self.add(pr.Variable(name='dacTriState',
-                description='dacTriState',
-                offset=0x288, bitSize=1, bitOffset=0, base='bool', mode='RW')) 
+        self.add(pr.RemoteVariable(
+            name='dacTriState',
+            description='dacTriState',
+            offset=0x288,
+            bitSize=1,
+            bitOffset=0,
+            base=pr.Bool,
+            mode='RW')) 
                 
-        self.add(pr.Variable(name='opGnd',
-                description='opGnd',
-                offset=0x28C, bitSize=1, bitOffset=0, base='bool', mode='RW'))   
+        self.add(pr.RemoteVariable(
+            name='opGnd',
+            description='opGnd',
+            offset=0x28C,
+            bitSize=1,
+            bitOffset=0,
+            base=pr.Bool,
+            mode='RW'))   
                 
-        self.add(pr.Variable(name='rbuf',
-                description='rbuf',
-                offset=0x290, bitSize=1, bitOffset=0, base='bool', mode='RW'))                   
+        self.add(pr.RemoteVariable(
+            name='rbuf',
+            description='rbuf',
+            offset=0x290,
+            bitSize=1,
+            bitOffset=0,
+            base=pr.Bool,
+            mode='RW'))                   
                 
-        self.add(pr.Variable(name='halfSckPeriod',
-                description='halfSckPeriod',
-                offset=0x294, bitSize=32, bitOffset=0, base='hex', mode='RW'))                
+        self.add(pr.RemoteVariable(
+            name='halfSckPeriod',
+            description='halfSckPeriod',
+            offset=0x294,
+            bitSize=32,
+            bitOffset=0,
+            base=pr.UInt,
+            mode='RW'))                
                 
-        self.add(pr.Variable(name='hardReset',description='HardReset',
-                offset=0x3F8, bitSize=1, bitOffset=0, base='hex', mode='WO', hidden=False)) 
+        self.add(pr.RemoteVariable(
+            name='hardReset',
+            description='HardReset',
+            offset=0x3F8,
+            bitSize=1,
+            bitOffset=0,
+            base=pr.UInt,
+            mode='WO',
+            hidden=False)) 
                 
