5203e82a7413a9855102044f48a0515486d0dda3
Larry Ruckman <ruckman@slac.stanford.edu>
2017-07-13 09:12:55 -0700

    pyrogue update

--- python/surf/devices/analog_devices/_ad5780.py
+++ python/surf/devices/analog_devices/_ad5780.py
@@ -28,103 +28,112 @@ class Ad5780(pr.Device):
         super().__init__(name=name, description=description, **kwargs)
                                              
         self.add(pr.RemoteVariable(
-            name='dacRefreshRate',
-            description='DAC Rate (in units of Hz)',
-            offset=0x040,
-            bitSize=32,
-            bitOffset=0,
-            base=pr.UInt,
-            # disp={:d},
-            mode='RO'))
+            name        = 'dacRefreshRate',
+            description = 'DAC Rate (in units of Hz)',
+            offset      = 0x040,
+            bitSize     = 32,
+            bitOffset   = 0,
+            base        = pr.UInt,
+            mode        = 'RO',
+        ))
 
         self.add(pr.RemoteVariable(
-            name='dacData',
-            description='DAC Data',
-            offset=0x0C0,
-            bitSize=18,
-            bitOffset=0,
-            base=pr.UInt,
-            mode='RO')) 
+            name        = 'dacData',
+            description = 'DAC Data',
+            offset      = 0x0C0,
+            bitSize     = 18,
+            bitOffset   = 0,
+            base        = pr.UInt,
+            mode        = 'RO',
+        )) 
 
         self.add(pr.RemoteVariable(
-            name='debugMux',
-            description='debugMux',
-            offset=0x200,
-            bitSize=1,
-            bitOffset=0,
-            base=pr.Bool,
-            mode='RW'))
+            name        = 'debugMux',
+            description = 'debugMux',
+            offset      = 0x200,
+            bitSize     = 1,
+            bitOffset   = 0,
+            base        = pr.Bool,
+            mode        = 'RW',
+        ))
         
         self.add(pr.RemoteVariable(
-            name='debugData',
-            description='debugData',
-            offset=0x240,
-            bitSize=18,
-            bitOffset=0,
-            base=pr.UInt,
-            mode='RW'))                 
+            name        = 'debugData',
+            description = 'debugData',
+            offset      = 0x240,
+            bitSize     = 18,
+            bitOffset   = 0,
+            base        = pr.UInt,
+            mode        = 'RW',
+        ))                 
                 
         self.add(pr.RemoteVariable(
-            name='sdoDisable',
-            description='sdoDisable',
-            offset=0x280,
-            bitSize=1,
-            bitOffset=0,
-            base=pr.Bool,
-            mode='RW')) 
+            name        = 'sdoDisable',
+            description = 'sdoDisable',
+            offset      = 0x280,
+            bitSize     = 1,
+            bitOffset   = 0,
+            base        = pr.Bool,
+            mode        = 'RW',
+        )) 
                 
         self.add(pr.RemoteVariable(
-            name='binaryOffset',
-            description='binaryOffset',
-            offset=0x284,
-            bitSize=1,
-            bitOffset=0,
-            base=pr.Bool,
-            mode='RW'))  
+            name        = 'binaryOffset',
+            description = 'binaryOffset',
+            offset      = 0x284,
+            bitSize     = 1,
+            bitOffset   = 0,
+            base        = pr.Bool,
+            mode        = 'RW',
+        ))  
                 
         self.add(pr.RemoteVariable(
-            name='dacTriState',
-            description='dacTriState',
-            offset=0x288,
-            bitSize=1,
-            bitOffset=0,
-            base=pr.Bool,
-            mode='RW')) 
+            name        = 'dacTriState',
+            description = 'dacTriState',
+            offset      = 0x288,
+            bitSize     = 1,
+            bitOffset   = 0,
+            base        = pr.Bool,
+            mode        = 'RW',
+        )) 
                 
         self.add(pr.RemoteVariable(
-            name='opGnd',
-            description='opGnd',
-            offset=0x28C,
-            bitSize=1,
-            bitOffset=0,
-            base=pr.Bool,
-            mode='RW'))   
+            name        = 'opGnd',
+            description = 'opGnd',
+            offset      = 0x28C,
+            bitSize     = 1,
+            bitOffset   = 0,
+            base        = pr.Bool,
+            mode        = 'RW',
+        ))   
                 
         self.add(pr.RemoteVariable(
-            name='rbuf',
-            description='rbuf',
-            offset=0x290,
-            bitSize=1,
-            bitOffset=0,
-            base=pr.Bool,
-            mode='RW'))                   
+            name        = 'rbuf',
+            description = 'rbuf',
+            offset      = 0x290,
+            bitSize     = 1,
+            bitOffset   = 0,
+            base        = pr.Bool,
+            mode        = 'RW',
+        ))                   
                 
         self.add(pr.RemoteVariable(
-            name='halfSckPeriod',
-            description='halfSckPeriod',
-            offset=0x294,
-            bitSize=32,
-            bitOffset=0,
-            base=pr.UInt,
-            mode='RW'))                
+            name        = 'halfSckPeriod',
+            description = 'halfSckPeriod',
+            offset      = 0x294,
+            bitSize     = 32,
+            bitOffset   = 0,
+            base        = pr.UInt,
+            mode        = 'RW',
+        ))                
                 
         self.add(pr.RemoteVariable(
-            name='hardReset',
-            description='HardReset',
-            offset=0x3F8,
-            bitSize=1,
-            bitOffset=0,
-            base=pr.UInt,
-            mode='WO',
-            hidden=False)) 
-                
+            name        = 'hardReset',
+            description = 'HardReset',
+            offset      = 0x3F8,
+            bitSize     = 1,
+            bitOffset   = 0,
+            base        = pr.UInt,
+            mode        = 'WO',
+            hidden      = False,
+        )) 
