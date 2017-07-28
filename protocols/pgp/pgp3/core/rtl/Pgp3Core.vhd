db55d40009d92b647b64e8bd3fd97704c0db7eb2
Benjamin Reese <bareese@slac.stanford.edu>
2017-07-28 15:55:53 -0700

    New module with Pgp3Tx and Pgp3Rx

---
+++ protocols/pgp/pgp3/core/rtl/Pgp3Core.vhd
@@ -0,0 +1,145 @@
+-------------------------------------------------------------------------------
+-- Title      : 
+-------------------------------------------------------------------------------
+-- Company    : SLAC National Accelerator Laboratory
+-- Platform   : 
+-- Standard   : VHDL'93/02
+-------------------------------------------------------------------------------
+-- Description: 
+-------------------------------------------------------------------------------
+-- This file is part of SURF. It is subject to
+-- the license terms in the LICENSE.txt file found in the top-level directory
+-- of this distribution and at:
+--    https://confluence.slac.stanford.edu/display/ppareg/LICENSE.html.
+-- No part of SURF, including this file, may be
+-- copied, modified, propagated, or distributed except according to the terms
+-- contained in the LICENSE.txt file.
+-------------------------------------------------------------------------------
+library ieee;
+use ieee.std_logic_1164.all;
+
+use work.StdRtlPkg.all;
+use work.AxiStreamPkg.all;
+use work.Pgp3Pkg.all;
+
+entity Pgp3Core is
+
+   generic (
+      TPD_G                           : time                  := 1 ns;
+      NUM_VC_G                        : integer range 1 to 16 := 4;
+      PGP_RX_ENABLE_G                 : boolean               := true;
+      RX_ALIGN_GOOD_COUNT_G           : integer               := 128;
+      RX_ALIGN_BAD_COUNT_G            : integer               := 16;
+      RX_ALIGN_SLIP_WAIT_G            : integer               := 32;
+      PGP_TX_ENABLE_G                 : boolean               := true;
+      TX_CELL_WORDS_MAX_G             : integer               := 256;  -- Number of 64-bit words per cell
+      TX_SKP_INTERVAL_G               : integer               := 5000;
+      TX_SKP_BURST_SIZE_G             : integer               := 8;
+      TX_MUX_MODE_G                   : string                := "INDEXED";  -- Or "ROUTED"
+      TX_MUX_TDEST_ROUTES_G           : Slv8Array             := (0 => "--------");  -- Only used in ROUTED mode
+      TX_MUX_TDEST_LOW_G              : integer range 0 to 7  := 0;
+      TX_MUX_INTERLEAVE_EN_G          : boolean               := true;
+      TX_MUX_INTERLEAVE_ON_NOTVALID_G : boolean               := true);
+   port (
+      -- Tx interface
+      pgpTxClk     : in  sl;
+      pgpTxRst     : in  sl;
+      pgpTxIn      : in  Pgp3TxInType;
+      pgpTxOut     : out Pgp3TxOutType;
+      pgpTxMasters : in  AxiStreamMasterArray(NUM_VC_G-1 downto 0);
+      pgpTxSlaves  : out AxiStreamSlaveArray(NUM_VC_G-1 downto 0);
+      pgpTxCtrl    : out AxiStreamCtrlArray(NUM_VC_G-1 downto 0);
+
+      -- Tx Phy interface
+      phyTxReady    : in  sl;
+      phyTxStart    : out sl;
+      phyTxSequence : out slv(5 downto 0);
+      phyTxData     : out slv(63 downto 0);
+      phyTxHeader   : out slv(1 downto 0);
+
+      -- Rx user interface
+      pgpRxClk     : in  sl;
+      pgpRxRst     : in  sl;
+      pgpRxIn      : in  Pgp3RxInType;
+      pgpRxOut     : out Pgp3RxOutType;
+      pgpRxMasters : out AxiStreamMasterArray(NUM_VC_G-1 downto 0);
+      pgpRxCtrl    : in  AxiStreamCtrlArray(NUM_VC_G-1 downto 0);
+
+      -- rx phy interface
+      phyRxClk      : in  sl;
+      phyRxRst      : in  sl;
+      phyRxInit     : out sl;
+      phyRxReady    : in  sl;
+      phyRxValid    : in  sl;
+      phyRxHeader   : in  slv(1 downto 0);
+      phyRxData     : in  slv(63 downto 0);
+      phyRxStartSeq : in  sl;
+      phyRxSlip     : out sl);
+end entity Pgp3Core;
+
+architecture rtl of Pgp3Core is
+
+   signal locRxLinkReady : sl;
+   signal remRxFifoCtrl : AxiStreamCtrlArray(NUM_VC_G-1 downto 0);
+   signal remRxLinkReady : sl;
+
+begin
+
+   U_Pgp3Tx_1 : entity work.Pgp3Tx
+      generic map (
+         TPD_G                        => TPD_G,
+         NUM_VC_G                     => NUM_VC_G,
+         CELL_WORDS_MAX_G             => TX_CELL_WORDS_MAX_G,
+         SKP_INTERVAL_G               => TX_SKP_INTERVAL_G,
+         SKP_BURST_SIZE_G             => TX_SKP_BURST_SIZE_G,
+         MUX_MODE_G                   => TX_MUX_MODE_G,
+         MUX_TDEST_ROUTES_G           => TX_MUX_TDEST_ROUTES_G,
+         MUX_TDEST_LOW_G              => TX_MUX_TDEST_LOW_G,
+         MUX_INTERLEAVE_EN_G          => TX_MUX_INTERLEAVE_EN_G,
+         MUX_INTERLEAVE_ON_NOTVALID_G => TX_MUX_INTERLEAVE_ON_NOTVALID_G)
+      port map (
+         pgpTxClk       => pgpTxClk,        -- [in]
+         pgpTxRst       => pgpTxRst,        -- [in]
+         pgpTxIn        => pgpTxIn,         -- [in]
+         pgpTxOut       => pgpTxOut,        -- [out]
+         pgpTxMasters   => pgpTxMasters,    -- [in]
+         pgpTxSlaves    => pgpTxSlaves,     -- [out]
+         pgpTxCtrl      => pgpTxCtrl,       -- [out]
+         locRxFifoCtrl  => pgpRxCtrl,       -- [in]
+         locRxLinkReady => locRxLinkReady,  -- [in]
+         remRxFifoCtrl  => remRxFifoCtrl,   -- [in]
+         remRxLinkReady => remRxLinkReady,  -- [in]
+         phyTxReady     => phyTxReady,      -- [in]
+         phyTxStart     => phyTxStart,      -- [out]
+         phyTxSequence  => phyTxSequence,   -- [out]
+         phyTxData      => phyTxData,       -- [out]
+         phyTxHeader    => phyTxHeader);    -- [out]
+
+   U_Pgp3Rx_1 : entity work.Pgp3Rx
+      generic map (
+         TPD_G              => TPD_G,
+         NUM_VC_G           => NUM_VC_G,
+         ALIGN_GOOD_COUNT_G => RX_ALIGN_GOOD_COUNT_G,
+         ALIGN_BAD_COUNT_G  => RX_ALIGN_BAD_COUNT_G,
+         ALIGN_SLIP_WAIT_G  => RX_ALIGN_SLIP_WAIT_G)
+      port map (
+         pgpRxClk       => pgpRxClk,        -- [in]
+         pgpRxRst       => pgpRxRst,        -- [in]
+         pgpRxIn        => pgpRxIn,         -- [in]
+         pgpRxOut       => pgpRxOut,        -- [out]
+         pgpRxMasters   => pgpRxMasters,    -- [out]
+         pgpRxCtrl      => pgpRxCtrl,       -- [in]
+         remRxFifoCtrl  => remRxFifoCtrl,   -- [out]
+         remRxLinkReady => remRxLinkReady,  -- [out]
+         locRxLinkReady => locRxLinkReady,  -- [out]
+         phyRxClk       => phyRxClk,        -- [in]
+         phyRxRst       => phyRxRst,        -- [in]
+         phyRxInit      => phyRxInit,       -- [out]
+         phyRxReady     => phyRxReady,      -- [in]
+         phyRxValid     => phyRxValid,      -- [in]
+         phyRxHeader    => phyRxHeader,     -- [in]
+         phyRxData      => phyRxData,       -- [in]
+         phyRxStartSeq  => phyRxStartSeq,   -- [in]
+         phyRxSlip      => phyRxSlip);      -- [out]
+
+end architecture rtl;
