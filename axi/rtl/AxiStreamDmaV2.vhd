0f692dc72444dc44824491e8c44fa7a42292e638
Larry Ruckman <ruckman@slac.stanford.edu>
2017-08-29 15:55:14 -0700

    ESCORE-215

--- axi/rtl/AxiStreamDmaV2.vhd
+++ axi/rtl/AxiStreamDmaV2.vhd
@@ -31,6 +31,7 @@ entity AxiStreamDmaV2 is
    generic (
       TPD_G             : time                    := 1 ns;
       DESC_AWIDTH_G     : integer range 4 to 12   := 12;
+      DESC_ARB_G        : boolean                 := true;
       AXIL_BASE_ADDR_G  : slv(31 downto 0)        := x"00000000";
       AXI_ERROR_RESP_G  : slv(1 downto 0)         := AXI_RESP_OK_C;
       AXI_READY_EN_G    : boolean                 := false;
@@ -93,6 +94,7 @@ begin
          AXI_READY_EN_G        => AXI_READY_EN_G,
          AXI_CONFIG_G          => AXI_DESC_CONFIG_G,
          DESC_AWIDTH_G         => DESC_AWIDTH_G,
+         DESC_ARB_G            => DESC_ARB_G,
          ACK_WAIT_BVALID_G     => true)
       port map (
          -- Clock/Reset
