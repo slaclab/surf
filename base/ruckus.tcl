f67d66cc2045a393d1117df927f04688898e5ada
Larry Ruckman <ruckman@slac.stanford.edu>
2017-09-12 21:45:53 -0700

    Added VHDL-2008 floating point and fixed point support

--- base/ruckus.tcl
+++ base/ruckus.tcl
@@ -6,3 +6,14 @@ loadRuckusTcl "$::DIR_PATH/fifo"
 loadRuckusTcl "$::DIR_PATH/general"
 loadRuckusTcl "$::DIR_PATH/ram"
 loadRuckusTcl "$::DIR_PATH/sync"
+
+# Added VHDL-2008 library for floating point DSP support if not supported in Vivado yet
+if { [VersionCheck 2017.2] <= 0 } {
+   loadSource -path "$::DIR_PATH/vhdl-libs/ieee2008/fixed_float_types.vhdl"      -lib "ieee" -fileType "VHDL 2008"
+   loadSource -path "$::DIR_PATH/vhdl-libs/ieee2008/fixed_generic_pkg.vhdl"      -lib "ieee" -fileType "VHDL 2008"
+   loadSource -path "$::DIR_PATH/vhdl-libs/ieee2008/fixed_generic_pkg-body.vhdl" -lib "ieee" -fileType "VHDL 2008"
+   loadSource -path "$::DIR_PATH/vhdl-libs/ieee2008/fixed_pkg.vhdl"              -lib "ieee" -fileType "VHDL 2008"
+   loadSource -path "$::DIR_PATH/vhdl-libs/ieee2008/float_generic_pkg.vhdl"      -lib "ieee" -fileType "VHDL 2008"
+   loadSource -path "$::DIR_PATH/vhdl-libs/ieee2008/float_generic_pkg-body.vhdl" -lib "ieee" -fileType "VHDL 2008"
+   loadSource -path "$::DIR_PATH/vhdl-libs/ieee2008/float_pkg.vhdl"              -lib "ieee" -fileType "VHDL 2008"
+}
--- dsp/ruckus.tcl
+++ dsp/ruckus.tcl
@@ -2,7 +2,7 @@
 source -quiet $::env(RUCKUS_DIR)/vivado_proc.tcl
 
 # Load Source Code
-loadSource -dir "$::DIR_PATH/rtl/"
+loadSource -dir "$::DIR_PATH/rtl" -fileType "VHDL 2008"
 
 # Load Simulation
 loadSource -sim_only -dir "$::DIR_PATH/tb/"
