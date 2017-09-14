babe0e7ec1cf31e89d1158ce2465b19e5d04e4ad
Larry Ruckman <ruckman@slac.stanford.edu>
2017-09-14 11:41:54 -0700

    using Vivado's copy of VHDL-2008, instead of surf's copy

--- base/ruckus.tcl
+++ base/ruckus.tcl
@@ -7,13 +7,17 @@ loadRuckusTcl "$::DIR_PATH/general"
 loadRuckusTcl "$::DIR_PATH/ram"
 loadRuckusTcl "$::DIR_PATH/sync"
 
-# Added VHDL-2008 library for floating point DSP support if not supported in Vivado yet
+# Added VHDL-2008 library for floating point DSP support if not supported in Vivado by default yet
 if { [VersionCheck 2017.2] <= 0 } {
-   loadSource -path "$::DIR_PATH/vhdl-libs/ieee2008/fixed_float_types.vhdl"      -lib "ieee" -fileType "VHDL 2008"
-   loadSource -path "$::DIR_PATH/vhdl-libs/ieee2008/fixed_generic_pkg.vhdl"      -lib "ieee" -fileType "VHDL 2008"
-   loadSource -path "$::DIR_PATH/vhdl-libs/ieee2008/fixed_generic_pkg-body.vhdl" -lib "ieee" -fileType "VHDL 2008"
-   loadSource -path "$::DIR_PATH/vhdl-libs/ieee2008/fixed_pkg.vhdl"              -lib "ieee" -fileType "VHDL 2008"
-   loadSource -path "$::DIR_PATH/vhdl-libs/ieee2008/float_generic_pkg.vhdl"      -lib "ieee" -fileType "VHDL 2008"
-   loadSource -path "$::DIR_PATH/vhdl-libs/ieee2008/float_generic_pkg-body.vhdl" -lib "ieee" -fileType "VHDL 2008"
-   loadSource -path "$::DIR_PATH/vhdl-libs/ieee2008/float_pkg.vhdl"              -lib "ieee" -fileType "VHDL 2008"
+   # Path to VHDL-2008
+   set vhdl2008Path "$::env(XILINX_VIVADO)/data/vhdl/src/ieee_2008"
+
+   # Add the fixed and floating point packages
+   loadSource -path "${vhdl2008Path}/fixed_float_types.vhdl"      -lib "ieee" -fileType "VHDL 2008"
+   loadSource -path "${vhdl2008Path}/fixed_generic_pkg.vhdl"      -lib "ieee" -fileType "VHDL 2008"
+   loadSource -path "${vhdl2008Path}/fixed_generic_pkg-body.vhdl" -lib "ieee" -fileType "VHDL 2008"
+   loadSource -path "${vhdl2008Path}/fixed_pkg.vhdl"              -lib "ieee" -fileType "VHDL 2008"
+   loadSource -path "${vhdl2008Path}/float_generic_pkg.vhdl"      -lib "ieee" -fileType "VHDL 2008"
+   loadSource -path "${vhdl2008Path}/float_generic_pkg-body.vhdl" -lib "ieee" -fileType "VHDL 2008"
+   loadSource -path "${vhdl2008Path}/float_pkg.vhdl"              -lib "ieee" -fileType "VHDL 2008"
 }
