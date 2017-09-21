b4665303b56d6458f0da3ba5262ba84c07ab9278
Ryan Herbst <rherbst@slac.stanford.edu>
2017-09-21 13:18:06 -0700

    remove prints for synthesis

--- base/general/rtl/TextUtilPkg.vhd
+++ base/general/rtl/TextUtilPkg.vhd
@@ -130,8 +130,10 @@ package body TextUtilPkg is
    procedure print(text : string) is
       variable msg_line : line;
    begin
+      -- sythensis translate_off
       write(msg_line, text);
       writeline(output, msg_line);
+      -- sythensis translate_on
    end print;
 
    -- prints text to the screen when active
