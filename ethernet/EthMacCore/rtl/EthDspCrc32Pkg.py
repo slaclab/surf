e0935da6d0470cd250c1d48129eb14c6b2763a66
Larry Ruckman <ruckman@slac.stanford.edu>
2017-09-25 13:58:13 -0700

    adding licensing

--- ethernet/EthMacCore/rtl/EthDspCrc32Pkg.py
+++ ethernet/EthMacCore/rtl/EthDspCrc32Pkg.py
@@ -1,4 +1,20 @@
 #!/usr/bin/env python3
+#-----------------------------------------------------------------------------
+# File       : EthDspCrc32Pkg.py
+# Created    : 2017-09-25
+# Last update: 2017-09-25
+#-----------------------------------------------------------------------------
+# Description:
+#-----------------------------------------------------------------------------
+# This file is part of the 'SLAC Firmware Standard Library'. It is subject to 
+# the license terms in the LICENSE.txt file found in the top-level directory 
+# of this distribution and at: 
+#    https://confluence.slac.stanford.edu/display/ppareg/LICENSE.html. 
+# No part of the 'SLAC Firmware Standard Library', including this file, may be 
+# copied, modified, propagated, or distributed except according to the terms 
+# contained in the LICENSE.txt file.
+#-----------------------------------------------------------------------------
+
 import sys
 
 ##################################################################################################
