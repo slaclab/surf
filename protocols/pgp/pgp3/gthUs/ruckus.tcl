99fe055b4a66acde7064c4ddcb6ec0b516491881
Larry Ruckman <ruckman@slac.stanford.edu>
2017-10-26 12:14:37 -0700

    using the DCP, instead of XCI

--- protocols/pgp/pgp3/gthUs/ruckus.tcl
+++ protocols/pgp/pgp3/gthUs/ruckus.tcl
@@ -3,8 +3,8 @@ source -quiet $::env(RUCKUS_DIR)/vivado_proc.tcl
 
 # Load Source Code
 loadSource -dir "$::DIR_PATH/rtl/"
-#loadSource -path "$::DIR_PATH/ip/Pgp3GthUsIp/Pgp3GthUsIp.dcp"
-loadIpCore -path "$::DIR_PATH/ip/Pgp3GthUsIp/Pgp3GthUsIp.xci"
+loadSource -path "$::DIR_PATH/ip/Pgp3GthUsIp/Pgp3GthUsIp.dcp"
+# loadIpCore -path "$::DIR_PATH/ip/Pgp3GthUsIp/Pgp3GthUsIp.xci"
 
 # Load Simulation
 #loadSource -sim_only -dir "$::DIR_PATH/tb/"
