217a147209bcaff2a654c3f588a32bf6f2b470bd
Till Straumann <strauman@slac.stanford.edu>
2017-11-29 14:02:24 -0800

    Renamed GigEthLVDS... -> GigEthLvds...

--- ethernet/GigEthCore/lvdsUltraScale/ruckus.tcl
+++ ethernet/GigEthCore/lvdsUltraScale/ruckus.tcl
@@ -3,10 +3,10 @@ source -quiet $::env(RUCKUS_DIR)/vivado_proc.tcl
 
 loadSource      -dir "$::DIR_PATH/rtl"
 
-loadConstraints -path "$::DIR_PATH/rtl/GigEthLVDSClockMux.xdc"
-set_property SCOPED_TO_REF    GigEthLVDSClockMux [get_files "$::DIR_PATH/rtl/GigEthLVDSClockMux.xdc"]
-set_property PROCESSING_ORDER LATE               [get_files "$::DIR_PATH/rtl/GigEthLVDSClockMux.xdc"]
+loadConstraints -path "$::DIR_PATH/rtl/GigEthLvdsClockMux.xdc"
+set_property SCOPED_TO_REF    GigEthLvdsClockMux [get_files "$::DIR_PATH/rtl/GigEthLvdsClockMux.xdc"]
+set_property PROCESSING_ORDER LATE               [get_files "$::DIR_PATH/rtl/GigEthLvdsClockMux.xdc"]
 
-loadConstraints -path "$::DIR_PATH/rtl/GigEthLVDSUltraScaleWrapper.xdc"
-set_property SCOPED_TO_REF    GigEthLVDSUltraScaleWrapper [get_files "$::DIR_PATH/rtl/GigEthLVDSUltraScaleWrapper.xdc"]
-set_property PROCESSING_ORDER LATE                        [get_files "$::DIR_PATH/rtl/GigEthLVDSUltraScaleWrapper.xdc"]
+loadConstraints -path "$::DIR_PATH/rtl/GigEthLvdsUltraScaleWrapper.xdc"
+set_property SCOPED_TO_REF    GigEthLvdsUltraScaleWrapper [get_files "$::DIR_PATH/rtl/GigEthLvdsUltraScaleWrapper.xdc"]
+set_property PROCESSING_ORDER LATE                        [get_files "$::DIR_PATH/rtl/GigEthLvdsUltraScaleWrapper.xdc"]
