0e39488a9e89ae733296968d00e7f49daf20efc2
Larry Ruckman <ruckman@slac.stanford.edu>
2017-12-06 12:33:40 -0800

    INCLUDE_ETH_SGMII_LVDS
    using INCLUDE_ETH_SGMII_LVDS Makefile variable to prevent the XDCs to be include when the Ethernet SGMII LVDS is not being used

--- ethernet/GigEthCore/lvdsUltraScale/ruckus.tcl
+++ ethernet/GigEthCore/lvdsUltraScale/ruckus.tcl
@@ -1,12 +1,18 @@
 # Load RUCKUS environment and library
 source -quiet $::env(RUCKUS_DIR)/vivado_proc.tcl
 
-loadSource      -dir "$::DIR_PATH/rtl"
+if { [info exists ::env(INCLUDE_ETH_SGMII_LVDS)] != 1 || $::env(INCLUDE_ETH_SGMII_LVDS) == 0 } {
+   set useEthSgmiiLvds 0
+} else {
 
-loadConstraints -path "$::DIR_PATH/rtl/GigEthLvdsClockMux.xdc"
-set_property SCOPED_TO_REF    GigEthLvdsClockMux [get_files "$::DIR_PATH/rtl/GigEthLvdsClockMux.xdc"]
-set_property PROCESSING_ORDER LATE               [get_files "$::DIR_PATH/rtl/GigEthLvdsClockMux.xdc"]
+   loadSource -dir "$::DIR_PATH/rtl"
 
-loadConstraints -path "$::DIR_PATH/rtl/GigEthLvdsUltraScaleWrapper.xdc"
-set_property SCOPED_TO_REF    GigEthLvdsUltraScaleWrapper [get_files "$::DIR_PATH/rtl/GigEthLvdsUltraScaleWrapper.xdc"]
-set_property PROCESSING_ORDER LATE                        [get_files "$::DIR_PATH/rtl/GigEthLvdsUltraScaleWrapper.xdc"]
+   loadConstraints -path "$::DIR_PATH/xdc/GigEthLvdsClockMux.xdc"
+   set_property SCOPED_TO_REF    GigEthLvdsClockMux [get_files "$::DIR_PATH/xdc/GigEthLvdsClockMux.xdc"]
+   set_property PROCESSING_ORDER LATE               [get_files "$::DIR_PATH/xdc/GigEthLvdsClockMux.xdc"]
+
+   loadConstraints -path "$::DIR_PATH/xdc/GigEthLvdsUltraScaleWrapper.xdc"
+   set_property SCOPED_TO_REF    GigEthLvdsUltraScaleWrapper [get_files "$::DIR_PATH/xdc/GigEthLvdsUltraScaleWrapper.xdc"]
+   set_property PROCESSING_ORDER LATE                        [get_files "$::DIR_PATH/xdc/GigEthLvdsUltraScaleWrapper.xdc"]
+   
+}
