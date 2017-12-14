369b76723434d2cbae58a3f1519a1368d31dc22c
Ryan Herbst <rherbst@slac.stanford.edu>
2017-12-13 21:00:31 -0800

    undo clock and packer changes

--- xilinx/7Series/general/rtl/ClockManager7.vhd
+++ xilinx/7Series/general/rtl/ClockManager7.vhd
@@ -32,11 +32,8 @@ entity ClockManager7 is
       TPD_G                  : time                             := 1 ns;
       TYPE_G                 : string                           := "MMCM";  -- or "PLL"
       INPUT_BUFG_G           : boolean                          := true;
-      INPUT_BUFR_G           : boolean                          := false;
       FB_BUFG_G              : boolean                          := true;
-      FB_BUFR_G              : boolean                          := false;
       OUTPUT_BUFG_G          : boolean                          := true;
-      OUTPUT_BUFR_G          : boolean                          := false;
       RST_IN_POLARITY_G      : sl                               := '1';     -- '0' for active low
       NUM_CLOCKS_G           : integer range 1 to 7;
       AXI_ERROR_RESP_G       : slv(1 downto 0)                  := AXI_RESP_DECERR_C;
@@ -279,17 +276,7 @@ begin
             O => clkInLoc);
    end generate;
 
-   InputBufrGen : if (not INPUT_BUFG_G) and (INPUT_BUFR_G) generate
-      U_Bufg : BUFR
-         port map (
-            CE  => '1',
-            CLR => '0',
-            I   => clkIn,
-            O   => clkInLoc);
-   end generate;
-
-
-   InputNoBufg : if (not INPUT_BUFG_G) and (not INPUT_BUFR_G) generate
+   InputNoBufg : if (not INPUT_BUFG_G) generate
       clkInLoc <= clkIn;
    end generate;
 
@@ -300,16 +287,7 @@ begin
             O => clkFbIn);
    end generate;
 
-   FbBufrGen : if (not FB_BUFG_G) and (FB_BUFR_G) generate
-      U_Bufg : BUFR
-         port map (
-            CE  => '1',
-            CLR => '0',
-            I   => clkFbOut,
-            O   => clkFbIn);
-   end generate;
-
-   FbNoBufg : if (not FB_BUFG_G) and (not FB_BUFR_G) generate
+   FbNoBufg : if (not FB_BUFG_G) generate
       clkFbOut <= clkFbIn;
    end generate;
 
@@ -323,19 +301,7 @@ begin
       end generate;
    end generate OutBufgGen;
 
-   OutBufrGen : if (not OUTPUT_BUFG_G) and (OUTPUT_BUFR_G) generate
-      ClkOutGen : for i in NUM_CLOCKS_G-1 downto 0 generate
-         U_Bufg : BUFR
-            port map (
-               CE  => '1',
-               CLR => '0',
-               I   => clkOutMmcm(i),
-               O   => clkOutLoc(i));
-         clkOut(i) <= clkOutLoc(i);
-      end generate;
-   end generate OutBufrGen;
-
-   NoOutBufgGen : if (not OUTPUT_BUFG_G) and (not OUTPUT_BUFR_G) generate
+   NoOutBufgGen : if (not OUTPUT_BUFG_G) generate
       clkOutLoc <= clkOutMmcm;
       clkOut    <= clkOutLoc;
    end generate NoOutBufgGen;
