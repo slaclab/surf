ce1a3e394bdf16ed99c960bea78112b0254ab01a
Benjamin Reese <bareese@slac.stanford.edu>
2017-12-15 09:02:48 -0800

    Use DCP cores

--- protocols/pgp/pgp3/gthUs/ruckus.tcl
+++ protocols/pgp/pgp3/gthUs/ruckus.tcl
@@ -4,7 +4,7 @@ source -quiet $::env(RUCKUS_DIR)/vivado_proc.tcl
 # Load Source Code
 loadSource -dir "$::DIR_PATH/rtl/"
 loadSource -path "$::DIR_PATH/ip/Pgp3GthUsIp/Pgp3GthUsIp.dcp"
-# loadIpCore -path "$::DIR_PATH/ip/Pgp3GthUsIp/Pgp3GthUsIp.xci"
+#loadIpCore -path "$::DIR_PATH/ip/Pgp3GthUsIp/Pgp3GthUsIp.xci"
 
 # Load Simulation
 #loadSource -sim_only -dir "$::DIR_PATH/tb/"
