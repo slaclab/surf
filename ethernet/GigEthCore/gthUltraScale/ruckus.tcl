d0dc4a52330856d07d01335c76812ca0fc12be89
Larry Ruckman <ruckman@slac.stanford.edu>
2017-12-17 16:15:54 -0800

    Update ruckus.tcl
    Updating the Vivado Version lock

--- ethernet/GigEthCore/gthUltraScale/ruckus.tcl
+++ ethernet/GigEthCore/gthUltraScale/ruckus.tcl
@@ -2,7 +2,7 @@
 source -quiet $::env(RUCKUS_DIR)/vivado_proc.tcl
 
 # Load Source Code
-if { $::env(VIVADO_VERSION) >= 2016.1 } {
+if { $::env(VIVADO_VERSION) >= 2016.4 } {
    loadSource -dir  "$::DIR_PATH/rtl"
    loadSource -path "$::DIR_PATH/images/GigEthGthUltraScaleCore.dcp"
 } else {
