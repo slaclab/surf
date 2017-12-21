75319f260f330a16c8a8920c50a75eaa10d95e46
Larry Ruckman <ruckman@slac.stanford.edu>
2017-12-20 19:25:43 -0800

    updating SaltUltraScaleRxOnly.dcp to Vivado 2017.3 core

--- protocols/salt/xilinxUltraScale/ruckus.tcl
+++ protocols/salt/xilinxUltraScale/ruckus.tcl
@@ -6,7 +6,7 @@ if { $::env(VIVADO_VERSION) >= 2016.4 } {
 
    loadSource -dir  "$::DIR_PATH/rtl"
    loadSource -path "$::DIR_PATH/ip/SaltUltraScaleCore.dcp"
-   loadSource -path "$::DIR_PATH/images/SaltUltraScaleRxOnly.dcp"
+   # loadSource -path "$::DIR_PATH/images/SaltUltraScaleRxOnly.dcp"
    loadSource -path "$::DIR_PATH/images/SaltUltraScaleTxOnly.dcp"
 
    # Load Simulation
@@ -14,4 +14,12 @@ if { $::env(VIVADO_VERSION) >= 2016.4 } {
    
 } else {
    puts "\n\nWARNING: $::DIR_PATH requires Vivado 2016.4 (or later)\n\n"
+} 
+
+if { $::env(VIVADO_VERSION) >= 2017.3 } {
+
+   loadSource -path "$::DIR_PATH/images/SaltUltraScaleRxOnly.dcp"
+   
+} else {
+   puts "\n\nWARNING: $::DIR_PATH/images/SaltUltraScaleRxOnly.dcp requires Vivado 2016.4 (or later)\n\n"
 }  
