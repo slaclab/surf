603c35cedde7949703017edbfef8812de46b65ff
till straumann <till.straumann@alumni.tu-berlin.de>
2017-12-26 00:03:51 -0600

    Broke stub and implementation architectures into separate files
    Vivado isn't really good at vhdl configurations. Thus, we let
    ruckus pick either the stub or the 'true' implementation.

--- protocols/jtag/ruckus.tcl
+++ protocols/jtag/ruckus.tcl
@@ -1,13 +1,31 @@
 # Load RUCKUS library
 source -quiet $::env(RUCKUS_DIR)/vivado_proc.tcl
 
+if { [info exists ::env(USE_XVC_DEBUG)] != 1 || $::env(USE_XVC_DEBUG) == 0 } {
+
+	loadSource -path  "$::DIR_PATH/rtl/AxisDebugBridge.vhd"
+	loadSource -path  "$::DIR_PATH/rtl/AxisDebugBridgeStub.vhd"
+
+} else {
+
 # Create Debug Bridge IP -- but only if it doesn't exist yet
 # which can happen if this is re-run.
-if { [llength [get_ips DebugBridgeJtag]] == 0 } { 
-    create_ip -name debug_bridge -vendor xilinx.com -library ip -module_name DebugBridgeJtag 
-	# C_DEBUG_MODE selects JTAG <-> BSCAN mode
-    set_property -dict [list CONFIG.C_DEBUG_MODE {4}] [get_ips DebugBridgeJtag] 
-}
+	if { [llength [get_ips DebugBridgeJtag]] == 0 } {
+    	create_ip -name debug_bridge -vendor xilinx.com -library ip -module_name DebugBridgeJtag
+		# C_DEBUG_MODE selects JTAG <-> BSCAN mode
+    	set_property -dict [list CONFIG.C_DEBUG_MODE {4}] [get_ips DebugBridgeJtag]
+	}
 
-# Load Source Code
-loadSource -dir  "$::DIR_PATH/rtl/"
+	foreach f {
+		AxisDebugBridge.vhd
+		AxisDebugBridgeImpl.vhd
+		AxisToJtag.vhd
+		AxisToJtagCore.vhd
+		AxisToJtagPkg.vhd
+		AxiStreamSelector.vhd
+		JtagSerDesCore.vhd
+	} {
+		loadSource -path  "$::DIR_PATH/rtl/$f"
+	}
+
+}
