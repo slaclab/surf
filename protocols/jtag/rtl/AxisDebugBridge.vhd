f176e7b837a384cead83d122e637a96eda69a467
Till Straumann <strauman@slac.stanford.edu>
2017-12-30 09:14:57 -0800

    Moved stub and 'real' architectures of AxisDebugBridge back into single file
    The user can pick the desired architecture.

--- protocols/jtag/rtl/AxisDebugBridge.vhd
+++ protocols/jtag/rtl/AxisDebugBridge.vhd
@@ -50,3 +50,159 @@ entity AxisDebugBridge is
       sAxisTdo         : in  AxiStreamSlaveType
    );
 end entity AxisDebugBridge;
+
+architecture AxisDebugBridgeImpl of AxisDebugBridge is
+
+   -- IP
+   -- Must be generated and named DebugBridgeJtag, e.g., with TCL commands:
+   --
+   --    create_ip -name debug_bridge -vendor xilinx.com -library ip -module_name DebugBridgeJtag
+   --    # C_DEBUG_MODE selects JTAG <-> BSCAN mode
+   --    set_property -dict [list CONFIG.C_DEBUG_MODE {4}] [get_ips DebugBridgeJtag]
+   --
+
+   component DebugBridgeJtag is
+     port (
+       jtag_tdi : in  std_logic;
+       jtag_tdo : out std_logic;
+       jtag_tms : in  std_logic;
+       jtag_tck : in  std_logic
+     );
+   end component DebugBridgeJtag;
+
+   signal tck, tdi, tms, tdo : sl;
+
+begin
+
+   U_AXIS_JTAG : entity work.AxisToJtag
+      generic map (
+         TPD_G        => TPD_G,
+         AXIS_WIDTH_G => AXIS_WIDTH_G,
+         AXIS_FREQ_G  => AXIS_FREQ_G,
+         CLK_DIV2_G   => CLK_DIV2_G,
+         MEM_DEPTH_G  => MEM_DEPTH_G,
+         MEM_STYLE_G  => MEM_STYLE_G
+      )
+      port map (
+         axisClk      => axisClk,
+         axisRst      => axisRst,
+
+         mAxisReq     => mAxisReq,
+         sAxisReq     => sAxisReq,
+
+         mAxisTdo     => mAxisTdo,
+         sAxisTdo     => sAxisTdo,
+
+         tck          => tck,
+         tms          => tms,
+         tdi          => tdi,
+         tdo          => tdo
+      );
+
+   U_JTAG_BSCAN : component DebugBridgeJtag
+      port map (
+         jtag_tdi     => tdi,
+         jtag_tdo     => tdo,
+         jtag_tms     => tms,
+         jtag_tck     => tck
+      );
+
+end architecture AxisDebugBridgeImpl;
+
+architecture AxisDebugBridgeStub of AxisDebugBridge is
+
+   type StateType is (READY, SKIP, REPLY);
+
+   type RegType is record
+      state    : StateType;
+      repValid : sl;
+      repData  : slv(31 downto 0);
+      reqReady : sl;
+   end record RegType;
+
+   constant REG_INIT_C : RegType := (
+      state    => READY,
+      repValid => '0',
+      repData  => (others => '0'),
+      reqReady => '1'
+   );
+
+   signal mReply : AxiStreamMasterType := AXI_STREAM_MASTER_INIT_C;
+   signal sReq   : AxiStreamSlaveType  := AXI_STREAM_SLAVE_INIT_C;
+
+   signal r      : RegType             := REG_INIT_C;
+   signal rin    : RegType;
+
+begin
+
+   sReq.tReady               <= r.reqReady;
+   mReply.tValid             <= r.repValid;
+   mReply.tLast              <= '1';
+   mReply.tData(31 downto 0) <= r.repData;
+
+   sAxisReq                  <= sReq;
+   mAxisTdo                  <= mReply;
+
+   U_COMB : process(r, mAxisReq, sAxisTdo) is
+      variable v : RegType;
+   begin
+      v := r;
+
+      case (r.state) is
+
+         when READY =>
+            if ( mAxisReq.tValid = '1' ) then
+               v.repData := ( others => '0' );
+               if ( getVersion( mAxisReq.tData ) /= PRO_VERSN_C ) then
+                  setVersion( PRO_VERSN_C      , v.repData );
+                  setErr    ( ERR_BAD_VERSION_C, v.repData );
+               elsif ( getCommand( mAxisReq.tData ) /= CMD_QUERY_C ) then
+                  setErr    ( ERR_BAD_COMMAND_C, v.repData );
+               else
+                  setErr    ( ERR_NOT_PRESENT_C, v.repData );
+               end if;
+               if ( mAxisReq.tLast = '1' ) then
+                  v.reqReady := '0';
+               end if;
+               v.repValid := '1';
+               v.state    := REPLY;
+            end if;
+
+         when REPLY =>
+            if ( (mAxisReq.tValid and mAxisReq.tLast and r.reqReady) = '1' ) then
+               v.reqReady := '0';
+            end if;
+            if ( sAxisTdo.tReady = '1' ) then
+               v.repValid := '0';
+               if ( v.reqReady = '1' ) then
+                  -- no TLAST seen yet
+                  v.state := SKIP;
+               else
+                  v.reqReady := '1';
+                  v.state    := READY;
+               end if;
+            end if;
+
+         when SKIP =>
+            if ( (mAxisReq.tValid and mAxisReq.tLast) = '1' ) then
+               v.state := READY;
+            end if;
+
+      end case;
+
+      rin <= v;
+
+   end process U_comb;
+
+   U_SEQ : process( axisClk ) is
+   begin
+      if ( rising_edge( axisClk ) ) then
+         if ( axisRst /= '0' ) then
+            r <= REG_INIT_C after TPD_G;
+         else
+            r <= rin after TPD_G;
+         end if;
+      end if;
+   end process U_seq;
+
+end architecture AxisDebugBridgeStub;
