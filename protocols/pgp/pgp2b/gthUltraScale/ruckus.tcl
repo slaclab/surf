1c29df1d6d79cc6a27497eeccfd0a0ebc37072d7
Benjamin Reese <bareese@slac.stanford.edu>
2018-01-11 14:21:30 -0800

    Remove XDC load

--- protocols/pgp/pgp2b/gthUltraScale/ruckus.tcl
+++ protocols/pgp/pgp2b/gthUltraScale/ruckus.tcl
@@ -8,11 +8,11 @@ if { $::env(VIVADO_VERSION) >= 2017.3 } {
 
    loadSource   -path "$::DIR_PATH/ip/PgpGthCore.dcp"
    # loadIpCore  -path "$::DIR_PATH/ip/PgpGthCore.xci" 
-   loadConstraints -path "$::DIR_PATH/rtl/Pgp2bGthUltra.xdc"
+   #loadConstraints -path "$::DIR_PATH/rtl/Pgp2bGthUltra.xdc"
 
-   set_property PROCESSING_ORDER {EARLY}   [get_files {Pgp2bGthUltra.xdc}]
-   set_property SCOPED_TO_REF {PgpGthCore} [get_files {Pgp2bGthUltra.xdc}]
-   set_property SCOPED_TO_CELLS {inst}     [get_files {Pgp2bGthUltra.xdc}]
+   #set_property PROCESSING_ORDER {EARLY}   [get_files {Pgp2bGthUltra.xdc}]
+   #set_property SCOPED_TO_REF {PgpGthCore} [get_files {Pgp2bGthUltra.xdc}]
+   #set_property SCOPED_TO_CELLS {inst}     [get_files {Pgp2bGthUltra.xdc}]
 
 } else {
    puts "\n\nWARNING: $::DIR_PATH requires Vivado 2017.3 (or later)\n\n"
