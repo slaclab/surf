c8157a5caa2018e99283a571e1874dd36f4e31e5
Larry Ruckman <ruckman@slac.stanford.edu>
2018-01-11 17:20:52 -0800

    removing .XDCs
    because there is Ben discovered a bug where the DRP interface can't be applied when these constraints are active.

--- protocols/pgp/pgp2b/gthUltraScale/ruckus.tcl
+++ protocols/pgp/pgp2b/gthUltraScale/ruckus.tcl
@@ -7,18 +7,7 @@ if { $::env(VIVADO_VERSION) >= 2017.3 } {
    loadSource   -dir "$::DIR_PATH/rtl/"
 
    loadSource    -path "$::DIR_PATH/ip/PgpGthCore.dcp"
-   # loadIpCore  -path "$::DIR_PATH/ip/PgpGthCore.xci" 
-   
-   if { [info exists ::env(INCLUDE_PGP2B)] != 1 || $::env(INCLUDE_PGP2B) == 0 } {
-      set nop 0
-   } else {    
-   
-      loadConstraints -path "$::DIR_PATH/rtl/Pgp2bGthUltra.xdc"
-      set_property PROCESSING_ORDER {EARLY}   [get_files {Pgp2bGthUltra.xdc}]
-      set_property SCOPED_TO_REF {PgpGthCore} [get_files {Pgp2bGthUltra.xdc}]
-      set_property SCOPED_TO_CELLS {inst}     [get_files {Pgp2bGthUltra.xdc}]
-      
-   }      
+   # loadIpCore  -path "$::DIR_PATH/ip/PgpGthCore.xci"
 
 } else {
    puts "\n\nWARNING: $::DIR_PATH requires Vivado 2017.3 (or later)\n\n"
--- protocols/pgp/pgp3/gthUs/ruckus.tcl
+++ protocols/pgp/pgp3/gthUs/ruckus.tcl
@@ -10,17 +10,6 @@ if { $::env(VIVADO_VERSION) >= 2017.3 } {
    loadSource   -path "$::DIR_PATH/ip/Pgp3GthUsIp/Pgp3GthUsIp.dcp"
    # loadIpCore -path "$::DIR_PATH/ip/Pgp3GthUsIp/Pgp3GthUsIp.xci"
    
-   if { [info exists ::env(INCLUDE_PGP3)] != 1 || $::env(INCLUDE_PGP3) == 0 } {
-      set nop 0
-   } else {   
-   
-      loadConstraints -path "$::DIR_PATH/xdc/Pgp3GthUsIp.xdc"
-      set_property PROCESSING_ORDER {EARLY}       [get_files {Pgp3GthUsIp.xdc}]
-      set_property SCOPED_TO_REF    {Pgp3GthUsIp} [get_files {Pgp3GthUsIp.xdc}]
-      set_property SCOPED_TO_CELLS  {inst}        [get_files {Pgp3GthUsIp.xdc}]   
-      
-   }   
-   
 } else {
    puts "\n\nWARNING: $::DIR_PATH requires Vivado 2017.3 (or later)\n\n"
 } 
