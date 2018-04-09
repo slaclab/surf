9265e4c9ae0af576be3db018bf8455dcd980f6b4
mkwiatko <mwkwiatko@gmail.com>
2018-04-09 15:51:41 -0700

    removed AXI_ERROR_RESP_G

--- devices/Ti/ads42lb69/rtl/AxiAds42lb69Core.vhd
+++ devices/Ti/ads42lb69/rtl/AxiAds42lb69Core.vhd
@@ -33,7 +33,6 @@ entity AxiAds42lb69Core is
       DMODE_INIT_G       : slv(1 downto 0)                         := "00";
       DELAY_INIT_G       : Slv9VectorArray(1 downto 0, 7 downto 0) := (others => (others => (others => '0')));
       IODELAY_GROUP_G    : string                                  := "AXI_ADS42LB69_IODELAY_GRP";
-      AXI_ERROR_RESP_G   : slv(1 downto 0)                         := AXI_RESP_SLVERR_C;
       XIL_DEVICE_G       : string                                  := "7SERIES"
    );      
    port (
@@ -110,8 +109,7 @@ begin
          TPD_G              => TPD_G,
          SIM_SPEEDUP_G      => SIM_SPEEDUP_G,
          ADC_CLK_FREQ_G     => ADC_CLK_FREQ_G,
-         DMODE_INIT_G       => DMODE_INIT_G,
-         AXI_ERROR_RESP_G   => AXI_ERROR_RESP_G)
+         DMODE_INIT_G       => DMODE_INIT_G)
       port map(
          -- AXI-Lite Register Interface (axiClk domain)
          axiReadMaster  => axiReadMaster,
