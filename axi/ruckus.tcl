0821ca48ffa257261caf6ff9fba52c42436b06fd
Larry Ruckman <ruckman@slac.stanford.edu>
2018-04-09 12:08:36 -0700

    moving AXI-stream source code from axi/rtl to axi/axi-stream

--- axi/ruckus.tcl
+++ axi/ruckus.tcl
@@ -4,6 +4,7 @@ source -quiet $::env(RUCKUS_DIR)/vivado_proc.tcl
 # Load Source Code
 loadSource -dir "$::DIR_PATH/rtl"
 loadSource -dir "$::DIR_PATH/axi-lite"
+loadSource -dir "$::DIR_PATH/axi-stream"
 loadSource -dir "$::DIR_PATH/bridge"
 loadSource -dir "$::DIR_PATH/dma"
 
