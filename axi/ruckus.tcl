c5ca098f0994a5e0b229475a26ef4772f5377c07
Larry Ruckman <ruckman@slac.stanford.edu>
2018-04-12 09:20:14 -0700

    created a rtl and tb folders in each of axi-lite, axi-stream, axi4, etc.

--- axi/ruckus.tcl
+++ axi/ruckus.tcl
@@ -2,12 +2,11 @@
 source -quiet $::env(RUCKUS_DIR)/vivado_proc.tcl
 
 # Load Source Code
-loadSource -dir "$::DIR_PATH/axi4"
-loadSource -dir "$::DIR_PATH/axi-lite"
-loadSource -dir "$::DIR_PATH/axi-stream"
-loadSource -dir "$::DIR_PATH/bridge"
-loadSource -dir "$::DIR_PATH/dma"
+loadRuckusTcl "$::DIR_PATH/axi4"
+loadRuckusTcl "$::DIR_PATH/axi-lite"
+loadRuckusTcl "$::DIR_PATH/axi-stream"
+loadRuckusTcl "$::DIR_PATH/bridge"
+loadRuckusTcl "$::DIR_PATH/dma"
 
 # Load Simulation
-loadSource -sim_only -dir "$::DIR_PATH/tb"
 loadSource -sim_only -dir "$::DIR_PATH/simlink/sim"
