3d868939c5ad64a403c25f334fff055f7489a4cb
Larry Ruckman <ruckman@slac.stanford.edu>
2018-05-04 14:55:29 -0700

    more pipelining between elastic buffer and RX frammer
    also some code clean up

--- protocols/jesd204b/rtl/JesdRxLane.vhd
+++ protocols/jesd204b/rtl/JesdRxLane.vhd
@@ -2,7 +2,7 @@
 -- File       : JesdRxLane.vhd
 -- Company    : SLAC National Accelerator Laboratory
 -- Created    : 2015-04-14
--- Last update: 2017-11-10
+-- Last update: 2018-05-04
 -------------------------------------------------------------------------------
 -- Description: JesdRx single lane module
 --              Receiver JESD204b standard.
@@ -58,67 +58,49 @@ use work.Jesd204bPkg.all;
 
 entity JesdRxLane is
    generic (
-      TPD_G : time := 1 ns;
-
+      TPD_G : time     := 1 ns;
       -- Number of bytes in a frame
-      F_G : positive := 2;
-
+      F_G   : positive := 2;
       -- Number of frames in a multi frame
-      K_G : positive := 32
-      );
+      K_G   : positive := 32);
    port (
-
-      -- JESD
       -- Clocks and Resets   
-      devClk_i : in sl;
-      devRst_i : in sl;
-
+      devClk_i      : in  sl;
+      devRst_i      : in  sl;
       -- JESD subclass selection: '0' or '1'(default)     
-      subClass_i : in sl;
-
+      subClass_i    : in  sl;
       -- SYSREF for subclass 1 fixed latency
-      sysRef_i : in sl;
-
+      sysRef_i      : in  sl;
       -- Clear registered errors     
-      clearErr_i : in sl;
-
+      clearErr_i    : in  sl;
       -- Control register
-      enable_i     : in  sl;
-      replEnable_i : in  sl;
-      scrEnable_i  : in  sl;
-      status_o     : out slv((RX_STAT_WIDTH_C)-1 downto 0);
-
+      enable_i      : in  sl;
+      replEnable_i  : in  sl;
+      scrEnable_i   : in  sl;
+      status_o      : out slv((RX_STAT_WIDTH_C)-1 downto 0);
       -- Data and character inputs from GT (transceivers)
-      r_jesdGtRx : in jesdGtRxLaneType;
-
+      r_jesdGtRx    : in  jesdGtRxLaneType;
       -- Local multi frame clock
-      lmfc_i : in sl;
-
+      lmfc_i        : in  sl;
       -- Error mask
-      linkErrMask_i : in slv(5 downto 0) := (others => '1');
-
+      linkErrMask_i : in  slv(5 downto 0) := (others => '1');
       -- One or more RX modules requested synchronization
-      nSyncAny_i   : in sl;
-      nSyncAnyD1_i : in sl;
-
+      nSyncAny_i    : in  sl;
+      nSyncAnyD1_i  : in  sl;
       -- Invert ADC data
-      inv_i : in sl := '0';
-
+      inv_i         : in  sl              := '0';
       -- Synchronization request output 
-      nSync_o : out sl;
-
+      nSync_o       : out sl;
       -- Synchronization process is complete and data is valid
-      dataValid_o  : out sl;
-      sampleData_o : out slv((GT_WORD_SIZE_C*8)-1 downto 0)
-      );
+      dataValid_o   : out sl;
+      sampleData_o  : out slv((GT_WORD_SIZE_C*8)-1 downto 0));
 end JesdRxLane;
-
 
 architecture rtl of JesdRxLane is
 
    constant ERR_REG_WIDTH_C : positive := 4+2*GT_WORD_SIZE_C;
 
--- Register
+
    type RegType is record
       bufWeD1         : sl;
       errReg          : slv(ERR_REG_WIDTH_C-1 downto 0);
@@ -126,40 +108,40 @@ architecture rtl of JesdRxLane is
       sampleDataValid : sl;
       jesdGtRx        : jesdGtRxLaneType;
    end record RegType;
-
    constant REG_INIT_C : RegType := (
       bufWeD1         => '0',
       errReg          => (others => '0'),
       sampleData      => (others => '0'),
       sampleDataValid => '0',
-      jesdGtRx        => JESD_GT_RX_LANE_INIT_C
-      );
+      jesdGtRx        => JESD_GT_RX_LANE_INIT_C);
 
    signal r   : RegType := REG_INIT_C;
    signal rin : RegType;
 
-   -- Internal signals
 
    -- Control signals from FSM
-   signal s_nSync         : sl;
-   signal s_readBuff      : sl;
-   signal s_alignFrame    : sl;
-   signal s_alignFrameDly : sl;
-   signal s_ila           : sl;
-   signal s_dataValid     : sl;
-   signal s_dataValidDly  : sl;
+   signal s_nSync          : sl;
+   signal s_readBuff       : sl;
+   signal s_alignFrame     : sl;
+   signal s_alignFrameDly1 : sl;
+   signal s_alignFrameDly0 : sl;
+   signal s_ila            : sl;
+   signal s_dataValid      : sl;
+   signal s_dataValidDly1  : sl;
+   signal s_dataValidDly0  : sl;
 
    -- Buffer control
    signal s_bufRst : sl;
    signal s_bufWe  : sl;
    signal s_bufRe  : sl;
 
-   -- Datapath
-   signal s_charAndData        : slv(((GT_WORD_SIZE_C*8)+GT_WORD_SIZE_C)-1 downto 0);
-   signal s_charAndDataBuff    : slv(s_charAndData'range);
-   signal s_charAndDataBuffDly : slv(s_charAndData'range);
-   signal s_sampleData         : slv(sampleData_o'range);
-   signal s_sampleDataValid    : sl;
+   -- Data path
+   signal s_charAndData         : slv(((GT_WORD_SIZE_C*8)+GT_WORD_SIZE_C)-1 downto 0);
+   signal s_charAndDataBuff     : slv(s_charAndData'range);
+   signal s_charAndDataBuffDly1 : slv(s_charAndData'range);
+   signal s_charAndDataBuffDly0 : slv(s_charAndData'range);
+   signal s_sampleData          : slv(sampleData_o'range);
+   signal s_sampleDataValid     : sl;
 
    -- Statuses
    signal s_bufOvf      : sl;
@@ -178,13 +160,16 @@ begin
 
    s_charAndData <= r.jesdGtRx.dataK & r.jesdGtRx.data;
 
+   -----------------
    -- Buffer control
+   -----------------
    s_bufRst <= devRst_i or not s_nSync or not enable_i;
    s_bufWe  <= not s_bufRst and not s_bufFull;
    s_bufRe  <= r.bufWeD1 and s_readBuff;
 
-   -- Buffer samples between first data and LMFC
-   -- Min size one LMFC period
+   -----------------------------------------------------------------------
+   -- Buffer samples between first data and LMFC Min size one LMFC period
+   -----------------------------------------------------------------------
    RX_buffer_fifo_INST : entity work.FifoSync
       generic map (
          TPD_G          => TPD_G,
@@ -203,27 +188,20 @@ begin
          FULL_THRES_G   => 1,
          EMPTY_THRES_G  => 1)
       port map (
-         rst          => s_bufRst,
-         clk          => devClk_i,
-         wr_en        => s_bufWe,       -- Always write when enabled
-         rd_en        => s_bufRe,  -- Hold read while sync not in sync with LMFC
-         din          => s_charAndData,
-         dout         => s_charAndDataBuff,
-         data_count   => s_buffLatency,
-         wr_ack       => open,
-         valid        => open,
-         overflow     => s_bufOvf,
-         underflow    => s_bufUnf,
-         prog_full    => open,
-         prog_empty   => open,
-         almost_full  => open,
-         almost_empty => open,
-         full         => s_bufFull,
-         not_full     => open,
-         empty        => open
-         );
+         rst        => s_bufRst,
+         clk        => devClk_i,
+         wr_en      => s_bufWe,         -- Always write when enabled
+         rd_en      => s_bufRe,  -- Hold read while sync not in sync with LMFC
+         din        => s_charAndData,
+         dout       => s_charAndDataBuff,
+         data_count => s_buffLatency,
+         overflow   => s_bufOvf,
+         underflow  => s_bufUnf,
+         full       => s_bufFull);
 
+   ----------------------
    -- Synchronization FSM
+   ----------------------
    syncFSM_INST : entity work.JesdSyncFsmRx
       generic map (
          TPD_G => TPD_G,
@@ -243,16 +221,16 @@ begin
          linkErr_i    => s_linkErr,
          nSync_o      => s_nSync,
          readBuff_o   => s_readBuff,
-         -- buffLatency_o => s_buffLatency,
          alignFrame_o => s_alignFrame,
          ila_o        => s_ila,
          kDetected_o  => s_kDetected,
          sysref_o     => s_refDetected,
          dataValid_o  => s_dataValid,
-         subClass_i   => subClass_i
-         );
+         subClass_i   => subClass_i);
 
-   -- Align the rx data within the GT word and replace the characters. 
+   ------------------------------------------------------------------
+   -- Align the rx data within the GT word and replace the characters
+   ------------------------------------------------------------------
    alignFrRepCh_INST : entity work.JesdAlignFrRepCh
       generic map (
          TPD_G => TPD_G,
@@ -262,24 +240,28 @@ begin
          rst               => devRst_i,
          replEnable_i      => replenable_i,
          scrEnable_i       => scrEnable_i,
-         alignFrame_i      => s_alignFrameDly,
-         dataValid_i       => s_dataValidDly,
-         dataRx_i          => s_charAndDataBuffDly((GT_WORD_SIZE_C*8)-1 downto 0),
-         chariskRx_i       => s_charAndDataBuffDly(((GT_WORD_SIZE_C*8)+GT_WORD_SIZE_C)-1 downto (GT_WORD_SIZE_C*8)),
+         alignFrame_i      => s_alignFrameDly1,
+         dataValid_i       => s_dataValidDly1,
+         dataRx_i          => s_charAndDataBuffDly1((GT_WORD_SIZE_C*8)-1 downto 0),
+         chariskRx_i       => s_charAndDataBuffDly1(((GT_WORD_SIZE_C*8)+GT_WORD_SIZE_C)-1 downto (GT_WORD_SIZE_C*8)),
          sampleDataValid_o => s_sampleDataValid,
          sampleData_o      => s_sampleData,
          alignErr_o        => s_alignErr,
-         positionErr_o     => s_positionErr
-         );
+         positionErr_o     => s_positionErr);
 
    process(devClk_i)
    begin
       if rising_edge(devClk_i) then
 
          -- Register to help with timing
-         s_alignFrameDly      <= s_alignFrame      after TPD_G;
-         s_dataValidDly       <= s_dataValid       after TPD_G;
-         s_charAndDataBuffDly <= s_charAndDataBuff after TPD_G;
+         s_alignFrameDly1      <= s_alignFrameDly0      after TPD_G;
+         s_dataValidDly1       <= s_dataValidDly0       after TPD_G;
+         s_charAndDataBuffDly1 <= s_charAndDataBuffDly0 after TPD_G;
+
+         -- Register to help with timing
+         s_alignFrameDly0      <= s_alignFrame      after TPD_G;
+         s_dataValidDly0       <= s_dataValid       after TPD_G;
+         s_charAndDataBuffDly0 <= s_charAndDataBuff after TPD_G;
 
          -- Link error masked by the mask from register and ORed
          s_linkErrVec <= s_positionErr & s_bufOvf & s_bufUnf & uOr(r.jesdGtRx.dispErr) & uOr(r.jesdGtRx.decErr) & s_alignErr after TPD_G;
@@ -291,19 +273,15 @@ begin
       end if;
    end process;
 
-   -- Synchronous process function:
-   -- - Registering of errors
-   -- - Delay the s_bufWe to use it for s_bufRe
-   -- - Inverting ADC data
-   -------------------------------------------------------------------------------
-   -------------------------------------------------------------------------------
-   comb : process (clearErr_i, devRst_i, enable_i, inv_i, r, r_jesdGtRx,
-                   s_bufWe, s_errComb, s_nSync, s_sampleData,
-                   s_sampleDataValid) is
+   comb : process (clearErr_i, enable_i, inv_i, r, r_jesdGtRx, s_bufWe,
+                   s_buffLatency, s_dataValidDly1, s_errComb, s_kDetected,
+                   s_nSync, s_refDetected, s_sampleData, s_sampleDataValid) is
       variable v : RegType;
    begin
+      -- Latch the current value
       v := r;
 
+      -- Keep a delayed copy 
       v.jesdGtRx := r_jesdGtRx;
       v.bufWeD1  := s_bufWe;
 
@@ -324,6 +302,7 @@ begin
       -- Invert sample data
       v.sampleDataValid := s_sampleDataValid;
 
+      -- Check if inverting the data
       if (inv_i = '1') then
          -- Invert sample data      
          v.sampleData := invData(s_sampleData, F_G, GT_WORD_SIZE_C);
@@ -331,12 +310,15 @@ begin
          v.sampleData := s_sampleData;
       end if;
 
-      -- Reset registers
-      if (devRst_i = '1') then
-         v := REG_INIT_C;
-      end if;
-
+      -- Register the variable for next clock cycle      
       rin <= v;
+
+      -- Output assignment
+      nSync_o      <= s_nSync;
+      dataValid_o  <= r.sampleDataValid;
+      sampleData_o <= endianSwapSlv(r.sampleData, GT_WORD_SIZE_C);
+      status_o     <= r.jesdGtRx.cdrStable & s_buffLatency & r.errReg(r.errReg'high downto 4) & s_kDetected & s_refDetected & enable_i & r.errReg(2 downto 0) & s_nSync & r.errReg(3) & s_dataValidDly1 & r.jesdGtRx.rstDone;
+
    end process comb;
 
    seq : process (devClk_i) is
@@ -346,10 +328,4 @@ begin
       end if;
    end process seq;
 
-   -- Output assignment
-   nSync_o      <= s_nSync;
-   dataValid_o  <= r.sampleDataValid;
-   sampleData_o <= endianSwapSlv(r.sampleData, GT_WORD_SIZE_C);
-   status_o     <= r.jesdGtRx.cdrStable & s_buffLatency & r.errReg(r.errReg'high downto 4) & s_kDetected & s_refDetected & enable_i & r.errReg(2 downto 0) & s_nSync & r.errReg(3) & s_dataValidDly & r.jesdGtRx.rstDone;
------------------------------------------------------------------------------------------
 end rtl;
