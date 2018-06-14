b98ca42d86145f1cc076427ba46acba8f3fae829
Larry Ruckman <ruckman@slac.stanford.edu>
2018-06-14 14:39:28 -0700

    adding zynquplus support to surf

--- ethernet/GigEthCore/ruckus.tcl
+++ ethernet/GigEthCore/ruckus.tcl
@@ -7,7 +7,16 @@ loadRuckusTcl "$::DIR_PATH/core"
 # Get the family type
 set family [getFpgaFamily]
 
-if { ${family} == "virtex7" } {
+if { ${family} eq {artix7} } {
+   loadRuckusTcl "$::DIR_PATH/gtp7"
+}
+
+if { ${family} eq {kintex7} ||
+     ${family} eq {zynq} } {
+   loadRuckusTcl "$::DIR_PATH/gtx7"
+}
+
+if { ${family} eq {virtex7} } {
    loadRuckusTcl "$::DIR_PATH/gth7"
 }
 
@@ -16,20 +25,14 @@ if { ${family} eq {kintexu} } {
    loadRuckusTcl "$::DIR_PATH/lvdsUltraScale"
 }
 
-if { ${family} eq {kintexuplus} } {
+if { ${family} eq {kintexuplus} ||
+     ${family} eq {zynquplus} } {
    loadRuckusTcl "$::DIR_PATH/gthUltraScale+"
    loadRuckusTcl "$::DIR_PATH/gtyUltraScale+"
    loadRuckusTcl "$::DIR_PATH/lvdsUltraScale"
 }
 
-if { ${family} == "artix7" } {
-   loadRuckusTcl "$::DIR_PATH/gtp7"
-}
-
-if { ${family} == "kintex7" } {
-   loadRuckusTcl "$::DIR_PATH/gtx7"
-}
-
-if { ${family} == "zynq" } {
-   loadRuckusTcl "$::DIR_PATH/gtx7"
+if { ${family} eq {virtexuplus} } {
+   loadRuckusTcl "$::DIR_PATH/gtyUltraScale+"
+   loadRuckusTcl "$::DIR_PATH/lvdsUltraScale"
 }
