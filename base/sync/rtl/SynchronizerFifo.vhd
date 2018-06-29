6f8ab1873eb5e3dbc86e3552d5a6e0bc0c5a22bb
Larry Ruckman <ruckman@slac.stanford.edu>
2018-06-29 08:43:54 -0700

    adding SYNTH_MODE_G generic

--- base/sync/rtl/SynchronizerFifo.vhd
+++ base/sync/rtl/SynchronizerFifo.vhd
@@ -2,7 +2,7 @@
 -- File       : SynchronizerFifo.vhd
 -- Company    : SLAC National Accelerator Laboratory
 -- Created    : 2013-07-10
--- Last update: 2016-03-09
+-- Last update: 2018-06-29
 -------------------------------------------------------------------------------
 -- Description: Synchronizing FIFO wrapper
 -------------------------------------------------------------------------------
@@ -26,6 +26,7 @@ entity SynchronizerFifo is
    generic (
       TPD_G         : time                       := 1 ns;
       COMMON_CLK_G  : boolean                    := false;  -- Bypass FifoAsync module for synchronous data configuration
+      SYNTH_MODE_G  : string                     := "inferred";
       MEMORY_TYPE_G : string                     := "distributed";
       SYNC_STAGES_G : integer range 3 to (2**24) := 3;
       PIPE_STAGES_G : natural range 0 to 16      := 0;
@@ -47,7 +48,7 @@ entity SynchronizerFifo is
 end SynchronizerFifo;
 
 architecture rtl of SynchronizerFifo is
-   
+
    constant INIT_C : slv(DATA_WIDTH_G-1 downto 0) := ite(INIT_G = "0", slvZero(DATA_WIDTH_G), INIT_G);
 
 begin
@@ -57,9 +58,10 @@ begin
 
    GEN_ASYNC : if (COMMON_CLK_G = false) generate
 
-      FifoAsync_1 : entity work.FifoAsync
+      U_Fifo : entity work.Fifo
          generic map (
             TPD_G         => TPD_G,
+            SYNTH_MODE_G  => SYNTH_MODE_G,
             MEMORY_TYPE_G => MEMORY_TYPE_G,
             FWFT_EN_G     => true,
             SYNC_STAGES_G => SYNC_STAGES_G,
@@ -68,32 +70,21 @@ begin
             ADDR_WIDTH_G  => ADDR_WIDTH_G,
             INIT_G        => INIT_C)
          port map (
-            rst           => rst,
-            wr_clk        => wr_clk,
-            wr_en         => wr_en,
-            din           => din,
-            wr_data_count => open,
-            wr_ack        => open,
-            overflow      => open,
-            prog_full     => open,
-            almost_full   => open,
-            full          => open,
-            rd_clk        => rd_clk,
-            rd_en         => rd_en,
-            dout          => dout,
-            rd_data_count => open,
-            valid         => valid,
-            underflow     => open,
-            prog_empty    => open,
-            almost_empty  => open,
-            empty         => open);
+            rst    => rst,
+            wr_clk => wr_clk,
+            wr_en  => wr_en,
+            din    => din,
+            rd_clk => rd_clk,
+            rd_en  => rd_en,
+            dout   => dout,
+            valid  => valid);
    end generate;
 
    GEN_SYNC : if (COMMON_CLK_G = true) generate
 
       dout  <= din;
       valid <= wr_en;
-      
+
    end generate;
-   
+
 end architecture rtl;
