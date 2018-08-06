0bd2c6084c30c1cdcbb3ac84e7c217870a7c6eb4
Ryan Herbst <rherbst@slac.stanford.edu>
2018-08-06 11:46:18 -0700

    Schematic Fix & Status Add

--- protocols/clink/hdl/ClinkTop.vhd
+++ protocols/clink/hdl/ClinkTop.vhd
@@ -31,6 +31,7 @@ entity ClinkTop is
       TPD_G              : time                 := 1 ns;
       CHAN_COUNT_G       : integer range 1 to 2 := 1;
       UART_READY_EN_G    : boolean              := true;
+      CB1_H0_34_FIX_G    : boolean              := false;  -- PC-248-100-22 schematic error
       DATA_AXIS_CONFIG_G : AxiStreamConfigType  := AXI_STREAM_CONFIG_INIT_C;
       UART_AXIS_CONFIG_G : AxiStreamConfigType  := AXI_STREAM_CONFIG_INIT_C);
    port (
@@ -58,8 +59,9 @@ entity ClinkTop is
       -- System clock and reset, > 100 Mhz
       sysClk          : in  sl;
       sysRst          : in  sl;
-      -- Camera Control Bits, async
+      -- Camera Control Bits & status, async
       camCtrl         : in  Slv4Array(CHAN_COUNT_G-1 downto 0);
+      camStatus       : out ClChanStatusArray(1 downto 0);
       -- Camera data
       dataClk         : in  sl;
       dataRst         : in  sl;
@@ -83,6 +85,8 @@ entity ClinkTop is
 end ClinkTop;
 
 architecture rtl of ClinkTop is
+
+   constant NOT_CB1_H0_34_FIX_C : boolean := not CB1_H0_34_FIX_G;
 
    type RegType is record
       chanConfig      : ClChanConfigArray(1 downto 0);
@@ -117,6 +121,8 @@ architecture rtl of ClinkTop is
    attribute IODELAY_GROUP of U_IDelayCtrl : label is "CLINK_CORE";
 
 begin
+
+   camStatus <= chanStatus;
 
    ----------------------------------------
    -- IO Modules
@@ -175,6 +181,7 @@ begin
       U_Cbl1Half0: entity work.ClinkCtrl
          generic map (
             TPD_G              => TPD_G,
+            INV_34_G           => CB1_H0_34_FIX_G,  -- Invert for bad schematic
             UART_READY_EN_G    => UART_READY_EN_G,
             UART_AXIS_CONFIG_G => UART_AXIS_CONFIG_G)
          port map (
@@ -210,7 +217,7 @@ begin
       U_Cbl1Half0: entity work.ClinkData
          generic map ( 
             TPD_G    => TPD_G,
-            INV_34_G => true)
+            INV_34_G => NOT_CB1_H0_34_FIX_C) -- Don't invert for bad schematic
          port map (
             cblHalfP   => cbl1Half0P,
             cblHalfM   => cbl1Half0M,
