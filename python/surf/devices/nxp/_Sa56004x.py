3d0f090a3cc8f04f423b6a2d7a217baddf6d01d6
Larry Ruckman <ruckman@slac.stanford.edu>
2018-10-30 10:40:12 -0700

    adding Sa56004x.simpleView support

--- python/surf/devices/nxp/_Sa56004x.py
+++ python/surf/devices/nxp/_Sa56004x.py
@@ -560,3 +560,12 @@ class Sa56004x(pr.Device):
         
         ############################################################################
         
+        # Default to simple view
+        self.simpleView()        
+        
+    def simpleView(self):
+        # Hide all the variable
+        self.hideVariables(hidden=True)
+        # Then unhide the most interesting ones
+        vars = ['LocalTemperature', 'RemoteTemperature']
+        self.hideVariables(hidden=False, variables=vars)        
