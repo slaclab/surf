98be85315388497073fa4d0cc7d8f7c32ef645a0
Larry Ruckman <ruckman@slac.stanford.edu>
2019-02-22 14:02:13 -0800

    ruckus.tcl update

--- protocols/i2c/ruckus.tcl
+++ protocols/i2c/ruckus.tcl
@@ -2,7 +2,7 @@
 source -quiet $::env(RUCKUS_DIR)/vivado_proc.tcl
 
 # Load Source Code
-loadSource -dir "$::DIR_PATH/rtl/"
+loadSource -dir "$::DIR_PATH/rtl"
 
 # Load Simulation
-loadSource -sim_only -dir "$::DIR_PATH/sim/"
+loadSource -sim_only -dir "$::DIR_PATH/sim"
--- protocols/pgp/pgp2b/gthUltraScale/ruckus.tcl
+++ protocols/pgp/pgp2b/gthUltraScale/ruckus.tcl
@@ -4,7 +4,7 @@ source -quiet $::env(RUCKUS_DIR)/vivado_proc.tcl
 # Load local source Code and constraints
 if { $::env(VIVADO_VERSION) >= 2017.3 } {
 
-   loadSource   -dir "$::DIR_PATH/rtl/"
+   loadSource   -dir "$::DIR_PATH/rtl"
 
    loadSource    -path "$::DIR_PATH/ip/PgpGthCore.dcp"
    # loadIpCore  -path "$::DIR_PATH/ip/PgpGthCore.xci"
