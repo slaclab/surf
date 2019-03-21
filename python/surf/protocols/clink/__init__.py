4e4276592bf6a7780a852a28ef062218a67fd418
Larry Ruckman <ruckman@slac.stanford.edu>
2019-03-21 16:31:59 -0700

    adding _UartPiranha4.py

--- python/surf/protocols/clink/__init__.py
+++ python/surf/protocols/clink/__init__.py
@@ -9,10 +9,11 @@
 ## the terms contained in the LICENSE.txt file.
 ##############################################################################
 
-from surf.protocols.clink._ClinkTop import *
+from surf.protocols.clink._ClinkTop      import *
 from surf.protocols.clink._ClinkSerialRx import *
 from surf.protocols.clink._ClinkSerialTx import *
-from surf.protocols.clink._ClinkChannel import *
+from surf.protocols.clink._ClinkChannel  import *
 
 # Library of support Camera UART interfaces
-from surf.protocols.clink._UartOpal000 import *
+from surf.protocols.clink._UartOpal000  import *
+from surf.protocols.clink._UartPiranha4 import *
