9c242290fb2357abb22107ce4808b8625419b17d
Benjamin Reese <bareese@slac.stanford.edu>
2019-04-11 16:17:28 -0700

    Fix formatting

--- protocols/uart/rtl/UartAxiLiteMaster.vhd
+++ protocols/uart/rtl/UartAxiLiteMaster.vhd
@@ -194,7 +194,7 @@ begin
          when WAIT_START_S =>
             -- Any characters before 'r' or 'w' are thrown out
             if (uartRxValid = '1') then
-			   v.axilReq.address := (Others => '0');
+               v.axilReq.address := (others => '0');
                if (uartRxData = toSlv(character'pos('w'), 8) or
                    uartRxData = toSlv(character'pos('W'), 8)) then
                   -- Write op
@@ -238,8 +238,8 @@ begin
                if (isSpace(uartRxData)) then
                   v.axilReq.address := r.axilReq.address;
                   if (r.axilReq.rnw = '0') then
-				     v.axilReq.wrData := (Others => '0');
-                     v.state := WR_DATA_S;
+                     v.axilReq.wrData := (others => '0');
+                     v.state          := WR_DATA_S;
                   else
                      v.state := WAIT_EOL_S;
                   end if;
