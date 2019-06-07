f620b942b4dc47021116e6ab10efdfaeaebc027a
Larry Ruckman <ruckman@slac.stanford.edu>
2019-06-07 15:46:12 -0700

    adding FifoXpm from surf@xpm-dev branch

--- base/fifo/ruckus.tcl
+++ base/fifo/ruckus.tcl
@@ -3,6 +3,10 @@ source -quiet $::env(RUCKUS_DIR)/vivado_proc.tcl
 
 # Load Source Code
 loadSource -dir "$::DIR_PATH/rtl"
+loadSource -dir "$::DIR_PATH/rtl/xilinx"
 
 # Load Simulation
 loadSource -sim_only -dir "$::DIR_PATH/tb"
+
+# # Bug Fix: https://www.xilinx.com/support/answers/67815.html
+# set_property XPM_LIBRARIES XPM_MEMORY [current_project]
