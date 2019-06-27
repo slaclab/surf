75b5e73e4f52fe19982f99ed7ef3e9e73c432734
Larry Ruckman <ruckman@slac.stanford.edu>
2019-06-27 13:46:00 -0700

    adding _UdpEngine.py
    ESCORE-425

--- python/surf/ethernet/udp/_UdpEngineClient.py
+++ python/surf/ethernet/udp/_UdpEngineClient.py
@@ -1,13 +1,5 @@
 #!/usr/bin/env python
 #-----------------------------------------------------------------------------
-# Title      : PyRogue UdpEngineClient
-#-----------------------------------------------------------------------------
-# File       : UdpEngineClient.py
-# Created    : 2017-04-12
-#-----------------------------------------------------------------------------
-# Description:
-# PyRogue UdpEngineClient
-#-----------------------------------------------------------------------------
 # This file is part of the rogue software platform. It is subject to
 # the license terms in the LICENSE.txt file found in the top-level directory
 # of this distribution and at:
@@ -36,8 +28,6 @@ class UdpEngineClient(pr.Device):
             description  = "ClientRemotePort (big-Endian configuration)",
             offset       =  0x00,
             bitSize      =  16,
-            bitOffset    =  0x00,
-            base         = pr.UInt,
             mode         = "RW",
             hidden       = True,            
         ))
@@ -56,8 +46,6 @@ class UdpEngineClient(pr.Device):
             description  = "ClientRemoteIp (big-Endian configuration)",
             offset       =  0x04,
             bitSize      =  32,
-            bitOffset    =  0x00,
-            base         = pr.UInt,
             mode         = "RW",
             hidden       = True,            
         ))
