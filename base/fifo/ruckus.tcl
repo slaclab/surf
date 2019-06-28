c79ec160d949ce0643835583a613f6a190258a6b
Larry Ruckman <ruckman@slac.stanford.edu>
2019-06-28 10:18:16 -0700

    FIFO dir reorg

--- base/fifo/ruckus.tcl
+++ base/fifo/ruckus.tcl
@@ -3,6 +3,8 @@ source -quiet $::env(RUCKUS_DIR)/vivado_proc.tcl
 
 # Load Source Code
 loadSource -dir "$::DIR_PATH/rtl"
+loadSource -dir "$::DIR_PATH/rtl/xilinx"
+loadSource -dir "$::DIR_PATH/rtl/inferred"
 
 # Load Simulation
 loadSource -sim_only -dir "$::DIR_PATH/tb"
