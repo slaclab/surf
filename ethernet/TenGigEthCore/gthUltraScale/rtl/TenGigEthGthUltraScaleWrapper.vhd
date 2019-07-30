6cdf601ff4ec97453c80377426cdd0cb9ac98fca
Larry Ruckman <ruckman@slac.stanford.edu>
2019-07-30 11:22:30 -0700

    updating TenGigEthCore/gthUltraScale for shared GT refclk

--- ethernet/TenGigEthCore/gthUltraScale/rtl/TenGigEthGthUltraScaleWrapper.vhd
+++ ethernet/TenGigEthCore/gthUltraScale/rtl/TenGigEthGthUltraScaleWrapper.vhd
@@ -30,6 +30,7 @@ entity TenGigEthGthUltraScaleWrapper is
       PAUSE_EN_G        : boolean                          := true;
       PAUSE_512BITS_G   : positive                         := 8;
       -- QUAD PLL Configurations
+      EXT_REF_G         : boolean                          := false;
       QPLL_REFCLK_SEL_G : slv(2 downto 0)                  := "001";
       -- AXI-Lite Configurations
       EN_AXI_REG_G      : boolean                          := false;
@@ -72,6 +73,7 @@ entity TenGigEthGthUltraScaleWrapper is
       gtTxPolarity        : in  sl                                             := '0';
       -- MGT Clock Port (156.25 MHz)
       gtRefClk            : in  sl                                             := '0';
+      gtRefClkBufg        : in  sl                                             := '0';
       gtClkP              : in  sl                                             := '1';
       gtClkN              : in  sl                                             := '0';
       -- MGT Ports
@@ -116,10 +118,12 @@ begin
    TenGigEthGthUltraScaleClk_Inst : entity work.TenGigEthGthUltraScaleClk
       generic map (
          TPD_G             => TPD_G,
+         EXT_REF_G         => EXT_REF_G,
          QPLL_REFCLK_SEL_G => QPLL_REFCLK_SEL_G)
       port map (
          -- MGT Clock Port (156.25 MHz)
          gtRefClk      => gtRefClk,
+         gtRefClkBufg  => gtRefClkBufg,
          gtClkP        => gtClkP,
          gtClkN        => gtClkN,
          coreClk       => coreClock,
