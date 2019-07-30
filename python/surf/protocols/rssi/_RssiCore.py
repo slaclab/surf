70ceee31aebc1c3988f11ebef93df5399f089338
Larry Ruckman <ruckman@slac.stanford.edu>
2019-07-30 15:23:58 -0700

    RssiCore.py update

--- python/surf/protocols/rssi/_RssiCore.py
+++ python/surf/protocols/rssi/_RssiCore.py
@@ -98,6 +98,7 @@ class RssiCore(pr.Device):
             bitOffset    =  0x00,
             base         = pr.UInt,
             mode         = "RW",
+            disp         = '{:d}',
         ))
 
         self.add(pr.RemoteVariable(    
@@ -108,6 +109,7 @@ class RssiCore(pr.Device):
             bitOffset    =  0x00,
             base         = pr.UInt,
             mode         = "RW",
+            disp         = '{:d}',
         ))
 
         self.add(pr.RemoteVariable(    
@@ -118,6 +120,7 @@ class RssiCore(pr.Device):
             bitOffset    =  0x00,
             base         = pr.UInt,
             mode         = "RW",
+            disp         = '{:d}',
         ))
 
         self.add(pr.RemoteVariable(    
@@ -128,6 +131,7 @@ class RssiCore(pr.Device):
             bitOffset    =  0x00,
             base         = pr.UInt,
             mode         = "RW",
+            disp         = '{:d}',
         ))
 
         self.add(pr.RemoteVariable(    
@@ -138,6 +142,7 @@ class RssiCore(pr.Device):
             bitOffset    =  0x00,
             base         = pr.UInt,
             mode         = "RW",
+            disp         = '{:d}',
         ))
 
         self.add(pr.RemoteVariable(    
@@ -148,6 +153,7 @@ class RssiCore(pr.Device):
             bitOffset    =  0x00,
             base         = pr.UInt,
             mode         = "RW",
+            disp         = '{:d}',
         ))
 
         self.add(pr.RemoteVariable(    
@@ -158,6 +164,7 @@ class RssiCore(pr.Device):
             bitOffset    =  0x00,
             base         = pr.UInt,
             mode         = "RW",
+            disp         = '{:d}',
         ))
 
         self.add(pr.RemoteVariable(    
@@ -168,6 +175,7 @@ class RssiCore(pr.Device):
             bitOffset    =  0x00,
             base         = pr.UInt,
             mode         = "RW",
+            disp         = '{:d}',
         ))
 
         self.add(pr.RemoteVariable(    
@@ -178,6 +186,7 @@ class RssiCore(pr.Device):
             bitOffset    =  0x00,
             base         = pr.UInt,
             mode         = "RW",
+            disp         = '{:d}',
         ))
 
         self.add(pr.RemoteVariable(    
@@ -352,13 +361,19 @@ class RssiCore(pr.Device):
         ##############################
         @self.command(name="C_OpenConn", description="Open connection request",)
         def C_OpenConn():        
-           self.OpenConn.set(0)
+           self.CloseConn.set(0)
+           self.OpenConn.set(1)
 
         @self.command(name="C_CloseConn", description="Close connection request",)
         def C_CloseConn():                        
+           self.OpenConn.set(0)
            self.CloseConn.set(1)
-           self.CloseConn.set(0)
-                        
+                    
+        @self.command(name="C_RestartConn", description="Restart connection request",)
+        def C_RestartConn():        
+           self.C_CloseConn()
+           self.C_OpenConn()
+           
         @self.command(name="C_InjectFault", description="Inject a single fault(for debug and test purposes only). Corrupts checksum during transmission",)
         def C_InjectFault():                        
            self.InjectFault.set(1)
