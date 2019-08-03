993d191426ab61b6dc801ae8619c2b98ce17e9d0
Larry Ruckman <ruckman@slac.stanford.edu>
2019-08-03 10:00:08 -0700

    adding AXI_DEFAULT_PKT_LEN_G

--- protocols/ssi/rtl/SsiPrbsTx.vhd
+++ protocols/ssi/rtl/SsiPrbsTx.vhd
@@ -29,7 +29,7 @@ entity SsiPrbsTx is
       -- General Configurations
       TPD_G                      : time                    := 1 ns;
       AXI_EN_G                   : sl                      := '1';
-      AXI_DEFAULT_PKT_LEN_C      : slv(31 downto 0)        := x"00000FFF";
+      AXI_DEFAULT_PKT_LEN_G      : slv(31 downto 0)        := x"00000FFF";
       -- FIFO Configurations
       VALID_THOLD_G              : natural                 := 1;
       VALID_BURST_MODE_G         : boolean                 := false;
@@ -116,7 +116,7 @@ architecture rtl of SsiPrbsTx is
       busy           => '1',
       overflow       => '0',
       length         => (others => '0'),
-      packetLength   => AXI_DEFAULT_PKT_LEN_C,
+      packetLength   => AXI_DEFAULT_PKT_LEN_G,
       dataCnt        => (others => '0'),
       trigDly        => (others => '0'),
       trigDlyCnt     => (others => '0'),
