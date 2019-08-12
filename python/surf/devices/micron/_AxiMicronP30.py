f02416a43645bf56e91de73f936dddc4777dc8bc
Larry Ruckman <ruckman@slac.stanford.edu>
2019-08-12 13:14:58 -0700

    python bug fixes

--- python/surf/devices/micron/_AxiMicronP30.py
+++ python/surf/devices/micron/_AxiMicronP30.py
@@ -203,7 +203,7 @@ class AxiMicronP30(pr.Device):
                     # Compare PROM to file
                     if (data != prom):
                         click.secho(("\nAddr = 0x%x: MCS = 0x%x != PROM = 0x%x" % (addr,data,prom)), fg='red')
-                        raise McsException('verifyProm() Failed\n\n')
+                        raise misc.McsException('verifyProm() Failed\n\n')
             # Close the status bar
             bar.update(self._mcs.size)  
         
