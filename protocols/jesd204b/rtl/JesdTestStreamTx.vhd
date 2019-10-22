54e3e4c08b46bb60f90235c52fb8e270b6e07e92
Larry Ruckman <ruckman@slac.stanford.edu>
2019-10-22 13:01:57 -0700

    resolving ieee.numeric_std v.s. ieee.std_logic_unsigned conflicts

--- protocols/jesd204b/rtl/JesdTestStreamTx.vhd
+++ protocols/jesd204b/rtl/JesdTestStreamTx.vhd
@@ -21,9 +21,8 @@
  
 library ieee;
 use ieee.std_logic_1164.all;
+use ieee.std_logic_arith.all;
 use ieee.std_logic_unsigned.all;
-
-use ieee.numeric_std.all;
 
 use work.StdRtlPkg.all;
 use work.jesd204bpkg.all;
--- xilinx/7Series/gth7/rtl/Gth7RxRst.vhd
+++ xilinx/7Series/gth7/rtl/Gth7RxRst.vhd
@@ -69,9 +69,8 @@
 
 library ieee;
 use ieee.std_logic_1164.all;
-use ieee.numeric_std.all;
-use ieee.std_logic_unsigned.all;
 use ieee.std_logic_arith.all;
+use ieee.std_logic_unsigned.all;
 
 entity Gth7RxRst is
    generic(
