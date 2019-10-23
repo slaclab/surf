b4a56e1403f2103a452016756fa3d0cf4eaad46f
Larry Ruckman <ruckman@slac.stanford.edu>
2019-10-23 14:46:27 -0700

    renaming and some polishing

--- axi/axi-stream/rtl/AxiStreamCombiner.vhd
+++ axi/axi-stream/rtl/AxiStreamCombiner.vhd
@@ -1,20 +1,14 @@
 ------------------------------------------------------------------------------
--- Title      : 
--------------------------------------------------------------------------------
--- File       : AxiStreamDeinterleave.vhd
--- Author     : Matt Weaver <weaver@slac.stanford.edu>
+-- File       : AxiStreamCombiner.vhd
 -- Company    : SLAC National Accelerator Laboratory
--- Created    : 2015-07-10
--- Last update: 2019-02-08
--- Platform   : 
--- Standard   : VHDL'93/02
 -------------------------------------------------------------------------------
+-- Description: Combines multiple "narrower" buses into a "wide" AXI stream bus
 -------------------------------------------------------------------------------
--- This file is part of 'LCLS2 DAQ Software'.
+-- This file is part of 'SLAC Firmware Standard Library'.
 -- It is subject to the license terms in the LICENSE.txt file found in the 
 -- top-level directory of this distribution and at: 
 --    https://confluence.slac.stanford.edu/display/ppareg/LICENSE.html. 
--- No part of 'LCLS2 DAQ Software', including this file, 
+-- No part of 'SLAC Firmware Standard Library', including this file, 
 -- may be copied, modified, propagated, or distributed except according to 
 -- the terms contained in the LICENSE.txt file.
 -------------------------------------------------------------------------------
@@ -28,233 +22,207 @@ use work.AxiStreamPkg.all;
 use work.AxiStreamPkg.all;
 use work.SsiPkg.all;
 
-entity AxiStreamDeinterleave is
-   generic ( LANES_G        : integer := 4;
-             AXIS_CONFIG_G  : AxiStreamConfigType := AXI_STREAM_CONFIG_INIT_C );
-   port ( axisClk         : in  sl;
-          axisRst         : in  sl;
-          sAxisMaster     : in  AxiStreamMasterArray( LANES_G-1 downto 0 );
-          sAxisSlave      : out AxiStreamSlaveArray ( LANES_G-1 downto 0 );
-          mAxisMaster     : out AxiStreamMasterType;
-          mAxisSlave      : in  AxiStreamSlaveType );
-end AxiStreamDeinterleave;
+entity AxiStreamCombiner is
+   generic (
+      TPD_G               : time     := 1 ns;
+      LANES_G             : positive := 4;
+      SLAVE_AXI_CONFIG_G  : AxiStreamConfigType;
+      MASTER_AXI_CONFIG_G : AxiStreamConfigType);
+   port (
+      -- Clock and Reset
+      axisClk      : in  sl;
+      axisRst      : in  sl;
+      -- Slave Ports
+      sAxisMasters : in  AxiStreamMasterArray(LANES_G-1 downto 0);
+      sAxisSlaves  : out AxiStreamSlaveArray (LANES_G-1 downto 0);
+      -- Master Port
+      mAxisMaster  : out AxiStreamMasterType;
+      mAxisSlave   : in  AxiStreamSlaveType);
+end AxiStreamCombiner;
 
-architecture top_level_app of AxiStreamDeinterleave is
+architecture rtl of AxiStreamCombiner is
 
-  constant SEQ_C : slv(15 downto 8) := x"55";
-  
-  constant MAXIS_CONFIG_C : AxiStreamConfigType := (
-    TSTRB_EN_C    => false,
-    TDATA_BYTES_C => LANES_G*AXIS_CONFIG_G.TDATA_BYTES_C,
-    TDEST_BITS_C  => AXIS_CONFIG_G.TDEST_BITS_C,
-    TID_BITS_C    => AXIS_CONFIG_G.TID_BITS_C,
-    TKEEP_MODE_C  => AXIS_CONFIG_G.TKEEP_MODE_C,
-    TUSER_BITS_C  => AXIS_CONFIG_G.TUSER_BITS_C,
-    TUSER_MODE_C  => AXIS_CONFIG_G.TUSER_MODE_C );
+   constant SEQ_C : slv(15 downto 8) := x"55";
 
-  type FrameState is ( SOF_S, EOF_S, ERR_S );
-  
-  type RegType is record
-    master  : AxiStreamMasterType;
-    state   : FrameState;
-    sof     : sl;
-    first   : slv                 (LANES_G-1 downto 0);
-    discard : slv                 (LANES_G-1 downto 0);
-    slaves  : AxiStreamSlaveArray (LANES_G-1 downto 0);
-  end record;
+   type FrameState is (
+      SOF_S,
+      EOF_S,
+      ERR_S);
 
-  constant REG_INIT_C : RegType := (
-    master  => axiStreamMasterInit(MAXIS_CONFIG_C),
-    state   => SOF_S,
-    sof     => '0',
-    first   => (others=>'1'),
-    discard => (others=>'0'),
-    slaves  => (others=>AXI_STREAM_SLAVE_INIT_C));
-  
-  signal r   : RegType := REG_INIT_C;
-  signal rin : RegType;
+   type RegType is record
+      master  : AxiStreamMasterType;
+      state   : FrameState;
+      sof     : sl;
+      first   : slv (LANES_G-1 downto 0);
+      discard : slv (LANES_G-1 downto 0);
+      slaves  : AxiStreamSlaveArray (LANES_G-1 downto 0);
+   end record;
 
-  component ila_0
-    port ( clk    : in sl;
-           probe0 : in slv(255 downto 0) );
-  end component;
+   constant REG_INIT_C : RegType := (
+      master  => axiStreamMasterInit(MASTER_AXI_CONFIG_G),
+      state   => SOF_S,
+      sof     => '0',
+      first   => (others => '1'),
+      discard => (others => '0'),
+      slaves  => (others => AXI_STREAM_SLAVE_INIT_C));
 
-  signal state_s : slv(1 downto 0);
+   signal r   : RegType := REG_INIT_C;
+   signal rin : RegType;
+
 begin
 
-  state_s <= "00" when r.state = ERR_S else
-             "01" when r.state = SOF_S else
-             "10" when r.state = EOF_S else
-             "11";
-  
-  U_ILA : ila_0
-    port map ( clk     => axisClk,
-               probe0( 15 downto   0) => sAxisMaster(0).tData(15 downto 0),
-               probe0( 31 downto  16) => sAxisMaster(1).tData(15 downto 0),
-               probe0( 47 downto  32) => sAxisMaster(2).tData(15 downto 0),
-               probe0( 63 downto  48) => sAxisMaster(3).tData(15 downto 0),
-               probe0(            64) => sAxisMaster(0).tValid,
-               probe0(            65) => sAxisMaster(1).tValid,
-               probe0(            66) => sAxisMaster(2).tValid,
-               probe0(            67) => sAxisMaster(3).tValid,
-               probe0(            68) => sAxisMaster(0).tLast,
-               probe0(            69) => sAxisMaster(1).tLast,
-               probe0(            70) => sAxisMaster(2).tLast,
-               probe0(            71) => sAxisMaster(3).tLast,
-               probe0( 75 downto  72) => r.discard,
-               probe0( 77 downto  76) => state_s,
-               probe0(            78) => r.sof,
-               probe0(            79) => mAxisSlave.tReady,
-               probe0(            80) => r.master.tValid,
-               probe0(            81) => r.master.tLast,
-               probe0( 85 downto  82) => r.first,
-               probe0(255 downto  86) => (others=>'0') );
+   assert (MASTER_AXI_CONFIG_G.TDATA_BYTES_C = LANES_G*SLAVE_AXI_CONFIG_G.TDATA_BYTES_C)
+      report "MASTER_AXI_CONFIG_G.TDATA_BYTES_C must be LANES_G*SLAVE_AXI_CONFIG_G.TDATA_BYTES_C" severity failure;
 
-  comb : process ( r, axisRst, sAxisMaster, mAxisSlave ) is
-    variable v : RegType;
-    variable m,n : integer;
-    variable hdrErr : sl;
-    variable seqOff : Slv8Array(LANES_G-1 downto 1);
-    variable notSeq : slv(LANES_G-1 downto 0);
-    variable ready  : sl;
-    variable tready : slv(LANES_G-1 downto 0);
-    variable tlast  : slv(LANES_G-1 downto 0);
-  begin
-    v := r;
+   comb : process (axisRst, mAxisSlave, r, sAxisMasters) is
+      variable v      : RegType;
+      variable m, n   : integer;
+      variable hdrErr : sl;
+      variable seqOff : Slv8Array(LANES_G-1 downto 1);
+      variable notSeq : slv(LANES_G-1 downto 0);
+      variable ready  : sl;
+      variable tready : slv(LANES_G-1 downto 0);
+      variable tlast  : slv(LANES_G-1 downto 0);
+   begin
+      -- Latch the current value
+      v := r;
 
-    v.sof := '0';
-    
-    tready := (others=>'0');
+      v.sof := '0';
 
-    for i in 0 to LANES_G-1 loop
-      tlast(i) := sAxisMaster(i).tLast;
-    end loop;
+      tready := (others => '0');
 
-    -- process acknowledge
-    if mAxisSlave.tReady = '1' then
-      v.master.tValid := '0';
-    end if;
+      for i in 0 to LANES_G-1 loop
+         tlast(i) := sAxisMasters(i).tLast;
+      end loop;
 
-    -- wait for all streams to contribute
-    ready := '1';
-    for i in 0 to LANES_G-1 loop
-      if sAxisMaster(i).tValid='0' then
-        ready := '0';
+      -- process acknowledge
+      if mAxisSlave.tReady = '1' then
+         v.master.tValid := '0';
       end if;
-    end loop;
 
-    case r.state is
-      when SOF_S =>
-        if ready = '1' then
-          if allBits(r.first,'1') then  -- wait for all lanes to start
-            --  test sequence numbers
-            notSeq := (others=>'0');
-            for i in 0 to LANES_G-1 loop
-              if sAxisMaster(i).tData(SEQ_C'range)/=SEQ_C then
-                notSeq(i) := '1';
-              end if;
-            end loop;
-            if notSeq/=0 then
-              v.discard := notSeq;
-              v.state   := ERR_S;
-            else
-              hdrErr := '0';
-              for i in 1 to LANES_G-1 loop
-                seqOff(i) := sAxisMaster(i).tData(7 downto 0) - sAxisMaster(0).tData(7 downto 0);
-                if sAxisMaster(0).tData(7 downto 0)/=sAxisMaster(i).tData(7 downto 0) then
-                  hdrErr := '1';
-                end if;
-              end loop;
-              if hdrErr = '1' then    -- sequence mismatch - discard late lanes
-                v.discard := (others=>'0');
-                for i in 1 to LANES_G-1 loop
-                  if seqOff(i)(7) = '1' then
-                    v.discard(i) := '1';
-                  elsif seqOff(i)/=0 then
-                    v.discard(0) := '1';
+      -- wait for all streams to contribute
+      ready := '1';
+      for i in 0 to LANES_G-1 loop
+         if sAxisMasters(i).tValid = '0' then
+            ready := '0';
+         end if;
+      end loop;
+
+      case r.state is
+         when SOF_S =>
+            if ready = '1' then
+               if allBits(r.first, '1') then  -- wait for all lanes to start
+                  --  test sequence numbers
+                  notSeq := (others => '0');
+                  for i in 0 to LANES_G-1 loop
+                     if sAxisMasters(i).tData(SEQ_C'range) /= SEQ_C then
+                        notSeq(i) := '1';
+                     end if;
+                  end loop;
+                  if notSeq /= 0 then
+                     v.discard := notSeq;
+                     v.state   := ERR_S;
+                  else
+                     hdrErr := '0';
+                     for i in 1 to LANES_G-1 loop
+                        seqOff(i) := sAxisMasters(i).tData(7 downto 0) - sAxisMasters(0).tData(7 downto 0);
+                        if sAxisMasters(0).tData(7 downto 0) /= sAxisMasters(i).tData(7 downto 0) then
+                           hdrErr := '1';
+                        end if;
+                     end loop;
+                     if hdrErr = '1' then  -- sequence mismatch - discard late lanes
+                        v.discard := (others => '0');
+                        for i in 1 to LANES_G-1 loop
+                           if seqOff(i)(7) = '1' then
+                              v.discard(i) := '1';
+                           elsif seqOff(i) /= 0 then
+                              v.discard(0) := '1';
+                           end if;
+                        end loop;
+                        v.state := ERR_S;
+                     else
+                        tready    := (others => '1');
+                        v.discard := (others => '0');
+                        v.sof     := '1';
+                        v.state   := EOF_S;
+                     end if;
                   end if;
-                end loop;
-                v.state := ERR_S;
-              else
-                tready    := (others=>'1');
-                v.discard := (others=>'0');
-                v.sof     := '1';
-                v.state   := EOF_S;
-              end if;
+               else
+                  v.discard := not r.first;
+                  v.state   := ERR_S;
+               end if;
             end if;
-          else
-            v.discard := not r.first;
-            v.state   := ERR_S;
-          end if;
-        end if;
-      when EOF_S =>
-        if ready = '1' and v.master.tValid = '0' then
-          tready   := (others=>'1');
-          v.master := sAxisMaster(0);
-          -- assemble the data
-          for i in 0 to LANES_G-1 loop
-            for j in 0 to AXIS_CONFIG_G.TDATA_BYTES_C-1 loop
-              m := 8*j;
-              n := 8*(LANES_G*j+i);
-              v.master.tData(n+7 downto n) := sAxisMaster(i).tData(m+7 downto m);
-              v.master.tKeep(LANES_G*j+i)  := sAxisMaster(i).tKeep(j);
+         when EOF_S =>
+            if ready = '1' and v.master.tValid = '0' then
+               tready   := (others => '1');
+               v.master := sAxisMasters(0);
+               -- assemble the data
+               for i in 0 to LANES_G-1 loop
+                  for j in 0 to MASTER_AXI_CONFIG_G.TDATA_BYTES_C-1 loop
+                     m                            := 8*j;
+                     n                            := 8*(LANES_G*j+i);
+                     v.master.tData(n+7 downto n) := sAxisMasters(i).tData(m+7 downto m);
+                     v.master.tKeep(LANES_G*j+i)  := sAxisMasters(i).tKeep(j);
+                  end loop;
+               end loop;
+               -- user bits
+               axiStreamSetUserBit(MASTER_AXI_CONFIG_G, v.master, SSI_SOF_C, r.sof, 0);
+               -- cleanup
+               if allBits(tlast, '0') then
+                  v.discard      := (others => '0');
+                  v.master.tLast := '0';
+                  axiStreamSetUserBit(MASTER_AXI_CONFIG_G, v.master, SSI_EOFE_C, '0');
+               elsif allBits(tlast, '1') then
+                  v.discard      := (others => '0');
+                  v.master.tLast := '1';
+                  axiStreamSetUserBit(MASTER_AXI_CONFIG_G, v.master, SSI_EOFE_C, '0');
+                  v.state        := SOF_S;
+               else
+                  v.discard      := not tlast;
+                  v.master.tLast := '1';
+                  axiStreamSetUserBit(MASTER_AXI_CONFIG_G, v.master, SSI_EOFE_C, '0');
+                  v.state        := ERR_S;
+               end if;
+            end if;
+         when ERR_S =>
+            for i in 0 to LANES_G-1 loop
+               if r.discard(i) = '1' then
+                  tready (i)   := '1';  -- sink
+                  v.discard(i) := not sAxisMasters(i).tLast;
+               end if;
+               if v.discard = 0 then
+                  v.state := SOF_S;
+               end if;
             end loop;
-          end loop;
-          -- user bits
-          axiStreamSetUserBit(AXIS_CONFIG_G, v.master, SSI_SOF_C , r.sof, 0);
-          -- cleanup
-          if    allBits(tlast,'0') then
-            v.discard      := (others=>'0');
-            v.master.tLast := '0';
-            axiStreamSetUserBit(AXIS_CONFIG_G, v.master, SSI_EOFE_C, '0');
-          elsif allBits(tlast,'1') then
-            v.discard      := (others=>'0');
-            v.master.tLast := '1';
-            axiStreamSetUserBit(AXIS_CONFIG_G, v.master, SSI_EOFE_C, '0');
-            v.state        := SOF_S;
-          else
-            v.discard      := not tlast;
-            v.master.tLast := '1';
-            axiStreamSetUserBit(AXIS_CONFIG_G, v.master, SSI_EOFE_C, '0');
-            v.state        := ERR_S;
-          end if;
-        end if;
-      when ERR_S =>
-        for i in 0 to LANES_G-1 loop
-          if r.discard(i) = '1' then
-            tready   (i) := '1';  -- sink
-            v.discard(i) := not sAxisMaster(i).tLast;
-          end if;
-          if v.discard=0 then
-            v.state := SOF_S;
-          end if;
-        end loop;
-    end case;
+      end case;
 
-    --  start of packet is first tValid after tLast acknowledged
-    for i in 0 to LANES_G-1 loop
-      v.slaves(i).tReady := tready(i);
-      if sAxisMaster(i).tValid = '1' and v.slaves(i).tReady = '1' then
-        v.first (i) := sAxisMaster(i).tLast;
+      --  start of packet is first tValid after tLast acknowledged
+      for i in 0 to LANES_G-1 loop
+         v.slaves(i).tReady := tready(i);
+         if sAxisMasters(i).tValid = '1' and v.slaves(i).tReady = '1' then
+            v.first (i) := sAxisMasters(i).tLast;
+         end if;
+      end loop;
+
+      -- Outputs
+      sAxisSlaves <= v.slaves;
+      mAxisMaster <= r.master;
+
+      -- Synchronous Reset
+      if axisRst = '1' then
+         v := REG_INIT_C;
       end if;
-    end loop;
-    
-    sAxisSlave  <= v.slaves;
-    mAxisMaster <= r.master;
-    
-    if axisRst = '1' then
-      v := REG_INIT_C;
-    end if;
 
-    rin <= v;
-    
-  end process comb;
+      -- Register the variable for next clock cycle
+      rin <= v;
 
-  seq : process ( axisClk ) is
-  begin
-    if rising_edge(axisClk) then
-      r <= rin;
-    end if;
-  end process seq;
-  
-end top_level_app;
+   end process comb;
+
+   seq : process (axisClk) is
+   begin
+      if rising_edge(axisClk) then
+         r <= rin after TPD_G;
+      end if;
+   end process seq;
+
+end rtl;
--- axi/axi-stream/rtl/AxiStreamSplitter.vhd
+++ axi/axi-stream/rtl/AxiStreamSplitter.vhd
@@ -1,20 +1,14 @@
 ------------------------------------------------------------------------------
--- Title      : 
--------------------------------------------------------------------------------
--- File       : AxiStreamInterleave.vhd
--- Author     : Matt Weaver <weaver@slac.stanford.edu>
+-- File       : AxiStreamSplitter.vhd
 -- Company    : SLAC National Accelerator Laboratory
--- Created    : 2015-07-10
--- Last update: 2019-05-11
--- Platform   : 
--- Standard   : VHDL'93/02
 -------------------------------------------------------------------------------
+-- Description: splits a "wide" AXI stream bus into multiple "narrower" buses
 -------------------------------------------------------------------------------
--- This file is part of 'LCLS2 DAQ Software'.
+-- This file is part of 'SLAC Firmware Standard Library'.
 -- It is subject to the license terms in the LICENSE.txt file found in the 
 -- top-level directory of this distribution and at: 
 --    https://confluence.slac.stanford.edu/display/ppareg/LICENSE.html. 
--- No part of 'LCLS2 DAQ Software', including this file, 
+-- No part of 'SLAC Firmware Standard Library', including this file, 
 -- may be copied, modified, propagated, or distributed except according to 
 -- the terms contained in the LICENSE.txt file.
 -------------------------------------------------------------------------------
@@ -28,151 +22,133 @@ use work.AxiStreamPkg.all;
 use work.AxiStreamPkg.all;
 use work.SsiPkg.all;
 
-entity AxiStreamInterleave is
-   generic ( LANES_G        : integer := 4;
-             SAXIS_CONFIG_G : AxiStreamConfigType;
-             MAXIS_CONFIG_G : AxiStreamConfigType );
-   port ( axisClk         : in  sl;
-          axisRst         : in  sl;
-          sAxisMaster     : in  AxiStreamMasterType;
-          sAxisSlave      : out AxiStreamSlaveType;
-          mAxisMaster     : out AxiStreamMasterArray( LANES_G-1 downto 0 );
-          mAxisSlave      : in  AxiStreamSlaveArray ( LANES_G-1 downto 0 ) );
-end AxiStreamInterleave;
+entity AxiStreamSplitter is
+   generic (
+      TPD_G               : time     := 1 ns;
+      LANES_G             : positive := 4;
+      SLAVE_AXI_CONFIG_G  : AxiStreamConfigType;
+      MASTER_AXI_CONFIG_G : AxiStreamConfigType);
+   port (
+      -- Clock and Reset
+      axisClk      : in  sl;
+      axisRst      : in  sl;
+      -- Slave Port
+      sAxisMaster  : in  AxiStreamMasterType;
+      sAxisSlave   : out AxiStreamSlaveType;
+      -- Master Ports
+      mAxisMasters : out AxiStreamMasterArray(LANES_G-1 downto 0);
+      mAxisSlaves  : in  AxiStreamSlaveArray (LANES_G-1 downto 0));
+end AxiStreamSplitter;
 
-architecture top_level_app of AxiStreamInterleave is
+architecture rtl of AxiStreamSplitter is
 
-  constant SEQ_C : slv(15 downto 8) := x"55";
+   constant SEQ_C : slv(15 downto 8) := x"55";
 
-  type RegType is record
-    masters : AxiStreamMasterArray(LANES_G-1 downto 0);
-    nready  : slv                 (LANES_G-1 downto 0);
-    tSeq    : slv                 (7 downto 0);
-    first   : sl;
-    slave   : AxiStreamSlaveType;
-  end record;
+   type RegType is record
+      masters : AxiStreamMasterArray(LANES_G-1 downto 0);
+      nready  : slv (LANES_G-1 downto 0);
+      tSeq    : slv (7 downto 0);
+      first   : sl;
+      slave   : AxiStreamSlaveType;
+   end record;
 
-  constant REG_INIT_C : RegType := (
-    masters => (others=>axiStreamMasterInit(MAXIS_CONFIG_G)),
-    nready  => (others=>'0'),
-    tSeq    => (others=>'0'),
-    first   => '1',
-    slave   => AXI_STREAM_SLAVE_INIT_C );
-  
-  signal r   : RegType := REG_INIT_C;
-  signal rin : RegType;
+   constant REG_INIT_C : RegType := (
+      masters => (others => axiStreamMasterInit(MAXIS_CONFIG_G)),
+      nready  => (others => '0'),
+      tSeq    => (others => '0'),
+      first   => '1',
+      slave   => AXI_STREAM_SLAVE_INIT_C);
 
-  component ila_0
-    port ( clk    : in sl;
-           probe0 : in slv(255 downto 0) );
-  end component;
+   signal r   : RegType := REG_INIT_C;
+   signal rin : RegType;
 
 begin
 
-  U_ILA : ila_0
-    port map ( clk     => axisClk,
-               probe0( 15 downto   0) => r.masters(0).tData(15 downto 0),
-               probe0( 31 downto  16) => r.masters(1).tData(15 downto 0),
-               probe0( 47 downto  32) => r.masters(2).tData(15 downto 0),
-               probe0( 63 downto  48) => r.masters(3).tData(15 downto 0),
-               probe0(            64) => r.masters(0).tValid,
-               probe0(            65) => r.masters(1).tValid,
-               probe0(            66) => r.masters(2).tValid,
-               probe0(            67) => r.masters(3).tValid,
-               probe0(            68) => r.masters(0).tLast,
-               probe0(            69) => r.masters(1).tLast,
-               probe0(            70) => r.masters(2).tLast,
-               probe0(            71) => r.masters(3).tLast,
-               probe0( 79 downto  72) => r.tSeq,
-               probe0( 83 downto  80) => r.nready,
-               probe0(            84) => r.first,
-               probe0(            85) => sAxisMaster.tValid,
-               probe0(            86) => sAxisMaster.tLast,
-               probe0(            87) => r.slave.tReady,
-               probe0(            88) => mAxisSlave(0).tReady,
-               probe0(            89) => mAxisSlave(1).tReady,
-               probe0(            90) => mAxisSlave(2).tReady,
-               probe0(            91) => mAxisSlave(3).tReady,
-               probe0(255 downto  92) => (others=>'0') );
-  
-  comb : process ( r, axisRst, sAxisMaster, mAxisSlave ) is
-    variable v : RegType;
-    variable m,n : integer;
-  begin
-    v := r;
+   assert (SLAVE_AXI_CONFIG_G.TDATA_BYTES_C = LANES_G*MASTER_AXI_CONFIG_G.TDATA_BYTES_C)
+      report "SLAVE_AXI_CONFIG_G.TDATA_BYTES_C must be LANES_G*MASTER_AXI_CONFIG_G.TDATA_BYTES_C" severity failure;
 
-    v.slave.tReady := '0';
-    
-    if v.nready /= 0 then
-      for i in 0 to LANES_G-1 loop
-        if mAxisSlave(i).tReady = '1' then
-          v.nready (i)        := '0';
-          v.masters(i).tValid := '0';
-        end if;
-      end loop;
-    end if;
+   comb : process (axisRst, mAxisSlaves, r, sAxisMaster) is
+      variable v    : RegType;
+      variable m, n : integer;
+   begin
+      -- Latch the current value
+      v := r;
 
-    if v.nready = 0 then
-      if sAxisMaster.tValid = '1' then
-
-        if (r.first = '1') then
-
-          --  Insert user sequence# for maintaining alignment of interleaved streams
-          v.first := '0';
-          for i in 0 to LANES_G-1 loop
-            axiStreamSetUserBit(MAXIS_CONFIG_G, v.masters(i), SSI_SOF_C, '1', 0);
-            v.nready (i)        := '1';
-            v.masters(i).tValid := '1';
-            v.masters(i).tLast  := '0';
-            v.masters(i).tData(SEQ_C'range) := SEQ_C;
-            v.masters(i).tData(r.tSeq'range) := r.tSeq;
-            v.masters(i).tKeep  := genTKeep(MAXIS_CONFIG_G.TDATA_BYTES_C);
-            v.tSeq              := r.tSeq+1;
-          end loop;
-
-        else
-
-          v.slave.tReady := '1';
-          v.first        := sAxisMaster.tLast;
-          for i in 0 to LANES_G-1 loop
-            v.nready (i)        := '1';
-            v.masters(i).tValid := '1';
-            v.masters(i).tLast  := sAxisMaster.tLast;
-            
-            -- set user bits
-            axiStreamSetUserBit(MAXIS_CONFIG_G, v.masters(i), SSI_SOF_C, '0', 0);
-            if sAxisMaster.tLast = '1' then
-              axiStreamSetUserBit(MAXIS_CONFIG_G, v.masters(i), SSI_EOFE_C, '0', 0);
+      -- AXI stream flow control
+      v.slave.tReady := '0';
+      if v.nready /= 0 then
+         for i in 0 to LANES_G-1 loop
+            if mAxisSlaves(i).tReady = '1' then
+               v.nready (i)        := '0';
+               v.masters(i).tValid := '0';
             end if;
-            
-            -- distribute data
-            for j in 0 to MAXIS_CONFIG_G.TDATA_BYTES_C-1 loop
-              m := 8*j;
-              n := 8*(LANES_G*j+i);
-              v.masters(i).tData(m+7 downto m) := sAxisMaster.tData(n+7 downto n);
-              v.masters(i).tKeep(j)            := sAxisMaster.tKeep(LANES_G*j+i);
-            end loop;
-          end loop;
-        end if;
+         end loop;
       end if;
-    end if;
 
-    sAxisSlave  <= v.slave;
-    mAxisMaster <= r.masters;
-    
-    if axisRst = '1' then
-      v := REG_INIT_C;
-    end if;
+      if v.nready = 0 then
+         if sAxisMaster.tValid = '1' then
 
-    rin <= v;
-    
-  end process comb;
+            if (r.first = '1') then
 
-  seq : process ( axisClk ) is
-  begin
-    if rising_edge(axisClk) then
-      r <= rin;
-    end if;
-  end process seq;
-  
-end top_level_app;
+               --  Insert user sequence# for maintaining alignment of interleaved streams
+               v.first := '0';
+               for i in 0 to LANES_G-1 loop
+                  axiStreamSetUserBit(MAXIS_CONFIG_G, v.masters(i), SSI_SOF_C, '1', 0);
+                  v.nready (i)                     := '1';
+                  v.masters(i).tValid              := '1';
+                  v.masters(i).tLast               := '0';
+                  v.masters(i).tData(SEQ_C'range)  := SEQ_C;
+                  v.masters(i).tData(r.tSeq'range) := r.tSeq;
+                  v.masters(i).tKeep               := genTKeep(MAXIS_CONFIG_G.TDATA_BYTES_C);
+                  v.tSeq                           := r.tSeq+1;
+               end loop;
+
+            else
+
+               v.slave.tReady := '1';
+               v.first        := sAxisMaster.tLast;
+               for i in 0 to LANES_G-1 loop
+                  v.nready (i)        := '1';
+                  v.masters(i).tValid := '1';
+                  v.masters(i).tLast  := sAxisMaster.tLast;
+
+                  -- set user bits
+                  axiStreamSetUserBit(MAXIS_CONFIG_G, v.masters(i), SSI_SOF_C, '0', 0);
+                  if sAxisMaster.tLast = '1' then
+                     axiStreamSetUserBit(MAXIS_CONFIG_G, v.masters(i), SSI_EOFE_C, '0', 0);
+                  end if;
+
+                  -- distribute data
+                  for j in 0 to MAXIS_CONFIG_G.TDATA_BYTES_C-1 loop
+                     m                                := 8*j;
+                     n                                := 8*(LANES_G*j+i);
+                     v.masters(i).tData(m+7 downto m) := sAxisMaster.tData(n+7 downto n);
+                     v.masters(i).tKeep(j)            := sAxisMaster.tKeep(LANES_G*j+i);
+                  end loop;
+               end loop;
+            end if;
+         end if;
+      end if;
+
+      -- Outputs
+      sAxisSlave   <= v.slave;
+      mAxisMasters <= r.masters;
+
+      -- Synchronous Reset
+      if axisRst = '1' then
+         v := REG_INIT_C;
+      end if;
+
+      -- Register the variable for next clock cycle
+      rin <= v;
+
+   end process comb;
+
+   seq : process (axisClk) is
+   begin
+      if rising_edge(axisClk) then
+         r <= rin after TPD_G;
+      end if;
+   end process seq;
+
+end rtl;
