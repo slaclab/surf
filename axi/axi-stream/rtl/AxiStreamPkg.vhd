a370cf719993b1e9796d4518ce4ec869d2c8308b
Larry Ruckman <ruckman@slac.stanford.edu>
2019-10-23 14:05:25 -0700

    increasing AXIS from 512-bit to 1024-bit support

--- axi/axi-stream/rtl/AxiStreamPkg.vhd
+++ axi/axi-stream/rtl/AxiStreamPkg.vhd
@@ -23,7 +23,7 @@ use work.StdRtlPkg.all;
 
 package AxiStreamPkg is
 
-   constant AXI_STREAM_MAX_TDATA_WIDTH_C : positive := 512;  -- Units of bits
+   constant AXI_STREAM_MAX_TDATA_WIDTH_C : positive := 1024;  -- Units of bits
    constant AXI_STREAM_MAX_TKEEP_WIDTH_C : positive := (AXI_STREAM_MAX_TDATA_WIDTH_C/8);  -- Units of bytes
 
    type AxiStreamMasterType is record
