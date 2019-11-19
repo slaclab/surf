d8c96c74d1e1d29c334f9670b433b5c7e32ce174
Larry Ruckman <ruckman@slac.stanford.edu>
2019-11-18 17:41:27 -0800

    ClkRst assert bug fix

--- base/general/tb/ClkRst.vhd
+++ base/general/tb/ClkRst.vhd
@@ -46,10 +46,10 @@ architecture ClkRst of ClkRst is
    signal rstFb : sl := '0';
 
 begin
-   
-   assert CLK_HIGH_C = CLK_LOW_C report
-      "ClkRst: CLK_HIGH_C (" & str(CLK_HIGH_C) & ") does not match CLK_LOW_C (" & str(CLK_LOW_C) & "). \n\
-      "If you need 50% duty cycle for this clock, you will need to increase the simulator time resolution."
+
+   assert (CLK_HIGH_C = CLK_LOW_C)
+      -- report "ClkRst: CLK_HIGH_C (" & str(CLK_HIGH_C) & ") does not match CLK_LOW_C (" & str(CLK_LOW_C) & "). \n\
+      report "ClkRst: CLK_HIGH_C does not match CLK_LOW_C. \n if you need 50 percent duty cycle for this clock, you will need to increase the simulator time resolution." 
       severity warning;
 
    process is
