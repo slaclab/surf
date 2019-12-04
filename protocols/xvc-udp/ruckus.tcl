e7029c0d616b981a455ac0a75a629fea04dca602
Larry Ruckman <ruckman@slac.stanford.edu>
2019-12-04 14:18:00 -0800

    Prevent exiting if Vivado version < 2018.3

--- protocols/xvc-udp/ruckus.tcl
+++ protocols/xvc-udp/ruckus.tcl
@@ -2,40 +2,44 @@
 source -quiet $::env(RUCKUS_DIR)/vivado_proc.tcl
 
 # Check for version 2018.3 of Vivado (or later)
-if { [VersionCheck 2018.3] < 0 } {exit -1}
+if { [VersionCheck 2018.3] < 0 } {
 
-# Load the wrapper source code
-loadSource -lib surf -dir "$::DIR_PATH/rtl"
+   # Load the wrapper source code
+   loadSource -lib surf -dir "$::DIR_PATH/rtl"
 
-# Get the family type
-set family [getFpgaFamily]
+   # Get the family type
+   set family [getFpgaFamily]
 
-if { ${family} eq {artix7}  ||
-     ${family} eq {kintex7} ||
-     ${family} eq {virtex7} ||
-     ${family} eq {zynq} } {
-   set dirType "7Series"
-}
+   if { ${family} eq {artix7}  ||
+        ${family} eq {kintex7} ||
+        ${family} eq {virtex7} ||
+        ${family} eq {zynq} } {
+      set dirType "7Series"
+   }
 
-if { ${family} eq {kintexu} ||
-     ${family} eq {kintexuplus} ||
-     ${family} eq {virtexuplus} ||
-     ${family} eq {virtexuplusHBM} ||
-     ${family} eq {zynquplus} } {
-   set dirType "UltraScale"
-}
+   if { ${family} eq {kintexu} ||
+        ${family} eq {kintexuplus} ||
+        ${family} eq {virtexuplus} ||
+        ${family} eq {virtexuplusHBM} ||
+        ${family} eq {zynquplus} } {
+      set dirType "UltraScale"
+   }
 
-if { [info exists ::env(USE_XVC_DEBUG)] != 1 || $::env(USE_XVC_DEBUG) == 0 } {
-	loadSource -lib surf -path "$::DIR_PATH/dcp/${dirType}/Stub/images/UdpDebugBridge.dcp"
-   set_property IS_GLOBAL_INCLUDE {1} [get_files UdpDebugBridge.dcp]
-    
-} elseif { $::env(USE_XVC_DEBUG) == -1 } {
-   puts "Note: USE_XVC_DEBUG = -1"
-   puts "The debug bridge is left as a black box"
-   puts "and it is the application's responsibility"
-   puts "to define a suitable implementation."
-   
+   if { [info exists ::env(USE_XVC_DEBUG)] != 1 || $::env(USE_XVC_DEBUG) == 0 } {
+      loadSource -lib surf -path "$::DIR_PATH/dcp/${dirType}/Stub/images/UdpDebugBridge.dcp"
+      set_property IS_GLOBAL_INCLUDE {1} [get_files UdpDebugBridge.dcp]
+       
+   } elseif { $::env(USE_XVC_DEBUG) == -1 } {
+      puts "Note: USE_XVC_DEBUG = -1"
+      puts "The debug bridge is left as a black box"
+      puts "and it is the application's responsibility"
+      puts "to define a suitable implementation."
+      
+   } else {
+      loadSource -lib surf -path "$::DIR_PATH/dcp/${dirType}/Impl/images/UdpDebugBridge.dcp"
+       set_property IS_GLOBAL_INCLUDE {1} [get_files UdpDebugBridge.dcp]
+   }
+
 } else {
-	loadSource -lib surf -path "$::DIR_PATH/dcp/${dirType}/Impl/images/UdpDebugBridge.dcp"
-    set_property IS_GLOBAL_INCLUDE {1} [get_files UdpDebugBridge.dcp]
+   puts "\n\nWARNING: $::DIR_PATH requires Vivado 2018.3 (or later)\n\n"
 }
