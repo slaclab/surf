0e45d62c4e98bcd56f1ae2ef5930ce571d9ec657
Ryan Herbst <rherbst@slac.stanford.edu>
2020-02-11 20:16:28 -0800

    Change python package name to lower case

--- setup.py
+++ setup.py
@@ -12,7 +12,7 @@ with open('python/surf/__init__.py','a') as vf:
     vf.write(f'\n__version__="{ver}"\n')
 
 setup (
-   name='Surf',
+   name='surf',
    version=ver,
    packages=['surf',
              'surf/axi',
