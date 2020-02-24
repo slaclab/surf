778c95e2b389011b404d4adb004cc8e96d380c9c
Benjamin Reese <bareese@slac.stanford.edu>
2020-02-24 13:18:36 -0800

    Add OUT_DELAY_G feature, use register chain for PULSE_WIDTH_G
    Also clean up generics

--- base/sync/rtl/SynchronizerOneShot.vhd
+++ base/sync/rtl/SynchronizerOneShot.vhd
@@ -21,40 +21,21 @@ use surf.StdRtlPkg.all;
 
 entity SynchronizerOneShot is
    generic (
-      TPD_G           : time     := 1 ns;   -- Simulation FF output delay
-      RST_POLARITY_G  : sl       := '1';  -- '1' for active HIGH reset, '0' for active LOW reset
-      RST_ASYNC_G     : boolean  := false;  -- Reset is asynchronous
-      BYPASS_SYNC_G   : boolean  := false;  -- Bypass RstSync module for synchronous data configuration
-      RELEASE_DELAY_G : positive := 3;  -- Delay between deassertion of async and sync resets
-      IN_POLARITY_G   : sl       := '1';  -- 0 for active LOW, 1 for active HIGH
-      OUT_POLARITY_G  : sl       := '1';  -- 0 for active LOW, 1 for active HIGH
-      PULSE_WIDTH_G   : positive := 1);  -- one-shot pulse width duration (units of clk cycles)
+      TPD_G          : time     := 1 ns;   -- Simulation FF output delay
+      BYPASS_SYNC_G  : boolean  := false;  -- Bypass RstSync module for synchronous data configuration
+      IN_POLARITY_G  : sl       := '1';    -- 0 for active LOW, 1 for active HIGH
+      OUT_POLARITY_G : sl       := '1';    -- 0 for active LOW, 1 for active HIGH
+      OUT_DELAY_G    : positive := 3;   -- Stages in output sync chain
+      PULSE_WIDTH_G  : positive := 1);  -- one-shot pulse width duration (units of clk cycles)
    port (
       clk     : in  sl;                 -- Clock to be SYNC'd to
-      rst     : in  sl := not RST_POLARITY_G;  -- Optional reset
       dataIn  : in  sl;                 -- Trigger to be sync'd
       dataOut : out sl);                -- synced one-shot pulse
 end SynchronizerOneShot;
 
 architecture rtl of SynchronizerOneShot is
 
-   type StateType is (
-      IDLE_S,
-      CNT_S);
-
-   type RegType is record
-      cnt     : natural range 0 to (PULSE_WIDTH_G-1);
-      dataOut : sl;
-      state   : StateType;
-   end record RegType;
-
-   constant REG_INIT_C : RegType := (
-      cnt     => 0,
-      dataOut => not(OUT_POLARITY_G),
-      state   => IDLE_S);
-
-   signal r   : RegType := REG_INIT_C;
-   signal rin : RegType;
+   constant NOT_OUT_POLARITY_C : sl := not OUT_POLARITY_G;
 
    signal pulseRst : sl;
    signal edgeDet  : sl;
@@ -71,10 +52,9 @@ begin
    GEN_ASYNC : if (BYPASS_SYNC_G = false) generate
       RstSync_Inst : entity surf.RstSync
          generic map (
-            TPD_G           => TPD_G,
-            RELEASE_DELAY_G => RELEASE_DELAY_G,
-            IN_POLARITY_G   => IN_POLARITY_G,
-            OUT_POLARITY_G  => '1')
+            TPD_G          => TPD_G,
+            IN_POLARITY_G  => IN_POLARITY_G,
+            OUT_POLARITY_G => '1')
          port map (
             clk      => clk,
             asyncRst => dataIn,
@@ -86,7 +66,7 @@ begin
          TPD_G          => TPD_G,
          RST_POLARITY_G => RST_POLARITY_G,
          OUT_POLARITY_G => OUT_POLARITY_G,
-         RST_ASYNC_G    => RST_ASYNC_G,
+         STAGES_G       => OUT_DELAY_G,
          BYPASS_SYNC_G  => BYPASS_SYNC_G)
       port map (
          clk        => clk,
@@ -97,61 +77,25 @@ begin
       dataOut <= edgeDet;
    end generate;
 
+
    U_PulseStretcher : if (PULSE_WIDTH_G > 1) generate
+      -- Strech the pulse using a simple synchronously reset register chain
+      -- Using PULSE_WIDTH_G > 1 will incur 1 extra cycle of OUT_DELAY_G
 
-      comb : process (edgeDet, r, rst) is
-         variable v : RegType;
-      begin
-         -- Latch the current value
-         v := r;
-
-         -- State Machine
-         case r.state is
-            ----------------------------------------------------------------------   
-            when IDLE_S =>
-               -- Reset the flag
-               v.dataOut := not(OUT_POLARITY_G);
-               -- Check for edge detection
-               if (edgeDet = OUT_POLARITY_G) then
-                  -- Next state
-                  v.state := CNT_S;
-               end if;
-            ----------------------------------------------------------------------   
-            when CNT_S =>
-               -- Set the flag
-               v.dataOut := OUT_POLARITY_G;
-               -- Check the counter
-               if r.cnt = (PULSE_WIDTH_G-1) then
-                  -- Reset the counter
-                  v.cnt   := 0;
-                  -- Next state
-                  v.state := IDLE_S;
-               else
-                  -- Increment the counter
-                  v.cnt := r.cnt + 1;
-               end if;
-         ----------------------------------------------------------------------   
-         end case;
-
-         -- Combinatorial outputs before the reset
-         dataOut <= v.dataOut;
-
-         -- Reset
-         if (rst = RST_POLARITY_G) then
-            v := REG_INIT_C;
-         end if;
-
-         -- Register the variable for next clock cycle
-         rin <= v;
-
-      end process;
-
-      seq : process (clk) is
-      begin
-         if rising_edge(clk) then
-            r <= rin after TPD_G;
-         end if;
-      end process seq;
+      U_Synchronizer_1 : entity surf.Synchronizer
+         generic map (
+            TPD_G          => TPD_G,
+            RST_POLARITY_G => OUT_POLARITY_G
+            OUT_POLARITY_G => OUT_POLARITY_G,
+            RST_ASYNC_G    => false,
+            STAGES_G       => PULSE_WIDTH_G,
+            BYPASS_SYNC_G  => false,
+            INIT_G         => OUT_POLARITY_G)
+         port map (
+            clk     => clk,                 -- [in]
+            rst     => edgeDet,             -- [in]
+            dataIn  => NOT_OUT_POLARITY_C,  -- [in]
+            dataOut => dataOut);            -- [out]
 
    end generate;
 
