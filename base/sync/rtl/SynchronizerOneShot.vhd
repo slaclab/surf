ff0cd80787dcf630a80ea10d43fb1a023b15e73f
Benjamin Reese <bareese@slac.stanford.edu>
2020-02-24 15:17:46 -0800

    Add reset

--- base/sync/rtl/SynchronizerOneShot.vhd
+++ base/sync/rtl/SynchronizerOneShot.vhd
@@ -22,6 +22,7 @@ use surf.StdRtlPkg.all;
 entity SynchronizerOneShot is
    generic (
       TPD_G          : time     := 1 ns;   -- Simulation FF output delay
+      RST_POLARITY_G : sl       := '1';    -- '1' for active HIGH reset, '0' for active LOW reset
       BYPASS_SYNC_G  : boolean  := false;  -- Bypass RstSync module for synchronous data configuration
       IN_POLARITY_G  : sl       := '1';    -- 0 for active LOW, 1 for active HIGH
       OUT_POLARITY_G : sl       := '1';    -- 0 for active LOW, 1 for active HIGH
@@ -29,13 +30,24 @@ entity SynchronizerOneShot is
       PULSE_WIDTH_G  : positive := 1);  -- one-shot pulse width duration (units of clk cycles)
    port (
       clk     : in  sl;                 -- Clock to be SYNC'd to
+      rst     : in  sl := not RST_POLARITY_G;  --Optional reset
       dataIn  : in  sl;                 -- Trigger to be sync'd
       dataOut : out sl);                -- synced one-shot pulse
 end SynchronizerOneShot;
 
 architecture rtl of SynchronizerOneShot is
 
-   constant NOT_OUT_POLARITY_C : sl := not OUT_POLARITY_G;
+   type RegType is record
+      dataOut : sl;
+      counter : integer range 0 to PULSE_WIDTH_G;
+   end record RegType;
+
+   constant REG_INIT_C : RegType := (
+      dataOut => '0',
+      counter => 0);
+
+   signal r   : RegType := REG_INIT_C;
+   signal rin : RegType;
 
    signal pulseRst : sl;
    signal edgeDet  : sl;
@@ -70,6 +82,7 @@ begin
          BYPASS_SYNC_G  => BYPASS_SYNC_G)
       port map (
          clk        => clk,
+         rst        => rst,
          dataIn     => pulseRst,
          risingEdge => edgeDet);
 
@@ -82,20 +95,44 @@ begin
       -- Strech the pulse using a simple synchronously reset register chain
       -- Using PULSE_WIDTH_G > 1 will incur 1 extra cycle of OUT_DELAY_G
 
-      U_Synchronizer_1 : entity surf.Synchronizer
-         generic map (
-            TPD_G          => TPD_G,
-            RST_POLARITY_G => OUT_POLARITY_G
-            OUT_POLARITY_G => OUT_POLARITY_G,
-            RST_ASYNC_G    => false,
-            STAGES_G       => PULSE_WIDTH_G,
-            BYPASS_SYNC_G  => false,
-            INIT_G         => OUT_POLARITY_G)
-         port map (
-            clk     => clk,                 -- [in]
-            rst     => edgeDet,             -- [in]
-            dataIn  => NOT_OUT_POLARITY_C,  -- [in]
-            dataOut => dataOut);            -- [out]
+      comb : process (edgeDet, r, rst) is
+         variable v : RegType;
+      begin
+         v := r;
+
+         -- Assert output and start counting when edge seen
+         if (edgeDet = OUT_POLARITY_G) then
+            v.dataOut := OUT_POLARITY_G;
+            v.counter := r.counter + 1;
+         end if;
+
+         -- Keep counting
+         if (r.dataOut = OUT_POLARITY_G) then
+            v.counter := r.counter + 1;
+         end if;
+
+         -- Stop counting when PULSE_WIDTH_G counter to
+         if (r.counter = PULSE_WIDTH_G) then
+            v.dataOut := not OUT_POLARITY_G;
+            v.counter := 0;
+         end if;
+
+         if (rst = RST_POLARITY_G) then
+            v := REG_INIT_C;
+         end if;
+
+         rin <= v;
+
+         dataOut <= r.dataOut;
+
+      end process comb;
+
+      seq : process (clk) is
+      begin
+         if (rising_edge(clk)) then
+            r <= rin after TPD_G;
+         end if;
+      end process seq;
 
    end generate;
 
