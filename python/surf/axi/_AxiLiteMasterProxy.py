5276f69534c8c4648e69de3d0fa0bf5ff334258c
Benjamin Reese <bareese@slac.stanford.edu>
2020-04-24 08:09:22 -0700

    Move registers and proxy to separate Devices

--- python/surf/axi/_AxiLiteMasterProxy.py
+++ python/surf/axi/_AxiLiteMasterProxy.py
@@ -3,13 +3,9 @@ import rogue
 
 import threading
 
-class AxiLiteMasterProxy(pr.Device):
-
+class _Regs(pr.Device):
     def __init__(self, **kwargs):
-
-        super().__init__(size=4 hubMin=4, hubMax=4, **kwargs)
-
-        self._dataBa = bytearray(4)
+        super().__init__(**kwargs)
 
         self.add(pr.RemoteVariable(
             name = 'Rnw',
@@ -54,6 +50,13 @@ class AxiLiteMasterProxy(pr.Device):
             bitSize = 32,
             base = pr.UInt))
 
+class _Proxy(pr.Device):
+
+    def __init__(self, regs, **kwargs):
+        super().__init__(size=4 hubMin=4, hubMax=4, **kwargs)
+        self._regs = regs
+        self._dataBa = bytearray(4)
+
     def _doTransaction(self, transaction):
         with self._memLock, transaction.lock():
             # Clear any existing errors
@@ -61,22 +64,22 @@ class AxiLiteMasterProxy(pr.Device):
 
             # Get the transaction address and write it to the proxy address register
             addr = transaction.address()
-            self.Addr.set(addr, write=True)
+            self._regs.Addr.set(addr, write=True)
 
             if transation.type() == rogue.interfaces.memory.Write:
                 # Convert data bytes to int and write data to proxy register
                 transaction.getData(self._dataBa, 0)
                 data = int.from_bytes(self._dataBa, 'little', signed=False)
-                self.Data.set(data, write=True)
+                self._regs.Data.set(data, write=True)
 
                 # Kick off the proxy transaction
-                self.Rnw.setDisp('Write', write=True)
+                self._regs.Rnw.setDisp('Write', write=True)
 
             elif transaction.type() == rogue.interfaces.memory.Read or
                  transaction.type() == rogue.interfaces.memory.Verify:
 
                  # Kick off the read proxy txn
-                 self.Rnw.setDisp('Read', write=True)
+                 self._regs.Rnw.setDisp('Read', write=True)
 
             else:
                 # Post transactions not allowed
@@ -87,23 +90,29 @@ class AxiLiteMasterProxy(pr.Device):
             # Probably need some timeout here
             done = False
             while done is False:
-                done = self.Done.get(read=True)
+                done = self._regs.Done.get(read=True)
                 time.sleep(.1)
 
             # Check for error flags
-            resp = self.Resp.get(read=True)
+            resp = self._regs.Resp.get(read=True)
             if resp != 0:
                 tran.error(f'AXIL tranaction failed with RESP: {resp}')
                 return
 
             # Finish the transaction
-            if self.Rnw.valueDisp() == 'Write':
+            if self._regs.Rnw.valueDisp() == 'Write':
                 tran.done()
             else:
-                data = self.Data.get(read=True)
+                data = self._regs.Data.get(read=True)
                 self._dataBa = data.toBytes(4, 'little', signed=False)
                 tran.setData(self._dataBa, 0)
                 tran.done()
 
 
-                
+class AxiLiteMasterProxy(pr.Device):
+    
+    def __init__(self, **kwargs):
+        super().__init__(**kwargs)
+
+        self.add(_Regs(name='Regs'))
+        self.add(_Proxy(name='Proxy', regs=self.Regs))
