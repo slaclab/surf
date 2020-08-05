46bc4782b5cf6823763127ac53941416280baa57
mkwiatko <mwkwiatko@gmail.com>
2020-08-04 17:09:36 -0700

    error fix

--- devices/AnalogDevices/ad9249/UltraScale/rtl/Ad9249Deserializer.vhd
+++ devices/AnalogDevices/ad9249/UltraScale/rtl/Ad9249Deserializer.vhd
@@ -85,10 +85,6 @@ architecture rtl of Ad9249Deserializer is
    attribute keep of sData_i       : signal is "true";
 
 begin
-   
-   adcData     <= bitReverse(adcDv7R.masterAdcData(6 downto 0)) & bitReverse(adcDv7R.masterAdcData(13 downto 7)) when BIT_REV_G = '1'
-              else adcDv7R.masterAdcData;
-
 
    -------------------------------------------------------------------------------------------------
    -- Create Clocks
