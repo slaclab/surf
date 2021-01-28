4b6b5103e1c086e31fbc73adaf44b7e72a4e70e3
Benjamin Reese <bareese@slac.stanford.edu>
2021-01-28 13:08:08 -0800

    Add checksum verification for K-Codes on RX side

--- protocols/pgp/pgp4/core/rtl/Pgp4RxProtocol.vhd
+++ protocols/pgp/pgp4/core/rtl/Pgp4RxProtocol.vhd
@@ -144,8 +144,11 @@ begin
             -- Check for k-code
             if (protRxHeader = PGP4_K_HEADER_C) then
 
+               if pgp4KCodeCrc(protRxData) /= protRxData(PGP4_K_CODE_CRC_FIELD_C) then
+                  v.pgpRxOut.linkError := '1';
+
                -- Check for IDLE k-code
-               if (btf = PGP4_IDLE_C) then
+               elsif (btf = PGP4_IDLE_C) then
 
                   -- Extract the LinkInfo
                   pgp4ExtractLinkInfo(
