497129136a68608b3b97754260f1ebcf8427e0cd
Larry Ruckman <ruckman@slac.stanford.edu>
2021-05-19 08:07:34 -0700

    delay module reorg

--- base/delay/ruckus.tcl
+++ base/delay/ruckus.tcl
@@ -3,7 +3,6 @@ source -quiet $::env(RUCKUS_DIR)/vivado_proc.tcl
 
 # Load Source Code
 loadSource -lib surf -dir "$::DIR_PATH/rtl"
-loadSource -lib surf -dir "$::DIR_PATH/ip_integrator"
 
 # Load Simulation
 loadSource -lib surf -sim_only -dir "$::DIR_PATH/tb"
