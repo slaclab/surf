09781e830d37a5f0735c94e421940c202e2d32e0
Larry Ruckman <ruckman@slac.stanford.edu>
2021-05-19 08:10:15 -0700

    CRC module reorg

--- base/ruckus.tcl
+++ base/ruckus.tcl
@@ -3,6 +3,7 @@ source -quiet $::env(RUCKUS_DIR)/vivado_proc.tcl
 
 # Load ruckus files
 loadRuckusTcl "$::DIR_PATH/delay"
+loadRuckusTcl "$::DIR_PATH/crc"
 loadRuckusTcl "$::DIR_PATH/fifo"
 loadRuckusTcl "$::DIR_PATH/general"
 loadRuckusTcl "$::DIR_PATH/ram"
