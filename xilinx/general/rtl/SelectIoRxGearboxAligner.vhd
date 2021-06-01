82f7550cda8bf70a6cedd0a62cc6e13521d02058
Larry Ruckman <ruckman@slac.stanford.edu>
2021-06-01 10:35:28 -0700

    adding STEP_SIZE_G generic to SelectIoRxGearboxAligner.vhd

--- xilinx/general/rtl/SelectIoRxGearboxAligner.vhd
+++ xilinx/general/rtl/SelectIoRxGearboxAligner.vhd
@@ -22,9 +22,10 @@ use surf.StdRtlPkg.all;
 
 entity SelectIoRxGearboxAligner is
    generic (
-      TPD_G        : time    := 1 ns;
-      CODE_TYPE_G  : string  := "LINE_CODE";  -- or "SCRAMBLER"
-      SIMULATION_G : boolean := false);
+      TPD_G        : time     := 1 ns;
+      CODE_TYPE_G  : string   := "LINE_CODE";  -- or "SCRAMBLER"
+      STEP_SIZE_G  : positive := 1;
+      SIMULATION_G : boolean  := false);
    port (
       -- Clock and Reset
       clk             : in  sl;
@@ -134,7 +135,7 @@ begin
          else
 
             -- Increment the counter
-            v.dlyConfig := r.dlyConfig + 1;
+            v.dlyConfig := r.dlyConfig + STEP_SIZE_G;
 
             -- Reset the flag
             v.firstError := '1';
@@ -281,7 +282,7 @@ begin
 
                      -- Update the Delay module
                      v.dlyLoad(1) := '1';
-                     v.dlyConfig  := r.dlyConfig + 1;
+                     v.dlyConfig  := r.dlyConfig + STEP_SIZE_G;
 
                      -- Next state
                      v.state := SLIP_WAIT_S;
@@ -311,7 +312,7 @@ begin
 
                   -- Update the Delay module
                   v.dlyLoad(1) := '1';
-                  v.dlyConfig  := r.dlyConfig + 1;
+                  v.dlyConfig  := r.dlyConfig + STEP_SIZE_G;
 
                   -- Check for last count or first header error after min. eye width
                   if (scanCnt >= 255) or (v.errorDet = '1') then
