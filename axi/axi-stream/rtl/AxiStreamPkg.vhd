8e45c1036689454c0dcb6faf2028e882c657c42e
Larry Ruckman <ruckman@slac.stanford.edu>
2022-02-08 19:57:21 -0800

    remove white space

--- axi/axi-stream/rtl/AxiStreamPkg.vhd
+++ axi/axi-stream/rtl/AxiStreamPkg.vhd
@@ -388,7 +388,7 @@ package body AxiStreamPkg is
             ----------------------------------------------------
             if (tKeepFull(i) = '1') then
                if (axisConfig.TKEEP_MODE_C = TKEEP_COMP_C) then
-                  -- Assume LSBs are active when calculating active bytes in TKEEP_COMP_C mode                  
+                  -- Assume LSBs are active when calculating active bytes in TKEEP_COMP_C mode
                   retVar := (i+1);
                else
                   -- TKEEP_NORMAL_C mode
