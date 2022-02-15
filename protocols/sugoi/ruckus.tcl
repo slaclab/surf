0220bf9f7c734ae8722cd1b656fa412b6db5d419
Benjamin Reese <bareese@slac.stanford.edu>
2022-02-15 12:45:06 -0800

    Fix typo

--- protocols/sugoi/ruckus.tcl
+++ protocols/sugoi/ruckus.tcl
@@ -4,7 +4,7 @@ source -quiet $::env(RUCKUS_DIR)/vivado_proc.tcl
 # Load Source Code
 loadSource -lib surf -dir "$::DIR_PATH/rtl"
 loadSource -lib surf -dir "$::DIR_PATH/rtl/7Series"
-loadSource -lib surf -dir "$::DIR_PATH/rtl/Ultrascale"
+loadSource -lib surf -dir "$::DIR_PATH/rtl/UltraScale"
 
 # Load Simulation
 loadSource -lib surf -sim_only -dir "$::DIR_PATH/tb"
