87808dc1124d8a1f0b3f3ebf62922162941642ee
Larry Ruckman <ruckman@slac.stanford.edu>
2022-02-28 08:25:25 -0800

    updating the ruckus.tcl to use non-symbolic link of proc.tcl

--- protocols/pgp/pgp2b/ruckus.tcl
+++ protocols/pgp/pgp2b/ruckus.tcl
@@ -1,5 +1,5 @@
 # Load RUCKUS library
-source -quiet $::env(RUCKUS_DIR)/vivado_proc.tcl
+source -quiet $::env(RUCKUS_DIR)/vivado/proc.tcl
 
 # Load the Core
 loadRuckusTcl "$::DIR_PATH/core"
--- protocols/rssi/ruckus.tcl
+++ protocols/rssi/ruckus.tcl
@@ -1,5 +1,5 @@
 # Load RUCKUS library
-source -quiet $::env(RUCKUS_DIR)/vivado_proc.tcl
+source -quiet $::env(RUCKUS_DIR)/vivado/proc.tcl
 
 loadRuckusTcl "$::DIR_PATH/v1"
 loadRuckusTcl "$::DIR_PATH/v1b"
