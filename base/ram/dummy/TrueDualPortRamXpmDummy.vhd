4afdf60b60767874caa333e49005cbf54dc682d9
Larry Ruckman <ruckman@slac.stanford.edu>
2022-03-01 16:40:33 -0800

    adding base/ram/dummy

---
+++ base/ram/dummy/TrueDualPortRamXpmDummy.vhd
@@ -0,0 +1,65 @@
+-------------------------------------------------------------------------------
+-- Company    : SLAC National Accelerator Laboratory
+-------------------------------------------------------------------------------
+-- Description: Wrapper for Xilinx XPM FIFO module
+-------------------------------------------------------------------------------
+-- This file is part of 'SLAC Firmware Standard Library'.
+-- It is subject to the license terms in the LICENSE.txt file found in the
+-- top-level directory of this distribution and at:
+--    https://confluence.slac.stanford.edu/display/ppareg/LICENSE.html.
+-- No part of 'SLAC Firmware Standard Library', including this file,
+-- may be copied, modified, propagated, or distributed except according to
+-- the terms contained in the LICENSE.txt file.
+-------------------------------------------------------------------------------
+
+library ieee;
+use ieee.std_logic_1164.all;
+use ieee.std_logic_unsigned.all;
+use ieee.std_logic_arith.all;
+
+library surf;
+use surf.StdRtlPkg.all;
+
+entity TrueDualPortRamXpm is
+   generic (
+      TPD_G               : time                       := 1 ns;
+      COMMON_CLK_G        : boolean                    := false;
+      RST_POLARITY_G      : sl                         := '1';  -- '1' for active high rst, '0' for active low
+      MEMORY_TYPE_G       : string                     := "block";
+      MEMORY_INIT_FILE_G  : string                     := "none";
+      MEMORY_INIT_PARAM_G : string                     := "0";
+      WRITE_MODE_G        : string                     := "no_change";
+      READ_LATENCY_G      : natural range 0 to 100     := 1;
+      DATA_WIDTH_G        : integer range 1 to (2**24) := 16;
+      BYTE_WR_EN_G        : boolean                    := false;
+      BYTE_WIDTH_G        : integer range 8 to 9       := 8;  -- If BRAM, should be multiple or 8 or 9
+      ADDR_WIDTH_G        : integer range 1 to (2**24) := 4);
+   port (
+      -- Port A
+      clka   : in  sl                                                                          := '0';
+      ena    : in  sl                                                                          := '1';
+      wea    : in  slv(ite(BYTE_WR_EN_G, wordCount(DATA_WIDTH_G, BYTE_WIDTH_G), 1)-1 downto 0) := (others => '0');
+      regcea : in  sl                                                                          := '1';
+      rsta   : in  sl                                                                          := not(RST_POLARITY_G);
+      addra  : in  slv(ADDR_WIDTH_G-1 downto 0)                                                := (others => '0');
+      dina   : in  slv(DATA_WIDTH_G-1 downto 0)                                                := (others => '0');
+      douta  : out slv(DATA_WIDTH_G-1 downto 0)                                                := (others => '0');
+      -- Port B
+      clkb   : in  sl                                                                          := '0';
+      enb    : in  sl                                                                          := '1';
+      web    : in  slv(ite(BYTE_WR_EN_G, wordCount(DATA_WIDTH_G, BYTE_WIDTH_G), 1)-1 downto 0) := (others => '0');
+      regceb : in  sl                                                                          := '1';
+      rstb   : in  sl                                                                          := not(RST_POLARITY_G);
+      addrb  : in  slv(ADDR_WIDTH_G-1 downto 0)                                                := (others => '0');
+      dinb   : in  slv(DATA_WIDTH_G-1 downto 0)                                                := (others => '0');
+      doutb  : out slv(DATA_WIDTH_G-1 downto 0)                                                := (others => '0'));
+end TrueDualPortRamXpm;
+
+architecture mapping of TrueDualPortRamXpm is
+
+begin
+
+   assert (false)
+      report "surf.base.ram: TrueDualPortRamXpm not supported" severity failure;
+
+end mapping;
--- base/ram/ruckus.tcl
+++ base/ram/ruckus.tcl
@@ -6,10 +6,14 @@ loadSource -lib surf -dir "$::DIR_PATH/inferred"
 
 # Check for non-zero Vivado version (in-case non-Vivado project)
 if {  $::env(VIVADO_VERSION) > 0.0} {
-   loadSource -lib surf -dir "$::DIR_PATH/xilinx"
+   loadSource -lib surf  -dir "$::DIR_PATH/xilinx"
+   loadSource -lib surf -path "$::DIR_PATH/dummy/SimpleDualPortRamAlteraMfDummy.vhd"
+   loadSource -lib surf -path "$::DIR_PATH/dummy/TrueDualPortRamXpmAlteraMfDummy.vhd"
 
    # https://support.xilinx.com/s/article/67815?language=en_US
    if { $::env(VIVADO_VERSION) >= 2021.2 } {
       set_property XPM_LIBRARIES XPM_MEMORY [current_project]
    }
+} else {
+   loadSource -lib surf -dir "$::DIR_PATH/dummy"
 }
