ed3cdd22ce4c103e44d8ad6598e7ee388338c506
Larry Ruckman <ruckman@slac.stanford.edu>
2023-03-05 12:01:09 -0800

    ruckus.tcl updates
    adding support ruckus.tcl source code loading for ASIC designs with PGP included

--- protocols/pgp/ruckus.tcl
+++ protocols/pgp/ruckus.tcl
@@ -2,9 +2,13 @@
 source $::env(RUCKUS_QUIET_FLAG) $::env(RUCKUS_PROC_TCL)
 
 # Load ruckus files
-loadRuckusTcl "$::DIR_PATH/pgp2b"
-loadRuckusTcl "$::DIR_PATH/pgp2fc"
 loadRuckusTcl "$::DIR_PATH/pgp3"
 loadRuckusTcl "$::DIR_PATH/pgp4"
-
 loadSource -lib surf -dir "$::DIR_PATH/shared"
+
+# Check for non-zero Vivado version (in-case non-Vivado project)
+if {  $::env(VIVADO_VERSION) > 0.0} {
+   # Load ruckus files
+   loadRuckusTcl "$::DIR_PATH/pgp2b"
+   loadRuckusTcl "$::DIR_PATH/pgp2fc"
+}
