73b3ab3c7eacc8e3c2769bfdfd524bb0db090d4a
Larry Ruckman <ruckman@slac.stanford.edu>
2023-04-11 10:28:11 -0700

    migrating ruckus.tcl to RUCKUS_PROC_TCL_COMBO env var

--- xilinx/ruckus.tcl
+++ xilinx/ruckus.tcl
@@ -1,5 +1,5 @@
 # Load RUCKUS library
-source $::env(RUCKUS_QUIET_FLAG) $::env(RUCKUS_PROC_TCL)
+source $::env(RUCKUS_PROC_TCL_COMBO)
 
 # Check for non-zero Vivado version (in-case non-Vivado project)
 if {  $::env(VIVADO_VERSION) > 0.0} {
