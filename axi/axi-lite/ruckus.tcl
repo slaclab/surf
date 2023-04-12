1cee27c8d347f7ca6b9f2d7d31456b694b805059
Larry Ruckman <ruckman@slac.stanford.edu>
2023-04-12 08:51:28 -0700

    adding axi-lite/ip_integrator

--- axi/axi-lite/ruckus.tcl
+++ axi/axi-lite/ruckus.tcl
@@ -3,11 +3,7 @@ source $::env(RUCKUS_PROC_TCL_COMBO)
 
 # Load Source Code
 loadSource -lib surf -dir "$::DIR_PATH/rtl"
-
-# Check for non-zero Vivado version (in-case non-Vivado project)
-if {  $::env(VIVADO_VERSION) > 0.0} {
-   loadSource -lib surf -dir "$::DIR_PATH/ip_integrator"
-}
+loadSource -lib surf -dir "$::DIR_PATH/ip_integrator"
 
 # Load Simulation
 loadSource -lib surf -sim_only -dir "$::DIR_PATH/tb"
