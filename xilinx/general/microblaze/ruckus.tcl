fbfd396b24cec25bcc606a7182a8908e35c88829
Larry Ruckman <ruckman@slac.stanford.edu>
2023-05-18 15:57:44 -0700

    updating microblaze/ruckus.tcl
    Vivado 2023.1 has not fixed the bug with AXI GPIO and system processor reset IP core blocks

--- xilinx/general/microblaze/ruckus.tcl
+++ xilinx/general/microblaze/ruckus.tcl
@@ -19,7 +19,8 @@ if { [info exists ::env(VITIS_SRC_PATH)] != 1 }  {
       loadSource -lib surf -path "$::DIR_PATH/generate/MicroblazeBasicCoreWrapper.vhd"
 
       # Load the .bd file
-      if  { $::env(VIVADO_VERSION) == 2022.2 } {
+      if { $::env(VIVADO_VERSION) == 2023.1 ||
+           $::env(VIVADO_VERSION) == 2022.2 } {
          puts "\nVivado v$::env(VIVADO_VERSION) not supported for general/microblaze\n"
          exit -1
       } elseif  { $::env(VIVADO_VERSION) >= 2021.1 } {
