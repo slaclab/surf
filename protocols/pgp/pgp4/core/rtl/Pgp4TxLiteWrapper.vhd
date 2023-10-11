6607e78f5621e988bb6e39208860f09a640cc041
lorenzrota <lorenzor@rdusr234.slac.stanford.edu>
2023-10-11 11:20:26 -0700

    Added rst to Scrambler

--- protocols/pgp/pgp4/core/rtl/Pgp4TxLiteWrapper.vhd
+++ protocols/pgp/pgp4/core/rtl/Pgp4TxLiteWrapper.vhd
@@ -56,6 +56,7 @@ architecture mapping of Pgp4TxLiteWrapper is
 
    signal pgpTxMaster : AxiStreamMasterType := AXI_STREAM_MASTER_INIT_C;
    signal pgpTxSlave  : AxiStreamSlaveType;
+   signal rstL        : sl;
 
 begin
 
@@ -89,11 +90,13 @@ begin
          remRxFifoCtrl(0)=> AXI_STREAM_CTRL_UNUSED_C,
          remRxLinkReady  => '1',       
          -- PHY interface
-         phyTxActive     => '1',
+         phyTxActive     => rstL,
          phyTxReady      => phyTxReady,
          phyTxValid      => phyTxValid,
          phyTxStart      => open,
          phyTxData       => phyTxData(63 downto 0),
          phyTxHeader     => phyTxData(65 downto 64));
+
+   rstL <= not(rst);
 
 end architecture mapping;
