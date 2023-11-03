62f68a1af4e76ec3d344ae82fce2084908a95698
Larry Ruckman <ruckman@slac.stanford.edu>
2023-11-03 11:03:21 -0700

    updating microblaze/ruckus.tcl for Vivado 2023.2
    Vivado bug for .bd not resolved yet

--- xilinx/general/microblaze/ruckus.tcl
+++ xilinx/general/microblaze/ruckus.tcl
@@ -19,7 +19,8 @@ if { [info exists ::env(VITIS_SRC_PATH)] != 1 }  {
       loadSource -lib surf -path "$::DIR_PATH/generate/MicroblazeBasicCoreWrapper.vhd"
 
       # Load the .bd file
-      if { $::env(VIVADO_VERSION) == 2023.1 ||
+      if { $::env(VIVADO_VERSION) == 2023.2 ||
+           $::env(VIVADO_VERSION) == 2023.1 ||
            $::env(VIVADO_VERSION) == 2022.2 } {
          puts "\nVivado v$::env(VIVADO_VERSION) not supported for general/microblaze\n"
          exit -1
