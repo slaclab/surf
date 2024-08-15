7ad37a4d26e8d6a66edc36e5b1c686c8d36d9c8b
Larry Ruckman <ruckman@slac.stanford.edu>
2024-08-15 15:53:42 -0700

    bug fix for microblaze/bd/2021.1

--- xilinx/general/microblaze/ruckus.tcl
+++ xilinx/general/microblaze/ruckus.tcl
@@ -20,13 +20,17 @@ if { [info exists ::env(VITIS_SRC_PATH)] != 1 }  {
 
       # Load the .bd file
       if { $::env(VIVADO_VERSION) >= 2021.1 } {
-         loadBlockDesign -path "$::DIR_PATH/bd/2021.1/MicroblazeBasicCore.bd"
+         loadBlockDesign -path "$::DIR_PATH/bd/2021.1/MicroblazeBasicCore.tcl"
 
          # Word around for the locked IPs
-         open_bd_design [get_bd_designs MicroblazeBasicCore]
-         upgrade_ip [get_ips {MicroblazeBasicCore_rst_clk_wiz_1_100M_0 MicroblazeBasicCore_axi_gpio_0_0}] -log ip_upgrade.log
-         export_ip_user_files -of_objects [get_ips {MicroblazeBasicCore_rst_clk_wiz_1_100M_0 MicroblazeBasicCore_axi_gpio_0_0}] -no_script -sync -force -quiet
-         close_bd_design [get_bd_designs MicroblazeBasicCore]
+         # VivadoRefresh $::env(VIVADO_PROJECT)
+         # update_compile_order -fileset sources_1
+         # open_bd_design [get_files {MicroblazeBasicCore.bd}]
+         # report_ip_status
+         # upgrade_ip [get_ips {MicroblazeBasicCore_rst_clk_wiz_1_100M_0 MicroblazeBasicCore_axi_gpio_0_0}] -log ip_upgrade.log
+         # export_ip_user_files -of_objects [get_ips {MicroblazeBasicCore_rst_clk_wiz_1_100M_0 MicroblazeBasicCore_axi_gpio_0_0}] -no_script -sync -force -quiet
+         # close_bd_design [get_bd_designs MicroblazeBasicCore]
+         # GenerateBdWrappers
 
       } else {
          loadBlockDesign -path "$::DIR_PATH/bd/2020.1/MicroblazeBasicCore.bd"
