e649813498936f4f2c7e319ad865428906727927
Benjamin Reese <bareese@slac.stanford.edu>
2024-09-05 20:41:10 -0700

    Cleanup

--- protocols/saci/sim/SaciAxiLiteMasterTbWrapper.vhd
+++ protocols/saci/sim/SaciAxiLiteMasterTbWrapper.vhd
@@ -21,31 +21,8 @@ library surf;
 use surf.StdRtlPkg.all;
 use surf.AxiLitePkg.all;
 
-entity SaciAxiLiteMasterTb is
-   port (
-      -- AXI-Lite Interface
-      S_AXI_ACLK    : in  std_logic;
-      S_AXI_ARESETN : in  std_logic;
-      S_AXI_AWADDR  : in  std_logic_vector(31 downto 0);
-      S_AXI_AWPROT  : in  std_logic_vector(2 downto 0);
-      S_AXI_AWVALID : in  std_logic;
-      S_AXI_AWREADY : out std_logic;
-      S_AXI_WDATA   : in  std_logic_vector(31 downto 0);
-      S_AXI_WSTRB   : in  std_logic_vector(3 downto 0);
-      S_AXI_WVALID  : in  std_logic;
-      S_AXI_WREADY  : out std_logic;
-      S_AXI_BRESP   : out std_logic_vector(1 downto 0);
-      S_AXI_BVALID  : out std_logic;
-      S_AXI_BREADY  : in  std_logic;
-      S_AXI_ARADDR  : in  std_logic_vector(31 downto 0);
-      S_AXI_ARPROT  : in  std_logic_vector(2 downto 0);
-      S_AXI_ARVALID : in  std_logic;
-      S_AXI_ARREADY : out std_logic;
-      S_AXI_RDATA   : out std_logic_vector(31 downto 0);
-      S_AXI_RRESP   : out std_logic_vector(1 downto 0);
-      S_AXI_RVALID  : out std_logic;
-      S_AXI_RREADY  : in  std_logic);
-end SaciAxiLiteMasterTb;
+entity SaciAxiLiteMasterTbWrapper is
+end SaciAxiLiteMasterTbWrapper;
 
 architecture mapping of SaciAxiLiteMasterTb is
 
