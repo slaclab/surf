6600cbfcaa9021319c8cab173b2bd08d565840eb
Larry Ruckman <ruckman@slac.stanford.edu>
2025-05-08 19:45:04 -0700

    DSP code reorg

--- dsp/ruckus.tcl
+++ dsp/ruckus.tcl
@@ -1,16 +1,10 @@
 # Load RUCKUS library
 source $::env(RUCKUS_PROC_TCL)
 
-
 # Load Source Code
-loadSource -lib surf -dir "$::DIR_PATH/core"  -fileType "VHDL 2008"
-loadSource -lib surf -dir "$::DIR_PATH/fixed" -fileType "VHDL 2008"
+loadRuckusTcl "$::DIR_PATH/generic"
 
 # Check for non-zero Vivado version (in-case non-Vivado project)
 if {  $::env(VIVADO_VERSION) > 2023.1} {
-   loadSource -lib surf -dir "$::DIR_PATH/logic"  -fileType "VHDL 2008"
-   loadSource -lib surf -dir "$::DIR_PATH/xilinx" -fileType "VHDL 2008"
+   loadRuckusTcl "$::DIR_PATH/xilinx"
 }
-
-# Load Simulation
-loadSource -lib surf -sim_only -dir "$::DIR_PATH/tb" -fileType "VHDL 2008"
