f31c8760ed6a4e7ed204ebed9589a063e29c80c7
Zion Kang <zkang@slac.stanford.edu>
2025-07-25 10:17:09 -0700

    fixing ruckus tcl for IP

--- protocols/pgp/pgp3/gthUs/ruckus.tcl
+++ protocols/pgp/pgp3/gthUs/ruckus.tcl
@@ -9,20 +9,20 @@ if { $::env(VIVADO_VERSION) >= 2020.1 } {
 
    loadIpCore -path "$::DIR_PATH/ip/Pgp3GthUsIbert/Pgp3GthUsIbert.xci"
    
-   #loadSource -lib surf   -path "$::DIR_PATH/ip/Pgp3GthUsIp15G/Pgp3GthUsIp15G.dcp"
-    loadIpCore -path "$::DIR_PATH/ip/Pgp3GthUsIp15G/Pgp3GthUsIp15G.xci"
+   loadSource -lib surf   -path "$::DIR_PATH/ip/Pgp3GthUsIp15G/Pgp3GthUsIp15G.dcp"
+   #loadIpCore -path "$::DIR_PATH/ip/Pgp3GthUsIp15G/Pgp3GthUsIp15G.xci"
 
-   #loadSource -lib surf   -path "$::DIR_PATH/ip/Pgp3GthUsIp12G/Pgp3GthUsIp12G.dcp"
-   loadIpCore -path "$::DIR_PATH/ip/Pgp3GthUsIp12G/Pgp3GthUsIp12G.xci"
+   loadSource -lib surf   -path "$::DIR_PATH/ip/Pgp3GthUsIp12G/Pgp3GthUsIp12G.dcp"
+   #loadIpCore -path "$::DIR_PATH/ip/Pgp3GthUsIp12G/Pgp3GthUsIp12G.xci"
 
-   #loadSource -lib surf   -path "$::DIR_PATH/ip/Pgp3GthUsIp10G/Pgp3GthUsIp10G.dcp"
-   loadIpCore -path "$::DIR_PATH/ip/Pgp3GthUsIp10G/Pgp3GthUsIp10G.xci"
+   loadSource -lib surf   -path "$::DIR_PATH/ip/Pgp3GthUsIp10G/Pgp3GthUsIp10G.dcp"
+   #loadIpCore -path "$::DIR_PATH/ip/Pgp3GthUsIp10G/Pgp3GthUsIp10G.xci"
 
-   #loadSource -lib surf   -path "$::DIR_PATH/ip/Pgp3GthUsIp6G/Pgp3GthUsIp6G.dcp"
-   loadIpCore -path "$::DIR_PATH/ip/Pgp3GthUsIp6G/Pgp3GthUsIp6G.xci"
+   loadSource -lib surf   -path "$::DIR_PATH/ip/Pgp3GthUsIp6G/Pgp3GthUsIp6G.dcp"
+   #loadIpCore -path "$::DIR_PATH/ip/Pgp3GthUsIp6G/Pgp3GthUsIp6G.xci"
 
-   #loadSource -lib surf   -path "$::DIR_PATH/ip/Pgp3GthUsIp3G/Pgp3GthUsIp3G.dcp"
-   loadIpCore -path "$::DIR_PATH/ip/Pgp3GthUsIp3G/Pgp3GthUsIp3G.xci"
+   loadSource -lib surf   -path "$::DIR_PATH/ip/Pgp3GthUsIp3G/Pgp3GthUsIp3G.dcp"
+   #loadIpCore -path "$::DIR_PATH/ip/Pgp3GthUsIp3G/Pgp3GthUsIp3G.xci"
 
 } else {
    puts "\n\nWARNING: $::DIR_PATH requires Vivado 2020.1 (or later)\n\n"
