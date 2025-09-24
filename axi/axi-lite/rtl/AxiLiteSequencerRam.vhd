b74e1edbd7cd935ebc73bbc792659784208939f4
Larry Ruckman <ruckman@slac.stanford.edu>
2025-09-23 19:54:30 -0700

    RST_ASYNC_G not decleared for TrueDualPortRamXpm/TrueDualPortRamAlteraMf

--- axi/axi-lite/rtl/AxiLiteSequencerRam.vhd
+++ axi/axi-lite/rtl/AxiLiteSequencerRam.vhd
@@ -158,7 +158,6 @@ begin
          generic map (
             TPD_G               => TPD_G,
             RST_POLARITY_G      => RST_POLARITY_G,
-            RST_ASYNC_G         => RST_ASYNC_G,
             COMMON_CLK_G        => true,
             MEMORY_TYPE_G       => MEMORY_TYPE_G,
             MEMORY_INIT_FILE_G  => MEMORY_INIT_FILE_G,
@@ -186,7 +185,6 @@ begin
          generic map (
             TPD_G          => TPD_G,
             RST_POLARITY_G => RST_POLARITY_G,
-            RST_ASYNC_G    => RST_ASYNC_G,
             COMMON_CLK_G   => true,
             MEMORY_TYPE_G  => MEMORY_TYPE_G,
             READ_LATENCY_G => READ_LATENCY_G,
