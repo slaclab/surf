c3204b3181a83742805e69f81adb0117dc10222e
Benjamin Reese <bareese@slac.stanford.edu>
2025-09-25 11:25:06 -0700

    Added core that runs at 2x refclk, moved each ip core to its own folder

--- protocols/pgp/pgp2fc/gthUltraScale+/ruckus.tcl
+++ protocols/pgp/pgp2fc/gthUltraScale+/ruckus.tcl
@@ -7,10 +7,10 @@ if { $::env(VIVADO_VERSION) >= 2024.1 } {
    loadSource -lib surf   -dir "$::DIR_PATH/rtl"
 
    if { [info exists ::env(PGP2FC_XCI)] != 0 && $::env(PGP2FC_XCI) == 1 } {
-       loadIpCore -path "$::DIR_PATH/ip/Pgp2fcGthCore.xci"
+       #loadIpCore -path "$::DIR_PATH/ip/Pgp2fcGthCore.xci"
        puts "Loading XCI file for PGP2FC"
    } else {
-       loadSource -lib surf    -path "$::DIR_PATH/ip/Pgp2fcGthCore.dcp"
+       #loadSource -lib surf    -path "$::DIR_PATH/ip/Pgp2fcGthCore.dcp"
    }
 
    #loadSource -lib surf    -path "$::DIR_PATH/ip/Pgp2fcGthCore.dcp"
--- protocols/pgp/pgp2fc/gtyUltraScale+/ruckus.tcl
+++ protocols/pgp/pgp2fc/gtyUltraScale+/ruckus.tcl
@@ -7,10 +7,12 @@ if { $::env(VIVADO_VERSION) >= 2024.1 } {
    loadSource -lib surf   -dir "$::DIR_PATH/rtl"
 
    if { [info exists ::env(PGP2FC_XCI)] != 0 && $::env(PGP2FC_XCI) == 1 } {
-       loadIpCore -path "$::DIR_PATH/ip/Pgp2fcGtyCore.xci"
+       loadIpCore -path "$::DIR_PATH/ip/Pgp2fcGtyCore/Pgp2fcGtyCore.xci"
+       loadIpCore -path "$::DIR_PATH/ip/Pgp2fcGtyCore_refclk2x/Pgp2fcGtyCore_refclk2x.xci"       
        puts "Loading XCI file for PGP2FC"
    } else {
-       loadSource -lib surf    -path "$::DIR_PATH/ip/Pgp2fcGtyCore.dcp"
+       loadSource -lib surf    -path "$::DIR_PATH/ip/Pgp2fcGtyCore/Pgp2fcGtyCore.dcp"
+       loadSource -lib surf    -path "$::DIR_PATH/ip/Pgp2fcGtyCore_refclk2x/Pgp2fcGtyCore_refclk2x.dcp"       
    }
    #loadSource -lib surf    -path "$::DIR_PATH/ip/Pgp2fcGtyCore.dcp"
    #loadIpCore  -path "$::DIR_PATH/ip/Pgp2fcGtyCore.xci"
