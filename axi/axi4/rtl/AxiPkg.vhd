fc8e30d490e28c2f0ae272dce068fb937fe6792b
Larry Ruckman <ruckman@slac.stanford.edu>
2025-09-29 11:55:41 -0700

    moving the common source of dependence from AxiStreamPkg to AxiPkg

--- axi/axi4/rtl/AxiPkg.vhd
+++ axi/axi4/rtl/AxiPkg.vhd
@@ -19,12 +19,11 @@ use ieee.std_logic_unsigned.all;
 
 library surf;
 use surf.StdRtlPkg.all;
-use surf.AxiStreamPkg.all;  -- Dependence with AxiStreamPkg due to DMA
 
 package AxiPkg is
 
-   constant AXI_MAX_DATA_WIDTH_C  : positive := AXI_STREAM_MAX_TDATA_WIDTH_C;  -- Units of bits
-   constant AXI_MAX_WSTRB_WIDTH_C : positive := AXI_STREAM_MAX_TKEEP_WIDTH_C;  -- Units of bytes
+   constant AXI_MAX_DATA_WIDTH_C  : positive := 1024;  -- Units of bits
+   constant AXI_MAX_WSTRB_WIDTH_C : positive := (AXI_MAX_DATA_WIDTH_C/8);  -- Units of bytes
 
    -------------------------------------
    -- AXI bus, read master signal record
