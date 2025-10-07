2de9b9d505f613817e02e88bfcdfe53ce44922cc
Benjamin Reese <bareese@slac.stanford.edu>
2025-10-06 23:01:36 -0700

    Fix conversion equation

--- python/surf/devices/analog_devices/_Ad9249.py
+++ python/surf/devices/analog_devices/_Ad9249.py
@@ -530,7 +530,7 @@ class Ad9249ReadoutGroup2(pr.Device):
                 mode = 'RO',
                 disp = '{:1.9f}',
                 variable = self.AdcChannel[i],
-                linkedGet = lambda read, check, r=self.AdcChannel[i]: 2*pr.twosComplement(r.get(read=read, check=check)>>18, 14)/2**14,
+                linkedGet = lambda read, check, r=self.AdcChannel[i]: 2*pr.twosComplement(r.get(read=read, check=check)>>16, 14)/2**14,
                 units = 'V'))
 
         self.add(pr.RemoteCommand(
