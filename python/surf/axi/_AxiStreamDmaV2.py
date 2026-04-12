55b317b37a0052ea3d820d93e497638740cf6a61
Larry Ruckman <ruckman@slac.stanford.edu>
2026-04-12 08:52:05 -0700

    feat(01-02): add descriptions to _AxiStreamDmaV2.py and _AxiStreamDmaV2Fifo.py
    - Added description field to all 22 RemoteVariable entries in _AxiStreamDmaV2.py
    - Added description field to all 20 RemoteVariable entries in _AxiStreamDmaV2Fifo.py
    - Descriptions cover DMA V2 descriptor config, buffer management, interrupt, FIFO status

--- python/surf/axi/_AxiStreamDmaV2.py
+++ python/surf/axi/_AxiStreamDmaV2.py
@@ -15,183 +15,206 @@ class AxiStreamDmaV2Desc(pr.Device):
 
 
         self.add(pr.RemoteVariable(
-            name='HwEnable',
-            mode = 'RO',
-            offset=0x0,
-            bitOffset=0,
-            bitSize=1,
+            name        = 'HwEnable',
+            description = 'Hardware DMA enable status flag',
+            mode        = 'RO',
+            offset      = 0x0,
+            bitOffset   = 0,
+            bitSize     = 1,
         ))
 
         self.add(pr.RemoteVariable(
-            name='Version',
-            mode = 'RO',
-            offset=0x0,
-            bitOffset=24,
-            bitSize=8,
+            name        = 'Version',
+            description = 'DMA V2 descriptor IP core version number',
+            mode        = 'RO',
+            offset      = 0x0,
+            bitOffset   = 24,
+            bitSize     = 8,
         ))
 
         self.add(pr.RemoteVariable(
-            name='IntEnable',
-            mode = 'RO',
-            offset=0x4,
-            bitOffset=0,
-            bitSize=1,
+            name        = 'IntEnable',
+            description = 'Interrupt enable flag for DMA completion events',
+            mode        = 'RO',
+            offset      = 0x4,
+            bitOffset   = 0,
+            bitSize     = 1,
         ))
 
         self.add(pr.RemoteVariable(
-            name='ContEn',
-            mode = 'RO',
-            offset=0x8,
-            bitOffset=0,
-            bitSize=1,
+            name        = 'ContEn',
+            description = 'Continuous mode enable: automatically re-arm descriptors after completion',
+            mode        = 'RO',
+            offset      = 0x8,
+            bitOffset   = 0,
+            bitSize     = 1,
         ))
 
         self.add(pr.RemoteVariable(
-            name='DropEn',
-            mode = 'RO',
-            offset=0xC,
-            bitOffset=0,
-            bitSize=1,
+            name        = 'DropEn',
+            description = 'Drop incoming frames when descriptor ring is full',
+            mode        = 'RO',
+            offset      = 0xC,
+            bitOffset   = 0,
+            bitSize     = 1,
         ))
 
         self.add(pr.RemoteVariable(
-            name='WrBaseAddr',
-            mode = 'RO',
-            offset=0x10,
-            bitOffset=0,
-            bitSize=64,
+            name        = 'WrBaseAddr',
+            description = 'Base address of the write descriptor ring in memory',
+            mode        = 'RO',
+            offset      = 0x10,
+            bitOffset   = 0,
+            bitSize     = 64,
         ))
 
         self.add(pr.RemoteVariable(
-            name='RdBaseAddr',
-            mode = 'RO',
-            offset=0x14,
-            bitOffset=0,
-            bitSize=64,
+            name        = 'RdBaseAddr',
+            description = 'Base address of the read descriptor ring in memory',
+            mode        = 'RO',
+            offset      = 0x14,
+            bitOffset   = 0,
+            bitSize     = 64,
         ))
 
         self.add(pr.RemoteVariable(
-            name='FifoReset',
-            mode = 'RO',
-            offset=0x20,
-            bitOffset=0,
-            bitSize=1,
+            name        = 'FifoReset',
+            description = 'Reset the internal descriptor FIFO',
+            mode        = 'RO',
+            offset      = 0x20,
+            bitOffset   = 0,
+            bitSize     = 1,
         ))
         self.add(pr.RemoteVariable(
-            name='BufBaseAddr',
-            mode = 'RO',
-            offset=0x24,
-            bitOffset=0,
-            bitSize=32,
-        ))
-
-        self.add(pr.RemoteVariable(
-            name='MaxSize',
-            mode = 'RO',
-            offset=0x28,
-            bitOffset=0,
-            bitSize=24,
-            disp='{:d}',
+            name        = 'BufBaseAddr',
+            description = 'Base address of the DMA data buffer pool',
+            mode        = 'RO',
+            offset      = 0x24,
+            bitOffset   = 0,
+            bitSize     = 32,
         ))
 
         self.add(pr.RemoteVariable(
-            name='Online',
-            mode = 'RO',
-            offset=0x2C,
-            bitOffset=0,
-            bitSize=1,
+            name        = 'MaxSize',
+            description = 'Maximum DMA buffer size in bytes',
+            mode        = 'RO',
+            offset      = 0x28,
+            bitOffset   = 0,
+            bitSize     = 24,
+            disp        = '{:d}',
         ))
 
         self.add(pr.RemoteVariable(
-            name='Acknowledge',
-            mode = 'RO',
-            offset=0x30,
-            bitOffset=0,
-            bitSize=1,
+            name        = 'Online',
+            description = 'DMA engine online status',
+            mode        = 'RO',
+            offset      = 0x2C,
+            bitOffset   = 0,
+            bitSize     = 1,
         ))
 
         self.add(pr.RemoteVariable(
-            name='ChanCount',
-            mode = 'RO',
-            offset=0x34,
-            bitOffset=0,
-            bitSize=8,
+            name        = 'Acknowledge',
+            description = 'DMA descriptor acknowledge flag',
+            mode        = 'RO',
+            offset      = 0x30,
+            bitOffset   = 0,
+            bitSize     = 1,
         ))
 
         self.add(pr.RemoteVariable(
-            name='DescAwidth',
-            mode = 'RO',
-            offset=0x38,
-            bitOffset=0,
-            bitSize=8,
+            name        = 'ChanCount',
+            description = 'Number of DMA channels supported by this instance',
+            mode        = 'RO',
+            offset      = 0x34,
+            bitOffset   = 0,
+            bitSize     = 8,
         ))
 
         self.add(pr.RemoteVariable(
-            name='DescCache',
-            mode = 'RO',
-            offset=0x3C,
-            bitOffset=0,
-            bitSize=4,
-        ))
-        self.add(pr.RemoteVariable(
-            name='BuffCache',
-            mode = 'RO',
-            offset=0x3C,
-            bitOffset=8,
-            bitSize=4,
+            name        = 'DescAwidth',
+            description = 'Address width of the descriptor ring memory',
+            mode        = 'RO',
+            offset      = 0x38,
+            bitOffset   = 0,
+            bitSize     = 8,
         ))
 
         self.add(pr.RemoteVariable(
-            name='FifoDin',
-            mode = 'RO',
-            offset=0x40,
-            bitOffset=0,
-            bitSize=32,
+            name        = 'DescCache',
+            description = 'AXI cache attribute for descriptor read/write transactions',
+            mode        = 'RO',
+            offset      = 0x3C,
+            bitOffset   = 0,
+            bitSize     = 4,
+        ))
+        self.add(pr.RemoteVariable(
+            name        = 'BuffCache',
+            description = 'AXI cache attribute for data buffer read/write transactions',
+            mode        = 'RO',
+            offset      = 0x3C,
+            bitOffset   = 8,
+            bitSize     = 4,
         ))
 
         self.add(pr.RemoteVariable(
-            name='IntAckCount',
-            mode = 'RO',
-            offset=0x4C,
-            bitOffset=0,
-            bitSize=16,
+            name        = 'FifoDin',
+            description = 'Data input value written to the descriptor FIFO',
+            mode        = 'RO',
+            offset      = 0x40,
+            bitOffset   = 0,
+            bitSize     = 32,
         ))
 
         self.add(pr.RemoteVariable(
-            name='IntEnableDup',
-            mode = 'RO',
-            offset=0x4C,
-            bitOffset=17,
-            bitSize=1,
+            name        = 'IntAckCount',
+            description = 'Number of interrupt acknowledgements issued',
+            mode        = 'RO',
+            offset      = 0x4C,
+            bitOffset   = 0,
+            bitSize     = 16,
         ))
 
         self.add(pr.RemoteVariable(
-            name='IntReqCount',
-            mode = 'RO',
-            offset=0x50,
-            bitOffset=0,
-            bitSize=32,
+            name        = 'IntEnableDup',
+            description = 'Duplicate interrupt enable status bit',
+            mode        = 'RO',
+            offset      = 0x4C,
+            bitOffset   = 17,
+            bitSize     = 1,
         ))
 
         self.add(pr.RemoteVariable(
-            name='WrIndex',
-            mode = 'RO',
-            offset=0x54,
-            bitOffset=0,
-            bitSize=32,
-        ))
-        self.add(pr.RemoteVariable(
-            name='RdIndex',
-            mode = 'RO',
-            offset=0x58,
-            bitOffset=0,
-            bitSize=32,
+            name        = 'IntReqCount',
+            description = 'Number of interrupt requests generated by the DMA engine',
+            mode        = 'RO',
+            offset      = 0x50,
+            bitOffset   = 0,
+            bitSize     = 32,
         ))
 
         self.add(pr.RemoteVariable(
-            name='WrReqMissed',
-            mode = 'RO',
-            offset=0x5C,
-            bitOffset=0,
-            bitSize=32,
+            name        = 'WrIndex',
+            description = 'Current write descriptor ring index',
+            mode        = 'RO',
+            offset      = 0x54,
+            bitOffset   = 0,
+            bitSize     = 32,
+        ))
+        self.add(pr.RemoteVariable(
+            name        = 'RdIndex',
+            description = 'Current read descriptor ring index',
+            mode        = 'RO',
+            offset      = 0x58,
+            bitOffset   = 0,
+            bitSize     = 32,
+        ))
+
+        self.add(pr.RemoteVariable(
+            name        = 'WrReqMissed',
+            description = 'Number of write requests missed due to full descriptor ring',
+            mode        = 'RO',
+            offset      = 0x5C,
+            bitOffset   = 0,
+            bitSize     = 32,
         ))
