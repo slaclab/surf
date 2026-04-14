fc5f25a7997a295459f530033c38b36c95aa4995
Larry Ruckman <ruckman@slac.stanford.edu>
2026-04-13 19:03:29 -0700

    feat(01-02): align = signs in _AxiStreamMonAxiL and _AxiStreamTimer
    - Per-block = alignment in all self.add(pr.*) blocks
    - _AxiStreamMonAxiL.py: 19 blocks aligned (9 previously unaligned corrected)
    - _AxiStreamTimer.py: 6 blocks aligned (all 6 were unaligned)
    - Only whitespace changed, no functional modifications

--- python/surf/axi/_AxiStreamTimer.py
+++ python/surf/axi/_AxiStreamTimer.py
@@ -9,23 +9,23 @@ class AxiStreamTimerChannel(pr.Device):
 
         for ev in range(self.NEvents):
             self.add(pr.RemoteVariable(
-                    name         = f'SOF[{ev}]',
-                    description  = 'Start-of-frame timestamp counter',
-                    offset       = (8*self.NStreams*ev),
-                    bitSize      = 32,
-                    mode         = 'RO',
-                    units        = 'Clock Cycles',
-                    disp         = '{:d}',
+                    name        = f'SOF[{ev}]',
+                    description = 'Start-of-frame timestamp counter',
+                    offset      = (8*self.NStreams*ev),
+                    bitSize     = 32,
+                    mode        = 'RO',
+                    units       = 'Clock Cycles',
+                    disp        = '{:d}',
                 ))
 
             self.add(pr.RemoteVariable(
-                name         = f'EOF[{ev}]',
-                description  = 'End-of-frame timestamp counter',
-                offset       = 4+(8*self.NStreams*ev),
-                bitSize      = 32,
-                mode         = 'RO',
-                units        = 'Clock Cycles',
-                disp         = '{:d}',
+                name        = f'EOF[{ev}]',
+                description = 'End-of-frame timestamp counter',
+                offset      = 4+(8*self.NStreams*ev),
+                bitSize     = 32,
+                mode        = 'RO',
+                units       = 'Clock Cycles',
+                disp        = '{:d}',
             ))
 
 
@@ -38,37 +38,37 @@ class AxiStreamTimer(pr.Device):
         self.NEvents  = NEvents
 
         self.add(pr.RemoteVariable(
-            name         = 'MeasureTime',
-            description  = 'Enable timing measurement',
-            offset       = 0x000,
-            bitSize      = 1,
-            base         = pr.Bool,
-            mode         = 'RW',
+            name        = 'MeasureTime',
+            description = 'Enable timing measurement',
+            offset      = 0x000,
+            bitSize     = 1,
+            base        = pr.Bool,
+            mode        = 'RW',
         ))
 
         self.add(pr.RemoteVariable(
-            name         = 'NumStreamsG',
-            description  = 'Number of streams generic value',
-            offset       = 0x004,
-            bitSize      = 32,
-            mode         = 'RO',
-            disp         = '{:d}',
+            name        = 'NumStreamsG',
+            description = 'Number of streams generic value',
+            offset      = 0x004,
+            bitSize     = 32,
+            mode        = 'RO',
+            disp        = '{:d}',
         ))
 
         self.add(pr.RemoteVariable(
-            name         = 'NumEventG',
-            description  = 'Number of events generic value',
-            offset       = 0x008,
-            bitSize      = 32,
-            mode         = 'RO',
-            disp         = '{:d}',
+            name        = 'NumEventG',
+            description = 'Number of events generic value',
+            offset      = 0x008,
+            bitSize     = 32,
+            mode        = 'RO',
+            disp        = '{:d}',
         ))
 
         for ch in range(self.NStreams):
             self.add(AxiStreamTimerChannel(
-                name         = f'Channel[{ch}]',
-                offset       = 12+(ch*8),
-                NEvents      = self.NEvents,
-                NStreams     = self.NStreams,
-                expand       = True,
+                name     = f'Channel[{ch}]',
+                offset   = 12+(ch*8),
+                NEvents  = self.NEvents,
+                NStreams = self.NStreams,
+                expand   = True,
             ))
