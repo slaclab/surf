04d41b7a24e6714ba5ba084b3e3791e97c767497
Luca Scomparin <scompar@slac.stanford.edu>
2026-04-22 16:58:37 -0700

    Created PhantomS711 class to follow device name convention

---
+++ python/surf/protocols/coaxpress/_PhantomS711.py
@@ -0,0 +1,16 @@
+#-----------------------------------------------------------------------------
+# This file is part of the 'SLAC Firmware Standard Library'. It is subject to
+# the license terms in the LICENSE.txt file found in the top-level directory
+# of this distribution and at:
+#    https://confluence.slac.stanford.edu/display/ppareg/LICENSE.html.
+# No part of the 'SLAC Firmware Standard Library', including this file, may be
+# copied, modified, propagated, or distributed except according to the terms
+# contained in the LICENSE.txt file.
+#-----------------------------------------------------------------------------
+
+import pyrogue as pr
+from surf.protocols.coaxpress._PhantomS641 import PhantomS641
+
+class PhantomS711(PhantomS641):
+    def __init__(self, **kwargs):
+        super().__init__(isPhantomS711=True, **kwargs)
