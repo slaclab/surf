cfb934adfadb9c1270e184881dbf49d4e1c8a6a8
Larry Ruckman <ruckman@slac.stanford.edu>
2026-06-10 06:55:44 -0700

    test(03-02): touch BoxcarFilter.vhd to drive selective-CI validation diff (D-05)
    Non-functional comment touch on the Phase 1 proof leaf so the validation PR
    produces a narrow, predictable production-.vhd diff that the selector resolves
    to tests/dsp/generic/test_BoxcarFilter.py. Entity/architecture byte-identical.

--- dsp/generic/fixed/BoxcarFilter.vhd
+++ dsp/generic/fixed/BoxcarFilter.vhd
@@ -1,7 +1,7 @@
 -------------------------------------------------------------------------------
 -- Company    : SLAC National Accelerator Laboratory
 -------------------------------------------------------------------------------
--- Description: Simple boxcar filter
+-- Description: Simple boxcar filter (CI selective-test validation touch)
 -------------------------------------------------------------------------------
 -- This file is part of 'SLAC Firmware Standard Library'.
 -- It is subject to the license terms in the LICENSE.txt file found in the
