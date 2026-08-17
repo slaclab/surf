70f113d8e3193c81f8ec088a792cdf14b734a486
Larry Ruckman <ruckman@slac.stanford.edu>
2026-08-16 20:08:21 -0700

    build(rocev2): load the converted RoCEv2 sources
    blue-rdma and blue-lib load as VHDL 2008 by directory. RoCEv2 is also
    reachable in the non-Vivado path now: its whole dependency closure is
    available without Vivado, unlike the other ethernet subdirectories, whose
    ruckus.tcl files call loadIpCore over an .xci or .dcp.

--- ethernet/RoCEv2/ruckus.tcl
+++ ethernet/RoCEv2/ruckus.tcl
@@ -3,11 +3,6 @@ source $::env(RUCKUS_PROC_TCL)
 
 # Load Source Code
 loadSource -lib surf -dir "$::DIR_PATH/rtl"
-loadSource -lib surf -dir "$::DIR_PATH/blue-crc"
-loadSource -lib surf -dir "$::DIR_PATH/blue-rdma"
-loadSource -lib surf -dir "$::DIR_PATH/blue-lib"
 
-# Load mem files
-for {set i 0} {$i <= 35} {incr i} {
-   add_files -norecurse "$::DIR_PATH/blue-crc/tab/crc_tab_$i.mem"
-}
+loadSource -lib surf -dir "$::DIR_PATH/blue-rdma" -fileType "VHDL 2008"
+loadSource -lib surf -dir "$::DIR_PATH/blue-lib"  -fileType "VHDL 2008"
